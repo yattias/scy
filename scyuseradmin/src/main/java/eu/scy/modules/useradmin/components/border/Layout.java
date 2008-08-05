41b9a1abe41e6d7d09e0499f13d6292655b2232e
henrik <henrik@enovate.no>
2008-08-05 11:26:57 +0000

    Added the layout component for a common border

---
+++ scyuseradmin/src/main/java/eu/scy/modules/useradmin/components/border/Layout.java
@@ -0,0 +1,11 @@
+package eu.scy.modules.useradmin.components.border;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 05.aug.2008
+ * Time: 13:01:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class Layout {
+}
