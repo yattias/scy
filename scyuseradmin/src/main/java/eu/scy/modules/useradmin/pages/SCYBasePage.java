8a93f04343f58f036be4096a770183e86d8bc101
henrik <henrik@enovate.no>
2008-08-08 11:23:06 +0000

    Linking users from userlist

--- scyuseradmin/src/main/java/eu/scy/modules/useradmin/pages/SCYBasePage.java
+++ scyuseradmin/src/main/java/eu/scy/modules/useradmin/pages/SCYBasePage.java
@@ -2,6 +2,7 @@ package eu.scy.modules.useradmin.pages;
 
 import org.springframework.security.context.SecurityContextHolder;
 import org.springframework.security.userdetails.UserDetails;
+import org.apache.log4j.Logger;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,6 +12,8 @@ import org.springframework.security.userdetails.UserDetails;
  * To change this template use File | Settings | File Templates.
  */
 public class SCYBasePage {
+
+    //protected Logger log = Logger.getLogger(getClass());
 
     public String getCurrentUsersUserName() {
         return ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
