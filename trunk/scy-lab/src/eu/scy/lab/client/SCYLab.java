e360af23644d708f04b42cbb0d5262d16a149474
adam.giemza <adam.giemza@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2008-09-08 21:14:18 +0000

    Fixed css and js imports and added some window

--- trunk/scy-lab/src/eu/scy/lab/client/SCYLab.java
+++ trunk/scy-lab/src/eu/scy/lab/client/SCYLab.java
@@ -4,30 +4,39 @@
 package eu.scy.lab.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.gwtext.client.widgets.form.FormPanel;
+import com.gwtext.client.widgets.Button;
+import com.gwtext.client.widgets.Window;
 import com.gwtext.client.widgets.form.TextField;
 
 /**
  * @author Sven
- * 
  */
 public class SCYLab implements EntryPoint {
-
-	public void onModuleLoad() {
-		// TODO Auto-generated method stub
-		Window.alert("Test");
-
-		FormPanel p = new FormPanel();
-		TextField field = new TextField("Username", "username");
-		p.add(field);
-		field = new TextField("Surename", "surename");
-		p.add(field);
-		field = new TextField("EMail", "email");
-		p.add(field);
-
-		RootPanel.get().add(p);
-	}
-
+    
+    public void onModuleLoad() {
+        Window p = new Window();
+        p.setTitle("Login");
+        
+        TextField field = new TextField("Username", "username");
+        p.add(field);
+        field = new TextField("Surename", "surename");
+        p.add(field);
+        field = new TextField("EMail", "email");
+        p.add(field);
+        
+        p.setFrame(true);
+        p.setWidth(250);
+        p.setFloating(true);
+        
+        Button[] buttons = new Button[2];
+        buttons[0] = new Button("Login");
+        buttons[1] = new Button("Reset");
+        p.setButtons(buttons);
+        
+        RootPanel.get().add(p);
+        
+        p.show();
+        p.setPosition(200, 200);
+    }
 }
