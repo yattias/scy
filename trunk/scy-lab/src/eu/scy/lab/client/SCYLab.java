9b634df0421d453e8e82418142f45c60f8b6991d
latinum1@gmx.de <latinum1@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2008-09-10 12:27:19 +0000

    new Listeners and Dialog-Classes integrated

--- trunk/scy-lab/src/eu/scy/lab/client/SCYLab.java
+++ trunk/scy-lab/src/eu/scy/lab/client/SCYLab.java
@@ -4,41 +4,25 @@
 package eu.scy.lab.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
 import eu.scy.lab.client.login.Login;
-import eu.scy.lab.client.usermanagement.CreateUser;
 
 /**
  * @author Sven Manske
  * 
  */
 public class SCYLab implements EntryPoint {
+	
 
 	public void onModuleLoad() {
 		
 		//new Login Panel
-		Login login = new Login();
-		
-		//Positioning in Center
-		//wrap Login-Panel in vertical Panel
-		VerticalPanel verticalPanel  = new VerticalPanel();
-		verticalPanel.setWidth("100%");
-		verticalPanel.setHeight("100%");
-		verticalPanel.setHorizontalAlignment(VerticalPanel.ALIGN_CENTER);
-		verticalPanel.setVerticalAlignment(VerticalPanel.ALIGN_MIDDLE);
-		
-		HorizontalPanel horizontalPanel = new HorizontalPanel();
-		horizontalPanel.add(login);
-		
-		//adding verticalPanel to RootPanel, horizontalPanel to verticalPanel, LoginPanel to horizontalPanel
-		RootPanel.get().add(verticalPanel);
-		verticalPanel.add(horizontalPanel);
-		
-		
+		Login loginPanel = new Login();
+		RootPanel.get().add(loginPanel.getCentredLoginDialog());
 		
 	}
+	
+	
 
 }
