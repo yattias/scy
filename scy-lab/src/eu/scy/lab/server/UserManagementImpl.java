a0d85dd4b8a82457a96cc20b4279b0b1fa675acb
adam.giemza <adam.giemza@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2008-09-17 12:32:53 +0000

    fake user database

---
+++ scy-lab/src/eu/scy/lab/server/UserManagementImpl.java
@@ -0,0 +1,18 @@
+package eu.scy.lab.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+import eu.scy.lab.client.UserManagement;
+
+public class UserManagementImpl extends RemoteServiceServlet implements UserManagement {
+    
+    public boolean login(String username, String password) {
+        return UserDB.authenticateUser(username, password);
+    }
+    
+    public boolean register(String username, String password, String title, String firstname, String lastname, String birthdate, String email) {
+        return UserDB.addUser(birthdate, email, firstname, lastname, password, title, username);
+    }
+    
+    
+}
