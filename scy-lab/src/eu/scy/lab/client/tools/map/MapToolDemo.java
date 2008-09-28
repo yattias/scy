c1d0af50d8bb094492c6993acaeac9744a4cc137
Benedikt.roth <Benedikt.roth@bratislav.org>
2008-09-28 12:45:59 +0000

    New class for EntryPoint to MapTool

---
+++ scy-lab/src/eu/scy/lab/client/tools/map/MapToolDemo.java
@@ -0,0 +1,12 @@
+package eu.scy.lab.client.tools.map;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class MapToolDemo implements EntryPoint {
+
+	public void onModuleLoad() {
+		RootPanel.get().add(new MapTool().getPanel());
+	}
+	
+}
