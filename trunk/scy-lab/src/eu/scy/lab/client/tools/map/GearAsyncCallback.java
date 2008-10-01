ec2badc254ff3811e10d2c7e6013390af84b7dc8
Benedikt.roth@bratislav.org <Benedikt.roth@bratislav.org@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2008-10-01 09:56:04 +0000

    comment

--- trunk/scy-lab/src/eu/scy/lab/client/tools/map/GearAsyncCallback.java
+++ trunk/scy-lab/src/eu/scy/lab/client/tools/map/GearAsyncCallback.java
@@ -2,6 +2,10 @@ package eu.scy.lab.client.tools.map;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
+/**
+ * Callback to be used to first do the normal callback to get/save data via rpc 
+ * and afterwards save data locally via gears 
+ */
 public abstract class GearAsyncCallback<T> implements AsyncCallback<T> {
 
 	private AsyncCallback<T> callback;
