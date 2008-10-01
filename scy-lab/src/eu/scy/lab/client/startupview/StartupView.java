56ec787faeb36a6cf849f623c46f035db261a2fb
latinum1 <latinum1@gmx.de>
2008-10-01 14:31:37 +0000

    minor changes

--- scy-lab/src/eu/scy/lab/client/startupview/StartupView.java
+++ scy-lab/src/eu/scy/lab/client/startupview/StartupView.java
@@ -18,48 +18,50 @@ import eu.scy.lab.client.startupview.missionBrowser.MissionBrowser;
 
 /**
  * @author Sven
- *
+ * 
  */
-public class StartupView extends Panel{
-	
-	private MissionBrowser missionBrowser;
-	private LastMissionPanel lastMissionPanel;
-	private LastELOPanel lastEloPanel;
-	private FeedbackPanel feedbackPanel;
-	
-	public StartupView(){
-		super();
-		setBorder(false);
-//		setLayout(new FitLayout());
-		
-		missionBrowser = new MissionBrowser();
-		missionBrowser.setSize(400, 250);
-		
-		lastMissionPanel = new LastMissionPanel();
-		lastMissionPanel.setSize(400, 250);
-		
-		lastEloPanel = new LastELOPanel();
-		lastEloPanel.setSize(400, 250);
-		
-		feedbackPanel = new FeedbackPanel();
-		feedbackPanel.setSize(400, 250);
-		
-		
-		Panel tableWrapper = new Panel();
-		tableWrapper.setLayout(new TableLayout(2));
-		
-		tableWrapper.add(missionBrowser);
-		tableWrapper.add(lastMissionPanel,new TableLayoutData(1));
-		tableWrapper.add(lastEloPanel);
-		tableWrapper.add(feedbackPanel,new TableLayoutData(1));
-		
-		add(tableWrapper);
-	}
-	
-	public VerticalPanel getCentredPanel(){
-		//Positioning in Center
-        //wrap Login-Panel in vertical Panel
-        VerticalPanel verticalPanel  = new VerticalPanel();
+public class StartupView extends Panel {
+
+    private MissionBrowser missionBrowser;
+
+    private LastMissionPanel lastMissionPanel;
+
+    private LastELOPanel lastEloPanel;
+
+    private FeedbackPanel feedbackPanel;
+
+    public StartupView() {
+        super();
+        setBorder(false);
+        // setLayout(new FitLayout());
+
+        missionBrowser = new MissionBrowser();
+        missionBrowser.setSize(400, 250);
+
+        lastMissionPanel = new LastMissionPanel();
+        lastMissionPanel.setSize(400, 250);
+
+        lastEloPanel = new LastELOPanel();
+        lastEloPanel.setSize(400, 250);
+
+        feedbackPanel = new FeedbackPanel();
+        feedbackPanel.setSize(400, 250);
+
+        Panel tableWrapper = new Panel();
+        tableWrapper.setLayout(new TableLayout(2));
+
+        tableWrapper.add(missionBrowser);
+        tableWrapper.add(lastMissionPanel, new TableLayoutData(1));
+        tableWrapper.add(lastEloPanel);
+        tableWrapper.add(feedbackPanel, new TableLayoutData(1));
+
+        add(tableWrapper);
+    }
+
+    public VerticalPanel getCentredPanel() {
+        // Positioning in Center
+        // wrap Login-Panel in vertical Panel
+        VerticalPanel verticalPanel = new VerticalPanel();
         verticalPanel.setWidth("100%");
         verticalPanel.setHeight("100%");
         verticalPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
@@ -67,7 +69,7 @@ public class StartupView extends Panel{
         HorizontalPanel horizontalPanel = new HorizontalPanel();
         verticalPanel.add(horizontalPanel);
         horizontalPanel.add(this);
-        return verticalPanel; 
-	}
+        return verticalPanel;
+    }
 
 }
