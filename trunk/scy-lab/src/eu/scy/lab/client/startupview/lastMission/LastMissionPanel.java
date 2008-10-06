356ba6f0c445967ee4cff8a3fde56eb96b9f7407
adam.giemza <adam.giemza@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2008-10-06 11:09:28 +0000

    fixed width

--- trunk/scy-lab/src/eu/scy/lab/client/startupview/lastMission/LastMissionPanel.java
+++ trunk/scy-lab/src/eu/scy/lab/client/startupview/lastMission/LastMissionPanel.java
@@ -28,141 +28,141 @@ import eu.scy.lab.client.date.DateRenderer;
 import eu.scy.lab.client.mission.Mission;
 
 public class LastMissionPanel extends Panel {
-
+    
     private Vector<Mission> missions;
-
+    
     private GridPanel grid;
-
+    
     private DateRenderer renderer = new DateRenderer("dd.MM.yyyy");
-
+    
     public LastMissionPanel() {
-
+        
         super("Last Missions");
         onModuleLoad();
-
+        
     }
-
+    
     public void onModuleLoad() {
-
+        
         setLayout(new FitLayout());
         setClosable(false);
-
+        
         PagingMemoryProxy proxy = new PagingMemoryProxy(getGridDataWithDate(getGridData()));
         RecordDef recordDef = new RecordDef(new FieldDef[] { new StringFieldDef("name"), new StringFieldDef("author"), new DateFieldDef("date", "dd.MM.yyyy"),
-        // new DateFieldDef("date"),
-        new StringFieldDef("relativedate") });
-
+                // new DateFieldDef("date"),
+                new StringFieldDef("relativedate") });
+        
         ArrayReader reader = new ArrayReader(recordDef);
         final Store store = new Store(reader);
         // store.setReader();
         store.setDataProxy(proxy);
         // store.setGroupField("relativedate");
-
+        
         ColumnConfig tempDateColumn = new ColumnConfig("Relative Date", "relativedate");
         tempDateColumn.setHidden(true);
-
+        
         ColumnConfig date = new ColumnConfig("Date", "date");
         date.setRenderer(renderer);
-        date.setWidth(50);
+        date.setWidth(80);
         date.setSortable(false);
-
+        
         ColumnConfig[] columns = new ColumnConfig[] {
-        // column ID is company which is later used in
-        new ColumnConfig("Name", "name", 160, false, null, "name"), new ColumnConfig("Author", "author", 160, false), date, tempDateColumn };
+                // column ID is company which is later used in
+                new ColumnConfig("Name", "name", 160, false, null, "name"), new ColumnConfig("Author", "author", 160, false), date, tempDateColumn };
         ColumnModel columnModel = new ColumnModel(columns);
-
+        
         store.setInitialSortState(new SortState("date", SortDir.DESC));
-
+        
         // FIXME GroupingView deactivated because GridView is not working
         // GroupingView gridView = new GroupingView();
         // gridView.setForceFit(true);
         // gridView.updateHeaderSortState();
         // gridView
         // .setGroupTextTpl("{text} ({[values.rs.length]} {[values.rs.length > 1 ? \"Items\" : \"Item\"]})");
-
+        
         // The Grid
         // TODO set layout and size
-
+        
         grid = new GridPanel();
         grid.setStore(store);
         grid.setColumnModel(columnModel);
-
+        
         grid.setFrame(false);
         grid.setStripeRows(true);
         grid.setLayout(new HorizontalLayout(0));
         grid.setMonitorResize(true);
         grid.setAutoExpandColumn("name");
         grid.setHeader(false);
-
+        
         // FIXME GroupingView deactivated because GridView is not working
         // gridView.fitColumns(true);
         // grid.setView(gridView);
-
+        
         grid.setIconCls("grid-icon");
-
+        
         grid.addGridRowListener(new GridRowListener() {
-
+            
             public void onRowClick(GridPanel grid, int rowIndex, EventObject e) {
-
+                
                 String name = (grid.getSelectionModel().getSelected().getAsString(grid.getSelectionModel().getSelected().getFields()[0]));
                 String author = (grid.getSelectionModel().getSelected().getAsString(grid.getSelectionModel().getSelected().getFields()[1]));
                 String date = (grid.getSelectionModel().getSelected().getAsString(grid.getSelectionModel().getSelected().getFields()[2]));
-
+                
             }
-
+            
             public void onRowContextMenu(GridPanel grid, int rowIndex, EventObject e) {
-
+                
             }
-
+            
             public void onRowDblClick(GridPanel grid, int rowIndex, EventObject e) {
-
+                
             }
-
+            
         });
-
+        
         Button browseMission = new Button("Browse Mission!", new ButtonListenerAdapter() {
-
+            
             public void onClick(Button button, EventObject e) {
                 // TODO Auto-generated method stub
                 MessageBox.alert("Starting Missionbrowser when feature is enabled");
             }
         });
         addButton(browseMission);
-
+        
         store.load();
-
+        
         grid.setBufferResize(true);
-
+        
         add(grid);
         setAutoScroll(true);
-
+        
     }
-
+    
     // The local Array-Data to display in the Grid
     @SuppressWarnings("deprecation")
     private Object[][] getGridData() {
-
+        
         return new Object[][] { new Object[] { "Kryptoarithmetics", "Sven", new Date(108, 8, 29) }, new Object[] { "Graphsearch", "Sven M", new Date(108, 8, 21) }, new Object[] { "Kryptoarithmetics II", "Sven", new Date(108, 8, 20) }, new Object[] { "Dancing with animals", "Sven", new Date(108, 8, 21) }, new Object[] { "Kryptoarithmetics III", "Sven", new Date(108, 8, 22) } };
     }
-
+    
     private Object[][] getGridDataWithDate(Object[][] ungroupedData) {
         Object[][] groupedData = new Object[ungroupedData.length][ungroupedData[0].length + 1];
-
+        
         for (int i = 0; i < ungroupedData.length; i++) {
             for (int j = 0; j < ungroupedData[i].length; j++) {
                 groupedData[i][j] = ungroupedData[i][j];
             }
-
+            
             Date indexDate = new Date();
             if (groupedData[i][2] instanceof Date) {
                 indexDate = (Date) groupedData[i][2];
             }
             groupedData[i][ungroupedData[i].length] = createRelativeDate(indexDate);
         }
-
+        
         return groupedData;
     }
-
+    
     @SuppressWarnings("deprecation")
     private String createRelativeDate(Date date) {
         Date today = new Date();
@@ -171,22 +171,23 @@ public class LastMissionPanel extends Panel {
             return "today";
         } else if ((date.getMonth() == today.getMonth()) && (date.getYear() == today.getYear())) {
             return "this month";
-        } else
+        } else {
             return "older";
+        }
     }
-
+    
     /**
      * @return the last missions from a remote service
      */
     // TODO connect to remote service and correct return statement
     public Vector<Mission> getLastMissions() {
-
+        
         Vector<Mission> missions = new Vector<Mission>();
         missions.add(new Mission("CO2 neutral house", "09/22/2008"));
         missions.add(new Mission("Tutorial Mission", "09/21/2008"));
         return missions;
     }
-
+    
     /**
      * @param missions
      *            the missions to set
@@ -194,12 +195,12 @@ public class LastMissionPanel extends Panel {
     public void setMissions(Vector<Mission> missions) {
         this.missions = missions;
     }
-
+    
     /**
      * @return the missions
      */
     public Vector<Mission> getMissions() {
         return missions;
     }
-
+    
 }
