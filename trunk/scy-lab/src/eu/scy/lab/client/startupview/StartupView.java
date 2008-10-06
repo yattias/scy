41985ad1ba62801db892b940554bc691bdf472e5
latinum1@gmx.de <latinum1@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2008-10-06 09:20:51 +0000

    fixed borders

--- trunk/scy-lab/src/eu/scy/lab/client/startupview/StartupView.java
+++ trunk/scy-lab/src/eu/scy/lab/client/startupview/StartupView.java
@@ -34,28 +34,48 @@ public class StartupView extends Panel {
     public StartupView() {
         super();
         History.newItem("startup");
-        setBorder(false);
-        // setLayout(new FitLayout());
+        setBorder(true);
 
+        //packed all Panels in wrapper Panels to set a nice border
         missionBrowser = new MissionBrowser();
         missionBrowser.setSize(400, 250);
+        missionBrowser.setBorder(false);
+        Panel wrapperMissionBrowser = new Panel();
+        wrapperMissionBrowser.setSize(400, 250);
+        wrapperMissionBrowser.setBorder(true);
+        wrapperMissionBrowser.add(missionBrowser);
 
         lastMissionPanel = new LastMissionPanel();
         lastMissionPanel.setSize(400, 250);
+        lastMissionPanel.setBorder(false);
+        Panel wrapperLastMissionPanel = new Panel();
+        wrapperLastMissionPanel.setSize(400, 250);
+        wrapperLastMissionPanel.setBorder(true);
+        wrapperLastMissionPanel.add(lastMissionPanel);
 
         lastEloPanel = new LastELOPanel();
         lastEloPanel.setSize(400, 250);
+        lastEloPanel.setBorder(false);
+        Panel wrapperLastEloPanel = new Panel();
+        wrapperLastEloPanel.setSize(400, 250);
+        wrapperLastEloPanel.setBorder(true);
+        wrapperLastEloPanel.add(lastEloPanel);
 
         feedbackPanel = new FeedbackPanel();
         feedbackPanel.setSize(400, 250);
+        feedbackPanel.setBorder(false);
+        Panel wrapperFeedBackPanel = new Panel();
+        wrapperFeedBackPanel.setSize(400, 250);
+        wrapperFeedBackPanel.setBorder(true);
+        wrapperFeedBackPanel.add(feedbackPanel);
 
         Panel tableWrapper = new Panel();
         tableWrapper.setLayout(new TableLayout(2));
 
-        tableWrapper.add(missionBrowser);
-        tableWrapper.add(lastMissionPanel, new TableLayoutData(1));
-        tableWrapper.add(lastEloPanel);
-        tableWrapper.add(feedbackPanel, new TableLayoutData(1));
+        tableWrapper.add(wrapperMissionBrowser);
+        tableWrapper.add(wrapperLastMissionPanel, new TableLayoutData(1));
+        tableWrapper.add(wrapperLastEloPanel);
+        tableWrapper.add(wrapperFeedBackPanel, new TableLayoutData(1));
 
         add(tableWrapper);
     }
