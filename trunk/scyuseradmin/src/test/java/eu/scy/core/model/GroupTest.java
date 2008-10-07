fd598e7f583e37b426d0fb13ceca0e3c4e3c656b
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2008-10-07 12:55:03 +0000

    some tests

--- trunk/scyuseradmin/src/test/java/eu/scy/core/model/GroupTest.java
+++ trunk/scyuseradmin/src/test/java/eu/scy/core/model/GroupTest.java
@@ -2,6 +2,7 @@ package eu.scy.core.model;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.apache.log4j.Logger;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,7 +14,12 @@ import org.testng.annotations.BeforeTest;
 @Test
 public class GroupTest {
 
-    Group testGroup = null;
+    private static Logger log = Logger.getLogger(GroupTest.class);
+
+    private Group testGroup = null;
+    private Group childGroup = null;
+
+    private User user1 ;
 
     public static final String GROUP_NAME = "HEnriks cool group";
 
@@ -22,6 +28,9 @@ public class GroupTest {
         testGroup = new Group();
         testGroup.setName(GROUP_NAME);
 
+        user1 = new User();
+        user1.setUserName("User_1");
+
     }
 
 
@@ -29,5 +38,34 @@ public class GroupTest {
     public void testName() {
         assert(testGroup.getName().equals(GROUP_NAME) ) : "Exptected something else";
     }
+
+    @Test
+    public void addUserToGroup() {
+        testGroup.addUser(user1);
+        System.out.println(testGroup.getUsers().size());
+        assert(testGroup.getUsers().contains(user1));
+    }
+
+    @Test
+    public void addNullUserToGroup() {
+        int size = testGroup.getUsers().size();
+        testGroup.addUser(null);
+        assert(size == testGroup.getUsers().size()) : "Size is " + size + " group list is " + testGroup.getUsers().size();
+    }
+
+    @Test
+    public void addChildGroupToGroup() {
+        childGroup = new Group();
+        testGroup.addChild(childGroup);
+        assert(testGroup.getChildren().contains(childGroup));
+    }
+
+    @Test
+    public void addNullChildGroup() {
+        int size = testGroup.getChildren().size();
+        testGroup.addChild(null);
+        assert(size == testGroup.getChildren().size());
+    }
+
 
 }
