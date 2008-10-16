c0ee8fdb64b256af9d7db49c6cf410375e5b46e1
adam.giemza <adam.giemza@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2008-10-16 13:05:59 +0000

    Added getter for metadatatypemanager and extensionmanager

--- tool-broker-api/src/main/java/eu/scy/toolbrokerapi/ToolBrokerAPI.java
+++ tool-broker-api/src/main/java/eu/scy/toolbrokerapi/ToolBrokerAPI.java
@@ -4,7 +4,9 @@
 package eu.scy.toolbrokerapi;
 
 import roolo.api.IELO;
+import roolo.api.IExtensionManager;
 import roolo.api.IMetadataKey;
+import roolo.api.IMetadataTypeManager;
 import roolo.api.IRepository;
 
 /**
@@ -23,5 +25,15 @@ public interface ToolBrokerAPI<K extends IMetadataKey> {
      * @return repository the repository instace
      */
     public IRepository<IELO<K>, K> getRepository();
+
+    /**
+     * @return the metaDataTypeManager
+     */
+    public IMetadataTypeManager<K> getMetaDataTypeManager();
+
+    /**
+     * @return the extensionManager
+     */
+    public IExtensionManager getExtensionManager();
     
 }
