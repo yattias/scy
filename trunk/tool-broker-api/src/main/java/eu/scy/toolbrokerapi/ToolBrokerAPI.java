5df39ece7447308ac5575643d1f73c885e4961c6
adam.giemza <adam.giemza@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2008-10-16 12:09:58 +0000

    Second approach of the tools broker api with implementation in the project tool broker

---
+++ trunk/tool-broker-api/src/main/java/eu/scy/toolbrokerapi/ToolBrokerAPI.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package eu.scy.toolbrokerapi;
+
+import roolo.api.IELO;
+import roolo.api.IMetadataKey;
+import roolo.api.IRepository;
+
+/**
+ * This is the interface of the Tool-Broker-API. The Tool-Broker-API can return
+ * references to all SCY broker services like the repository, the user
+ * management etc. These services can then be used by the tools.
+ * 
+ * @author Giemza
+ */
+public interface ToolBrokerAPI<K extends IMetadataKey> {
+    
+    /**
+     * This method returns the repository instance to be used by clients of the
+     * ToolsBrokerAPI.
+     * 
+     * @return repository the repository instace
+     */
+    public IRepository<IELO<K>, K> getRepository();
+    
+}
