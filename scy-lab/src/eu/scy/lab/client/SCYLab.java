4a0a579acd306f2268f087581a6f038289418314
Benedikt.roth <Benedikt.roth@bratislav.org>
2008-10-20 14:50:52 +0000

    integrated video tool

--- scy-lab/src/eu/scy/lab/client/SCYLab.java
+++ scy-lab/src/eu/scy/lab/client/SCYLab.java
@@ -6,6 +6,7 @@ package eu.scy.lab.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
 
+import eu.scy.lab.client.desktop.Desktop;
 import eu.scy.lab.client.login.Login;
 
 /**
@@ -17,9 +18,7 @@ public class SCYLab implements EntryPoint {
 
 	public void onModuleLoad() {
 		
-		//new Login Panel
-		Login loginPanel = new Login();
-		RootPanel.get().add(loginPanel.getCentredLoginDialog());
+		RootPanel.get().add( new Desktop() );
 		
 	}
 	
