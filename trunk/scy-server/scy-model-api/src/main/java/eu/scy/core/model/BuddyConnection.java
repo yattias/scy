77697d7762fbb2b9ade9448f176edf8c1b760dd8
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2008-10-20 09:52:28 +0000

    Removed reference to parent project

--- trunk/scy-server/scy-model-api/src/main/java/eu/scy/core/model/BuddyConnection.java
+++ trunk/scy-server/scy-model-api/src/main/java/eu/scy/core/model/BuddyConnection.java
@@ -5,7 +5,6 @@ package eu.scy.core.model;
  * User: Henrik
  * Date: 16.okt.2008
  * Time: 06:26:30
- * To change this template use File | Settings | File Templates.
  */
 public interface BuddyConnection extends ScyBase{
 
