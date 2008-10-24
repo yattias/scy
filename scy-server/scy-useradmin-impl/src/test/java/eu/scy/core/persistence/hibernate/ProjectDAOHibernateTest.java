a014f22408f75e97cb0e86e1ec9af116f4b7a154
henrik <henrik@enovate.no>
2008-10-24 11:47:05 +0000

    Lots of updates and transactional unittesting implemented

---
+++ scy-server/scy-useradmin-impl/src/test/java/eu/scy/core/persistence/hibernate/ProjectDAOHibernateTest.java
@@ -0,0 +1,64 @@
+package eu.scy.core.persistence.hibernate;
+
+import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
+import org.springframework.test.AbstractTransactionalSpringContextTests;
+import org.testng.annotations.Test;
+import eu.scy.core.persistence.ProjectDAO;
+import eu.scy.core.model.Project;
+import eu.scy.core.model.impl.ProjectImpl;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 24.okt.2008
+ * Time: 12:34:47
+ * To change this template use File | Settings | File Templates.
+ */
+@Test
+public class ProjectDAOHibernateTest extends AbstractTransactionalSpringContextTests {
+
+    private ProjectDAO projectDAO;
+
+    protected String[] getConfigLocations() {
+        return new String[]{"classpath:/eu/scy/core/persistence/hibernate/applciationContext-hibernate-OnlyForTesting.xml"};
+    }
+
+    public ProjectDAO getProjectDAO() {
+        return projectDAO;
+    }
+
+    public void setProjectDAO(ProjectDAO projectDAO) {
+        this.projectDAO = projectDAO;
+    }
+
+    @Test
+    public void testBeanDefined() {
+        assert (getProjectDAO() != null);
+    }
+
+    @Test
+    public void testSaveProject() {
+        Project p = new ProjectImpl();
+        p.setName("test projejcct");
+        assertNull(p.getId());
+        p = (Project) getProjectDAO().save(p);
+        assertNotNull(p.getId());
+    }
+
+    //Test
+    public void testFindProjectsByName() {
+        String projectName = "Henrik";
+        Project testProject = new ProjectImpl();
+        testProject.setName(projectName);
+        getProjectDAO().save(testProject);
+
+        List projects = getProjectDAO().findProjectsByName(projectName);
+        assert(projects.size() == 1);
+
+    }
+
+}
+
+
