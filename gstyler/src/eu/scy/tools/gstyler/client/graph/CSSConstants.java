7982a4af3f5c447fab1df5d7a7f6470915e4c2d2
Benedikt.roth <Benedikt.roth@bratislav.org>
2008-10-29 20:17:55 +0000

    highlight target nodes when drawing edges

--- gstyler/src/eu/scy/tools/gstyler/client/graph/CSSConstants.java
+++ gstyler/src/eu/scy/tools/gstyler/client/graph/CSSConstants.java
@@ -1,8 +1,7 @@
 package eu.scy.tools.gstyler.client.graph;
 
 public interface CSSConstants {
-
     public static final String CSS_DRAW_EDGES = "draw-edges";
     public static final String CSS_DRAG_HANDLE = "drag-handle";
-
+    public static final String CSS_DROPTARGET_NODE_ENGAGE = "dropTarget-node-engage";
 }
