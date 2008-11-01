ee876e0f3a9b7f5134e195896f928c05f30ad029
Benedikt.roth <Benedikt.roth@bratislav.org>
2008-11-01 14:15:38 +0000

    renamed palette to plugin according to new api

--- gstyler/src/main/java/eu/scy/tools/gstyler/client/PaletteContainer.java
+++ gstyler/src/main/java/eu/scy/tools/gstyler/client/PaletteContainer.java
@@ -4,7 +4,7 @@ import com.google.gwt.user.client.ui.TabPanel;
 
 import eu.scy.tools.gstyler.client.graph.application.GraphApplication;
 import eu.scy.tools.gstyler.client.graph.application.GraphPlugin;
-import eu.scy.tools.gstyler.client.plugins.mindmap.MindmapPalette;
+import eu.scy.tools.gstyler.client.plugins.mindmap.MindmapPlugin;
 
 /**
  * Class responsible for adding Palettes to the right part of GStylers UI.
@@ -13,7 +13,7 @@ public class PaletteContainer extends TabPanel {
 
     public PaletteContainer(GraphApplication gstyler) {
         setSize("100%", "300px");
-        addPalette(new MindmapPalette(gstyler));
+        addPalette(new MindmapPlugin(gstyler));
     }
 
     private void addPalette(GraphPlugin palette) {
