c124346fabcccb8304e3fadcb79f3cb26f873b88
adam.giemza <adam.giemza@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2008-11-14 13:56:31 +0000

    Share project "action-logging-api" into "https://scy.googlecode.com/svn"

---
+++ trunk/action-logging-api/src/main/java/eu/scy/actionlogging/api/IActionLogger.java
@@ -0,0 +1,10 @@
+package eu.scy.actionlogging.api;
+
+public interface IActionLogger
+{
+    /**
+     * logs an action
+     * @param action	the IAction object that will be logged
+     */
+    public void log(IAction action);
+}
