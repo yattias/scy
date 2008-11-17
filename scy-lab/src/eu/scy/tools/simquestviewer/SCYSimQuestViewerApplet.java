17b4c45b3a5b346a59e42659cb11231b26f3ac5c
larsbollen <larsbollen@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2008-11-17 13:50:05 +0000

    added some comments

--- scy-lab/src/eu/scy/tools/simquestviewer/SCYSimQuestViewerApplet.java
+++ scy-lab/src/eu/scy/tools/simquestviewer/SCYSimQuestViewerApplet.java
@@ -6,6 +6,11 @@ import sqv.SimQuestViewerApplet;
 
 public class SCYSimQuestViewerApplet extends SimQuestViewerApplet {
 
+	// run it with
+	// <PARAM NAME="file" VALUE="http://pervulgo.hopto.org/sqv/balance.html/balance.sqzx">
+	//    (or wherever you have put a *.sqzx file)
+	// <PARAM NAME="security" VALUE="false">
+
 	public void init() {
 		super.init();
 		// injecting the SCY datacollector
