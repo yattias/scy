a06e4a727512b453dd2dc5d6f936647a844db422
adam.giemza <adam.giemza@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2008-11-19 10:05:38 +0000

    Share project "simquestviewer" into "https://scy.googlecode.com/svn"

---
+++ simquestviewer/src/main/java/eu/scy/tools/simquestviewer/SCYSimQuestViewerApplet.java
@@ -0,0 +1,20 @@
+package eu.scy.tools.simquestviewer;
+
+import java.awt.BorderLayout;
+
+import sqv.SimQuestViewerApplet;
+
+public class SCYSimQuestViewerApplet extends SimQuestViewerApplet {
+
+	// run it with
+	// <PARAM NAME="file" VALUE="http://pervulgo.hopto.org/sqv/balance.html/balance.sqzx">
+	//    (or wherever you have put a *.sqzx file)
+	// <PARAM NAME="security" VALUE="false">
+
+	public void init() {
+		super.init();
+		// injecting the SCY datacollector
+		DataCollector dataCollector = new DataCollector(this.getSimQuestViewer());
+		this.getContentPane().add(dataCollector, BorderLayout.SOUTH);
+	}
+}
