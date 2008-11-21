32257c08238fc6ec84f1f242454ddfb6c6d8d37a
philippgerling <philippgerling@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2008-11-21 12:09:51 +0000

    changed package declarations

--- notification-impl/src/main/java/eu/scy/notification/NotificationSender.java
+++ notification-impl/src/main/java/eu/scy/notification/NotificationSender.java
@@ -1,4 +1,4 @@
-package main.java.eu.scy.notification;
+package eu.scy.notification;
 
 import javax.jms.Connection;
 import javax.jms.DeliveryMode;
@@ -9,8 +9,8 @@ import javax.jms.TextMessage;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 
-import main.java.eu.scy.notification.api.INotification;
-import main.java.eu.scy.notification.api.INotificationSender;
+import eu.scy.notification.api.INotification;
+import eu.scy.notification.api.INotificationSender;
 
 
 public class NotificationSender implements INotificationSender {
