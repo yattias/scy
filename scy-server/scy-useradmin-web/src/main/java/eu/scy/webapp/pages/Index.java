b6cb75efd8d5dcf9bcaa6edbd6113f7d21c36a29
henrik <henrik@enovate.no>
2008-11-25 09:00:11 +0000

    Added some defaults

--- scy-server/scy-useradmin-web/src/main/java/eu/scy/webapp/pages/Index.java
+++ scy-server/scy-useradmin-web/src/main/java/eu/scy/webapp/pages/Index.java
@@ -11,6 +11,8 @@ import java.util.logging.Logger;
 
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.annotations.InjectPage;
+import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.annotations.Persist;
 
 /**
  * Start page of application scy-useradmin-web.
@@ -102,5 +104,28 @@ public class Index extends ScyModelPage {
         log.info("Creating new proejct!");
         return _projectEditor;
     }
+
+
+
+
+@Persist
+    private String _activePanel;
+
+    public String getActivePanel()
+    {
+        return _activePanel;
+    }
+
+    public void setActivePanel(String activePanel)
+    {
+        _activePanel = activePanel;
+    }
+
+    @OnEvent(component = "tabset", value = "action")
+    public void onChange(String choosenPanelId)
+    {
+        _activePanel = choosenPanelId;
+    }
+    
 
 }
