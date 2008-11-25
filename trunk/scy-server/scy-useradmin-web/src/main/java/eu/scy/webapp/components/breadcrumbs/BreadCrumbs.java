40ccfc4f78943de05e82e31a251acc4ee08adff5
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2008-11-25 05:27:51 +0000

    BreadCrumbs

---
+++ trunk/scy-server/scy-useradmin-web/src/main/java/eu/scy/webapp/components/breadcrumbs/BreadCrumbs.java
@@ -0,0 +1,13 @@
+package eu.scy.webapp.components.breadcrumbs;
+
+import eu.scy.webapp.pages.TapestryContextAware;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 25.nov.2008
+ * Time: 06:23:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class BreadCrumbs extends TapestryContextAware {
+}
