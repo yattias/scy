2f8d26583369a378d8d89089eda1d75e86fa06fc
Benedikt.roth <Benedikt.roth@bratislav.org>
2008-12-01 15:42:53 +0000

    Use test version of the drawing tool integration using gwt-ai

--- scy-lab/lab/src/main/java/eu/scy/lab/client/desktop/tools/Tools.java
+++ scy-lab/lab/src/main/java/eu/scy/lab/client/desktop/tools/Tools.java
@@ -3,20 +3,17 @@ package eu.scy.lab.client.desktop.tools;
 import com.google.gwt.core.client.GWT;
 import com.gwtext.client.core.EventObject;
 import com.gwtext.client.data.Node;
-import com.gwtext.client.widgets.Panel;
 import com.gwtext.client.widgets.tree.TreeNode;
 import com.gwtext.client.widgets.tree.TreePanel;
 import com.gwtext.client.widgets.tree.event.TreeNodeListenerAdapter;
 
 import eu.scy.lab.client.desktop.Desktop;
 import eu.scy.tools.co2sim.client.CO2Sim;
+import eu.scy.tools.drawing.client.DrawingTool;
 import eu.scy.tools.map.client.MapTool;
 import eu.scy.tools.youtube.client.VideoInput;
 
 class Tools extends TreePanel {
-    
-    protected static final String DRAWTOOL_ID = "draw-tool";
-    protected static final String DRAWTOOL_PATH = "applet/drawTool.html";
     
     
     public Tools(final Desktop desktop) {
@@ -26,23 +23,24 @@ class Tools extends TreePanel {
         TreeNode drawing = new TreeNode("Drawing");
         drawing.setExpanded(true);
         
-        TreeNode jDT = new TreeNode("Drawing Tool");
-        jDT.addListener(new TreeNodeListenerAdapter() {
+        TreeNode drawingTool = new TreeNode("Drawing Tool");
+        drawingTool.addListener(new TreeNodeListenerAdapter() {
             
             public void onDblClick(Node node, EventObject e) {
-                if (!desktop.getWorkspace().containsComponentID(DRAWTOOL_ID)) {
-                    Panel panel = new Panel("Draw Tool");
-                    panel.setClosable(true);
-                    panel.setId(DRAWTOOL_ID);
-                    panel.setHtml("<applet archive=\"applet/libs/clientDrawTool.jar,applet/libs/appFramework-1.0.jar,applet/libs/spring.jar,applet/libs/commons-logging-1.1.1.jar,applet/libs/jdom-1.1.jar,applet/libs/whiteboard.jar,applet/libs/roolo-api.jar,applet/libs/roolo-mock.jar\" code=\"eu.scy.client.tools.drawing.DrawingApplet\" width=\"600\" height=\"500\"></applet>");
-                    desktop.getWorkspace().add(panel);
+                if (!desktop.getWorkspace().containsComponentID(DrawingTool.DRAWINGTOOL_ID)) {
+                    desktop.getWorkspace().add( new DrawingTool() );
+//                    Panel panel = new Panel("Draw Tool");
+//                    panel.setClosable(true);
+//                    panel.setId(DRAWTOOL_ID);
+//                    panel.setHtml("<applet archive=\"applet/libs/clientDrawTool.jar,applet/libs/appFramework-1.0.jar,applet/libs/spring.jar,applet/libs/commons-logging-1.1.1.jar,applet/libs/jdom-1.1.jar,applet/libs/whiteboard.jar,applet/libs/roolo-api.jar,applet/libs/roolo-mock.jar\" code=\"eu.scy.client.tools.drawing.DrawingApplet\" width=\"600\" height=\"500\"></applet>");
+//                    desktop.getWorkspace().add(panel);
                 }
-                desktop.getWorkspace().activate(DRAWTOOL_ID);
+                desktop.getWorkspace().activate(DrawingTool.DRAWINGTOOL_ID);
             }
         });
-        jDT.setExpanded(true);
+        drawingTool.setExpanded(true);
         
-        drawing.appendChild(jDT);
+        drawing.appendChild(drawingTool);
         
         TreeNode simulation = new TreeNode("Simulation");
         simulation.setExpanded(true);
