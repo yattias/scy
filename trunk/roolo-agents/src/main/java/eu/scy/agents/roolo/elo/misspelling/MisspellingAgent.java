71e3c92088339963c377429d2a67cf3b511b595f
hanbei@gmx.de <hanbei@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2008-12-16 10:53:34 +0000

    implementing agents sorted by functionality

---
+++ trunk/roolo-agents/src/main/java/eu/scy/agents/roolo/elo/misspelling/MisspellingAgent.java
@@ -0,0 +1,50 @@
+package eu.scy.agents.roolo.elo.misspelling;
+
+import info.collide.sqlspaces.commons.Tuple;
+import info.collide.sqlspaces.commons.TupleSpaceException;
+
+import java.io.File;
+import java.net.URL;
+
+import com.swabunga.spell.engine.SpellDictionary;
+import com.swabunga.spell.engine.SpellDictionaryHashMap;
+import com.swabunga.spell.event.SpellChecker;
+import com.swabunga.spell.event.StringWordTokenizer;
+
+import eu.scy.agents.impl.AbstractProcessingAgent;
+
+public class MisspellingAgent extends AbstractProcessingAgent {
+    
+    public MisspellingAgent() {
+        super();
+    }
+    
+    @Override
+    protected void doRun() {
+        Tuple t;
+        try {
+            t = getTupleSpace().waitToTake(new Tuple("misspellings", Long.class, String.class));
+        } catch (TupleSpaceException e) {
+            throw new RuntimeException(e);
+        }
+        
+        String content = (String) t.getField(2).getValue();
+        content = content.replaceAll("<[^>]*>", "");
+        
+        URL url = MisspellingAgent.class.getResource("/de.dic");// eng_com.dic
+        File dictFile;
+        try {
+            dictFile = new File(url.toURI());
+            SpellDictionary dictionary = new SpellDictionaryHashMap(dictFile);
+            SpellChecker spellChecker = new SpellChecker(dictionary);
+            int numOfErrors = spellChecker.checkSpelling(new StringWordTokenizer(content));
+            if (numOfErrors > 0) {
+                getTupleSpace().write(new Tuple("misspellings", System.currentTimeMillis(), numOfErrors));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+    
+}
