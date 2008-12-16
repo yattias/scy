7b178ed761757f79bc4b63e2f7d8c4cd8f6e54ce
hanbei@gmx.de <hanbei@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2008-12-16 15:32:12 +0000

    First version of the elo-importer

---
+++ trunk/elo-importer/src/main/java/eu/scy/eloimporter/LearningResourceType.java
@@ -0,0 +1,19 @@
+package eu.scy.eloimporter;
+
+public enum LearningResourceType {
+    exercise, 
+    simulation, 
+    questionnaire, 
+    diagram, 
+    figure, 
+    graph, 
+    index, 
+    slide, 
+    table, 
+    narrativetext, 
+    exam, 
+    experiment, 
+    problemstatement, 
+    selfassessment, 
+    lecture;
+}
