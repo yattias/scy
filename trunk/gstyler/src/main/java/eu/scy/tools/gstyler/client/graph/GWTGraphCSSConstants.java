9569098c5ee0da42963f21923629a5c3f2b6dc76
Benedikt.roth@bratislav.org <Benedikt.roth@bratislav.org@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-01-08 11:58:21 +0000

    moved to another repository.

--- trunk/gstyler/src/main/java/eu/scy/tools/gstyler/client/graph/GWTGraphCSSConstants.java
+++
@@ -1,7 +0,0 @@
-package eu.scy.tools.gstyler.client.graph;
-
-public interface GWTGraphCSSConstants {
-    public static final String CSS_CLICKABLE_WIDGET = "graph-clickable-widget";
-    public static final String CSS_NODE_DRAG_HANDLE = "graph-node-drag-handle";
-    public static final String CSS_NODE_DROPTARGET_ENGAGE = "graph-node-dropTarget-engage";
-}
