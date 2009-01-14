9366950b02af688397040fc2f700258d06f40f50
larschla <larschla@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-01-14 11:45:53 +0000

    SCYLab object created

---
+++ trunk/scy-server/scy-useradmin-web/src/main/webapp/scripts/application.js
@@ -0,0 +1,15 @@
+//var sLab = new SCYLab();
+//lLab.init();
+
+
+
+var SCYLab = {
+
+            init: function() {
+                alert("SCY-Lab here");
+            },
+
+            bla: function() {
+
+            }
+        }
