e0372b56f77d3a35f7b83e3d623c3970d58144fa
adam.giemza <adam.giemza@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-01-16 09:26:27 +0000

    Changed interface to use INotification

--- notification-api/src/main/java/eu/scy/notification/api/INotificationSender.java
+++ notification-api/src/main/java/eu/scy/notification/api/INotificationSender.java
@@ -3,8 +3,10 @@ package eu.scy.notification.api;
 public interface INotificationSender {
 	/**
 	 * sends a notification
+	 * @param user user to send to
+	 * @param tool	tool sending from
 	 * @param notification
 	 */
-	public void send(INotification notification);
+	public void send(String user, String tool, INotification notification);
 
 }
