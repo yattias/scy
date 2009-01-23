323f5ad5a889cf3f0148a550923de98e78877296
adam.giemza <adam.giemza@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-01-23 13:06:42 +0000

    added notification

--- trunk/roolo-agents/src/main/java/eu/scy/agents/roolo/elo/misspelling/MisspellingNotificationAgent.java
+++ trunk/roolo-agents/src/main/java/eu/scy/agents/roolo/elo/misspelling/MisspellingNotificationAgent.java
@@ -4,28 +4,35 @@ import info.collide.sqlspaces.commons.Tuple;
 import info.collide.sqlspaces.commons.TupleSpaceException;
 import roolo.elo.api.IMetadataKey;
 import eu.scy.agents.impl.AbstractCommunicationAgent;
+import eu.scy.notification.Notification;
+import eu.scy.notification.NotificationSender;
+import eu.scy.notification.api.INotification;
 
-public class MisspellingNotificationAgent<K extends IMetadataKey> extends
-		AbstractCommunicationAgent<K> {
-
-	@Override
-	protected void doRun() {
-		Tuple t;
-		try {
-			t = getTupleSpace().waitToTake(
-					new Tuple("misspellings", String.class, Long.class, Integer.class));
-			String uri = (String) t.getField(1).getValue();
-			Integer numberOfErrors = (Integer) t.getField(3).getValue();
-			getTupleSpace().write(new Tuple("notification", "misspelling", uri, numberOfErrors));
-			System.out.println("***************** your document " + uri + " has " + numberOfErrors
-					+ " spelling errors ********** ");
-		} catch (TupleSpaceException e) {
-			throw new RuntimeException(e);
-		}
-
-		// INotification notification = new Notification();
-		// notification.addProperty("errors", "" + numberOfErrors);
-		// notification.addProperty("target", "misspellings");
-		// getToolBrokerAPI().getNotificationService().notifyCallbacks(notification);
-	}
+public class MisspellingNotificationAgent<K extends IMetadataKey> extends AbstractCommunicationAgent<K> {
+    
+    private NotificationSender sender;
+    
+    public MisspellingNotificationAgent() {
+        sender = new NotificationSender();
+    }
+    
+    @Override
+    protected void doRun() {
+        Tuple t;
+        try {
+            t = getTupleSpace().waitToTake(new Tuple("misspellings", String.class, Long.class, Integer.class));
+            String uri = (String) t.getField(1).getValue();
+            Integer numberOfErrors = (Integer) t.getField(3).getValue();
+            getTupleSpace().write(new Tuple("notification", "misspelling", uri, numberOfErrors));
+            System.out.println("***************** your document " + uri + " has " + numberOfErrors + " spelling errors ********** ");
+            
+            INotification notification = new Notification();
+            notification.addProperty("errors", "" + numberOfErrors);
+            notification.addProperty("target", "misspellings");
+            sender.send("adam", "textpad", notification);
+        } catch (TupleSpaceException e) {
+            throw new RuntimeException(e);
+        }
+        
+    }
 }
