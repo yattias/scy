6794368bc5017722e2fda494b504ec57d453005f
adam.giemza <adam.giemza@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-01-23 10:50:24 +0000

    Removed MetaData access and changed access to TupleSpace

--- agent-impl/src/main/java/eu/scy/agents/impl/AbstractAgent.java
+++ agent-impl/src/main/java/eu/scy/agents/impl/AbstractAgent.java
@@ -2,11 +2,8 @@ package eu.scy.agents.impl;
 
 import info.collide.sqlspaces.client.TupleSpace;
 import info.collide.sqlspaces.commons.TupleSpaceException;
-import roolo.elo.api.IMetadataKey;
-import roolo.elo.api.IMetadataTypeManager;
 import eu.scy.agents.api.IAgent;
 import eu.scy.agents.api.IPersistentStorage;
-import eu.scy.toolbroker.ToolBrokerImpl;
 
 /**
  * Implementation of the IAgent interface.
@@ -14,26 +11,25 @@ import eu.scy.toolbroker.ToolBrokerImpl;
  * @author fschulz
  */
 public class AbstractAgent implements IAgent {
-
-	protected static IMetadataTypeManager<IMetadataKey> typeManager = new ToolBrokerImpl<IMetadataKey>()
-			.getMetaDataTypeManager();
-
-	@Override
-	public IPersistentStorage getPersistentStorage() {
-		return new PersistentStorage();
-	}
-
-	@Override
-	public TupleSpace getTupleSpace() {
-		try {
-			return new TupleSpace();
-		} catch (TupleSpaceException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	@Override
-	public IMetadataTypeManager<IMetadataKey> getMetadataTypeManager() {
-		return typeManager;
-	}
+    
+    private TupleSpace tupleSpace;
+    
+    public AbstractAgent() {
+        try {
+            tupleSpace = new TupleSpace();
+        } catch (TupleSpaceException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Override
+    public IPersistentStorage getPersistentStorage() {
+        return new PersistentStorage();
+    }
+    
+    @Override
+    public TupleSpace getTupleSpace() {
+        return tupleSpace;
+    }
+    
 }
