8ad8e2f97d234cc8cef4d904ee122bda533b0ef6
adam.giemza <adam.giemza@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-01-23 13:05:42 +0000

    just style

--- trunk/roolo-agents/src/main/java/eu/scy/agents/roolo/elo/misspelling/MisspellingProcessELO.java
+++ trunk/roolo-agents/src/main/java/eu/scy/agents/roolo/elo/misspelling/MisspellingProcessELO.java
@@ -10,18 +10,17 @@ import roolo.elo.api.IELO;
 import roolo.elo.api.IMetadataKey;
 import eu.scy.agents.impl.elo.AbstractELOAgent;
 
-public class MisspellingProcessELO<T extends IELO<K>, K extends IMetadataKey> extends
-		AbstractELOAgent<T, K> {
-
-	@Override
-	public void processElo(T elo) {
-		try {
-			TupleSpace ts = this.getTupleSpace();
-			ts.write(new Tuple("misspellings", elo.getUri().toString(), System.currentTimeMillis(),
-					elo.getContent(Locale.GERMAN).getXml()));
-		} catch (TupleSpaceException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-	}
+public class MisspellingProcessELO<T extends IELO<K>, K extends IMetadataKey> extends AbstractELOAgent<T, K> {
+    
+    @Override
+    public void processElo(T elo) {
+        try {
+            TupleSpace ts = getTupleSpace();
+            ts.write(new Tuple("misspellings", elo.getUri().toString(), System.currentTimeMillis(),
+                    elo.getContent(Locale.ENGLISH).getXml()));
+        } catch (TupleSpaceException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
 }
