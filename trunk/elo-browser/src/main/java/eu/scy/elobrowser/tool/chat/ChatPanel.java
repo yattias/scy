d3b6bcfccf2706646ca7aefb9d66a231f34b28d9
larsbollen <larsbollen@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-01-27 09:33:11 +0000

    chat added

---
+++ trunk/elo-browser/src/main/java/eu/scy/elobrowser/tool/chat/ChatPanel.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eu.scy.elobrowser.tool.chat;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author lars
+ */
+public class ChatPanel extends JPanel {
+
+    public ChatPanel() {
+        super();
+        this.add(new JLabel("chatpanel goes here..."));
+        this.setVisible(true);
+    }
+}
