e745ebed2ec7a90a0b56846d26b9520ea14ac682
hanbei@gmx.de <hanbei@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-02-02 12:20:21 +0000

    allows elo agents to change or add metadata

--- trunk/agent-impl/src/main/java/eu/scy/agents/impl/elo/AbstractELOAgent.java
+++ trunk/agent-impl/src/main/java/eu/scy/agents/impl/elo/AbstractELOAgent.java
@@ -2,11 +2,22 @@ package eu.scy.agents.impl.elo;
 
 import roolo.elo.api.IELO;
 import roolo.elo.api.IMetadataKey;
+import roolo.elo.api.IMetadataTypeManager;
 import eu.scy.agents.api.elo.IELOFilterAgent;
 import eu.scy.agents.impl.AbstractAgent;
+import eu.scy.toolbroker.ToolBrokerImpl;
 
-public abstract class AbstractELOAgent<T extends IELO<K>, K extends IMetadataKey> extends AbstractAgent implements IELOFilterAgent<T, K> {
+public abstract class AbstractELOAgent<T extends IELO<K>, K extends IMetadataKey>
+		extends AbstractAgent implements IELOFilterAgent<T, K> {
 
-    // abstract convenience class
-    
+	private IMetadataTypeManager<IMetadataKey> typeManger;
+
+	protected IMetadataTypeManager<IMetadataKey> getMetadataTypeManager() {
+		if (typeManger == null) {
+			typeManger = new ToolBrokerImpl<IMetadataKey>()
+					.getMetaDataTypeManager();
+		}
+		return typeManger;
+	}
+
 }
