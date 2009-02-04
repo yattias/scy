69b80dd81a52461c26cf0d0eab9c98b9db595da2
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-02-04 11:59:22 +0000

    update nutpad client

--- trunk/collaborationservice/src/main/java/eu/scy/collaborationservice/liteclient/AwarenessClient.java
+++ trunk/collaborationservice/src/main/java/eu/scy/collaborationservice/liteclient/AwarenessClient.java
@@ -1,10 +1,7 @@
 package eu.scy.collaborationservice.liteclient;
 
-
 import java.awt.BorderLayout;
 import java.awt.Font;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -14,6 +11,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
 import javax.swing.table.TableModel;
 
 import org.apache.log4j.Logger;
@@ -22,132 +20,148 @@ import eu.scy.collaborationservice.CollaborationService;
 import eu.scy.collaborationservice.CollaborationServiceClientInterface;
 import eu.scy.core.model.impl.ScyBaseObject;
 
+public class AwarenessClient extends JFrame implements
+		CollaborationServiceClientInterface {
 
-public class AwarenessClient extends JFrame implements CollaborationServiceClientInterface {
-    
-    private final static Logger logger = Logger.getLogger(AwarenessClient.class.getName());
-    private static final String HARD_CODED_TOOL_NAME = "Spiffy Awareness Client";
-    private static final long LOGIN_KEEPALIVE_DURATION = 1 * 1000;
-    //private static final String[] COLUMN_NAMES = { "username", "status", "doc id"};
+	private final static Logger logger = Logger.getLogger(AwarenessClient.class
+			.getName());
+	private static final String HARD_CODED_TOOL_NAME = "Spiffy Awareness Client";
+	private static final long LOGIN_KEEPALIVE_DURATION = 1 * 1000;
+	// private static final String[] COLUMN_NAMES = { "username", "status",
+	// "doc id"};
 
-    private JTextArea textArea;
-    private JPanel panel;
-    private JScrollPane scrollPane;
-    private ArrayList<String> usersToWatch;
-    private HashMap<String, Object> toolsToWatch = new HashMap<String, Object>();
-    private String userName;
-    private String loginId;
+	private JTextArea textArea;
+	private JPanel panel;
+	private JScrollPane scrollPane;
+	private HashMap<String, Object> toolsToWatch = new HashMap<String, Object>();
+	private String userName;
+	private String loginId;
 
-    private CollaborationService cs;
-    
-    private Timer loginTimer;
-    private Timer buddyTimer;
-    
-    private TableModel tableModel;
+	private CollaborationService cs;
 
-    
-    public AwarenessClient() {        
-    }
+	private Timer loginTimer;
+	private Timer buddyTimer;
 
-    
-    public static AwarenessClient createAwarenessClient(String userName, String toolName) {
-        logger.debug("Awareness is upon you.");
-        
-        AwarenessClient ac = new AwarenessClient();
-        ac.userName = userName;
-        ac.toolsToWatch.put(toolName, toolName);
-        ac.usersToWatch = new ArrayList<String>(Arrays.asList(new String[]{"jeremyt", "olesm", "janad"})); //this list will be populated by usermanagement
-        
-        ac.cs = CollaborationService.createCollaborationService(ac.userName, CollaborationService.AWARENESS_SERVICE_SPACE, ac);
-        ac.signUp();
-        
-        // Set the frame characteristics
-        ac.setTitle( "Awereness client makes " + userName + " happy");
-        ac.setSize(300, 500);
-       
-        ac.textArea = new JTextArea(15, 80);
-        ac.textArea.setEditable(false);
-        ac.textArea.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
-        ac.textArea.setFont(new Font("monospaced", Font.PLAIN, 14));
-        JScrollPane scrollingText = new JScrollPane(ac.textArea);
-        
-        ac.panel = new JPanel();
-        ac.panel.setLayout(new BorderLayout());
-        ac.panel.add(scrollingText, BorderLayout.CENTER);
-        
-        
-        ac.getContentPane().add(ac.panel);
-        ac.setLocationRelativeTo(null);
-        ac.setLocation(ac.getLocation().x + 200, ac.getLocation().y + 200);
-        ac.setVisible(true);
-        
-        ac.loginTimer = new Timer();
-        ac.loginTimer.schedule(ac.new LoginTimer(), 1, LOGIN_KEEPALIVE_DURATION);
-        ac.buddyTimer = new Timer();
-        ac.buddyTimer.schedule(ac.new BuddyPresenceTimer(), 1, LOGIN_KEEPALIVE_DURATION);
-        return ac;
-    }
-    
-    
-    private void refreshBuddyList() {
-        logger.debug("SAC refreshes buddylist every " + LOGIN_KEEPALIVE_DURATION);
-        String outputToTextArea = "";
-        String user;
-        String status;
-        for (int i = 0 ; i<usersToWatch.size() ; i++) {
-            user = usersToWatch.get(i);
-            if (cs.read(user, HARD_CODED_TOOL_NAME) != null) {
-                status = "online";
-            } else {
-                status = "offline";
-            }
-        	outputToTextArea = outputToTextArea + user + "\t" + status + "\n";
-        }
-        textArea.setText(outputToTextArea);
-    }
+	private TableModel tableModel;
 
+	private HashMap<String, Boolean> usersToWatch = new HashMap<String, Boolean>();
 
-    private class LoginTimer extends TimerTask {
-        public void run() {
-            signOff();
-            signUp();
-        }
-    }
-    
-    private class BuddyPresenceTimer extends TimerTask {
-        public void run() {
-           refreshBuddyList();
-        }
-    }
+	public AwarenessClient() {
+	}
 
+	public AwarenessClient(String userName, String toolName) {
+		logger.debug("Awareness is upon you.");
 
-    private void signUp() {
-        logger.debug("SAC signing up");
-        ScyBaseObject sbo = new ScyBaseObject();
-        sbo.setId("");
-        sbo.setName("");
-        sbo.setDescription("");
-        this.loginId = cs.write(HARD_CODED_TOOL_NAME, sbo, LOGIN_KEEPALIVE_DURATION + 2000);
-    }
-    
-    
-    private void signOff() {
-        logger.debug("SAC signing off");
-        cs.takeById(this.loginId);
-    }
-    
-    public void shutDown() {
-    	 setVisible(false);
-         dispose();
-    }
+		userName = userName;
+		toolsToWatch.put(toolName, toolName);
 
+		// needs to come from the collab services
+		usersToWatch.put("thomasd", false);
+		usersToWatch.put("olesm", false);
+		usersToWatch.put("janad", false);
 
-    public void actionUponDelete(String username) {
-        // TODO Auto-generated method stub
-    }
+		// ac.usersToWatch = new ArrayList<String>(Arrays.asList(new
+		// String[]{"jeremyt", "olesm", "janad"})); //this list will be
+		// populated by usermanagement
 
+		cs = CollaborationService.createCollaborationService(userName,
+				CollaborationService.AWARENESS_SERVICE_SPACE, this);
+		signUp();
 
-    public void actionUponWrite(String username) {
-        // TODO Auto-generated method stub     
-    }
+		// Set the frame characteristics
+		setTitle("Awereness client makes " + userName + " happy");
+		setSize(300, 500);
+
+		textArea = new JTextArea(15, 80);
+		textArea.setEditable(false);
+		textArea.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+		textArea.setFont(new Font("monospaced", Font.PLAIN, 14));
+		JScrollPane scrollingText = new JScrollPane(textArea);
+
+		panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.add(scrollingText, BorderLayout.CENTER);
+
+		getContentPane().add(panel);
+		setLocationRelativeTo(null);
+		setLocation(getLocation().x + 200, getLocation().y + 200);
+		setVisible(true);
+
+		loginTimer = new Timer();
+		loginTimer.schedule(new LoginTimer(), 1, LOGIN_KEEPALIVE_DURATION);
+	}
+
+	private void generateBuddyList() {
+		// logger.debug("SAC refreshes buddylist every " +
+		// LOGIN_KEEPALIVE_DURATION);
+		String outputToTextArea = "";
+		String status;
+		for (String user : usersToWatch.keySet()) {
+			if (cs.read(user, HARD_CODED_TOOL_NAME) != null) {
+				status = "online";
+			} else {
+				status = "offline";
+			}
+			outputToTextArea = outputToTextArea + user + "\t" + status + "\n";
+		}
+		textArea.setText(outputToTextArea);
+	}
+
+	private class LoginTimer extends TimerTask {
+		public void run() {
+			signOff();
+			signUp();
+		}
+	}
+
+	private void refreshBuddyList() {
+		String outputToTextArea = "";
+		String status;
+		for (String user : usersToWatch.keySet()) {
+			if (usersToWatch.get(user)) {
+				status = "online";
+			} else {
+				status = "offline";
+			}
+			outputToTextArea = outputToTextArea + user + "\t" + status + "\n";
+		}
+		final String text = outputToTextArea;
+		SwingUtilities.invokeLater(new Runnable() {
+
+			public void run() {
+				textArea.setText(text);
+			}
+		});
+
+	}
+
+	private void signUp() {
+		logger.debug("SAC signing up");
+		ScyBaseObject sbo = new ScyBaseObject();
+		sbo.setId("");
+		sbo.setName("");
+		sbo.setDescription("");
+		this.loginId = cs.write(HARD_CODED_TOOL_NAME, sbo,
+				LOGIN_KEEPALIVE_DURATION + 2000);
+	}
+
+	private void signOff() {
+		logger.debug("SAC signing off");
+		cs.takeById(this.loginId);
+	}
+
+	public void shutDown() {
+		setVisible(false);
+		dispose();
+	}
+
+	public void actionUponDelete(String username) {
+		this.usersToWatch.put(username, false);
+		refreshBuddyList();
+	}
+
+	public void actionUponWrite(String username) {
+		this.usersToWatch.put(username, true);
+		refreshBuddyList();
+	}
 }
