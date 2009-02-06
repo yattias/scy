573f696b70d4be01dc13cc6a38191b0df4e79d5e
thomas.drevon <thomas.drevon@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-02-06 10:37:02 +0000

    packet interception works!

--- tuplespaceconnector/src/main/java/eu/scy/collaborationservice/tuplespaceconnector/TuplespaceConnector.java
+++ tuplespaceconnector/src/main/java/eu/scy/collaborationservice/tuplespaceconnector/TuplespaceConnector.java
@@ -5,6 +5,7 @@ import java.io.File;
 import org.apache.log4j.Logger;
 import org.jivesoftware.openfire.container.Plugin;
 import org.jivesoftware.openfire.container.PluginManager;
+import org.jivesoftware.openfire.interceptor.InterceptorManager;
 import org.jivesoftware.openfire.interceptor.PacketInterceptor;
 import org.jivesoftware.openfire.interceptor.PacketRejectedException;
 import org.jivesoftware.openfire.session.Session;
@@ -12,21 +13,39 @@ import org.xmpp.packet.Packet;
 
 
 public class TuplespaceConnector implements Plugin, PacketInterceptor {
-    
+
     private static final Logger logger = Logger.getLogger(TuplespaceConnector.class.getName());
+    private InterceptorManager interceptorManager;                                       
+    
+    public TuplespaceConnector() {
+        interceptorManager = InterceptorManager.getInstance();
+    }
+
+
+    public void initializePlugin(PluginManager manager, File pluginDirectory) {
+        // register with interceptor manager
+        logger.debug("Connections make you happy.");
+        interceptorManager.addInterceptor(this);
+    }
 
     public void destroyPlugin() {
-        // TODO Auto-generated method stub        
+        // unregister with interceptor manager
+        interceptorManager.removeInterceptor(this);
     }
 
-    public void initializePlugin(PluginManager arg0, File arg1) {
-        // TODO Auto-generated method stub
-        logger.debug("You are connected to cheesy connector. Connections make you happy.");
-    }
 
-    public void interceptPacket(Packet arg0, Session arg1, boolean arg2, boolean arg3) throws PacketRejectedException {
-        logger.debug("Packet?: gotcha! " + arg0.toXML());
-        System.out.println("Packet?: gotcha! " + arg0.toXML());
+    public void interceptPacket(Packet packet, Session session, boolean incoming, boolean processed) throws PacketRejectedException {
+        if (processed) {
+            logger.debug("packet processed.");
+        } else {
+            logger.debug("packet not processed.");
+        }
+        if (incoming) {
+            logger.debug("packet incoming.");
+        } else {
+            logger.debug("packet outgoing.");
+        }
+        logger.debug("packet: " + packet.toXML());        
+        logger.debug("============================================");        
     }
-
-}
+}   
