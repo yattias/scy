448069b56f82deb980f0ff9e157ecd2610725fe9
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-02-19 16:00:22 +0000

    package reshuffle and naming reshuffle

--- trunk/collaboration-service/src/main/java/eu/scy/collaborationservice/CollaborationService.java
+++ trunk/collaboration-service/src/main/java/eu/scy/collaborationservice/CollaborationService.java
@@ -1,176 +1,79 @@
+//package eu.scy.collaborationservice.tuplespaceconnector;
 package eu.scy.collaborationservice;
-
-import info.collide.sqlspaces.client.TupleSpace;
-import info.collide.sqlspaces.commons.Callback;
-import info.collide.sqlspaces.commons.Field;
-import info.collide.sqlspaces.commons.Tuple;
-import info.collide.sqlspaces.commons.TupleID;
-import info.collide.sqlspaces.commons.TupleSpaceException;
-
-import java.util.ArrayList;
 
 import org.apache.log4j.Logger;
 
 import eu.scy.core.model.impl.ScyBaseObject;
+import eu.scy.tuple.TupleAdapter;
 
-public class CollaborationService implements Callback {
-    
-    private final static Logger logger = Logger.getLogger(CollaborationService.class.getName());
-    private static final String DEFAULT_SERVER_IP = "129.240.212.15";
-    private static final int DEFAULT_SERVER_PORT = 2525;
-    public static final String COLLABORATION_SERVICE_SPACE = "COLLABORATION_SERVICE_SPACE";
-    public static final String AWARENESS_SERVICE_SPACE = "AWARENESS_SERVICE_SPACE";
-    
-    private TupleSpace tupleSpace;
-    private String userName = "unregistered_user";
-    
-    private CollaborationServiceClientInterface client;
 
+public class CollaborationService implements ICollaborationService {
     
-    
+    private static final Logger logger = Logger.getLogger(CollaborationService.class.getName());
+    private static final long DEFAULT_EXPIRATION_TIME = 30*1000;
+    private TupleAdapter tupleAdapter;
+    private static ICollaborationService collaborationService;
+
+
     public CollaborationService() {
+        logger.debug("Empty Constructor Collaboration created");
     }
-    
-    
-    public static CollaborationService createCollaborationService(String userName, String sqlSpaceName, CollaborationServiceClientInterface csci) {
-        return createCollaborationService(DEFAULT_SERVER_IP, userName, sqlSpaceName, csci);
-    }
-    
-    public static CollaborationService createCollaborationService(String serverIpAddress, String userName, String sqlSpaceName, CollaborationServiceClientInterface csci) {
-        CollaborationService cs = null;
-        cs = new CollaborationService();
-        cs.client = csci;
-        cs.userName = userName;
-        TupleSpace ts;
-        Tuple template = new Tuple(String.class, String.class, String.class, String.class, String.class, String.class);
-        try {
-            ts = new TupleSpace(serverIpAddress, DEFAULT_SERVER_PORT, sqlSpaceName);
-            
-            //setup the events that client will use
-            ts.eventRegister(Command.WRITE, template, cs, true);
-            ts.eventRegister(Command.DELETE, template, cs, true);
-        } catch (TupleSpaceException e) {
-            logger.error("Tupplespace pb " + e);
-            return null;
+
+    public static ICollaborationService getInstance() {
+        if (collaborationService == null) {
+            logger.debug("Created Tuple Spaces");
+            collaborationService = new CollaborationService();
         }
-        cs.tupleSpace = ts;
-        return cs;
+        return collaborationService;
+    }
+
+
+    public void actionUponDelete(String username) {
+        // TODO Auto-generated method stub
+    }
+    
+    public void actionUponWrite(String username) {
+        // TODO Auto-generated method stub
     }
     
     
-    public String write(String tool, ScyBaseObject object, long expiration) {
-        return write(null, tool, object, expiration);
+    public String userPing(String jabberUser) {
+        logger.debug("Attempting to write PING to CS");
+        String id = write(jabberUser, "ping");
+        logger.debug("ID returned: " + id);
+        return id;
     }
     
     
-    public String write(String tool, ScyBaseObject object) {
-        return write(null, tool, object, 0);
+    public String userUnavailable(String jabberUser) {
+        logger.debug("Attempting to write UNAVAILABLE to CS");
+        String id = write(jabberUser, "unavailable");
+        logger.debug("ID returned: " + id);
+        return id;
     }
     
     
-    public String write(String sqlSpaceId, String tool, ScyBaseObject object) {
-        return write(sqlSpaceId, tool, object, 0);
-    }
-    
-    public String write(String sqlSpaceId, String tool, ScyBaseObject object, long expiration) {
-        // sqlSpaceId == tupleId
-        Tuple tuple = new Tuple(this.userName, tool, object.getId(), object.getClass().getName(), object.getName(), object.getDescription());
-        if (expiration > 0) {
-            tuple.setExpiration(expiration);
-        }
-        TupleID tid = null;
-        try {
-            if (sqlSpaceId == null) {
-                tid = this.tupleSpace.write(tuple);                
-            } else {
-                tid = new TupleID(Long.valueOf(sqlSpaceId));
-                //tid = new TupleID(sqlSpaceId);
-                this.tupleSpace.update(tid, tuple);                
-            }
-        } catch (TupleSpaceException e) {
-            logger.error("Trouble while writing or updating touple " + e);
-        }
-        return String.valueOf(tid.getID());
-    }
-    
-    
-    public ArrayList<String> read(String userName, String tool) {
-        Tuple tuple;
-        if (userName == null) {
-            tuple = new Tuple(String.class, tool, String.class, String.class, String.class, String.class);		    
+    public String write(String username, String status) {
+        logger.debug("switchboard says it's about to write: " + username + "/" + status);
+        ScyBaseObject sbo = new ScyBaseObject();
+        sbo.setId("54321");
+        sbo.setName("a nice name for the object");
+        sbo.setDescription(status);
+        String id = getTupleAdapter().write(null, "openfire", sbo, DEFAULT_EXPIRATION_TIME); // if documentSqlSpaceId != null this will update the tuple
+        if (id != null) {
+            logger.debug("Write to CS ok");
         } else {
-            tuple = new Tuple(userName, tool, String.class, String.class, String.class, String.class);          
+            logger.error("Trouble while writing to CS");
         }
-        Tuple returnTuple = null;
-        try {
-            returnTuple = this.tupleSpace.read(tuple);
-        } catch (TupleSpaceException e) {
-            logger.error("Trouble while reading touple " + e);
-            return null;
-        }
-        return convertTupleToStringArray(returnTuple);
+        return id;
     }
     
-    
-    public ArrayList<String> take(String tool) {
-        Tuple tuple = new Tuple(String.class, tool, String.class, String.class, String.class, String.class);
-        Tuple returnTuple = null;
-        try {
-            returnTuple = tupleSpace.take(tuple);
-        } catch (TupleSpaceException e) {
-            logger.error("Trouble while taking touple " + e);
-            return null;
+    private TupleAdapter getTupleAdapter() {
+        if (tupleAdapter == null) {
+            logger.debug("Created Tuple Spaces");
+            tupleAdapter = TupleAdapter.createTupleAdapter(this.getClass().getName(), ICollaborationService.AWARENESS_SERVICE_SPACE, this);
         }
-        return convertTupleToStringArray(returnTuple);
+        return tupleAdapter;
     }
-    
-    
-    public ArrayList<String> readById(String id) {
-        Tuple returnTuple = null;
-        try {
-            returnTuple = tupleSpace.readTupleById(new TupleID(id));
-        } catch (TupleSpaceException e) {
-            logger.error("Trouble while reading touple " + e);
-        }
-        return convertTupleToStringArray(returnTuple);
-    }
-    
-    
-    public ArrayList<String> takeById(String id) {
-        Tuple returnTuple = null;
-        try {
-            returnTuple = tupleSpace.takeTupleById(new TupleID(id));
-        } catch (TupleSpaceException e) {
-            logger.error("Trouble while take touple " + e);
-        }
-        return convertTupleToStringArray(returnTuple);
-    }
-    
-    
-    private ArrayList<String> convertTupleToStringArray(Tuple tuple) {
-        ArrayList<String> returnValues = null;
-        if (tuple != null) {
-            returnValues = new ArrayList<String>();
-            Field field;
-            returnValues.add(String.valueOf(tuple.getTupleID()));
-            for (int i = 0; i < tuple.getFields().length; i++) {
-                field = tuple.getFields()[i];
-                returnValues.add(field.getValue().toString());
-            }
-        }
-        return returnValues;
-    }
-
-
-	public void call(Command cmd, int seq, Tuple afterCmd, Tuple beforeCmd) {
-		switch (cmd) {
-		case WRITE:
-			client.actionUponWrite(afterCmd.getField(0).getValue().toString());
-			break;
-		case DELETE:
-			client.actionUponDelete(beforeCmd.getField(0).getValue().toString());
-			break;
-		}
-	}
 
 }
