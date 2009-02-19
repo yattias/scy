68a77037d6dce5c4db25e88cbaaf8352baf8f62a
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-02-19 16:00:22 +0000

    package reshuffle and naming reshuffle

--- collaboration-service/src/main/java/eu/scy/collaborationservice/CollaborationServiceClientInterface.java
+++
@@ -1,8 +0,0 @@
-package eu.scy.collaborationservice;
-
-
-public interface CollaborationServiceClientInterface {
-    
-    void actionUponWrite(String username);
-	 void actionUponDelete(String username);
-}
