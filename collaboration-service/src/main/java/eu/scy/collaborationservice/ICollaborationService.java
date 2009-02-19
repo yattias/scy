6d4784db20e99da994c88b5f0e52ca3ffc761858
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-02-19 16:01:21 +0000

    package reshuffle and naming reshuffle

---
+++ collaboration-service/src/main/java/eu/scy/collaborationservice/ICollaborationService.java
@@ -0,0 +1,14 @@
+package eu.scy.collaborationservice;
+
+
+public interface ICollaborationService {
+    
+    public static final String SERVER_IP = "129.240.212.15"; //TODO needs to be a properties file
+    public static final int SERVER_PORT = 2525; //TODO needs to be a properties file
+    
+    public static final String COLLABORATION_SERVICE_SPACE = "COLLABORATION_SERVICE_SPACE";
+    public static final String AWARENESS_SERVICE_SPACE = "AWARENESS_SERVICE_SPACE";
+    
+    void actionUponWrite(String username);
+	void actionUponDelete(String username);
+}
