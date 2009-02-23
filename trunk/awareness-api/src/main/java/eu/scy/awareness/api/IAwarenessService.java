ad8e04abdceb2d8accdbc97aebc288f7c474c36a
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-02-23 21:44:18 +0000

    added temp user object to test awareness clients

--- trunk/awareness-api/src/main/java/eu/scy/awareness/api/IAwarenessService.java
+++ trunk/awareness-api/src/main/java/eu/scy/awareness/api/IAwarenessService.java
@@ -3,8 +3,8 @@ package eu.scy.awareness.api;
 import java.util.ArrayList;
 
 public interface IAwarenessService {
-    
-    public ArrayList<String> getBuddies(String username);
+    //to be replace with scy user
+    public ArrayList<IAwarenessUser> getBuddies(String username);
     public void sendMessage(String recipient, String message);
     public void setStatus(String username, String status);
 
