b15dd7463c8b94ce01e9957d9c607e3fd5fa39b9
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-02-23 21:46:04 +0000

    update to client now gets buddy list, can chat with users

--- awareness-client/src/main/java/eu/scy/awareness/controller/ChatController.java
+++ awareness-client/src/main/java/eu/scy/awareness/controller/ChatController.java
@@ -1,26 +1,47 @@
 package eu.scy.awareness.controller;
 
+import java.util.ArrayList;
+
 import javax.swing.AbstractListModel;
 import javax.swing.DefaultListModel;
+import javax.swing.JTextArea;
+
+import eu.scy.awareness.AwarenessEvent;
+import eu.scy.awareness.AwarenessService;
+import eu.scy.awareness.IAwarenessListener;
+import eu.scy.awareness.api.IAwarenessUser;
 
 
 public class ChatController {
     
-    DefaultListModel buddyList = new DefaultListModel();
+    private DefaultListModel buddyList = new DefaultListModel();
+    private String password;
+    private String username;
+    private AwarenessService awarenessService;
+    
     
     
     public ChatController() {
-        
        populateBuddyList();
         
     }
 
-    protected void populateBuddyList() {
-        //get this from the awareness service
+    public ChatController(final String username, final String password) {
+        this.username = username;
+        this.password = password;
+
+         awarenessService = AwarenessService.createAwarenessService(username, password);
+         System.out.println("awareness starting");
         
-        buddyList.addElement("obama");
-        buddyList.addElement("biden");
-        buddyList.addElement("bill"); 
+    }
+
+    public void populateBuddyList() {
+        //get this from the awareness service
+        ArrayList<IAwarenessUser> buddies = awarenessService.getBuddies(username);
+        
+        for (IAwarenessUser b : buddies) {
+            buddyList.addElement(b);
+        }// for
         
     }
 
@@ -28,8 +49,11 @@ public class ChatController {
         return buddyList;
     }
     
-    public void sendMessage(String message) {
+    public void sendMessage(Object recipient, String message) {
         
+        IAwarenessUser user = (IAwarenessUser) recipient;
+        
+        awarenessService.sendMessage(user.getUsername(), message);
     }
     
     public void receiveMessage() {
@@ -42,5 +66,17 @@ public class ChatController {
     
     public void removeBuddy(String buddy){
         buddyList.removeElement(buddy);
+    }
+    
+    public void registerChatArea(final JTextArea chatArea) {
+        awarenessService.addAwarenessListener(new IAwarenessListener(){
+
+            @Override
+            public void handleAwarenessEvent(AwarenessEvent e) {
+                String oldText = chatArea.getText();
+                
+                chatArea.setText(oldText+e.getParticipant() +": " + e.getMessage() + "\n");
+                
+            }});
     }
 }
