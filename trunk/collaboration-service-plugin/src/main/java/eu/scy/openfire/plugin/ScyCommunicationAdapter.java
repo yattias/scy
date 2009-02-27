b8b88e73c89d113ca7a2ad5282ff554b570c3d2a
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-02-27 13:21:25 +0000

    refactored

---
+++ trunk/collaboration-service-plugin/src/main/java/eu/scy/openfire/plugin/ScyCommunicationAdapter.java
@@ -0,0 +1,80 @@
+//package eu.scy.collaborationservice.tuplespaceconnector;
+package eu.scy.openfire.plugin;
+
+import org.apache.log4j.Logger;
+
+import eu.scy.core.model.impl.ScyBaseObject;
+import eu.scy.tuple.TupleAdapter;
+
+
+public class ScyCommunicationAdapter implements IScyCommunicationAdapter {
+    
+    public static final Logger logger = Logger.getLogger(ScyCommunicationAdapter.class.getName());
+    private static final long DEFAULT_EXPIRATION_TIME = 30*1000;
+    private TupleAdapter tupleAdapter;
+    private static ScyCommunicationAdapter collaborationService;
+
+
+    public ScyCommunicationAdapter() {
+        logger.debug("Empty Constructor Collaboration created");
+    }
+
+    public static ScyCommunicationAdapter getInstance() {
+        if (collaborationService == null) {
+            logger.debug("Created Tuple Spaces");
+            collaborationService = new ScyCommunicationAdapter();
+        }
+        return collaborationService;
+    }
+
+
+    public void actionUponDelete(String username) {
+        // TODO Auto-generated method stub
+    }
+    
+    public void actionUponWrite(String username) {
+        // TODO Auto-generated method stub
+    }
+    
+    
+    public String userPing(String jabberUser) {
+        logger.debug("Attempting to write PING to CS");
+        String id = write(jabberUser, "ping");
+        logger.debug("ID returned: " + id);
+        return id;
+    }
+    
+    
+    public String userUnavailable(String jabberUser) {
+        logger.debug("Attempting to write UNAVAILABLE to CS");
+        String id = write(jabberUser, "unavailable");
+        logger.debug("ID returned: " + id);
+        return id;
+    }
+    
+    
+    public String write(String username, String status) {
+        logger.debug("switchboard says it's about to write: " + username + "/" + status);
+        ScyBaseObject sbo = new ScyBaseObject();
+        sbo.setId("54321");
+        sbo.setName("a nice name for the object");
+        sbo.setDescription(status);
+        String id = getTupleAdapter().write(null, "openfire", sbo, DEFAULT_EXPIRATION_TIME); // if documentSqlSpaceId != null this will update the tuple
+        if (id != null) {
+            logger.debug("Write to CS ok");
+        } else {
+            logger.error("Trouble while writing to CS");
+        }
+        return id;
+    }
+    
+    
+    private TupleAdapter getTupleAdapter() {
+        if (tupleAdapter == null) {
+            logger.debug("Created Tuple Spaces");
+            tupleAdapter = TupleAdapter.createTupleAdapter(this.getClass().getName(), TupleAdapter.AWARENESS_SERVICE_SPACE, this);
+        }
+        return tupleAdapter;
+    }
+
+}
