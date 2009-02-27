e91deb77dcb683d62e92e1f603d1177966cbac4c
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-02-27 13:19:38 +0000

    refactored

---
+++ trunk/collaboration-service-impl/src/test/java/eu/scy/collaborationservice/CollaborationServiceTestCase.java
@@ -0,0 +1,54 @@
+package eu.scy.collaborationservice;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.apache.log4j.Logger;
+
+
+public class CollaborationServiceTestCase extends TestCase {
+    
+    private static final Logger logger = Logger.getLogger(CollaborationServiceTestCase.class.getName());
+    private CollaborationService as;
+    
+    
+    public CollaborationServiceTestCase(String testName) {
+        super(testName);
+    }
+    
+    
+    public static Test suite() {
+        return new TestSuite(CollaborationServiceTestCase.class);
+    }
+    
+    
+    private void setUpAwarenessService() {
+        if (as == null) {
+            as = CollaborationService.createAwarenessService("bling", "vaffel");            
+        }
+    }
+    
+    
+    public void testCreateAwarenessService() {
+        setUpAwarenessService();
+        assertNotNull(as);
+    }
+    
+    // TODO: write this one so that it passes on all clients running a test
+//    public void testGetBuddies(String username) {
+//        setUpAwarenessService();
+//        ArrayList<String> buddies = as.getBuddies("bling");
+//        assertNotNull(buddies);
+//        assertTrue(buddies.size() > 0);
+//        assertEquals(buddies.get(0), "aperritano@wiki.intermedia.uio.no");
+//    }
+    
+    
+//    public void testSendMessage() {
+//        setUpAwarenessService();
+//        as.sendMessage("aperritano@imediamac09.uio.no", "hoy");
+//        assertTrue(true);
+//    }
+    
+}
