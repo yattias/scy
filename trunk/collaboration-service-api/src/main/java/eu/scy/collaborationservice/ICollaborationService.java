455634778516eb0fd242e4c5f5fbce78d5afe6cd
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-02-27 13:19:31 +0000

    refactored

--- trunk/collaboration-service-api/src/main/java/eu/scy/collaborationservice/ICollaborationService.java
+++ trunk/collaboration-service-api/src/main/java/eu/scy/collaborationservice/ICollaborationService.java
@@ -1,11 +1,14 @@
 package eu.scy.collaborationservice;
 
+import eu.scy.core.model.impl.ScyBaseObject;
+
 
 
 public interface ICollaborationService {
 
-    void actionUponWrite(String username);
-	void actionUponDelete(String username);
-	public String write(String username, String status);
-	 
+  public void create(ScyBaseObject scyBaseObject);
+  public void read(String id);
+  public void update(ScyBaseObject scyBaseObject);
+  public void delete(ScyBaseObject scyBaseObject);
+  
 }
