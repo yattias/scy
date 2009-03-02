3ece4a0e556a0da0fd0ccb1b43f4d0392c1b147d
thomas.drevon <thomas.drevon@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-03-02 14:05:15 +0000

    small cleanup, now this baby compiles properly

--- collaboration-service-impl/src/test/java/eu/scy/collaborationservice/CollaborationServiceTestCase.java
+++ collaboration-service-impl/src/test/java/eu/scy/collaborationservice/CollaborationServiceTestCase.java
@@ -10,7 +10,7 @@ import org.apache.log4j.Logger;
 public class CollaborationServiceTestCase extends TestCase {
     
     private static final Logger logger = Logger.getLogger(CollaborationServiceTestCase.class.getName());
-    private CollaborationService as;
+    private CollaborationService cs;
     
     
     public CollaborationServiceTestCase(String testName) {
@@ -24,15 +24,15 @@ public class CollaborationServiceTestCase extends TestCase {
     
     
     private void setUpAwarenessService() {
-        if (as == null) {
-            as = CollaborationService.createAwarenessService("bling", "vaffel");            
+        if (cs == null) {
+            cs = new CollaborationService();         
         }
     }
     
     
     public void testCreateAwarenessService() {
         setUpAwarenessService();
-        assertNotNull(as);
+        assertNotNull(cs);
     }
     
     // TODO: write this one so that it passes on all clients running a test
