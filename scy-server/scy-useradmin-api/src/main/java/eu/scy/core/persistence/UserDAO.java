81dea950fbcf45b7700ab9cb99c3e76cba2cf42a
henrik <henrik@enovate.no>
2009-03-03 12:52:02 +0000

    package extends the SAIL portal user management classes

--- scy-server/scy-useradmin-api/src/main/java/eu/scy/core/persistence/UserDAO.java
+++ scy-server/scy-useradmin-api/src/main/java/eu/scy/core/persistence/UserDAO.java
@@ -92,5 +92,5 @@ public interface UserDAO extends SCYBaseDAO{
      * removes the user from the system permanently
      * @param id
      */
-    void deleteUser(Long id);
+    public void deleteUser(Long id);
 }
