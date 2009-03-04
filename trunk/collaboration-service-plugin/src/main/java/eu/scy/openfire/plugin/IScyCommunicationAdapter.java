a1831f0613835f6ba1db494504db6ad2b3b7d883
thomas.drevon <thomas.drevon@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-03-04 09:06:48 +0000

    added tests and tweaked the interface/impl

--- trunk/collaboration-service-plugin/src/main/java/eu/scy/openfire/plugin/IScyCommunicationAdapter.java
+++ trunk/collaboration-service-plugin/src/main/java/eu/scy/openfire/plugin/IScyCommunicationAdapter.java
@@ -6,14 +6,17 @@ import eu.scy.core.model.impl.ScyBaseObject;
 
 public interface IScyCommunicationAdapter {
 
+    public void addScyCommunicationListener(IScyCommunicationListener listener);
+
+    // callback methods
     void actionUponWrite(String username);
 	void actionUponDelete(String username);
 	
+	// various CRUDs
     public String create(ScyBaseObject sbo);
+    public String createWithExpiration(ScyBaseObject sbo, long expiration);
     public ScyBaseObject read(String id);
     public String update(ScyBaseObject sbo, String id);
-    public ScyBaseObject delete(String id);
-    
-    public void addScyCommunicationListener(IScyCommunicationListener listener);
-	 
+    public String updateWithExpiration(ScyBaseObject sbo, String id, long expiration);
+    public String delete(String id);
 }
--- trunk/collaboration-service-plugin/src/test/java/eu/scy/openfire/plugin/SQLSpaceAdapterTestCase.java
+++ trunk/collaboration-service-plugin/src/test/java/eu/scy/openfire/plugin/SQLSpaceAdapterTestCase.java
@@ -14,6 +14,8 @@ public class SQLSpaceAdapterTestCase extends TestCase implements IScyCommunicati
     
     private static final Logger logger = Logger.getLogger(SQLSpaceAdapter.class.getName());
     private SQLSpaceAdapter sqlSpaceAdapter;
+    private static final long TWO_SECONDS = 2*1000;
+    private static final long FOUR_SECONDS = 4*1000;
     
     
     public SQLSpaceAdapterTestCase(String testName) {
@@ -26,63 +28,111 @@ public class SQLSpaceAdapterTestCase extends TestCase implements IScyCommunicati
     }
     
     
-    private SQLSpaceAdapter getSQLSpaceAdapter() {
+    private SQLSpaceAdapter getTupleAdapter() {
         if (sqlSpaceAdapter == null) {
             sqlSpaceAdapter = SQLSpaceAdapter.createAdapter("thomasd", SQLSpaceAdapter.COLLABORATION_SERVICE_SPACE, this);         
         }
         return sqlSpaceAdapter;
     }
     
+    private ScyBaseObject getScyBaseObject() {
+        ScyBaseObject sbo = new ScyBaseObject();
+        sbo.setId("1337");
+        sbo.setName("a nice name for this test object");
+        sbo.setDescription("dezkript");
+        return sbo;
+    }
     
-    public void testCreateSQLSpaceAdapter() {
-        assertNotNull(getSQLSpaceAdapter());
+    
+    public void testCreateTupleAdapter() {
+        assertNotNull(getTupleAdapter());
     }
 
+    
+    public void testWriteReadDelete() {
+        String id = null;
+        // write
+        id = getTupleAdapter().write("someTestCase", getScyBaseObject());
+        assertNotNull(id);
+        // read
+        ScyBaseObject sbo = getTupleAdapter().readById(id);
+        assertNotNull(sbo);
+        // delete
+        id = getTupleAdapter().delete(id);
+        assertNotNull(id);
+    }
 
+    
+    public void testWriteWithExpiration() {
+        String id = null;
+        // write with expiration
+        id = getTupleAdapter().write("someTestCase", getScyBaseObject(), TWO_SECONDS);
+        assertNotNull(id);
+        // confirm tuple
+        ScyBaseObject sbo = getTupleAdapter().readById(id);
+        assertNotNull(sbo);
+        try {
+            Thread.sleep(FOUR_SECONDS);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        // confirm tuple has expired
+        sbo = getTupleAdapter().readById(id);
+        assertNull(sbo);
+    }
+    
 
     @Override
     public void actionUponDelete(String username) {
-        // TODO Auto-generated method stub
-        
+        logger.info("Callback sez: Stuff deleted from sqlspaces");
     }
 
 
     @Override
     public void actionUponWrite(String username) {
-        // TODO Auto-generated method stub
-        
+        logger.info("Callback sez: Stuff written to sqlspaces");
+    }
+
+    @Override
+    public void addScyCommunicationListener(IScyCommunicationListener listener) {
+        // TODO Auto-generated method stub        
     }
 
 
     @Override
     public String create(ScyBaseObject sbo) {
-        // TODO Auto-generated method stub
+        // TODO Auto-generated method stub        
         return null;
     }
-
 
     @Override
-    public ScyBaseObject delete(String id) {
-        // TODO Auto-generated method stub
+    public String createWithExpiration(ScyBaseObject sbo, long expiration) {
+        // TODO Auto-generated method stub        
         return null;
     }
 
+    @Override
+    public String delete(String id) {
+        // TODO Auto-generated method stub        
+        return null;
+    }
 
     @Override
     public ScyBaseObject read(String id) {
-        // TODO Auto-generated method stub
+        // TODO Auto-generated method stub        
         return null;
     }
-
 
     @Override
     public String update(ScyBaseObject sbo, String id) {
-        // TODO Auto-generated method stub
+        // TODO Auto-generated method stub        
         return null;
     }
 
-
-    
-
-    
+    @Override
+    public String updateWithExpiration(ScyBaseObject sbo, String id, long expiration) {
+        // TODO Auto-generated method stub        
+        return null;
+    }    
 }
