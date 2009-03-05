7805c90f6e575744f901762c887b0c339192a5d2
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-03-05 21:13:04 +0000

    uses the scy-commons dependency

--- collaboration-service-impl/src/main/java/eu/scy/collaborationservice/CollaborationService.java
+++ collaboration-service-impl/src/main/java/eu/scy/collaborationservice/CollaborationService.java
@@ -1,4 +1,3 @@
-
 package eu.scy.collaborationservice;
 
 import java.io.FileNotFoundException;
@@ -12,24 +11,22 @@ import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.ConnectionListener;
 import org.jivesoftware.smack.MessageListener;
-import org.jivesoftware.smack.PacketCollector;
 import org.jivesoftware.smack.PacketListener;
 import org.jivesoftware.smack.Roster;
 import org.jivesoftware.smack.RosterGroup;
 import org.jivesoftware.smack.RosterListener;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
-import org.jivesoftware.smack.filter.AndFilter;
-import org.jivesoftware.smack.filter.PacketFilter;
-import org.jivesoftware.smack.filter.PacketTypeFilter;
+import org.jivesoftware.smack.filter.PacketExtensionFilter;
 import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Packet;
 import org.jivesoftware.smack.packet.Presence;
 
+import eu.scy.commons.packet.extension.ScyObjectPacketExtension;
+import eu.scy.core.model.ScyBase;
 import eu.scy.core.model.impl.ScyBaseObject;
 
-
-public class CollaborationService implements ICollaborationService, PacketListener {
+public class CollaborationService implements ICollaborationService {
     
     private final static Logger logger = Logger.getLogger(CollaborationService.class.getName());
     private ConnectionConfiguration config;
@@ -38,10 +35,8 @@ public class CollaborationService implements ICollaborationService, PacketListen
     private String groupName;
     private ArrayList<ICollaborationListener> collaborationListeners = new ArrayList<ICollaborationListener>();
     
-    public CollaborationService() {        
-    }
+    public CollaborationService() {}
     
-
     public XMPPConnection connect(String username, String password, String groupName) {
         this.groupName = groupName;
         Properties props = new Properties();
@@ -52,7 +47,7 @@ public class CollaborationService implements ICollaborationService, PacketListen
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        
         String address = props.getProperty("collaborationservice.address");
         String port = props.getProperty("collaborationservice.port");
         String name = props.getProperty("collaborationservice.name");
@@ -60,63 +55,87 @@ public class CollaborationService implements ICollaborationService, PacketListen
         config = new ConnectionConfiguration(address, new Integer(port).intValue(), name);
         
         this.xmppConnection = new XMPPConnection(config);
-       
+        
         this.xmppConnection.DEBUG_ENABLED = true;
         try {
             
-            
             this.xmppConnection.connect();
-            this.xmppConnection.addConnectionListener(new ConnectionListener(){
-
+            this.xmppConnection.addConnectionListener(new ConnectionListener() {
+                
                 @Override
                 public void connectionClosed() {
                     System.out.println("collaboration server closed;");
                     
                 }
-
+                
                 @Override
                 public void connectionClosedOnError(Exception arg0) {
                     System.out.println("collaboration server error closed;");
                     
                 }
-
+                
                 @Override
                 public void reconnectingIn(int arg0) {
                     System.out.println("collaboration server reconnecting;");
                     
                 }
-
+                
                 @Override
                 public void reconnectionFailed(Exception arg0) {
-                    System.out.println("collaboration server reconnecting failed");                    
+                    System.out.println("collaboration server reconnecting failed");
                 }
-
+                
                 @Override
                 public void reconnectionSuccessful() {
                     System.out.println("collaboration server reconnectings success");
                     
-                }});
-//            PacketFilter pf = new PacketTypeFilter(Message.class);
-//            PacketCollector pc = xmppConnection.createPacketCollector(pf);
-            
-            PacketFilter scyFilter = new PacketFilter() {
-                public boolean accept(Packet packet) {
-                    
-                    Message mess = (Message)packet;
-                    return "scy".equals(mess.getProperty("groupId"));
                 }
-            };
+            });
+            // PacketFilter pf = new PacketTypeFilter(Message.class);
+            // PacketCollector pc = xmppConnection.createPacketCollector(pf);
             
+//            PacketFilter scyFilter = new PacketFilter() {
+//                
+//                public boolean accept(Packet packet) {
+//                    
+//                    Message mess = (Message) packet;
+//                    return "scy".equals(mess.getProperty("groupId"));
+//                }
+//            };
             
-//            PacketFilter andFilter = new AndFilter(new PacketTypeFilter(Message.class),scyFilter);
-//
-//
-//         
-//            this.xmppConnection.addPacketListener(this, null);
+            // PacketFilter andFilter = new AndFilter(new
+            // PacketTypeFilter(Message.class),scyFilter);
+            //
+            //
+            //         
+            // this.xmppConnection.addPacketListener(this, null);
             
-            //PacketCollector pc = xmppConnection.createPacketCollector(scyPackets);
+            // PacketCollector pc =
+            // xmppConnection.createPacketCollector(scyPackets);
             
-            if( username == null || password == null ) {
+            xmppConnection.addPacketListener(new PacketListener(){
+
+                @Override
+                public void processPacket(Packet scyPacket) {
+                    ScyObjectPacketExtension eventPacketExtension =
+                        (ScyObjectPacketExtension) scyPacket.getExtension(
+                                ScyObjectPacketExtension.ELEMENT_NAME, ScyObjectPacketExtension.NAMESPACE);
+
+                    
+                    if (eventPacketExtension != null) {
+                        for (ICollaborationListener cl : collaborationListeners) {
+                            if (cl != null) {
+                                CollaborationEvent collaborationEvent = new CollaborationEvent(this, eventPacketExtension.getName(), eventPacketExtension.getDescription());
+                                cl.handleCollaborationEvent(collaborationEvent);
+                            }// if
+                        }// for
+                }// processPacket
+
+                    
+                }},  new PacketExtensionFilter(ScyObjectPacketExtension.ELEMENT_NAME,
+                    ScyObjectPacketExtension.NAMESPACE));
+            
+            if (username == null || password == null) {
                 this.xmppConnection.loginAnonymously();
             } else {
                 this.xmppConnection.login(username, password);
@@ -127,7 +146,7 @@ public class CollaborationService implements ICollaborationService, PacketListen
         }
         
         this.setUpRoster(groupName);
-
+        
         return xmppConnection;
     }
     
@@ -135,43 +154,44 @@ public class CollaborationService implements ICollaborationService, PacketListen
         roster = xmppConnection.getRoster();
         roster.setSubscriptionMode(Roster.SubscriptionMode.accept_all);
         RosterGroup group = roster.getGroup(groupName);
-        logger.debug("Registered Groups: "+roster.getGroupCount()+" Entries: "+roster.getEntryCount());
-        roster.addRosterListener(new RosterListener(){
-
+        logger.debug("Registered Groups: " + roster.getGroupCount() + " Entries: " + roster.getEntryCount());
+        roster.addRosterListener(new RosterListener() {
+            
             @Override
             public void entriesAdded(Collection<String> arg0) {
-               System.out.println("entriesAdded " + arg0);
+                System.out.println("entriesAdded " + arg0);
             }
-
+            
             @Override
             public void entriesDeleted(Collection<String> arg0) {
                 System.out.println("entriesDeleted " + arg0);
             }
-
+            
             @Override
             public void entriesUpdated(Collection<String> arg0) {
-                System.out.println("entriesUpdated " + arg0 );
+                System.out.println("entriesUpdated " + arg0);
             }
-
+            
             @Override
             public void presenceChanged(Presence arg0) {
                 System.out.println("presenceChanged " + arg0);
-            }});
+            }
+        });
     }
-
+    
     public void closeCollaborationService() {
         xmppConnection.disconnect();
     }
     
-    
     public void sendMessage(String recipient, String message, Object objToSend) {
-        Chat chat = xmppConnection.getChatManager().createChat(recipient, new MessageListener(){
-
+        Chat chat = xmppConnection.getChatManager().createChat(recipient, new MessageListener() {
+            
             @Override
             public void processMessage(Chat arg0, Message message) {
-                System.out.println("chat; " + arg0 + " message; " +message);
+                System.out.println("chat; " + arg0 + " message; " + message);
                 
-            }});
+            }
+        });
         
         try {
             Message newMessage = new Message();
@@ -184,52 +204,44 @@ public class CollaborationService implements ICollaborationService, PacketListen
             e.printStackTrace();
         }
     }
-
-    public void sendPacket(Object objToSend,String message) {
+    
+    public void sendPacket(Object objToSend, String message) {
         logger.error("sending packet from the collaboration service");
         Message newMessage = new Message();
-        newMessage.setTo("scy-bot");
+        newMessage.setFrom(xmppConnection.getUser());
         newMessage.setBody(message);
         newMessage.setProperty("groupId", "scy");
         newMessage.setProperty("group", groupName);
-        newMessage.setProperty("customData", objToSend);
+        //newMessage.setProperty("customData", objToSend);
+        ScyObjectPacketExtension ext = new ScyObjectPacketExtension();
+        ext.setScyBase((ScyBase) objToSend);
+        newMessage.addExtension(ext);
         xmppConnection.sendPacket(newMessage);
     }
     
     @Override
     public void create(ScyBaseObject scyBaseObject) {
-       this.sendPacket(scyBaseObject,"create");
+        this.sendPacket(scyBaseObject, "create");
         
     }
-
+    
     @Override
     public void delete(ScyBaseObject scyBaseObject) {
-        this.sendPacket(scyBaseObject,"delete");
+        this.sendPacket(scyBaseObject, "delete");
         
     }
-
+    
     @Override
     public void read(String id) {
-       
-    }
 
+    }
+    
     @Override
     public void update(ScyBaseObject scyBaseObject) {
-        this.sendPacket(scyBaseObject,"update");
-    }    
+        this.sendPacket(scyBaseObject, "update");
+    }
     
-    public void addCollaborationListener(ICollaborationListener collaborationListener){
+    public void addCollaborationListener(ICollaborationListener collaborationListener) {
         collaborationListeners.add(collaborationListener);
-   }
-
-    @Override
-    public void processPacket(Packet arg0) {
-        for (ICollaborationListener cl : collaborationListeners) {
-            if (cl != null){
-                CollaborationEvent collaborationEvent = new CollaborationEvent(this, "name", "test");
-                cl.handleCollaborationEvent(collaborationEvent);
-            }
-        }
-        
     }
 }
