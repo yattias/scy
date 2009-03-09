064e4d4a3e13280d34ad286a69c667964fa2e67c
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-03-09 21:39:07 +0000

    diabled login and just works with the collaboration service for now

--- awareness-client/src/main/java/eu/scy/awareness/controller/ChatController.java
+++ awareness-client/src/main/java/eu/scy/awareness/controller/ChatController.java
@@ -10,6 +10,10 @@ import eu.scy.awareness.AwarenessService;
 import eu.scy.awareness.api.IAwarenessEvent;
 import eu.scy.awareness.api.IAwarenessListener;
 import eu.scy.awareness.api.IAwarenessUser;
+import eu.scy.collaborationservice.CollaborationService;
+import eu.scy.collaborationservice.ICollaborationEvent;
+import eu.scy.collaborationservice.ICollaborationListener;
+import eu.scy.core.model.impl.ScyBaseObject;
 
 
 public class ChatController {
@@ -18,30 +22,42 @@ public class ChatController {
     private String password;
     private String username;
     private AwarenessService awarenessService;
-    
-    
+    private CollaborationService cs;
     
     public ChatController() {
        populateBuddyList();
-        
     }
 
     public ChatController(final String username, final String password) {
         this.username = username;
         this.password = password;
 
-         awarenessService = AwarenessService.createAwarenessService(username, password);
-         System.out.println("awareness starting");
+         awarenessService = new AwarenessService(); 
+           
+         //just working with the collaboration service right now
+         //System.out.println("awareness starting");
+         //awarenessService.createAwarenessService(username, password);
+         
+         System.out.println("collaboration service starting");
+         cs = new CollaborationService();
+         cs.connect("biden", "biden", "colermo");
         
+         System.out.println("collaboration starting");
     }
 
     public void populateBuddyList() {
         //get this from the awareness service
-        ArrayList<IAwarenessUser> buddies = awarenessService.getBuddies(username);
+        if( awarenessService != null && awarenessService.isConnected() ) {
+            ArrayList<IAwarenessUser> buddies = awarenessService.getBuddies(username);
         
-        for (IAwarenessUser b : buddies) {
-            buddyList.addElement(b);
-        }// for
+            if( buddies != null ) {
+                for (IAwarenessUser b : buddies) {
+                    buddyList.addElement(b);
+                }// for
+            }
+        } else {
+            buddyList.addElement("no buddies");
+        }
         
     }
 
@@ -50,10 +66,21 @@ public class ChatController {
     }
     
     public void sendMessage(Object recipient, String message) {
+       
+        //send a message to the awareness server
+//        if (recipient != null) {
+//            IAwarenessUser user = (IAwarenessUser) recipient;
+//            awarenessService.sendMessage(user.getUsername(), message);
+//        }
         
-        IAwarenessUser user = (IAwarenessUser) recipient;
+        //send a message to the collaboration server
+        System.out.println("sending message to CS");
         
-        awarenessService.sendMessage(user.getUsername(), message);
+        ScyBaseObject scy = new ScyBaseObject();
+        scy.setDescription("this is a scy");
+        scy.setId("6");
+        scy.setName("im scy");
+        cs.create(scy);
     }
     
     public void receiveMessage() {
@@ -79,5 +106,16 @@ public class ChatController {
                 chatArea.setText(oldText+e.getParticipant() +": " + e.getMessage() + "\n");
                 
             }});
+        
+        cs.addCollaborationListener(new ICollaborationListener() {
+            
+
+            @Override
+            public void handleCollaborationEvent(ICollaborationEvent e) {
+                System.out.println("Collaboration Event called Chat controller" + e);
+                String oldText = chatArea.getText();
+                chatArea.setText(oldText+e.getParticipant() +": " + e.getMessage() + "\n");
+            }
+         });
     }
 }
