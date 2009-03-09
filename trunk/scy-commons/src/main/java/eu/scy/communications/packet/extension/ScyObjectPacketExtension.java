a13a851edf9a6781779db4c5b2cad749c062e0d1
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-03-09 09:59:28 +0000

    update to extensions

--- trunk/scy-commons/src/main/java/eu/scy/communications/packet/extension/ScyObjectPacketExtension.java
+++ trunk/scy-commons/src/main/java/eu/scy/communications/packet/extension/ScyObjectPacketExtension.java
@@ -1,11 +1,13 @@
 package eu.scy.communications.packet.extension;
 
-
-import org.jivesoftware.smack.packet.PacketExtension;
+import org.dom4j.Element;
+import org.dom4j.QName;
+import org.xmpp.packet.PacketExtension;
 
 import eu.scy.core.model.ScyBase;
 
-public class ScyObjectPacketExtension implements PacketExtension, ScyBase {
+
+public class ScyObjectPacketExtension extends PacketExtension implements org.jivesoftware.smack.packet.PacketExtension, ScyBase {
     
     public static final String NAMESPACE = "http://scy.eu/scy-object";
     public static final String ELEMENT_NAME = "scy-object";
@@ -13,8 +15,17 @@ public class ScyObjectPacketExtension implements PacketExtension, ScyBase {
     private String name;
     private String id;
     
+    static {
+        // Register that scy object packet ext uses the jabber:x:data namespace
+        registeredExtensions.put(QName.get(ELEMENT_NAME, NAMESPACE), ScyObjectPacketExtension.class);
+    }
+    
     public ScyObjectPacketExtension() {
-        super();
+        super(ELEMENT_NAME, NAMESPACE);
+    }
+
+    public ScyObjectPacketExtension(Element element) {
+        super(element);
     }
     
     public String getElementName() {
@@ -36,26 +47,39 @@ public class ScyObjectPacketExtension implements PacketExtension, ScyBase {
     }
     
     public String getDescription() {
-        return description;
+        return element.elementTextTrim("description");
     }
     
     public void setDescription(String description) {
+        if (element.element("description") != null) {
+            element.remove(element.element("description"));
+        }
+        element.addElement("description").setText(description);
         this.description = description;
     }
     
     public String getId() {
-        return id;
+        return  element.elementTextTrim("id");
     }
     
     public void setId(String id) {
+        // Remove an existing title element.
+        if (element.element("id") != null) {
+            element.remove(element.element("id"));
+        }
+        element.addElement("id").setText(id);
         this.id = id;
     }
     
     public String getName() {
-        return name;
+        return element.elementTextTrim("name");
     }
     
     public void setName(String name) {
+        if (element.element("name") != null) {
+            element.remove(element.element("name"));
+        }
+        element.addElement("name").setText(name);
         this.name = name;
     }
     
@@ -63,6 +87,10 @@ public class ScyObjectPacketExtension implements PacketExtension, ScyBase {
         this.setId(scyObject.getId());
         this.setDescription(scyObject.getDescription());
         this.setName(scyObject.getName());
+    }
+    
+    public ScyObjectPacketExtension createCopy() {
+        return new ScyObjectPacketExtension(this.getElement().createCopy());
     }
     
 }
