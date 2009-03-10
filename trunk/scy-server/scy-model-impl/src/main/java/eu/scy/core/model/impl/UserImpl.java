cac876ca67bfed63aff82c1ab86fdca85c65f08c
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-03-10 05:39:46 +0000

    UserImpl does not extend sail classes any more

--- trunk/scy-server/scy-model-impl/src/main/java/eu/scy/core/model/impl/UserImpl.java
+++ trunk/scy-server/scy-model-impl/src/main/java/eu/scy/core/model/impl/UserImpl.java
@@ -1,14 +1,12 @@
 package eu.scy.core.model.impl;
 
 import eu.scy.core.model.*;
+import net.sf.sail.webapp.domain.authentication.MutableUserDetails;
+import net.sf.sail.webapp.domain.sds.SdsUser;
 
 import javax.persistence.*;
-import java.util.List;
 import java.util.LinkedList;
-
-import net.sf.sail.webapp.domain.authentication.MutableUserDetails;
-import net.sf.sail.webapp.domain.authentication.impl.PersistentUserDetails;
-import net.sf.sail.webapp.domain.sds.SdsUser;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,9 +17,149 @@ import net.sf.sail.webapp.domain.sds.SdsUser;
  */
 
 @Entity(name = "eu.scy.core.model.impl.UserImpl")
-@Table(name = "user")
+@Table(name = "users")
 @org.hibernate.annotations.Proxy(proxyClass = User.class)
-public class UserImpl extends net.sf.sail.webapp.domain.impl.UserImpl implements User {
+public class UserImpl implements User {
+
+    @Transient
+    public static final String DATA_STORE_NAME = "users";
+
+    @Transient
+    public static final String COLUMN_NAME_SDS_USER_FK = "sds_user_fk";
+
+    @Transient
+    public static final String COLUMN_NAME_USER_DETAILS_FK = "user_details_fk";
+
+    @Transient
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id = null;
+
+    @Version
+    @Column(name = "OPTLOCK")
+    private Integer version = null;
+
+    private SdsUser sdsUser = null;
+
+
+    /**
+     * @return the id
+     */
+    @SuppressWarnings("unused")
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     *            the id to set
+     */
+    @SuppressWarnings("unused")
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the version
+     */
+    @SuppressWarnings("unused")
+    private Integer getVersion() {
+        return version;
+    }
+
+    /**
+     * @param version
+     *            the version to set
+     */
+    @SuppressWarnings("unused")
+    private void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    /**
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int PRIME = 31;
+        int result = 1;
+        result = PRIME * result
+                + ((this.sdsUser == null) ? 0 : this.sdsUser.hashCode());
+        result = PRIME
+                * result
+                + ((this.userDetails == null) ? 0 : this.userDetails.hashCode());
+        return result;
+    }
+
+    /**
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final UserImpl other = (UserImpl) obj;
+        if (this.sdsUser == null) {
+            if (other.sdsUser != null)
+                return false;
+        } else if (!this.sdsUser.equals(other.sdsUser))
+            return false;
+        if (this.userDetails == null) {
+            if (other.userDetails != null)
+                return false;
+        } else if (!this.userDetails.equals(other.userDetails))
+            return false;
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     private String userName;
     private String password;
