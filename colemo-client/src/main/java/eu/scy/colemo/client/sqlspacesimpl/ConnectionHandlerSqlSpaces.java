651da49f7139620970d22dd289917e934408764f
thomas.drevon <thomas.drevon@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-03-11 14:51:15 +0000

    refactored to only use collaboration service

--- colemo-client/src/main/java/eu/scy/colemo/client/sqlspacesimpl/ConnectionHandlerSqlSpaces.java
+++ colemo-client/src/main/java/eu/scy/colemo/client/sqlspacesimpl/ConnectionHandlerSqlSpaces.java
@@ -1,10 +1,6 @@
 package eu.scy.colemo.client.sqlspacesimpl;
 
-import info.collide.sqlspaces.client.TupleSpace;
-import info.collide.sqlspaces.commons.Callback;
-import info.collide.sqlspaces.commons.Field;
-import info.collide.sqlspaces.commons.Tuple;
-import info.collide.sqlspaces.commons.TupleSpaceException;
+import java.util.ArrayList;
 
 import org.apache.log4j.Logger;
 
@@ -20,7 +16,6 @@ import eu.scy.collaborationservice.CollaborationServiceFactory;
 import eu.scy.collaborationservice.ICollaborationService;
 import eu.scy.collaborationservice.event.ICollaborationServiceEvent;
 import eu.scy.collaborationservice.event.ICollaborationServiceListener;
-import eu.scy.communications.adapter.ScyCommunicationAdapter;
 import eu.scy.communications.message.ScyMessage;
 
 /**
@@ -30,59 +25,48 @@ import eu.scy.communications.message.ScyMessage;
  * Time: 23:02:33
  * To change this template use File | Settings | File Templates.
  */
-public class ConnectionHandlerSqlSpaces implements ConnectionHandler, Callback, ICollaborationServiceListener {
+public class ConnectionHandlerSqlSpaces implements ConnectionHandler, ICollaborationServiceListener {
 
-    private TupleSpace tupleSpace = null;
+    //private TupleSpace tupleSpace = null;
 
     public static String MESSAGE_TYPE = "MESSAGE_TYPE";
 
-    private Tuple conceptTemplate = new Tuple(String.class, String.class, String.class, String.class, String.class, String.class);
-    private Tuple linkTemplate = new Tuple(String.class, String.class, String.class, String.class, String.class);
-    public static final Logger logger = Logger.getLogger(ConnectionHandlerSqlSpaces.class.getName());
+    //private Tuple conceptTemplate = new Tuple(String.class, String.class, String.class, String.class, String.class, String.class);
+    //private Tuple linkTemplate = new Tuple(String.class, String.class, String.class, String.class, String.class);
+    private static final Logger logger = Logger.getLogger(ConnectionHandlerSqlSpaces.class.getName());
     private ICollaborationService cs;
-    
-    
-    public ConnectionHandlerSqlSpaces() {
-        try {
-            cs = CollaborationServiceFactory.getCollaborationService(CollaborationServiceFactory.LOCAL_STYLE);
-            cs.addCollaborationListener(this);
-        } catch (CollaborationServiceException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
 
     
     public void sendMessage(String message) {
-        try {
-            System.out.println("Sending message: " + message);
-            if (tupleSpace != null) {
-                Field typeField = new Field(MESSAGE_TYPE);
-                Field messageField = new Field(message);
-
-                Tuple t = new Tuple(typeField, messageField);
-                try {
-                    tupleSpace.write(t);
-                } catch (TupleSpaceException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            Tuple templateTuple = new Tuple(String.class, String.class, String.class, String.class, String.class);
-            Tuple returnTuple = tupleSpace.read(templateTuple);
-            if (returnTuple != null) {
-                System.out.println("return tuple" + returnTuple + returnTuple.getField(0) + " " + returnTuple.getField(1));
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
+//        try {
+//            System.out.println("Sending message: " + message);
+//            if (tupleSpace != null) {
+//                Field typeField = new Field(MESSAGE_TYPE);
+//                Field messageField = new Field(message);
+//
+//                Tuple t = new Tuple(typeField, messageField);
+//                try {
+//                    tupleSpace.write(t);
+//                } catch (TupleSpaceException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//            Tuple templateTuple = new Tuple(String.class, String.class, String.class, String.class, String.class);
+//            Tuple returnTuple = tupleSpace.read(templateTuple);
+//            if (returnTuple != null) {
+//                System.out.println("return tuple" + returnTuple + returnTuple.getField(0) + " " + returnTuple.getField(1));
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//        }
     }
 
     
     public void sendObject(Object object) {
         System.out.println("Sending object:" + object);
-        ScyCommunicationAdapter communicationAdapter = ScyCommunicationAdapter.getInstance();
+        
         ObjectTranslator translator = new ObjectTranslator();
         ScyMessage sendMe = null;
 
@@ -103,101 +87,132 @@ public class ConnectionHandlerSqlSpaces implements ConnectionHandler, Callback, 
         
         if (sendMe != null) {
             logger.debug("Sending ScyMessage: \n" + sendMe.toString());
-            communicationAdapter.create(sendMe);
-        }
-    }
-
-    public void sendObject2(Object object) {
-        try {
-            System.out.println("Sending object:" + object);
-            ObjectTranslator translator = new ObjectTranslator();
-            Tuple sendMe = null;
-            if (object instanceof AddClass) {
-                AddClass addClass = (AddClass) object;
-                System.out.println("NAME: " + addClass.getName());
-                sendMe = translator.getTuple(addClass);
-            } else if (object instanceof MoveClass) {
-                MoveClass mc = (MoveClass) object;
-                mc.setId(mc.getUmlClass().getId());
-                System.out.println("moving class with id:  " + mc.getId() + " to position: " + mc.getUmlClass().getX() + ", " + mc.getUmlClass().getY());
-                sendMe = translator.getTuple(mc);
-            } else if (object instanceof UmlLink) {
-                UmlLink addLink = (UmlLink) object;
-                System.out.println("Adding link");
-                sendMe = translator.getTuple(addLink);
-            }
-
             try {
-                if (sendMe != null) {
-                    tupleSpace.write(sendMe);
-                }
-
-            } catch (TupleSpaceException e) {
+                cs.create(sendMe);
+            } catch (CollaborationServiceException e) {
+                logger.error("Bummer. Create in CollaborationService failed somehow: \n" + e);
                 e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
     }
+
+//    public void sendObject2(Object object) {
+//        try {
+//            System.out.println("Sending object:" + object);
+//            ObjectTranslator translator = new ObjectTranslator();
+//            Tuple sendMe = null;
+//            if (object instanceof AddClass) {
+//                AddClass addClass = (AddClass) object;
+//                System.out.println("NAME: " + addClass.getName());
+//                sendMe = translator.getTuple(addClass);
+//            } else if (object instanceof MoveClass) {
+//                MoveClass mc = (MoveClass) object;
+//                mc.setId(mc.getUmlClass().getId());
+//                System.out.println("moving class with id:  " + mc.getId() + " to position: " + mc.getUmlClass().getX() + ", " + mc.getUmlClass().getY());
+//                sendMe = translator.getTuple(mc);
+//            } else if (object instanceof UmlLink) {
+//                UmlLink addLink = (UmlLink) object;
+//                System.out.println("Adding link");
+//                sendMe = translator.getTuple(addLink);
+//            }
+//
+//            try {
+//                if (sendMe != null) {
+//                    tupleSpace.write(sendMe);
+//                }
+//
+//            } catch (TupleSpaceException e) {
+//                e.printStackTrace();
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//        }
+//    }
 
 
     public void initialize() throws Exception {
 
-        System.out.println("initializing");
-        tupleSpace = new TupleSpace("scy.collide.info", 2525, "AIRFORCE_ONE");
-        //tupleSpace = new TupleSpace("localhost", 2525, "AIRFORCE_ONE");
+        logger.debug("initializing");
+//        tupleSpace = new TupleSpace("scy.collide.info", 2525, "AIRFORCE_ONE");
+//        //tupleSpace = new TupleSpace("localhost", 2525, "AIRFORCE_ONE");
+//
+//        //tupleSpace = new TupleSpace("129.240.212.15", 2525, "AIRFORCE_ONE");
+//        Tuple tmp = new Tuple(String.class, String.class);
+//        //Tuple colemoBus = new Tuple(String.class, String.class, String.class, String.class, String.class, String.class);
+//        Callback cb = this;
+//        int seqNo = tupleSpace.eventRegister(Command.WRITE, tmp, cb, false);
+//        int conceptSeq= tupleSpace.eventRegister(Command.WRITE, conceptTemplate, cb, false);
+//        int linkSeq = tupleSpace.eventRegister(Command.WRITE, linkTemplate, cb, false);
+//        //int seqNo = tupleSpace.eventRegister(Command.WRITE, tmp, cb, false);
 
-        //tupleSpace = new TupleSpace("129.240.212.15", 2525, "AIRFORCE_ONE");
-        Tuple tmp = new Tuple(String.class, String.class);
-        //Tuple colemoBus = new Tuple(String.class, String.class, String.class, String.class, String.class, String.class);
-        Callback cb = this;
-        int seqNo = tupleSpace.eventRegister(Command.WRITE, tmp, cb, false);
-        int conceptSeq= tupleSpace.eventRegister(Command.WRITE, conceptTemplate, cb, false);
-        int linkSeq = tupleSpace.eventRegister(Command.WRITE, linkTemplate, cb, false);
-        //int seqNo = tupleSpace.eventRegister(Command.WRITE, tmp, cb, false);
-
-        Tuple [] allConcepts = tupleSpace.readAll(conceptTemplate);
-        Tuple [] allLinks = tupleSpace.readAll(linkTemplate);
+//        Tuple [] allConcepts = tupleSpace.readAll(conceptTemplate);
+//        Tuple [] allLinks = tupleSpace.readAll(linkTemplate);
         ObjectTranslator ot = new ObjectTranslator();
-        synchronizeDiagramElements(allConcepts, ot);
-        synchronizeDiagramElements(allLinks, ot);
+        
+        ArrayList<ScyMessage> messages = new ArrayList<ScyMessage>();
+        try {
+            cs = CollaborationServiceFactory.getCollaborationService(CollaborationServiceFactory.LOCAL_STYLE);
+            cs.addCollaborationListener(this);
+            ScyMessage queryMessage = ScyMessage.createScyMessage("anthonjp", "Colemo", null, null, ScyMessage.MESSAGE_TYPE_QUERY, ScyMessage.QUERY_TYPE_ALL, null, null, null, 0);
+            messages = cs.doQuery(queryMessage);
+        } catch (CollaborationServiceException e) {
+            logger.error("CollaborationService failure: " + e);
+            e.printStackTrace();
+        }       
+        
+//        synchronizeDiagramElements(allConcepts, ot);
+//        synchronizeDiagramElements(allLinks, ot);
+        synchronizeDiagramElements(messages, ot);        
     }
 
-    private void synchronizeDiagramElements(Tuple[] allTuples, ObjectTranslator ot) {
-        for (int i = 0; i < allTuples.length; i++) {
-            Tuple allTuple = allTuples[i];
-            Object newNOde = ot.getObject(allTuple);
+//    private void synchronizeDiagramElements(Tuple[] allTuples, ObjectTranslator ot) {
+//        for (int i = 0; i < allTuples.length; i++) {
+//            Tuple allTuple = allTuples[i];
+//            Object newNOde = ot.getObject(allTuple);
+//            addNewNode(newNOde);
+//        }
+//        //TODO: Find a better place for this
+//        ApplicationController.getDefaultInstance().getColemoPanel().setBounds(0,0, 800, 700);
+//    }
+    
+    private void synchronizeDiagramElements(ArrayList<ScyMessage> messages, ObjectTranslator ot) {
+        ScyMessage scyMessage;
+        for (int i = 0; i < messages.size(); i++) {
+            scyMessage = messages.get(i);
+            Object newNOde = ot.getObject(scyMessage);
             addNewNode(newNOde);
         }
         //TODO: Find a better place for this
         ApplicationController.getDefaultInstance().getColemoPanel().setBounds(0,0, 800, 700);
     }
+    
 
     public void cleanUp() {
-        try {
-            tupleSpace.takeAll(conceptTemplate);
-        } catch (TupleSpaceException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
+        //TODO: implement this in collaborationservice
+//        try {
+//            tupleSpace.takeAll(conceptTemplate);
+//        } catch (TupleSpaceException e) {
+//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//        }
     }
 
-    public void call(Command command, int i, Tuple tuple, Tuple tuple1) {
-        System.out.println("i: " + i);
-        if (tuple != null && tuple.getFields().length == conceptTemplate.getFields().length) {
-            System.out.println("UPDATING CONCEPT FROM SERVER!");
-            ObjectTranslator ot = new ObjectTranslator();
-
-            System.out.println("CALL: Before add class");
-            BaseConceptMapNode node = (BaseConceptMapNode) ot.getObject(tuple);
-
-            addNewNode(node);
-        } else if(tuple != null && tuple.getFields().length == linkTemplate.getFields().length) {
-            System.out.println("UPDATING LINK FROM SERVER!");
-            ObjectTranslator ot = new ObjectTranslator();
-            UmlLink link = (UmlLink) ot.getObject(tuple);
-            addNewNode(link);
-        }
-    }
+//    public void call(Command command, int i, Tuple tuple, Tuple tuple1) {
+//        System.out.println("i: " + i);
+//        if (tuple != null && tuple.getFields().length == conceptTemplate.getFields().length) {
+//            System.out.println("UPDATING CONCEPT FROM SERVER!");
+//            ObjectTranslator ot = new ObjectTranslator();
+//
+//            System.out.println("CALL: Before add class");
+//            BaseConceptMapNode node = (BaseConceptMapNode) ot.getObject(tuple);
+//
+//            addNewNode(node);
+//        } else if(tuple != null && tuple.getFields().length == linkTemplate.getFields().length) {
+//            System.out.println("UPDATING LINK FROM SERVER!");
+//            ObjectTranslator ot = new ObjectTranslator();
+//            UmlLink link = (UmlLink) ot.getObject(tuple);
+//            addNewNode(link);
+//        }
+//    }
 
     private void addNewNode(Object node) {
         UmlClass umlClass;
@@ -226,7 +241,7 @@ public class ConnectionHandlerSqlSpaces implements ConnectionHandler, Callback, 
             logger.debug("UPDATING FROM SERVER!");
             ObjectTranslator ot = new ObjectTranslator();
             logger.debug("CALL: Before add class");
-            Object object = ot.getObject2(sm);
+            Object object = ot.getObject(sm);
             if (object instanceof BaseConceptMapNode) {
                 addNewNode((BaseConceptMapNode) object);                
             }
