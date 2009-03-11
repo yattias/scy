e26bfccf1c62defa6f34cec30ac2975143d789cb
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-03-11 19:09:29 +0000

    added some comments

--- scy-commons/src/main/java/eu/scy/communications/adapter/IScyCommunicationAdapter.java
+++ scy-commons/src/main/java/eu/scy/communications/adapter/IScyCommunicationAdapter.java
@@ -2,19 +2,73 @@ package eu.scy.communications.adapter;
 
 import eu.scy.communications.message.ScyMessage;
 
-
+/**
+ * The ScyCommunicationAdapter allows a coordinating server
+ * to use it to write and retrieve data
+ * 
+ * @author anthonyp
+ *
+ */
 public interface IScyCommunicationAdapter {
 
+    /**
+     * Adds listeners to the adapter
+     * 
+     * @param listener
+     */
     public void addScyCommunicationListener(IScyCommunicationListener listener);
 
-    // callback methods
+    /**
+     * performs an action upon write
+     * 
+     * @param scyMessage
+     */
     void actionUponWrite(ScyMessage scyMessage);
+    
+    /**
+     * performs an action upon delete
+     * 
+     * @param scyMessage
+     */
 	void actionUponDelete(ScyMessage scyMessage);
 	
-	// various CRUDs
-    public String create(ScyMessage sm);
+	/**
+	 * Does a create operation
+	 * 
+	 * @param scyMessage
+	 * @return
+	 */
+    public String create(ScyMessage scyMessage);
+    
+    /**
+     * Does a read operation
+     * 
+     * @param id
+     * @return
+     */
     public ScyMessage read(String id);
+    
+    /**
+     * Does an update operation
+     * 
+     * @param sm
+     * @param id
+     * @return
+     */
     public String update(ScyMessage sm, String id);
+    
+    /**
+     * Does a delete operation
+     * 
+     * @param id
+     * @return
+     */
     public String delete(String id);
+    
+    /**
+     * Sends a call back to a receiver
+     * 
+     * @param scyMessage
+     */
     public void sendCallBack(ScyMessage scyMessage);
 }
