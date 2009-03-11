2868c1503546fb28bfcba36e0d22b360c07ad2ab
thomas.drevon <thomas.drevon@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-03-11 14:51:15 +0000

    refactored to only use collaboration service

--- trunk/colemo-client/src/main/java/eu/scy/colemo/client/sqlspacesimpl/ObjectTranslator.java
+++ trunk/colemo-client/src/main/java/eu/scy/colemo/client/sqlspacesimpl/ObjectTranslator.java
@@ -13,6 +13,8 @@ import eu.scy.communications.message.ScyMessage;
 
 import java.awt.*;
 
+import org.apache.log4j.Logger;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Henrik
@@ -23,6 +25,7 @@ import java.awt.*;
 public class ObjectTranslator {
 
     private static final long DEFAULT_EXPIRATION_TIME = 30*1000;
+    private static final Logger logger = Logger.getLogger(ObjectTranslator.class.getName());
     
     public Tuple translate(Object object) {
         return null;
@@ -30,74 +33,73 @@ public class ObjectTranslator {
     }
 
 
-    // TODO: refactor this one to handle a ScyMessage, not a Tuple
-    public Object getObject(Tuple tuple) {
-        Field[] fields = tuple.getFields();
-
-        if (fields.length == 6) {
-            Field userNameField = fields[0];
-            Field toolName = fields[1];
-            Field objectIdField = fields[2];
-            Field typeField = fields[3];
-            Field nameField = fields[4];
-            Field descriptionField = fields[5];
-
-            try {
-                Class c = Class.forName((String) typeField.getValue());
-                if (c.getName().indexOf("AddClass") > 0) {
-                    System.out.println("Creating new add class");
-                    AddClass ac = new AddClass((String) nameField.getValue(), (String) typeField.getValue(), "HEI", null, null);
-                    ac.setId((String) objectIdField.getValue());
-                    return ac;
-                } else if (c.getName().indexOf("MoveClass") > 0) {
-                    System.out.println("Creating new MoveClass!");
-                    String pos = (String) descriptionField.getValue();
-                    String[] posArray = pos.split(",");
-                    Integer xPos = new Integer(posArray[0]);
-                    Integer yPos = new Integer(posArray[1]);
-
-                    GraphicsDiagram diagram = ApplicationController.getDefaultInstance().getGraphicsDiagram();
-                    ConceptNode node = diagram.getNodeByClassId((String) objectIdField.getValue());
-
-                    System.out.println("xpos = " + xPos);
-                    System.out.println("Y = " + yPos);
-                    node.setLocation(xPos, yPos);
-
-                    UmlClass cls = node.getModel();
-                    cls.setX(xPos);
-                    cls.setY(yPos);
-
-                    return new MoveClass(cls, null, null);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            }
-        } else if (fields.length == 5) {
-            Field userNameField = fields[0];
-            Field toolName = fields[1];
-            Field objectIdField = fields[2];
-            Field fromField = fields[3];
-            Field toField = fields[4];
-            UmlLink link = new UmlLink((String)fromField.getValue(), (String) toField.getValue(), "Henrik");
-            link.setId((String) objectIdField.getValue());
-            return link;
-        }
-        System.out.println("** *** ** Object Translator returning null");
-        return null;
-
-    }
+//    public Object getObject(Tuple tuple) {
+//        Field[] fields = tuple.getFields();
+//
+//        if (fields.length == 6) {
+//            Field userNameField = fields[0];
+//            Field toolName = fields[1];
+//            Field objectIdField = fields[2];
+//            Field typeField = fields[3];
+//            Field nameField = fields[4];
+//            Field descriptionField = fields[5];
+//
+//            try {
+//                Class c = Class.forName((String) typeField.getValue());
+//                if (c.getName().indexOf("AddClass") > 0) {
+//                    System.out.println("Creating new add class");
+//                    AddClass ac = new AddClass((String) nameField.getValue(), (String) typeField.getValue(), "HEI", null, null);
+//                    ac.setId((String) objectIdField.getValue());
+//                    return ac;
+//                } else if (c.getName().indexOf("MoveClass") > 0) {
+//                    System.out.println("Creating new MoveClass!");
+//                    String pos = (String) descriptionField.getValue();
+//                    String[] posArray = pos.split(",");
+//                    Integer xPos = new Integer(posArray[0]);
+//                    Integer yPos = new Integer(posArray[1]);
+//
+//                    GraphicsDiagram diagram = ApplicationController.getDefaultInstance().getGraphicsDiagram();
+//                    ConceptNode node = diagram.getNodeByClassId((String) objectIdField.getValue());
+//
+//                    System.out.println("xpos = " + xPos);
+//                    System.out.println("Y = " + yPos);
+//                    node.setLocation(xPos, yPos);
+//
+//                    UmlClass cls = node.getModel();
+//                    cls.setX(xPos);
+//                    cls.setY(yPos);
+//
+//                    return new MoveClass(cls, null, null);
+//                }
+//            } catch (Exception e) {
+//                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//            }
+//        } else if (fields.length == 5) {
+//            Field userNameField = fields[0];
+//            Field toolName = fields[1];
+//            Field objectIdField = fields[2];
+//            Field fromField = fields[3];
+//            Field toField = fields[4];
+//            UmlLink link = new UmlLink((String)fromField.getValue(), (String) toField.getValue(), "Henrik");
+//            link.setId((String) objectIdField.getValue());
+//            return link;
+//        }
+//        System.out.println("** *** ** Object Translator returning null");
+//        return null;
+//
+//    }
 
     
-    public Object getObject2(ScyMessage scyMessage) {
+    public Object getObject(ScyMessage scyMessage) {
         
         if (scyMessage.getObjectType().indexOf("AddClass") > 0) {
-            System.out.println("Creating new add class");
+            logger.debug("Creating new add class");
             AddClass ac = new AddClass(scyMessage.getName(), scyMessage.getObjectType(), "HEI", null, null);
             ac.setId(scyMessage.getId());
             return ac;
         }
         else if (scyMessage.getObjectType().indexOf("MoveClass") > 0) {
-            System.out.println("Creating new MoveClass!");
+            logger.debug("Creating new MoveClass!");
             String pos = scyMessage.getDescription();
             String[] posArray = pos.split(",");
             Integer xPos = new Integer(posArray[0]);
@@ -106,8 +108,8 @@ public class ObjectTranslator {
             GraphicsDiagram diagram = ApplicationController.getDefaultInstance().getGraphicsDiagram();
             ConceptNode node = diagram.getNodeByClassId(scyMessage.getId());
 
-            System.out.println("xpos = " + xPos);
-            System.out.println("Y = " + yPos);
+            logger.debug("xpos = " + xPos);
+            logger.debug("Y = " + yPos);
             node.setLocation(xPos, yPos);
 
             UmlClass cls = node.getModel();
@@ -126,51 +128,51 @@ public class ObjectTranslator {
     }
     
     
-    public Tuple getTuple(AddClass addClass) {
-        Field userNameField = new Field("henrik@enovate.no");
-        Field toolName = new Field("Colemo");
-        Field objectIdField = new Field(String.valueOf(addClass.hashCode()));
-        Field objectType = new Field(addClass.getClass().getName());
-        Field objectName = new Field(addClass.getName());
-        Field objectDescription = new Field("Some description");
-
-        return new Tuple(userNameField, toolName, objectIdField, objectType, objectName, objectDescription);
-    }
-
-    public Tuple getTuple(MoveClass moveClass) {
-        Field userNameField = new Field("henrik@enovate.no");
-        Field toolName = new Field("Colemo");
-        Field objectIdField = new Field(moveClass.getUmlClass().getId());
-        Field objectType = new Field(moveClass.getClass().getName());
-        Field objectName = new Field(moveClass.getUmlClass().getName());
-        Field objectDescription = new Field("" + moveClass.getUmlClass().getX() + "," + moveClass.getUmlClass().getY());
-
-        return new Tuple(userNameField, toolName, objectIdField, objectType, objectName, objectDescription);
-    }
-
-    public Tuple getTuple(UmlLink addLink) {
-
-        Field userNameField = new Field("henrik@enovate.no");
-        Field toolName = new Field("Colemo");
-        Field objectIdField = new Field(addLink.getId());
-        Field fromField = new Field(addLink.getFrom());
-        Field toField = new Field(addLink.getTo());
-        System.out.println("toField = " + toField);
-        System.out.println("fromField = " + fromField);
-        return new Tuple(userNameField, toolName, objectIdField, fromField, toField);
-    }
+//    public Tuple getTuple(AddClass addClass) {
+//        Field userNameField = new Field("henrik@enovate.no");
+//        Field toolName = new Field("Colemo");
+//        Field objectIdField = new Field(String.valueOf(addClass.hashCode()));
+//        Field objectType = new Field(addClass.getClass().getName());
+//        Field objectName = new Field(addClass.getName());
+//        Field objectDescription = new Field("Some description");
+//
+//        return new Tuple(userNameField, toolName, objectIdField, objectType, objectName, objectDescription);
+//    }
+//
+//    public Tuple getTuple(MoveClass moveClass) {
+//        Field userNameField = new Field("henrik@enovate.no");
+//        Field toolName = new Field("Colemo");
+//        Field objectIdField = new Field(moveClass.getUmlClass().getId());
+//        Field objectType = new Field(moveClass.getClass().getName());
+//        Field objectName = new Field(moveClass.getUmlClass().getName());
+//        Field objectDescription = new Field("" + moveClass.getUmlClass().getX() + "," + moveClass.getUmlClass().getY());
+//
+//        return new Tuple(userNameField, toolName, objectIdField, objectType, objectName, objectDescription);
+//    }
+//
+//    public Tuple getTuple(UmlLink addLink) {
+//
+//        Field userNameField = new Field("henrik@enovate.no");
+//        Field toolName = new Field("Colemo");
+//        Field objectIdField = new Field(addLink.getId());
+//        Field fromField = new Field(addLink.getFrom());
+//        Field toField = new Field(addLink.getTo());
+//        System.out.println("toField = " + toField);
+//        System.out.println("fromField = " + fromField);
+//        return new Tuple(userNameField, toolName, objectIdField, fromField, toField);
+//    }
     
     
     public ScyMessage getScyMessage(AddClass addClass) {
-        return ScyMessage.createScyMessage("henrik@enovate.no", "Colemo", String.valueOf(addClass.hashCode()), addClass.getClass().getName(), addClass.getName(), "Some description", null, null, null, DEFAULT_EXPIRATION_TIME);
+        return ScyMessage.createScyMessage("henrik@enovate.no", "Colemo", String.valueOf(addClass.hashCode()), addClass.getClass().getName(), addClass.getName(), "Some description", null, null, null, 0);
     }
     
     
     public ScyMessage getScyMessage(MoveClass moveClass) {
-        return ScyMessage.createScyMessage("henrik@enovate.no", "Colemo", moveClass.getUmlClass().getId(), moveClass.getClass().getName(), moveClass.getUmlClass().getName(), "" + moveClass.getUmlClass().getX() + "," + moveClass.getUmlClass().getY(), null, null, null, DEFAULT_EXPIRATION_TIME);
+        return ScyMessage.createScyMessage("henrik@enovate.no", "Colemo", moveClass.getUmlClass().getId(), moveClass.getClass().getName(), moveClass.getUmlClass().getName(), "" + moveClass.getUmlClass().getX() + "," + moveClass.getUmlClass().getY(), null, null, null, 0);
     }
     
     public ScyMessage getScyMessage(UmlLink addLink) {
-        return ScyMessage.createScyMessage("henrik@enovate.no", "Colemo", addLink.getId(), addLink.getClass().getName(), addLink.getName(), "Some description", addLink.getTo(), addLink.getFrom(), null, DEFAULT_EXPIRATION_TIME);
+        return ScyMessage.createScyMessage("henrik@enovate.no", "Colemo", addLink.getId(), addLink.getClass().getName(), addLink.getName(), "Some description", addLink.getTo(), addLink.getFrom(), null, 0);
     }
 }
