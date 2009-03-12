e47c5be0dd3567ad20d28468c0125d842609bec7
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-03-12 10:58:40 +0000

    Updated to SCYUserImpl

--- trunk/colemo-model/src/main/java/eu/scy/colemo/network/Person.java
+++ trunk/colemo-model/src/main/java/eu/scy/colemo/network/Person.java
@@ -6,7 +6,7 @@
  */
 package eu.scy.colemo.network;
 
-import eu.scy.core.model.impl.UserImpl;
+import eu.scy.core.model.impl.SCYUserImpl;
 
 import java.io.Serializable;
 
@@ -16,7 +16,7 @@ import java.io.Serializable;
  * TODO To change the template for this generated type comment go to
  * Window - Preferences - Java - Code Style - Code Templates
  */
-public class Person extends UserImpl implements Serializable{
+public class Person extends SCYUserImpl implements Serializable{
 
 	private boolean typing = false;
 	
