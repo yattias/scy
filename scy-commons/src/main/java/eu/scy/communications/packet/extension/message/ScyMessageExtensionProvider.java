56dae5ed96803bb6eb91294c334cce54b7923e7a
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-03-16 09:56:13 +0000

    implements the interface

--- scy-commons/src/main/java/eu/scy/communications/packet/extension/message/ScyMessageExtensionProvider.java
+++ scy-commons/src/main/java/eu/scy/communications/packet/extension/message/ScyMessageExtensionProvider.java
@@ -4,16 +4,13 @@ import org.jivesoftware.smack.packet.PacketExtension;
 import org.jivesoftware.smack.provider.PacketExtensionProvider;
 import org.xmlpull.v1.XmlPullParser;
 
-
-
 /**
- * XMPP Packet extension provider for ScyMessages transported with an xmpp server
+ * XMPP Packet extension provider for ScyMessages transported with an xmpp
+ * server
  * 
  * @author anthonyp
- *
  */
 public class ScyMessageExtensionProvider implements PacketExtensionProvider {
-    
     
     public ScyMessageExtensionProvider() {
         System.out.println("ScyMessageExtensionProvider.ScyMessageExtensionProvider()");
@@ -30,28 +27,59 @@ public class ScyMessageExtensionProvider implements PacketExtensionProvider {
         
         try {
             boolean done = false;
-            StringBuffer buffer = new StringBuffer();;
+            StringBuffer buffer = new StringBuffer();
+            
             while (!done) {
                 int eventType = parser.next();
                 if (eventType == XmlPullParser.START_TAG) {
-                    if ( parser.getName() != null && parser.getName().equals("id")) {
+                    if (parser.getName() != null && parser.getName().equals("id")) {
                         String nt = parser.nextText();
                         System.out.println(nt);
                         scyExt.setId(nt);
                         
-                    }
-                    else if (parser.getName() != null && parser.getName().equals("name")) {
+                    } else if (parser.getName() != null && parser.getName().equals("name")) {
                         String nt = parser.nextText();
                         System.out.println(nt);
                         scyExt.setName(nt);
-                    }
-                    else if ( parser.getName() != null && parser.getName().equals("description")) {
+                    } else if (parser.getName() != null && parser.getName().equals("description")) {
                         String nt = parser.nextText();
                         System.out.println(nt);
                         scyExt.setDescription(nt);
-                    }
+                    } else if (parser.getName() != null && parser.getName().equals("userName")) {
+                        String nt = parser.nextText();
+                        System.out.println(nt);
+                        scyExt.setUserName(nt);
+                    } else if (parser.getName() != null && parser.getName().equals("toolName")) {
+                        String nt = parser.nextText();
+                        System.out.println(nt);
+                        scyExt.setToolName(nt);
+                    } else if (parser.getName() != null && parser.getName().equals("to")) {
+                        String nt = parser.nextText();
+                        System.out.println(nt);
+                        scyExt.setTo(nt);
+                    } else if (parser.getName() != null && parser.getName().equals("from")) {
+                        String nt = parser.nextText();
+                        System.out.println(nt);
+                        scyExt.setFrom(nt);
+                    } else if (parser.getName() != null && parser.getName().equals("session")) {
+                        String nt = parser.nextText();
+                        System.out.println(nt);
+                        scyExt.setSession(nt);
+                    } else if (parser.getName() != null && parser.getName().equals("objectType")) {
+                        String nt = parser.nextText();
+                        System.out.println(nt);
+                        scyExt.setObjectType(nt);
+                    } else if (parser.getName() != null && parser.getName().equals("messagePurpose")) {
+                        String nt = parser.nextText();
+                        System.out.println(nt);
+                        scyExt.setMessagePurpose(nt);
+                    } else if (parser.getName() != null && parser.getName().equals("expiraton")) {
+                        String nt = parser.nextText();
+                        System.out.println(nt);
+                        scyExt.setExpiraton(new Long(nt).longValue());
+                    } 
                 } else if (eventType == XmlPullParser.END_TAG) {
-                    System.out.println( "name; + " + scyExt.getName() + " " + scyExt.getElementName());
+                    System.out.println("name; + " + scyExt.getName() + " " + scyExt.getElementName());
                     if (parser.getName().equals(scyExt.getElementName())) {
                         done = true;
                     }
@@ -62,7 +90,7 @@ public class ScyMessageExtensionProvider implements PacketExtensionProvider {
         }
         
         return (PacketExtension) scyExt;
-
+        
     }
     
 }
