9e520336722175b7802a5f2eb8755ff11482aec5
bjoerge <bjoerge@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-03-20 19:58:23 +0000

    Created GUI for review demo mobile app. To test it, install the Sun Wireless Toolkit and type mvn -Dj2me.sdkPath=<WTK_HOME> compile package j2me:run For example: mvn -Dj2me.sdkPath=C:\WTK2.5.2_01 compile package j2me:run

--- trunk/tool-broker-mobile-demoapp/src/main/java/eu/scy/mobile/toolbroker/demo/SCYMobileDemo.java
+++ trunk/tool-broker-mobile-demoapp/src/main/java/eu/scy/mobile/toolbroker/demo/SCYMobileDemo.java
@@ -1,49 +1,35 @@
 package eu.scy.mobile.toolbroker.demo;
 
-import eu.scy.mobile.toolbroker.model.*;
-import eu.scy.mobile.toolbroker.model.impl.MobileToolBrokerImpl;
-import eu.scy.mobile.toolbroker.IELOService;
+import eu.scy.mobile.toolbroker.demo.model.ImageELO;
+import eu.scy.mobile.toolbroker.demo.ui.CameraCanvas;
+import eu.scy.mobile.toolbroker.demo.ui.ImageEloForm;
+import eu.scy.mobile.toolbroker.demo.ui.MainForm;
+import eu.scy.mobile.toolbroker.demo.ui.GlobalCommands;
 
 import javax.microedition.lcdui.*;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
-import javax.microedition.io.HttpConnection;
-import javax.microedition.io.Connector;
-import java.util.Vector;
-import java.io.InputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-
-import com.sun.me.web.request.Arg;
-import com.sun.me.web.request.ProgressInputStream;
-import com.sun.me.web.request.RequestListener;
-import com.sun.me.web.request.Response;
 
 /**
  * Created: 11.feb.2009 12:23:07
  *
  * @author Bjørge Næss
  */
-public class SCYMobileDemo extends MIDlet implements CommandListener, RequestListener {
-	private Form mainForm;
-	private IELOService toolBroker;
-	private TextField eloIdField;
-	private Form editELOForm;
-	private TextField eloName;
-	private IELO currentELO;
-	private TextField eloContent;
+public class SCYMobileDemo extends MIDlet implements CommandListener {
+	private MainForm mainForm;
+	private ImageEloForm eloForm;
+	private CameraCanvas cameraCanvas;
+
+	// Commands
+	private final Command takePictureCommand = new Command("Take a picture!",Command.ITEM, 1);
+	private final Command exitCommand = new Command("Exit",Command.EXIT, 1);
 
 	public Display getDisplay() {
-        return Display.getDisplay(this);
-    }
-	protected void startApp() throws MIDletStateChangeException {
+		return Display.getDisplay(this);
+	}
 
-                
-        toolBroker = new MobileToolBrokerImpl().getELOService();
-        System.out.println("toolBroker = " + toolBroker);
-		eloIdField = new TextField("Enter ELO Id", null, 2, TextField.NUMERIC);
-		getDisplay().setCurrent(getMainForm());
+	protected void startApp() throws MIDletStateChangeException {
+		getDisplay().setCurrent(createMainForm());
 	}
 
 	protected void pauseApp() {
@@ -52,115 +38,55 @@ public class SCYMobileDemo extends MIDlet implements CommandListener, RequestLis
 	protected void destroyApp(boolean b) throws MIDletStateChangeException {
 	}
 
-	private void updateELOForm(IELO elo) {
-        if (editELOForm == null) {
-	        editELOForm = new Form("Edit ELO", null);
-			eloName = new TextField("ELO name", null, 100, TextField.PLAIN);
-			eloContent = new TextField("ELO content", null, 100, TextField.PLAIN);
-	        editELOForm.addCommand(new Command("Save", Command.ITEM, 1));
-            editELOForm.setCommandListener(this);
-        }
-		eloName.setString(elo.getTitle());
-		editELOForm.append(eloName);
-        Object content = currentELO.getContent();
-		if (content != null) {
-            if (content instanceof IGeoImageList) {
-                editELOForm.append("The content of this ELO is a image collector. Displaying images:");
-                IGeoImageList imageList = (IGeoImageList) content;
-                Vector images = imageList.getImages();
-                for (int i = 0; i < images.size(); i++) {
-                    String location = (String) images.elementAt(i);
-                    Image img = loadImage(location);
-                    ImageItem imgItem = new ImageItem(null, img, ImageItem.LAYOUT_DEFAULT, "Thumbnail", ImageItem.BUTTON);
-                    editELOForm.append(imgItem);
-                }
-            }
-            else {
-                eloContent.setString(content.toString());
-                editELOForm.append(eloContent);
-            }
-        }
-        else {
-            editELOForm.append("<no content>");
-        }
-    }
-	private Form getMainForm() {
-        if (mainForm == null) {
-	        mainForm = new Form("Welcome to the test!", null);
-	        mainForm.append(eloIdField);
-	        mainForm.addCommand(new Command("Get ELO!", Command.ITEM, 1));
-            mainForm.setCommandListener(this);
-        }
-        return mainForm;
-    }
-	public void commandAction(Command command, Displayable displayable) {
-		if (command.getLabel().equals("Save")) {
-			currentELO.setTitle(eloName.getString());
-
-            Object content = currentELO.getContent();
-			if (content instanceof ITextContent)
-				((ITextContent)content).setContent(eloContent.getString());
-			else
-				System.out.println("Unsupported content type: "+ content.getClass());
-
-			System.out.println("Current ELO is "+currentELO.getContent().getClass());
-
-			toolBroker.updateELO(currentELO);
+	private Form createMainForm() {
+		if (mainForm == null) {
+			mainForm = new MainForm("Welcome to Mobile SCY!", null);
+			mainForm.addCommand(takePictureCommand);
+			mainForm.setCommandListener(this);
 		}
-		if (command.getLabel().equals("Get ELO!")) {
-			// Retrieve the first ELO
-			System.out.println("ELO??");
-			currentELO = toolBroker.getELO(Integer.parseInt(eloIdField.getString()));
-			mainForm.setTicker(new Ticker("Congratulations! You just retrieved an ELO!"));
-			System.out.println("elo = " + currentELO);
-			updateELOForm(currentELO);
-			getDisplay().setCurrent(editELOForm);
-		}
+		return mainForm;
 	}
 
-    public Image loadImage(final String location) {
-        HttpConnection conn = null;
-        InputStream is = null;
-        try {
-            conn = (HttpConnection) Connector.open(location);
-                        conn.setRequestProperty("accept", "image/*");
+	private Canvas createCameraCanvas() {
+		if (cameraCanvas == null) {
+			cameraCanvas = new CameraCanvas();
+			cameraCanvas.addCommand(CameraCanvas.CMD_CAPTURE);
+			cameraCanvas.addCommand(GlobalCommands.CMD_CANCEL);
+			cameraCanvas.setCommandListener(this);
+		}
+		return cameraCanvas;
+	}
 
-            final int responseCode = conn.getResponseCode();
-            if (responseCode != HttpConnection.HTTP_OK) return null;
+	private Form createEloForm(ImageELO elo) {
+		if (eloForm == null) {
+			eloForm = new ImageEloForm("Edit image", elo);
+			eloForm.addCommand(ImageEloForm.CMD_SAVE);
+			eloForm.addCommand(GlobalCommands.CMD_CANCEL);
+			eloForm.setCommandListener(this);
+		}
+		return eloForm;
+	}
 
-            final int totalToReceive = conn.getHeaderFieldInt(Arg.CONTENT_LENGTH, 0);
-            is = new ProgressInputStream(conn.openInputStream(), totalToReceive, this, null, 1024);
+	public void commandAction(Command command, Displayable displayable) {
+		if (command.equals(takePictureCommand)) {
+			getDisplay().setCurrent(createCameraCanvas());
+		}
+		else if (command.equals(CameraCanvas.CMD_CAPTURE)) {
 
-            final ByteArrayOutputStream bos = new ByteArrayOutputStream(Math.max(totalToReceive, 8192));
-            final byte[] buffer = new byte[4096];
-            for (int nread = is.read(buffer); nread >= 0; nread = is.read(buffer)) {
-                bos.write(buffer, 0, nread);
-            }
-            return Image.createImage(new ByteArrayInputStream(bos.toByteArray()));
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-             try {
-                if (is != null)is.close();
-                if (conn != null) conn.close();
+			Image i = cameraCanvas.capture();
 
-            } catch (IOException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            }
-        }
-        return null;
-    }
+			ImageELO elo = new ImageELO();
+			elo.setImage(i);
 
-    public void done(Object o, Response response) throws Exception {
-        
-    }
-
-    public void readProgress(Object o, int i, int i1) {
-        
-    }
-
-    public void writeProgress(Object o, int i, int i1) {
-
-    }
+			getDisplay().setCurrent(createEloForm(elo));
+		}
+		else if (command.equals(ImageEloForm.CMD_SAVE)) {
+			Alert a = new Alert("Image saved", "The image was sendt to your desktop! Please go there for more happy times!", null, null);
+			a.setTimeout(Alert.FOREVER);
+			getDisplay().setCurrent(a);
+		}
+		else if (command.equals(GlobalCommands.CMD_CANCEL)) {
+			getDisplay().setCurrent(createMainForm());
+		}
+	}
 }
---
+++ trunk/tool-broker-mobile-demoapp/src/main/java/eu/scy/mobile/toolbroker/demo/ui/CameraCanvas.java
@@ -0,0 +1,99 @@
+package eu.scy.mobile.toolbroker.demo.ui;
+/* License
+ *
+ * Copyright 1994-2004 Sun Microsystems, Inc. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistribution of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *
+ *  * Redistribution in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of Sun Microsystems, Inc. or the names of contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * This software is provided "AS IS," without a warranty of any kind. ALL
+ * EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING
+ * ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN")
+ * AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE
+ * AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+ * DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR ANY LOST
+ * REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL,
+ * INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY
+ * OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE,
+ * EVEN IF SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ *
+ * You acknowledge that this software is not designed, licensed or intended
+ * for use in the design, construction, operation or maintenance of any
+ * nuclear facility.
+ */
+
+
+import javax.microedition.lcdui.*;
+import javax.microedition.media.MediaException;
+import javax.microedition.media.Manager;
+import javax.microedition.media.Player;
+import javax.microedition.media.control.VideoControl;
+import java.io.IOException;
+
+public class CameraCanvas extends Canvas {
+	private VideoControl videoControl;
+	public static final Command CMD_CAPTURE = new Command("Take photo", Command.OK, 0);
+
+	public CameraCanvas() {
+
+		try {
+			Player mPlayer = Manager.createPlayer("capture://video");
+			mPlayer.realize();
+
+
+			videoControl = (VideoControl) mPlayer.getControl("VideoControl");
+			int width = getWidth();
+			int height = getHeight();
+
+			videoControl.initDisplayMode(VideoControl.USE_DIRECT_VIDEO, this);
+			videoControl.setDisplayLocation(2, 2);
+			videoControl.setDisplaySize(width - 4, height - 4);
+			mPlayer.start();
+			videoControl.setVisible(true);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		catch (MediaException me) {
+			try {
+				videoControl.setDisplayFullScreen(true);
+			}
+			catch (MediaException me2) {
+				me2.printStackTrace();
+			}
+		}
+	}
+
+	public Image capture() {
+		try {
+			// Get the image.
+			byte[] raw = videoControl.getSnapshot(null);
+			return Image.createImage(raw, 0, raw.length);
+		}
+		catch (MediaException me) {
+			me.printStackTrace();
+		}
+		return null;
+	}
+	public void paint(Graphics g) {
+		int width = getWidth();
+		int height = getHeight();
+		// Draw a green border around the VideoControl.
+		g.setColor(0x00ff00);
+		g.drawRect(0, 0, width - 1, height - 1);
+		g.drawRect(1, 1, width - 3, height - 3);
+	}
+}
