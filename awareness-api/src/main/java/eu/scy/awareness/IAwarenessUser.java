578c46b11fdce183af406258e528eaa7e62e3847
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-03-22 20:42:16 +0000

    changed api

---
+++ awareness-api/src/main/java/eu/scy/awareness/IAwarenessUser.java
@@ -0,0 +1,14 @@
+package eu.scy.awareness;
+
+public interface IAwarenessUser {
+
+    public void setUsername(String username) ;
+    public String getUsername() ;
+    public void setName(String name) ;
+    public String getName() ;
+    public void setStatus(String status);
+    public String getStatus() ;
+    public void setPresence(String presence);
+    public String getPresence();
+    
+}
