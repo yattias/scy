781bfd19d55e159b88874a148d697d6f51a0db23
hanbei <hanbei@gmx.de>
2009-03-23 13:04:17 +0000

    Agent to notify the elobrowser of newly created elos.

---
+++ roolo-agents/src/main/java/eu/scy/agents/roolo/elo/elobrowsernotification/NotifiyELOBroserAgent.java
@@ -0,0 +1,41 @@
+package eu.scy.agents.roolo.elo.elobrowsernotification;
+
+import roolo.elo.api.IELO;
+import roolo.elo.api.IMetadataKey;
+import eu.scy.agents.api.ICommunicationAgent;
+import eu.scy.agents.impl.elo.AbstractELOAgent;
+import eu.scy.notification.Notification;
+import eu.scy.notification.NotificationSender;
+import eu.scy.notification.api.INotification;
+import eu.scy.toolbroker.ToolBrokerImpl;
+import eu.scy.toolbrokerapi.ToolBrokerAPI;
+
+public class NotifiyELOBroserAgent<T extends IELO<K>, K extends IMetadataKey>
+		extends AbstractELOAgent<T, K> implements ICommunicationAgent<K> {
+
+	private NotificationSender sender;
+
+	public NotifiyELOBroserAgent() {
+		sender = new NotificationSender("scy.collide.info", 2525,
+				"notifications");
+	}
+
+	@Override
+	public void processElo(T elo) {
+		INotification notification = new Notification();
+		notification.addProperty("eloUri", elo.getUri().toString());
+		notification.addProperty("target", "elobrowser");
+		sender.send("roolo", "roolo", notification);
+	}
+
+	@Override
+	public ToolBrokerAPI<K> getToolBrokerAPI() {
+		return new ToolBrokerImpl<K>();
+	}
+
+	@Override
+	public void run() {
+		// agent is only activated on process elo.
+	}
+
+}
