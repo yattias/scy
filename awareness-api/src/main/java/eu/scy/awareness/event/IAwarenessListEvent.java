d28baf3bfe5eb63d82f66fb32d551d35cea3f877
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-03-24 15:20:26 +0000

    added some comments

--- awareness-api/src/main/java/eu/scy/awareness/event/IAwarenessListEvent.java
+++ awareness-api/src/main/java/eu/scy/awareness/event/IAwarenessListEvent.java
@@ -1,5 +1,11 @@
 package eu.scy.awareness.event;
 
+/**
+ * Event for when buddies are added and removed from a list
+ * 
+ * @author anthonyp
+ *
+ */
 public interface IAwarenessListEvent extends IAwarenessEvent {
     
     public static String ADD = "ADD";
