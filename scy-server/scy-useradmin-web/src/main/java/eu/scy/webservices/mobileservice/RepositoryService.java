3c79f671a1979ac64beb7f6d0c78db901a35804d
henrik <henrik@enovate.no>
2009-03-24 10:42:19 +0000

    MockRepository added to resource service for web services

--- scy-server/scy-useradmin-web/src/main/java/eu/scy/webservices/mobileservice/RepositoryService.java
+++ scy-server/scy-useradmin-web/src/main/java/eu/scy/webservices/mobileservice/RepositoryService.java
@@ -1,6 +1,12 @@
 package eu.scy.webservices.mobileservice;
 
+import roolo.api.IRepository;
+import roolo.cms.repository.mock.MockRepository;
+import roolo.elo.api.IELO;
+
 import javax.jws.WebService;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import javax.xml.bind.annotation.XmlTransient;
 import java.util.logging.Logger;
 
 /**
@@ -16,8 +22,17 @@ public class RepositoryService {
 
     private static Logger log = Logger.getLogger("RepositoryService.class");
 
+    @XmlTransient
+    private IRepository repository;
+
     public void doTheBartMan(String bartManString) {
         log.info("KICKING OFF WITH THIS FUNKY MESSAGE: " + bartManString);
+        IELO elo = null;//new HyperELO(bartManString);
+        repository.addELO(elo);
     }
 
+    @XmlTransient 
+    public void setRepository(MockRepository repository) {
+        this.repository = repository;
+    }
 }
