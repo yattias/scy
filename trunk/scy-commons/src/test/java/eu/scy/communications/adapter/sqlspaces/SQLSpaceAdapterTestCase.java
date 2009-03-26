7fc153e933aea84d4eae847910b1209d8b2dd658
thomas.drevon <thomas.drevon@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-03-26 11:39:59 +0000

    fixed the tests

--- trunk/scy-commons/src/test/java/eu/scy/communications/adapter/sqlspaces/SQLSpaceAdapterTestCase.java
+++ trunk/scy-commons/src/test/java/eu/scy/communications/adapter/sqlspaces/SQLSpaceAdapterTestCase.java
@@ -1,36 +1,38 @@
 package eu.scy.communications.adapter.sqlspaces;
 
-import org.apache.log4j.Logger;
-import org.junit.Ignore;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.Test;
 
-import eu.scy.communications.adapter.IScyCommunicationAdapter;
-import eu.scy.communications.adapter.IScyCommunicationListener;
+import org.apache.log4j.Logger;
+
 import eu.scy.communications.message.IScyMessage;
 import eu.scy.communications.message.impl.ScyMessage;
 
 
-
-public class SQLSpaceAdapterTestCase implements IScyCommunicationAdapter  {
+public class SQLSpaceAdapterTestCase {
     
-    private static final Logger logger = Logger.getLogger(SQLSpaceAdapter.class.getName());
+    private static final Logger logger = Logger.getLogger(SQLSpaceAdapterTestCase.class.getName());
     private SQLSpaceAdapter sqlSpaceAdapter;
     private static final long TWO_SECONDS = 2*1000;
     private static final long FOUR_SECONDS = 4*1000;
     private static final long TEN_SECONDS = 10*1000;
     
     
-    public SQLSpaceAdapterTestCase(String testName) {
+    public SQLSpaceAdapterTestCase() {
     }
-    
-    
-//    public static Test suite() {
-//        return new TestSuite(SQLSpaceAdapterTestCase.class);
-//    }
+
+
+    public static Test suite() { 
+        return new JUnit4TestAdapter(SQLSpaceAdapterTestCase.class); 
+    }
     
     
     private SQLSpaceAdapter getTupleAdapter() {
         if (sqlSpaceAdapter == null) {
-//            sqlSpaceAdapter = SQLSpaceAdapter.createAdapter("thomasd", SQLSpaceAdapter.COLLABORATION_SERVICE_SPACE, this);         
+            sqlSpaceAdapter = new SQLSpaceAdapter();
+            sqlSpaceAdapter.initialize("thomasd", SQLSpaceAdapter.COLLABORATION_SERVICE_SPACE);         
         }
         return sqlSpaceAdapter;
     }
@@ -43,101 +45,48 @@ public class SQLSpaceAdapterTestCase implements IScyCommunicationAdapter  {
         return sm;
     }
     
-    @Ignore
+    @org.junit.Test
     public void testCreateTupleAdapter() {
-//        assertNotNull(getTupleAdapter());
+        assertNotNull(getTupleAdapter());
     }
-//
-//    
-//    @Ignore
-//    public void testWriteReadDelete() {
-//        String id = null;
-//        // write
-//        id = getTupleAdapter().write(getScyMessage());
-//        assertNotNull(id);
-//        // read
-//        IScyMessage sm = getTupleAdapter().readById(id);
-//        assertNotNull(sm);
-//        // delete
-//        id = getTupleAdapter().delete(id);
-//        assertNotNull(id);
-//    }
-//
-//    @Ignore
-//    public void testWriteWithExpiration() {
-//        String id = null;
-//        IScyMessage sm = getScyMessage();
-//        sm.setExpiraton(TWO_SECONDS);
-//        // write with expiration
-//        id = getTupleAdapter().write(sm);
-//        assertNotNull(id);
-//        // confirm tuple
-//        sm = getTupleAdapter().readById(id);
-//        assertNotNull(sm);
-//        try {
-//            Thread.sleep(TEN_SECONDS);
-//        } catch (InterruptedException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//        // confirm tuple has expired
-//        sm = getTupleAdapter().readById(id);
-//        assertNull(sm);
-//    }
+
     
 
-    @Override
-    public void actionUponDelete(IScyMessage scyMessage) {
-        logger.info("Callback sez: Stuff deleted from sqlspaces");
+    @org.junit.Test
+    public void testWriteReadDelete() {
+        String id = null;
+        // write
+        id = getTupleAdapter().write(getScyMessage());
+        assertNotNull(id);
+        // read
+        IScyMessage sm = getTupleAdapter().readById(id);
+        assertNotNull(sm);
+        // delete
+        id = getTupleAdapter().delete(id);
+        assertNotNull(id);
     }
 
 
-    @Override
-    public void actionUponWrite(IScyMessage scyMessage) {
-        logger.info("Callback sez: Stuff written to sqlspaces");
+    @org.junit.Test
+    public void testWriteWithExpiration() {
+        String id = null;
+        IScyMessage sm = getScyMessage();
+        sm.setExpiraton(TWO_SECONDS);
+        // write with expiration
+        id = getTupleAdapter().write(sm);
+        assertNotNull(id);
+        // confirm tuple
+        sm = getTupleAdapter().readById(id);
+        assertNotNull(sm);
+        try {
+            Thread.sleep(TEN_SECONDS);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        // confirm tuple has expired
+        sm = getTupleAdapter().readById(id);
+        assertNull(sm);
     }
-
-    @Override
-    public void addScyCommunicationListener(IScyCommunicationListener listener) {
-        // TODO Auto-generated method stub        
-    }
-
-
-    @Override
-    public String create(IScyMessage sm) {
-        // TODO Auto-generated method stub        
-        return null;
-    }
-
-    @Override
-    public String delete(String id) {
-        // TODO Auto-generated method stub        
-        return null;
-    }
-
-    @Override
-    public IScyMessage read(String id) {
-        // TODO Auto-generated method stub        
-        return null;
-    }
-
-    @Override
-    public String update(IScyMessage sm, String id) {
-        // TODO Auto-generated method stub        
-        return null;
-    }
-
-
-    @Override
-    public void sendCallBack(IScyMessage scyMessage) {
-        // TODO Auto-generated method stub
-        
-    }
-
-
-    @Override
-    public void actionUponUpdate(IScyMessage scyMessage) {
-        // TODO Auto-generated method stub
-        
-    }    
+  
 }
