13ea27f246d01734f34d2ed622bc7f067a4f9632
adam.giemza <adam.giemza@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-03-30 15:20:42 +0000

    Finalized first version of the Android SCY Data Collector

--- SCYDataCollector/src/eu/scy/DataCollector.java
+++ SCYDataCollector/src/eu/scy/DataCollector.java
@@ -11,14 +11,16 @@ import org.ksoap2.transport.HttpTransportSE;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.SubMenu;
 import android.view.View;
-import android.view.WindowManager.LayoutParams;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -26,6 +28,8 @@ import android.widget.TextView;
 public class DataCollector extends Activity {
     /** Called when the activity is first created. */
 
+	public static final String PICTURE = "PICTURE";
+	
 	public final static int IMAGE_TAKEN = 1;
 	
 	private Button takePhoto;
@@ -64,45 +68,50 @@ public class DataCollector extends Activity {
 
 			public void onClick(View v) {
 				if(picture == null) {
-					AlertDialog alertDialog = new AlertDialog.Builder(DataCollector.this).create();
-					alertDialog.setMessage("You have to take a picture first!");
-					alertDialog.setTitle("Warning");
-					alertDialog.setCanceledOnTouchOutside(true);
-					alertDialog.show();
+					showDialog("Warning", "You have to take a photo first!");
 				} else if(title.getText() != null && title.getText().length() == 0) {
-					AlertDialog alertDialog = new AlertDialog.Builder(DataCollector.this).create();
-					alertDialog.setMessage("You have to provide a title first!");
-					alertDialog.setTitle("Warning");
-					alertDialog.setCanceledOnTouchOutside(true);
-					alertDialog.show();
+					showDialog("Warning", "You have to provide a title first!");
 				} else {
 					saveELO();
 				}
 			}
         	
         });
+        
+        if(savedInstanceState != null && savedInstanceState.containsKey(PICTURE)) {
+        	Log.d("DataCollector->onCreate", "contains key? " + Boolean.toString(savedInstanceState.containsKey(PICTURE)));
+        	picture = (Bitmap) savedInstanceState.get(PICTURE);
+        	updatePreview();
+        }
     }
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
     	SubMenu subMenu = menu.addSubMenu("Exit");
     	
-    	
     	return super.onCreateOptionsMenu(menu);
     }
     
     @Override
-    public void onWindowAttributesChanged(LayoutParams params) {
-    	super.onWindowAttributesChanged(params);
-    	updatePreview();
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	if("Exit".equals(item.getTitle())) {
+    		finish();
+    	}
+    	return super.onOptionsItemSelected(item);
     }
     
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+    	super.onSaveInstanceState(outState);
+    	Log.d("DataCollector->onSaveInstanceState", "Saving picture to bundle");
+    	outState.putParcelable(PICTURE, picture);
+    }
     
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
     	super.onActivityResult(requestCode, resultCode, data);
     	
-    	if(requestCode == IMAGE_TAKEN) {
+    	if(requestCode == IMAGE_TAKEN && data != null) {
     		Bundle bundle = data.getExtras();
     		if(bundle != null) {
     			picture = (Bitmap) bundle.get("data");
@@ -116,13 +125,25 @@ public class DataCollector extends Activity {
     		preview.setImageBitmap(picture);
     	}
     }
-    
-    // webservice stuff - very hard coded right now :(
 
+	private void showDialog(String title, String message) {
+		AlertDialog alertDialog = new AlertDialog.Builder(DataCollector.this).create();
+		alertDialog.setTitle(title);
+		alertDialog.setMessage(message);
+		alertDialog.setButton("Ok", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				// we do nothing here
+			}
+		});
+		alertDialog.show();
+	}
+
+	// webservice stuff - very hard coded right now :(
+	
 	private static final String SOAP_ACTION = "saveData";
     private static final String METHOD_NAME = "saveData";
     private static final String NAMESPACE = "http://mobileservice.webservices.scy.eu/";
-    private static final String URL = "http://192.168.178.29:8080/scy-useradmin-web/services/RepositoryService";
+    private static final String URL = "http://192.168.178.30:8080/scy-useradmin-web/services/RepositoryService";
      
     void saveELO() {
         try {
@@ -135,8 +156,9 @@ public class DataCollector extends Activity {
 
         	// set-up SOAP message
             SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
-            request.addProperty("arg0", t);
-            request.addProperty("arg1", pic);
+            request.addProperty("title", t);
+            request.addProperty("description", "-");
+            request.addProperty("b64image", pic);
      
             // set-up transport and send
             SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
@@ -144,13 +166,15 @@ public class DataCollector extends Activity {
             HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
             androidHttpTransport.call(SOAP_ACTION, envelope);
      
-            // handle result
-            Object result = envelope.getResponse();
-        } catch (Exception e) {
-            e.printStackTrace();
+            showDialog("Success", "The photo has been stored successfully!");
             
-            String x = e.toString();
-            System.out.println(x);
+            title.setText("");
+            preview.setImageBitmap(null);
+            // handle result
+//            Object result = envelope.getResponse();
+        } catch (Exception e) {
+            Log.d("DataCollector->SOAP", e.getMessage());
+            showDialog("Error", e.toString());
         }
     }
 
