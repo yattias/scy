0906d8564f2be1c31bb57e011906ee340060f649
hanbei@gmx.de <hanbei@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-03-31 14:08:01 +0000

    adapted to changed super class

--- trunk/roolo-agents/src/main/java/eu/scy/agents/roolo/elo/misspelling/MisspellingProcessELO.java
+++ trunk/roolo-agents/src/main/java/eu/scy/agents/roolo/elo/misspelling/MisspellingProcessELO.java
@@ -3,24 +3,22 @@ package eu.scy.agents.roolo.elo.misspelling;
 import info.collide.sqlspaces.client.TupleSpace;
 import info.collide.sqlspaces.commons.Tuple;
 import info.collide.sqlspaces.commons.TupleSpaceException;
-
-import java.util.Locale;
-
 import roolo.elo.api.IELO;
 import roolo.elo.api.IMetadataKey;
 import eu.scy.agents.impl.elo.AbstractELOAgent;
 
-public class MisspellingProcessELO<T extends IELO<K>, K extends IMetadataKey> extends AbstractELOAgent<T, K> {
-    
-    @Override
-    public void processElo(T elo) {
-        try {
-            TupleSpace ts = getTupleSpace();
-            ts.write(new Tuple("misspellings", elo.getUri().toString(), System.currentTimeMillis(),
-                    elo.getContent(Locale.ENGLISH).getXml()));
-        } catch (TupleSpaceException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-    }
+public class MisspellingProcessELO<T extends IELO<K>, K extends IMetadataKey>
+		extends AbstractELOAgent<T, K> {
+
+	@Override
+	public void processElo(T elo) {
+		try {
+			TupleSpace ts = getTupleSpace();
+			ts.write(new Tuple("misspellings", elo.getUri().toString(), System
+					.currentTimeMillis(), elo.getContent()));
+		} catch (TupleSpaceException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
 }
