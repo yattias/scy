5c599fb6c1c6d2826fed685c0a0a7912ff25ea9e
hanbei@gmx.de <hanbei@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-03-31 14:05:00 +0000

    IN PROGRESS - issue SCY-90: Awareness-Agent Mock for the Demo (Agent Part) https://base.enovate.no/browse/SCY-90
    Finished the two agents

---
+++ trunk/roolo-agents/src/main/java/eu/scy/agents/roolo/elo/conceptawareness/ConceptMapSavedAgent.java
@@ -0,0 +1,116 @@
+package eu.scy.agents.roolo.elo.conceptawareness;
+
+import info.collide.sqlspaces.client.TupleSpace;
+import info.collide.sqlspaces.commons.Tuple;
+import info.collide.sqlspaces.commons.TupleSpaceException;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+
+import roolo.elo.api.I18nType;
+import roolo.elo.api.IContent;
+import roolo.elo.api.IELO;
+import roolo.elo.api.IMetadata;
+import roolo.elo.api.IMetadataKey;
+import roolo.elo.api.IMetadataValueContainer;
+import roolo.elo.api.metadata.MetadataValueCount;
+import roolo.elo.metadata.keys.StringMetadataKey;
+import roolo.elo.metadata.value.validators.StringValidator;
+import eu.scy.agents.impl.elo.AbstractELOAgent;
+import eu.scy.toolbrokerapi.ToolBrokerAPI;
+
+public class ConceptMapSavedAgent<T extends IELO<K>, K extends IMetadataKey>
+		extends AbstractELOAgent<T, K> {
+
+	private IMetadataKey nodeLabelKey;
+	private IMetadataKey linkLabelKey;
+
+	public ConceptMapSavedAgent() {
+	}
+
+	private void registerKeys() {
+		nodeLabelKey = new StringMetadataKey("nodeLabel",
+				"/agentdata/conceptmaps/nodeLabel", I18nType.UNIVERSAL,
+				MetadataValueCount.LIST, new StringValidator());
+		if (!getMetadataTypeManager().isMetadataKeyRegistered(nodeLabelKey)) {
+			getMetadataTypeManager().registerMetadataKey(nodeLabelKey);
+		}
+
+		linkLabelKey = new StringMetadataKey("linkLabel",
+				"/agentdata/conceptmaps/linkLabel", I18nType.UNIVERSAL,
+				MetadataValueCount.LIST, new StringValidator());
+		if (!getMetadataTypeManager().isMetadataKeyRegistered(linkLabelKey)) {
+			getMetadataTypeManager().registerMetadataKey(linkLabelKey);
+		}
+	}
+
+	@Override
+	public void processElo(T elo) {
+		registerKeys();
+
+		IMetadata<K> metadata = elo.getMetadata();
+		IContent content = elo.getContent();
+		try {
+			enrichMetadata(metadata, content.getXmlString());
+
+			TupleSpace ts = getTupleSpace();
+			ts.write(new Tuple("scymapper", System.currentTimeMillis(), elo
+					.getUri().toString()));
+		} catch (TupleSpaceException e) {
+			e.printStackTrace();
+		} catch (JDOMException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private void enrichMetadata(IMetadata<K> metadata, String xmlString)
+			throws JDOMException, IOException {
+		StringReader stringReader = new StringReader(xmlString);
+		SAXBuilder builder = new SAXBuilder();
+		Document xmlDocument = builder.build(stringReader);
+
+		Element rootElement = xmlDocument.getRootElement();
+
+		Element nodesElement = rootElement.getChild("nodes");
+		List<Element> nodes = nodesElement.getChildren("node");
+		Set<String> nodeLabels = new HashSet<String>();
+		IMetadataValueContainer nodeValue = metadata
+				.getMetadataValueContainer((K) nodeLabelKey);
+		for (Element node : nodes) {
+			String nodeLabel = node.getAttributeValue("name");
+			if (!nodeLabels.contains(nodeLabel)) {
+				nodeValue.addValue(nodeLabel);
+				nodeLabels.add(nodeLabel);
+			}
+		}
+
+		Element linksElement = rootElement.getChild("links");
+		List<Element> links = linksElement.getChildren("link");
+		IMetadataValueContainer linkValue = metadata
+				.getMetadataValueContainer((K) linkLabelKey);
+		Set<String> linkLabels = new HashSet<String>();
+		for (Element link : links) {
+			String linkLabel = link.getAttributeValue("label");
+			if (!linkLabels.contains(linkLabel)) {
+				linkValue.addValue(linkLabel);
+				linkLabels.add(linkLabel);
+			}
+		}
+	}
+
+	@Override
+	protected void setToolBroker(ToolBrokerAPI<IMetadataKey> toolBroker) {
+		super.setToolBroker(toolBroker);
+	}
+}
---
+++ trunk/roolo-agents/src/main/java/eu/scy/agents/roolo/elo/conceptawareness/SearchForSimilarConceptsAgent.java
@@ -0,0 +1,114 @@
+package eu.scy.agents.roolo.elo.conceptawareness;
+
+import info.collide.sqlspaces.commons.Tuple;
+import info.collide.sqlspaces.commons.TupleSpaceException;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import roolo.api.IRepository;
+import roolo.api.search.IQuery;
+import roolo.api.search.ISearchResult;
+import roolo.cms.repository.mock.BasicMetadataQuery;
+import roolo.cms.repository.search.BasicSearchOperations;
+import roolo.elo.api.IELO;
+import roolo.elo.api.IMetadata;
+import roolo.elo.api.IMetadataKey;
+import roolo.elo.api.IMetadataTypeManager;
+import roolo.elo.api.IMetadataValueContainer;
+import roolo.elo.api.metadata.RooloMetadataKeys;
+import roolo.elo.metadata.keys.Contribute;
+import eu.scy.agents.impl.AbstractProcessingAgent;
+import eu.scy.notification.Notification;
+import eu.scy.notification.NotificationSender;
+import eu.scy.notification.api.INotification;
+import eu.scy.toolbrokerapi.ToolBrokerAPI;
+
+public class SearchForSimilarConceptsAgent<Key extends IMetadataKey> extends
+		AbstractProcessingAgent<Key> {
+
+	private static final String USER = "similarEloAgent";
+	private IMetadataTypeManager<Key> metadataTypeManager;
+	private static HashMap<String, String> userMap = new HashMap<String, String>();
+
+	static {
+		userMap.put("e3457b7e-73c4-418b-8cc2-b8534f01eee1", "adam");
+		userMap.put("6903ce4b-6643-4bb2-9ba7-123a19e05e5d", "henrik");
+		userMap.put("4270398a-90e5-4588-8182-85c62832d377", "florian");
+		userMap.put("5ee4d993-c938-4bc8-91db-cc5e6433c5dc", "lars");
+	}
+
+	public SearchForSimilarConceptsAgent() {
+		super("SearchForSimilarConceptsAgent");
+		metadataTypeManager = getMetadataTypeManager();
+	}
+
+	public SearchForSimilarConceptsAgent(ToolBrokerAPI<Key> toolBroker) {
+		super("SearchForSimilarConceptsAgent", toolBroker);
+		metadataTypeManager = getMetadataTypeManager();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected void doRun() {
+		try {
+			Tuple tuple = getTupleSpace().waitToTake(
+					new Tuple("scymapper", Long.class, String.class));
+			String eloUri = (String) tuple.getField(2).getValue();
+
+			IRepository<IELO<Key>, Key> repository = getRepository();
+			IELO<Key> elo = repository.retrieveELO(new URI(eloUri));
+			IMetadata<Key> metadata = elo.getMetadata();
+
+			Key nodeLabelKey = metadataTypeManager.getMetadataKey("nodeLabel");
+			IMetadataValueContainer values = metadata
+					.getMetadataValueContainer(nodeLabelKey);
+
+			List<String> nodeLabels = (List<String>) values.getValueList();
+			Set<URI> hits = new HashSet<URI>();
+
+			for (String nodeLabel : nodeLabels) {
+				IQuery query = new BasicMetadataQuery<Key>(nodeLabelKey,
+						BasicSearchOperations.EQUALS, nodeLabel, null);
+				List<ISearchResult> results = repository.search(query);
+				for (ISearchResult result : results) {
+					hits.add(result.getUri());
+				}
+			}
+
+			Set<String> relatedUsers = new HashSet<String>();
+			for (URI hitUri : hits) {
+				IMetadata<Key> hitMetadata = repository
+						.retrieveMetadata(hitUri);
+				IMetadataValueContainer value = hitMetadata
+						.getMetadataValueContainer(metadataTypeManager
+								.getMetadataKey(RooloMetadataKeys.AUTHOR
+										.getId()));
+				Contribute contribution = (Contribute) value.getValue();
+				String userGUID = contribution.getVCard();
+				relatedUsers.add(userGUID);
+			}
+
+			StringBuffer relatedUserList = new StringBuffer();
+			for (String relatedUser : relatedUsers) {
+				relatedUserList.append(userMap.get(relatedUser));
+				relatedUserList.append(" ");
+			}
+
+			INotification notification = new Notification();
+			notification.addProperty("users", relatedUserList.toString());
+			notification.addProperty("target", "awareness");
+			notification.addProperty("eloUri", elo.getUri().toString());
+			NotificationSender sender = new NotificationSender();
+			sender.send(USER, "searchSimilarElosAgent", notification);
+		} catch (TupleSpaceException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+}
