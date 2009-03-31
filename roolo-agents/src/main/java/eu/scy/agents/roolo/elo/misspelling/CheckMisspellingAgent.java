fea3630403ffba7a9bfc5e1370492d582bdb6551
hanbei <hanbei@gmx.de>
2009-03-31 14:08:01 +0000

    adapted to changed super class

--- roolo-agents/src/main/java/eu/scy/agents/roolo/elo/misspelling/CheckMisspellingAgent.java
+++ roolo-agents/src/main/java/eu/scy/agents/roolo/elo/misspelling/CheckMisspellingAgent.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import roolo.elo.api.IMetadataKey;
+
 import com.swabunga.spell.engine.SpellDictionary;
 import com.swabunga.spell.engine.SpellDictionaryHashMap;
 import com.swabunga.spell.event.SpellChecker;
@@ -16,53 +18,64 @@ import com.swabunga.spell.event.StringWordTokenizer;
 
 import eu.scy.agents.impl.AbstractProcessingAgent;
 
-public class CheckMisspellingAgent extends AbstractProcessingAgent {
-    
-    private SpellChecker spellChecker;
-    
-    public CheckMisspellingAgent() {
-        super();
-        // First parse dictinary
-        InputStream inputStream = CheckMisspellingAgent.class.getResourceAsStream("/eng_com.dic");// eng_com.dic
-        SpellDictionary dictionary;
-        try {
-            dictionary = new SpellDictionaryHashMap(new InputStreamReader(inputStream));
-            spellChecker = new SpellChecker(dictionary);
-            
-            // Register for notifications
-            getTupleSpace().eventRegister(Command.WRITE, new Tuple("misspellings", String.class, Long.class, String.class), new Callback() {
-                
-                @Override
-                public void call(Command command, int seq, Tuple after, Tuple before) {
-                    System.out.println(after);
-                    String content = (String) after.getField(3).getValue();
-                    content = content.replaceAll("<[^>]*>", "");
-                    String uri = (String) after.getField(1).getValue();
-                    checkMispelling(uri, content);
-                }
-            }, true);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (TupleSpaceException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    @Override
-    protected void doRun() {
-        // FIXME
-        done = true;
-    }
-    
-    private void checkMispelling(String uri, String content) {
-        int numOfErrors = spellChecker.checkSpelling(new StringWordTokenizer(content));
-        if (numOfErrors > 0) {
-            try {
-                getTupleSpace().write(new Tuple("misspellings", uri, System.currentTimeMillis(), numOfErrors));
-            } catch (TupleSpaceException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-    
+public class CheckMisspellingAgent<K extends IMetadataKey> extends
+		AbstractProcessingAgent<K> {
+
+	private SpellChecker spellChecker;
+
+	public CheckMisspellingAgent() {
+		super("CheckMisspellingAgent");
+		// First parse dictinary
+		InputStream inputStream = CheckMisspellingAgent.class
+				.getResourceAsStream("/eng_com.dic");// eng_com.dic
+		SpellDictionary dictionary;
+		try {
+			dictionary = new SpellDictionaryHashMap(new InputStreamReader(
+					inputStream));
+			spellChecker = new SpellChecker(dictionary);
+
+			// Register for notifications
+			getTupleSpace().eventRegister(
+					Command.WRITE,
+					new Tuple("misspellings", String.class, Long.class,
+							String.class), new Callback() {
+
+						@Override
+						public void call(Command command, int seq, Tuple after,
+								Tuple before) {
+							System.out.println(after);
+							String content = (String) after.getField(3)
+									.getValue();
+							content = content.replaceAll("<[^>]*>", "");
+							String uri = (String) after.getField(1).getValue();
+							checkMispelling(uri, content);
+						}
+					}, true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (TupleSpaceException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	protected void doRun() {
+		// FIXME
+		done = true;
+	}
+
+	private void checkMispelling(String uri, String content) {
+		int numOfErrors = spellChecker.checkSpelling(new StringWordTokenizer(
+				content));
+		if (numOfErrors > 0) {
+			try {
+				getTupleSpace().write(
+						new Tuple("misspellings", uri, System
+								.currentTimeMillis(), numOfErrors));
+			} catch (TupleSpaceException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
