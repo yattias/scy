aec0cbfdd3baeb9d3950737ba35797b4fe680e76
hanbei <hanbei@gmx.de>
2009-04-02 09:35:29 +0000

    CLOSED - issue SCY-90: Awareness-Agent Mock for the Demo (Agent Part) https://base.enovate.no/browse/SCY-90

--- agent-api/src/main/java/eu/scy/agents/api/IProcessingAgent.java
+++ agent-api/src/main/java/eu/scy/agents/api/IProcessingAgent.java
@@ -1,8 +1,28 @@
 package eu.scy.agents.api;
 
+import roolo.api.IRepository;
+import roolo.elo.api.IELO;
+import roolo.elo.api.IMetadataKey;
+import roolo.elo.api.IMetadataTypeManager;
 
-public interface IProcessingAgent extends IAgent, Runnable {
-    
-    public void run();
-    
+public interface IProcessingAgent<K extends IMetadataKey> extends IAgent,
+		Runnable {
+
+	/**
+	 * Run the agent.
+	 */
+	public void run();
+
+	/**
+	 * Get the repository to retrieve, search and alter elos.
+	 * 
+	 * @return
+	 */
+	public IRepository<IELO<K>, K> getRepository();
+
+	/**
+	 * Get the MetadataTypeManger.
+	 */
+	public IMetadataTypeManager<K> getMetadataTypeManager();
+
 }
