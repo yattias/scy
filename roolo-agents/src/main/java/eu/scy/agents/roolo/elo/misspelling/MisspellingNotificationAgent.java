68c3a429555326b125532eafc5a5a918d3b5ca32
hanbei <hanbei@gmx.de>
2009-04-03 11:30:24 +0000

    svn problems

---
+++ roolo-agents/src/main/java/eu/scy/agents/roolo/elo/misspelling/MisspellingNotificationAgent.java
@@ -0,0 +1,38 @@
+package eu.scy.agents.roolo.elo.misspelling;
+
+import info.collide.sqlspaces.commons.Tuple;
+import info.collide.sqlspaces.commons.TupleSpaceException;
+import roolo.elo.api.IMetadataKey;
+import eu.scy.agents.impl.AbstractCommunicationAgent;
+import eu.scy.notification.Notification;
+import eu.scy.notification.NotificationSender;
+import eu.scy.notification.api.INotification;
+
+public class MisspellingNotificationAgent<K extends IMetadataKey> extends AbstractCommunicationAgent<K> {
+    
+    private NotificationSender sender;
+    
+    public MisspellingNotificationAgent() {
+        sender = new NotificationSender("scy.collide.info", 2525, "notifications");
+    }
+    
+    @Override
+    protected void doRun() {
+        Tuple t;
+        try {
+            t = getTupleSpace().waitToTake(new Tuple("misspellings", String.class, Long.class, Integer.class));
+            String uri = (String) t.getField(1).getValue();
+            Integer numberOfErrors = (Integer) t.getField(3).getValue();
+            getTupleSpace().write(new Tuple("notification", "misspelling", uri, numberOfErrors));
+            System.out.println("***************** your document " + uri + " has " + numberOfErrors + " spelling errors ********** ");
+            
+            INotification notification = new Notification();
+            notification.addProperty("errors", "" + numberOfErrors);
+            notification.addProperty("target", "misspellings");
+            sender.send("adam", "textpad", notification);
+        } catch (TupleSpaceException e) {
+            throw new RuntimeException(e);
+        }
+        
+    }
+}
