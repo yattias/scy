eb062667e25e8331022ed3b725b6771747fd3789
hanbei@gmx.de <hanbei@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-04-03 13:58:38 +0000

    made repository and metadatatypemanager spring injectable

--- trunk/agent-impl/src/main/java/eu/scy/agents/impl/AbstractProcessingAgent.java
+++ trunk/agent-impl/src/main/java/eu/scy/agents/impl/AbstractProcessingAgent.java
@@ -5,14 +5,15 @@ import roolo.elo.api.IELO;
 import roolo.elo.api.IMetadataKey;
 import roolo.elo.api.IMetadataTypeManager;
 import eu.scy.agents.api.IProcessingAgent;
-import eu.scy.toolbroker.ToolBrokerImpl;
-import eu.scy.toolbrokerapi.ToolBrokerAPI;
 
 public abstract class AbstractProcessingAgent<K extends IMetadataKey> extends
 		AbstractAgent implements IProcessingAgent<K> {
 
 	protected boolean done;
-	private ToolBrokerAPI<K> toolBrokerAPI;
+	protected IRepository<IELO<K>, K> repository;
+	protected IMetadataTypeManager<K> metadataTypeManager;
+
+	// private ToolBrokerAPI<K> toolBrokerAPI;
 
 	@Override
 	public void run() {
@@ -22,25 +23,35 @@ public abstract class AbstractProcessingAgent<K extends IMetadataKey> extends
 	}
 
 	public AbstractProcessingAgent(String threadName) {
-		this(threadName, new ToolBrokerImpl<K>());
-	}
-
-	protected AbstractProcessingAgent(String threadName,
-			ToolBrokerAPI<K> toolBroker) {
-		toolBrokerAPI = toolBroker;
+		// this(threadName);, new ToolBrokerImpl<K>());
+		super();
 		done = false;
 		Thread t = new Thread(this, threadName);
 		t.start();
 	}
 
+	// protected AbstractProcessingAgent(String threadName,
+	// ToolBrokerAPI<K> toolBroker) {
+	// toolBrokerAPI = toolBroker;
+	// done = false;
+	// Thread t = new Thread(this, threadName);
+	// t.start();
+	// }
+
 	protected abstract void doRun();
 
-	public IRepository<IELO<K>, K> getRepository() {
-		return toolBrokerAPI.getRepository();
+	@Override
+	public void setMetadataTypeManager(IMetadataTypeManager<K> typeManager) {
+		metadataTypeManager = typeManager;
 	}
 
-	public IMetadataTypeManager<K> getMetadataTypeManager() {
-		return toolBrokerAPI.getMetaDataTypeManager();
+	@Override
+	public void setRepository(IRepository<IELO<K>, K> repo) {
+		repository = repo;
 	}
+
+	// public IMetadataTypeManager<K> getMetadataTypeManager() {
+	// return toolBrokerAPI.getMetaDataTypeManager();
+	// }
 
 }
--- trunk/agent-impl/src/main/java/eu/scy/agents/impl/elo/AbstractELOAgent.java
+++ trunk/agent-impl/src/main/java/eu/scy/agents/impl/elo/AbstractELOAgent.java
@@ -5,27 +5,19 @@ import roolo.elo.api.IMetadataKey;
 import roolo.elo.api.IMetadataTypeManager;
 import eu.scy.agents.api.elo.IELOFilterAgent;
 import eu.scy.agents.impl.AbstractAgent;
-import eu.scy.toolbroker.ToolBrokerImpl;
-import eu.scy.toolbrokerapi.ToolBrokerAPI;
 
 public abstract class AbstractELOAgent<T extends IELO<K>, K extends IMetadataKey>
 		extends AbstractAgent implements IELOFilterAgent<T, K> {
 
-	private ToolBrokerAPI<IMetadataKey> toolBroker;
-	private IMetadataTypeManager<IMetadataKey> typeManger;
+	protected IMetadataTypeManager<IMetadataKey> metadataTypeManager;
 
 	protected IMetadataTypeManager<IMetadataKey> getMetadataTypeManager() {
-		if (typeManger == null) {
-			if (toolBroker == null) {
-				toolBroker = new ToolBrokerImpl<IMetadataKey>();
-			}
-			typeManger = toolBroker.getMetaDataTypeManager();
-		}
-		return typeManger;
+		return metadataTypeManager;
 	}
 
-	protected void setToolBroker(ToolBrokerAPI<IMetadataKey> toolBroker) {
-		this.toolBroker = toolBroker;
+	public void setMetadataTypeManager(
+			IMetadataTypeManager<IMetadataKey> typeManager) {
+		metadataTypeManager = typeManager;
 	}
 
 }
