db69ca384c28d85b324fa7fd2d24085c58583451
hanbei@gmx.de <hanbei@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-04-03 11:30:24 +0000

    svn problems

---
+++ trunk/roolo-agents/src/main/java/eu/scy/agents/roolo/elo/misspelling/CheckMisspellingAgent.java
@@ -0,0 +1,81 @@
+package eu.scy.agents.roolo.elo.misspelling;
+
+import info.collide.sqlspaces.commons.Callback;
+import info.collide.sqlspaces.commons.Tuple;
+import info.collide.sqlspaces.commons.TupleSpaceException;
+import info.collide.sqlspaces.commons.Callback.Command;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import roolo.elo.api.IMetadataKey;
+
+import com.swabunga.spell.engine.SpellDictionary;
+import com.swabunga.spell.engine.SpellDictionaryHashMap;
+import com.swabunga.spell.event.SpellChecker;
+import com.swabunga.spell.event.StringWordTokenizer;
+
+import eu.scy.agents.impl.AbstractProcessingAgent;
+
+public class CheckMisspellingAgent<K extends IMetadataKey> extends
+		AbstractProcessingAgent<K> {
+
+	private SpellChecker spellChecker;
+
+	public CheckMisspellingAgent() {
+		super("CheckMisspellingAgent");
+		// First parse dictinary
+		InputStream inputStream = CheckMisspellingAgent.class
+				.getResourceAsStream("/eng_com.dic");// eng_com.dic
+		SpellDictionary dictionary;
+		try {
+			dictionary = new SpellDictionaryHashMap(new InputStreamReader(
+					inputStream));
+			spellChecker = new SpellChecker(dictionary);
+
+			// Register for notifications
+			getTupleSpace().eventRegister(
+					Command.WRITE,
+					new Tuple("misspellings", String.class, Long.class,
+							String.class), new Callback() {
+
+						@Override
+						public void call(Command command, int seq, Tuple after,
+								Tuple before) {
+							System.out.println(after);
+							String content = (String) after.getField(3)
+									.getValue();
+							content = content.replaceAll("<[^>]*>", "");
+							String uri = (String) after.getField(1).getValue();
+							checkMispelling(uri, content);
+						}
+					}, true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (TupleSpaceException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	protected void doRun() {
+		// FIXME
+		done = true;
+	}
+
+	private void checkMispelling(String uri, String content) {
+		int numOfErrors = spellChecker.checkSpelling(new StringWordTokenizer(
+				content));
+		if (numOfErrors > 0) {
+			try {
+				getTupleSpace().write(
+						new Tuple("misspellings", uri, System
+								.currentTimeMillis(), numOfErrors));
+			} catch (TupleSpaceException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ trunk/roolo-agents/src/main/java/eu/scy/agents/roolo/elo/misspelling/MisspellingProcessELO.java
@@ -0,0 +1,31 @@
+package eu.scy.agents.roolo.elo.misspelling;
+
+import info.collide.sqlspaces.client.TupleSpace;
+import info.collide.sqlspaces.commons.Tuple;
+import info.collide.sqlspaces.commons.TupleSpaceException;
+import roolo.elo.api.IContent;
+import roolo.elo.api.IELO;
+import roolo.elo.api.IMetadataKey;
+import eu.scy.agents.impl.elo.AbstractELOAgent;
+
+public class MisspellingProcessELO<T extends IELO<K>, K extends IMetadataKey>
+		extends AbstractELOAgent<T, K> {
+
+	@Override
+	public void processElo(T elo) {
+		if (elo == null) {
+			return;
+		}
+		try {
+			IContent content = elo.getContent();
+			if (content != null) {
+				TupleSpace ts = getTupleSpace();
+				ts.write(new Tuple("misspellings", elo.getUri().toString(),
+						System.currentTimeMillis(), content.getXmlString()));
+			}
+		} catch (TupleSpaceException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+}
