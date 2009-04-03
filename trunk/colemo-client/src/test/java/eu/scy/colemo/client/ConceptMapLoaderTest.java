cb36b237c5e89ae93c201ee0a08c8222d1e9ec62
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-04-03 10:22:33 +0000

    Loading xml

--- trunk/colemo-client/src/test/java/eu/scy/colemo/client/ConceptMapLoaderTest.java
+++ trunk/colemo-client/src/test/java/eu/scy/colemo/client/ConceptMapLoaderTest.java
@@ -5,8 +5,10 @@ import junit.framework.TestCase;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 import eu.scy.colemo.server.uml.UmlClass;
+import eu.scy.colemo.server.uml.UmlLink;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -22,38 +24,70 @@ public class ConceptMapLoaderTest extends TestCase {
 
     private String xmlContents;
     private Collection nodes = null;
+    private Collection links = null;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
 
         nodes = new LinkedList();
+        links = new LinkedList();
 
-        UmlClass umlClass = new UmlClass("Some Concept", "c", "Henrik");
-        umlClass.setX(200);
-        umlClass.setY(300);
+        UmlClass umlClassA = new UmlClass("Concept A", "c", "Henrik");
+        umlClassA.setX(200);
+        umlClassA.setY(300);
+        umlClassA.setId("1");
 
-        ConceptNode node = new ConceptNode(umlClass);
-        nodes.add(node);
+        UmlClass umlClassB = new UmlClass("Concept B", "c", "Henrik");
+        umlClassB.setX(200);
+        umlClassB.setY(300);
+        umlClassB.setId("2");
 
+        UmlLink link = new UmlLink(umlClassA.getId(), umlClassB.getId(), "Henrik");
+
+        
+        ConceptNode nodea = new ConceptNode(umlClassA);
+        ConceptNode nodeb = new ConceptNode(umlClassB);
+
+        nodes.add(nodea);
+        nodes.add(nodeb);
+
+        ConceptLink conceptLink = new ConceptLink(link);
+        links.add(conceptLink);
+
+        ConceptMapExporter.getDefaultInstance().initialize();
+        xmlContents = ConceptMapExporter.getDefaultInstance().createXML(nodes.iterator(), links.iterator());
     }
 
     private Iterator getNodeIterator() {
         return nodes.iterator();
     }
 
+    private Iterator getLinkIterator() {
+        return links.iterator();
+    }
+
     
 
-    public void testParseConcepts() {
-        /*Document xmldoc = ConceptMapExporter.getDefaultInstance().createXMLDocument();
-        Element root = ConceptMapExporter.getDefaultInstance().createRootElement(xmldoc);
-        xmldoc.appendChild(root);
-        ConceptMapExporter.getDefaultInstance().parseNodes(xmldoc,  root, getNodeIterator());
-        xmlContents = ConceptMapExporter.getDefaultInstance().createXML();
+    public void testCreateXMLContents() {
         assertNotNull(xmlContents);
-        */
-        assert(true);
+
     }
+
+
+    public void testParseConcepts() {
+        List concepts = ConceptMapLoader.getDefaultInstance().parseConcepts(xmlContents);
+        assertEquals(concepts.size(), nodes.size());
+    }
+
+    public void testParseLinks() {
+        List links = ConceptMapLoader.getDefaultInstance().parseLinks(xmlContents);
+        assertEquals(links.size(), this.links.size());
+    }
+
+
+
+
 
 
 }
