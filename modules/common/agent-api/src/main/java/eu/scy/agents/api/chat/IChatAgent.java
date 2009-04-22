f39eef1f6240c7105d26edf226fc64ea5d3eaae0
jakob.sikken <jakob.sikken@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-04-22 17:33:55 +0000

    deleted content of wrong positioned code

--- modules/common/agent-api/src/main/java/eu/scy/agents/api/chat/IChatAgent.java
+++
@@ -1,9 +0,0 @@
-package eu.scy.agents.api.chat;
-
-import eu.scy.agents.api.IAgent;
-
-public interface IChatAgent extends IAgent {
-    
-    public void doSomethingWithChatMessage(String message);
-    
-}
