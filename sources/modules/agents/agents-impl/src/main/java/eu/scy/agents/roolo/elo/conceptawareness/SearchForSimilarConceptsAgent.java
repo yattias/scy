706d71e8cc25ed07cf0e6c74838e7e5ab8e83feb
jakob.sikken <jakob.sikken@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-04-24 09:49:45 +0000

    Added the two agent modules and the updates pom files.

---
+++ sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/roolo/elo/conceptawareness/SearchForSimilarConceptsAgent.java
@@ -0,0 +1,174 @@
+package eu.scy.agents.roolo.elo.conceptawareness;
+
+import info.collide.sqlspaces.commons.Tuple;
+import info.collide.sqlspaces.commons.TupleSpaceException;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import roolo.api.IRepository;
+import roolo.api.search.IQuery;
+import roolo.api.search.ISearchResult;
+import roolo.cms.repository.mock.BasicMetadataQuery;
+import roolo.cms.repository.search.BasicSearchOperations;
+import roolo.elo.api.IELO;
+import roolo.elo.api.IMetadata;
+import roolo.elo.api.IMetadataKey;
+import roolo.elo.api.IMetadataValueContainer;
+import roolo.elo.api.metadata.RooloMetadataKeys;
+import roolo.elo.metadata.keys.Contribute;
+import eu.scy.agents.impl.AbstractProcessingAgent;
+import eu.scy.notification.Notification;
+import eu.scy.notification.NotificationSender;
+import eu.scy.notification.api.INotification;
+
+public class SearchForSimilarConceptsAgent<Key extends IMetadataKey> extends
+		AbstractProcessingAgent<Key> {
+
+	// private static final String USER = "similarEloAgent";
+	// private IMetadataTypeManager<Key> metadataTypeManager;
+	private HashMap<URI, Double> score;
+
+	// private IRepository<IELO<Key>, Key> repository;
+
+	// public SearchForSimilarConceptsAgent() {
+	// super("SearchForSimilarConceptsAgent");
+	// // metadataTypeManager = getMetadataTypeManager();
+	// score = new HashMap<URI, Double>();
+	// }
+
+	public SearchForSimilarConceptsAgent() {
+		super("SearchForSimilarConceptsAgent");
+		score = new HashMap<URI, Double>();
+	}
+
+	// SearchForSimilarConceptsAgent(ToolBrokerAPI<Key> toolBroker) {
+	// super("SearchForSimilarConceptsAgent", toolBroker);
+	// metadataTypeManager = getMetadataTypeManager();
+	// score = new HashMap<URI, Double>();
+	// }
+
+	@Override
+	protected void doRun() {
+		try {
+			Tuple tuple = getTupleSpace().waitToTake(
+					new Tuple("scymapper", Long.class, String.class));
+			System.err.println("################ " + tuple);
+
+			URI eloUri = new URI((String) tuple.getField(2).getValue());
+
+			IELO<Key> elo = repository.retrieveELO(eloUri);
+			IMetadata<Key> metadata = elo.getMetadata();
+
+			Set<URI> hits = searchForRelatedElos(eloUri, repository, metadata);
+
+			Set<String> relatedUsers = new HashSet<String>();
+			for (URI hitUri : hits) {
+				if (score.get(hitUri) < 0.6) {
+					continue;
+				}
+				IMetadata<Key> hitMetadata = repository
+						.retrieveMetadata(hitUri);
+				IMetadataValueContainer value = hitMetadata
+						.getMetadataValueContainer(metadataTypeManager
+								.getMetadataKey(RooloMetadataKeys.AUTHOR
+										.getId()));
+				Contribute contribution = (Contribute) value.getValue();
+				String userGUID = contribution.getVCard();
+				relatedUsers.add(userGUID);
+			}
+
+			StringBuffer relatedUserList = new StringBuffer();
+			for (String relatedUser : relatedUsers) {
+				relatedUserList.append(relatedUser);
+				relatedUserList.append(";");
+			}
+
+			Key authorKey = metadataTypeManager.getMetadataKey("author");
+			IMetadataValueContainer authorContainer = metadata
+					.getMetadataValueContainer(authorKey);
+			Contribute author = (Contribute) authorContainer.getValue();
+			String user = author.getVCard();
+
+			INotification notification = new Notification();
+			notification
+					.addProperty("users", relatedUserList.toString().trim());
+			notification.addProperty("target", "awareness");
+			notification.addProperty("eloUri", elo.getUri().toString());
+			NotificationSender sender = new NotificationSender();
+			sender.send(user, "searchSimilarElosAgent", notification);
+			System.err.println();
+		} catch (TupleSpaceException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private Set<URI> searchForRelatedElos(URI originalEloUri,
+			IRepository<IELO<Key>, Key> repository, IMetadata<Key> metadata) {
+
+		Set<URI> hits = new HashSet<URI>();
+
+		double maxScore = -1;
+
+		Key nodeLabelKey = metadataTypeManager.getMetadataKey("nodeLabel");
+		IMetadataValueContainer nodeValues = metadata
+				.getMetadataValueContainer(nodeLabelKey);
+		List<String> nodeLabels = (List<String>) nodeValues.getValueList();
+		for (String nodeLabel : nodeLabels) {
+			IQuery query = new BasicMetadataQuery<Key>(nodeLabelKey,
+					BasicSearchOperations.EQUALS, nodeLabel, null);
+			List<ISearchResult> results = repository.search(query);
+			for (ISearchResult result : results) {
+				hits.add(result.getUri());
+
+				Double oldScore = score.get(result.getUri());
+				if (oldScore == null) {
+					oldScore = 0.0;
+				}
+				score.put(result.getUri(), oldScore + 1);
+				if (maxScore < oldScore + 1.0) {
+					maxScore = oldScore + 1.0;
+				}
+			}
+		}
+
+		Key linkLabelKey = metadataTypeManager.getMetadataKey("linkLabel");
+		IMetadataValueContainer linkValues = metadata
+				.getMetadataValueContainer(linkLabelKey);
+		List<String> linkLabels = (List<String>) linkValues.getValueList();
+		for (String linkLabel : linkLabels) {
+			IQuery query = new BasicMetadataQuery<Key>(linkLabelKey,
+					BasicSearchOperations.EQUALS, linkLabel, null);
+			List<ISearchResult> results = repository.search(query);
+			for (ISearchResult result : results) {
+				hits.add(result.getUri());
+
+				Double oldScore = score.get(result.getUri());
+				if (oldScore == null) {
+					oldScore = 0.0;
+				}
+				score.put(result.getUri(), oldScore + 1);
+				if (maxScore < oldScore + 1.0) {
+					maxScore = oldScore + 1.0;
+				}
+			}
+		}
+
+		for (URI hit : score.keySet()) {
+			Double hitScore = score.get(hit);
+			hitScore = hitScore / maxScore;
+			score.put(hit, hitScore);
+		}
+
+		hits.remove(originalEloUri);
+		score.remove(originalEloUri);
+		return hits;
+	}
+}
---
+++ sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/roolo/elo/drawing/DrawingAnalyserAgent.java
@@ -0,0 +1,146 @@
+package eu.scy.agents.roolo.elo.drawing;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.jdom.Element;
+
+import roolo.elo.JDomStringConversion;
+import roolo.elo.api.IELO;
+import roolo.elo.api.IMetadata;
+import roolo.elo.api.IMetadataKey;
+import roolo.elo.api.IMetadataValueContainer;
+import eu.scy.agents.api.elo.IELOFilterAgent;
+import eu.scy.agents.impl.AbstractAgent;
+
+public class DrawingAnalyserAgent<T extends IELO<K>, K extends IMetadataKey>
+		extends AbstractAgent implements IELOFilterAgent<T, K> {
+
+	private static final String scyDrawType = "scy/drawing";
+	private static final String contentTagName = "content";
+	private JDomStringConversion jdomStringConversion = new JDomStringConversion();
+	private K formatKey;
+	private K lineCountKey;
+	private K rectangleCountKey;
+	private K ovalCountKey;
+	private K freehandCountKey;
+	private K textCountKey;
+	private K imageCountKey;
+	private K tagPointerCountKey;
+
+	private boolean isCompatibleType(T elo) {
+		return scyDrawType.equals(elo.getMetadata().getMetadataValueContainer(
+				formatKey).getValue());
+	}
+
+	private synchronized void analyseElo(T elo) {
+		String contentXml = elo.getContent().getXml();
+		Element drawRoot = jdomStringConversion.stringToXml(contentXml);
+		NameCounters nameCounters = new NameCounters();
+		scanElements(drawRoot, nameCounters);
+		placeNameCountersInMetadata(elo.getMetadata(), nameCounters);
+	}
+
+	private void scanElements(Element element, NameCounters nameCounters) {
+		@SuppressWarnings("unchecked")
+		List<Element> children = element.getChildren();
+		if (contentTagName.equals(element.getName())) {
+			if (children.size() > 0) {
+				String childTagName = children.get(0).getName();
+				nameCounters.increment(childTagName);
+			}
+		} else {
+			for (Element child : children) {
+				scanElements(child, nameCounters);
+			}
+		}
+	}
+
+	private void placeNameCountersInMetadata(IMetadata<K> metatdata,
+			NameCounters nameCounters) {
+		Map<String, AtomicInteger> counters = nameCounters.getCounters();
+		setCountMetadataValue(
+				metatdata.getMetadataValueContainer(lineCountKey), counters
+						.get(WhiteboardXmlNames.line));
+		setCountMetadataValue(metatdata
+				.getMetadataValueContainer(rectangleCountKey), counters
+				.get(WhiteboardXmlNames.rectangle));
+		setCountMetadataValue(
+				metatdata.getMetadataValueContainer(ovalCountKey), counters
+						.get(WhiteboardXmlNames.oval));
+		setCountMetadataValue(metatdata
+				.getMetadataValueContainer(freehandCountKey), counters
+				.get(WhiteboardXmlNames.freehand));
+		setCountMetadataValue(
+				metatdata.getMetadataValueContainer(textCountKey), counters
+						.get(WhiteboardXmlNames.text));
+		setCountMetadataValue(metatdata
+				.getMetadataValueContainer(imageCountKey), counters
+				.get(WhiteboardXmlNames.image));
+		setCountMetadataValue(metatdata
+				.getMetadataValueContainer(tagPointerCountKey), counters
+				.get(WhiteboardXmlNames.tagPointer));
+	}
+
+	private void setCountMetadataValue(IMetadataValueContainer valueContainer,
+			AtomicInteger value) {
+		int count = 0;
+		if (value != null)
+			count = value.get();
+		valueContainer.setValue(Long.valueOf(count));
+	}
+
+	// public void setTypeKey(K typeKey)
+	// {
+	// this.formatKey = typeKey;
+	// }
+	//
+	public void setJdomStringConversion(
+			JDomStringConversion jdomStringConversion) {
+		this.jdomStringConversion = jdomStringConversion;
+	}
+
+	public void setFormatKey(K formatKey) {
+		this.formatKey = formatKey;
+	}
+
+	public void setLineCountKey(K lineCountKey) {
+		this.lineCountKey = lineCountKey;
+	}
+
+	public void setRectangleCountKey(K rectangleCountKey) {
+		this.rectangleCountKey = rectangleCountKey;
+	}
+
+	public void setOvalCountKey(K ovalCountKey) {
+		this.ovalCountKey = ovalCountKey;
+	}
+
+	public void setFreehandCountKey(K freehandCountKey) {
+		this.freehandCountKey = freehandCountKey;
+	}
+
+	public void setTextCountKey(K textCountKey) {
+		this.textCountKey = textCountKey;
+	}
+
+	public void setImageCountKey(K imageCountKey) {
+		this.imageCountKey = imageCountKey;
+	}
+
+	public void setTagPointerCountKey(K tagPointerCountKey) {
+		this.tagPointerCountKey = tagPointerCountKey;
+	}
+
+	@Override
+	public void processElo(T elo) {
+		if (elo == null) {
+			return;
+		}
+		if (isCompatibleType(elo)) {
+			analyseElo(elo);
+		}
+	}
+
+}
