052785bbf66d2b655490ff7bf8ae87268a151868
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-05-04 12:20:55 +0000

    new impls

---
+++ sources/modules/common/awareness-impl/src/main/java/eu/scy/presence/event/PresenceStatusEvent.java
@@ -0,0 +1,28 @@
+package eu.scy.presence.event;
+
+import eu.scy.awareness.AwarenessServiceException;
+import eu.scy.presence.IPresenceStatusEvent;
+
+
+public class PresenceStatusEvent extends PresenceEvent implements IPresenceStatusEvent {
+
+    private String presence;
+    private String status;
+    
+    public PresenceStatusEvent(Object source, String user, String message, String presence, String status) {
+        super(source, user, message);
+        this.presence = presence;
+        this.status = status;
+    }
+
+    @Override
+    public String getPresence() {
+        return presence;
+    }
+
+    @Override
+    public String getStatus() {
+        return this.status;
+    }
+
+}
