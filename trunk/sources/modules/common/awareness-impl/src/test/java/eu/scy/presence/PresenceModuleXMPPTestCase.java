0763445b6305e36625192ba86fa8e5ea1f2af317
thomas.drevon <thomas.drevon@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-05-04 14:20:44 +0000

    writing a test that may some day pass

--- trunk/sources/modules/common/awareness-impl/src/test/java/eu/scy/presence/PresenceModuleXMPPTestCase.java
+++ trunk/sources/modules/common/awareness-impl/src/test/java/eu/scy/presence/PresenceModuleXMPPTestCase.java
@@ -1,6 +1,58 @@
 package eu.scy.presence;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+
+import com.sun.source.tree.AssertTree;
+
+import eu.scy.presence.IPresenceModule;
+import eu.scy.presence.PresenceModuleException;
+import eu.scy.presence.PresenceModuleFactory;
+import eu.scy.presence.impl.PresenceModuleXMPPImpl;
+
 
 public class PresenceModuleXMPPTestCase {
 
+    
+    private static final Logger logger = Logger.getLogger(PresenceModuleXMPPTestCase.class.getName());
+    IPresenceModule presenceModule = null;        
+    
+    public PresenceModuleXMPPTestCase() {
+    }
+    
+
+    @org.junit.Before
+    public void presenceModuleSetup() {
+        if (presenceModule == null) {
+            try {
+                presenceModule = PresenceModuleFactory.getPresenceModule(PresenceModuleFactory.XMPP_STYLE);
+                ((PresenceModuleXMPPImpl) presenceModule).createPresenceModule("presence_spider", "presence_spider");
+                //presenceModule.cr = PresenceModuleFactory.getPresenceModule(PresenceModuleFactory.MOCK_STYLE);
+            } catch (PresenceModuleException e) {
+                logger.error("presence noodle test case bummer");
+                e.printStackTrace();
+            }
+        }
+        assertNotNull(presenceModule);
+    }
+    
+    
+    @org.junit.Test
+    public void testGetGroups() {      
+        ArrayList<String> groups = null;
+        try {
+            //groups = (ArrayList<String>) presenceModule.getGroups("thomasd");
+            groups = (ArrayList<String>) presenceModule.getGroups();
+        } catch (PresenceModuleException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        assertNotNull(groups);
+        assertTrue(groups.size() > 0);
+    }
+    
+    
 }
