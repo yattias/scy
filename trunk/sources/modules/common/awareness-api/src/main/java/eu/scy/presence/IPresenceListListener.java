12f5047a04b14a620a8a202c5e74fec4aaea5839
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-05-04 12:19:11 +0000

    moved to its own

---
+++ trunk/sources/modules/common/awareness-api/src/main/java/eu/scy/presence/IPresenceListListener.java
@@ -0,0 +1,19 @@
+package eu.scy.presence;
+
+
+/**
+ * Fired when buddies are added or removed
+ * 
+ * @author anthonyp
+ *
+ */
+public interface IPresenceListListener  {
+    
+    /**
+     * handles list events
+     * 
+     * @param e
+     */
+    public void handleAwarenessListEvent(IPresenceListEvent e);
+
+}
