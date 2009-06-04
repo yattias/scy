bf75e97d718bf4a68ae4280985668148e2d853e7
larsbollen <larsbollen@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-06-04 13:23:34 +0000

    SCYDynamics can log to SQLSpaces

--- sources/modules/client/tools/scydynamics/src/main/java/eu/scy/client/tools/scydynamics/main/ModellingStandalone.java
+++ sources/modules/client/tools/scydynamics/src/main/java/eu/scy/client/tools/scydynamics/main/ModellingStandalone.java
@@ -1,8 +1,13 @@
 package eu.scy.client.tools.scydynamics.main;
+
 import java.awt.BorderLayout;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Date;
+import java.util.Properties;
 
 import javax.swing.*;
 import colab.um.JColab;
@@ -17,33 +22,40 @@ public class ModellingStandalone extends JFrame {
 	private static final long serialVersionUID = -3066655673856117223L;
 	private ModelEditor editor;
 
-	public ModellingStandalone(boolean log) {
-		super("Modelling standalone alpha");
+	public ModellingStandalone() {
+		super("SCYDynamics - standalone version");
 		this.addWindowListener(new WindowEventHandler());
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//new JTools(JColab.JCOLABAPP_RESOURCES, JColab.JCOLABSYS_RESOURCES);		
-		editor = new ModelEditor(log);
+		editor = new ModelEditor(getProperties());
 		this.getContentPane().setLayout(new BorderLayout());
-		
 		this.getContentPane().add(editor, BorderLayout.CENTER);
-		this.setSize(600,400);
+		this.setSize(600, 400);
 		this.pack();
-		this.setVisible(true);		
+		this.setVisible(true);
+	}
+
+	private Properties getProperties() {
+		File confFile = new File("scydynamics.properties");
+		Properties props = ModelEditor.getDefaultProperties();
+		try {
+			System.out.println("ModellingStandalone.getProperties(). expecting file at "+confFile.getAbsolutePath());
+			if (confFile.exists())
+				props.load(new FileInputStream(confFile));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return props;
 	}
 
 	public static void main(String[] args) {
-		if ((args.length > 0) && args[0].equals("log")) {
-			new ModellingStandalone(true);
-		} else {
-			new ModellingStandalone(false);
-		}
+		new ModellingStandalone();
 	}
-	
+
 	class WindowEventHandler extends WindowAdapter {
-		  public void windowClosing(WindowEvent evt) {
+		public void windowClosing(WindowEvent evt) {
 			editor.getActionLogger().logSimpleAction("exit_application");
 			editor.getActionLogger().close();
-		    System.exit(0);
-		  }
+			System.exit(0);
 		}
+	}
 }
