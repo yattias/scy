ab3f181bdb23152e0d111f01915194fd8610a98d
thomas.drevon <thomas.drevon@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-06-19 12:19:46 +0000

    redid the scyhub test case

--- trunk/sources/modules/server/scy-hub/src/main/java/eu/scy/scyhub/SCYHubComponent.java
+++ trunk/sources/modules/server/scy-hub/src/main/java/eu/scy/scyhub/SCYHubComponent.java
@@ -2,171 +2,180 @@ package eu.scy.scyhub;
 
 import java.util.Date;
 
+import org.apache.log4j.Logger;
+import org.jivesoftware.smack.packet.PacketExtension;
 import org.xmpp.component.Component;
 import org.xmpp.component.ComponentManager;
 import org.xmpp.component.ComponentManagerFactory;
 import org.xmpp.packet.JID;
 import org.xmpp.packet.Message;
 import org.xmpp.packet.Packet;
-import org.xmpp.packet.PacketExtension;
 
+import eu.scy.communications.message.ISyncMessage;
+import eu.scy.communications.message.impl.DataSyncPacketExtension;
 import eu.scy.datasync.api.DataSyncException;
 import eu.scy.datasync.api.IDataSyncModule;
-import eu.scy.datasync.api.ISyncMessage;
 import eu.scy.datasync.api.event.IDataSyncEvent;
 import eu.scy.datasync.api.event.IDataSyncListener;
 import eu.scy.datasync.api.session.IDataSyncSession;
-import eu.scy.datasync.extension.DataSyncPacketExtension;
 import eu.scy.datasync.impl.factory.DataSyncModuleFactory;
 
 /**
- * SCYHub is the central communicator, processes packets and routes them to the correct 
- * places
+ * SCYHub is the central communicator, processes packets and routes them to the correct server side module
  *
  * @author anthonjp
  */
 public class SCYHubComponent implements Component {
-
+    
+    private static final Logger logger = Logger.getLogger(SCYHubComponent.class.getName());
+    
     private IDataSyncModule dataSyncModule;
-	private IDataSyncSession dataSyncSession;
-
-	public String getName() {
+    private IDataSyncSession dataSyncSession;
+    
+    
+    public String getName() {
         return "SCY HUB";
     }
-
+    
     public String getDescription() {
         return "SCY Hub Component";
     }
-
-	/**
-	 * process the packet and route the it to the correct place 
-	 */
+    
+    /**
+     * process the packet and route the it to the correct place 
+     */
     public void processPacket(Packet packet) {
-        System.out.println("Received package:"+packet.toXML());
+        logger.debug("Received package:" + packet.toXML());
         // Only process Message packets
         if (packet instanceof Message) {
+            logger.debug("It's a Message");
             // Get the requested station to obtain it's weather information
-            Message message = (Message) packet;
+            Message message = (Message) packet;            
+            PacketExtension packetExtension = (PacketExtension) message.getExtension(DataSyncPacketExtension.ELEMENT_NAME, DataSyncPacketExtension.NAMESPACE);
             
-            DataSyncPacketExtension dataSyncPacketExtension = (DataSyncPacketExtension) message.getExtension(DataSyncPacketExtension.ELEMENT_NAME, DataSyncPacketExtension.NAMESPACE);
-            
-            if( dataSyncPacketExtension instanceof DataSyncPacketExtension ) {
-            	
-            	DataSyncPacketExtension dsp = (DataSyncPacketExtension)dataSyncPacketExtension;
-            	 try {
-                     // pass syncMessage to DataSyncModule for storing
-                     dataSyncModule.create(dsp.toPojo());
-                 } catch (DataSyncException e1) {
-                     e1.printStackTrace();
-                 }
-            	
+            if (packetExtension != null) {
+                logger.debug("It contains a DataSyncPacketExtension");
+                //found a datasync extension, yay!
+                DataSyncPacketExtension dspe = (DataSyncPacketExtension) packetExtension;
+                try {
+                    // pass syncMessage to DataSyncModule for storing
+                    dataSyncModule.create(dspe.toPojo());
+                } catch (DataSyncException e1) {
+                    e1.printStackTrace();
+                }                
+            } else {
+                logger.debug("It didn't contain a DataSyncPacketExtension");
             }
             
             
-//            ISyncMessage syncMessage = SyncMessage.createSyncMessage("12", "toolid", "bob", "something something", "EVENT", null, SyncMessage.DEFAULT_MESSAGE_EXPIRATION_TIME);
-//           
-//            DataSyncPacketExtension dsp = new DataSyncPacketExtension(syncMessage);
-//            
-//            Message newMessage = new Message();
-//            
-//            newMessage.setTo("scyhub.imediamac09.uio.no");
-//            newMessage.addExtension(dsp);
-//            
-//            try {
-//				ComponentManagerFactory.getComponentManager().sendPacket(this, newMessage);
-//			} catch (ComponentException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
+            //            ISyncMessage syncMessage = SyncMessage.createSyncMessage("12", "toolid", "bob", "something something", "EVENT", null, SyncMessage.DEFAULT_MESSAGE_EXPIRATION_TIME);
+            //           
+            //            DataSyncPacketExtension dsp = new DataSyncPacketExtension(syncMessage);
+            //            
+            //            Message newMessage = new Message();
+            //            
+            //            newMessage.setTo("scyhub.imediamac09.uio.no");
+            //            newMessage.addExtension(dsp);
+            //            
+            //            try {
+            //				ComponentManagerFactory.getComponentManager().sendPacket(this, newMessage);
+            //			} catch (ComponentException e) {
+            //				// TODO Auto-generated catch block
+            //				e.printStackTrace();
+            //			}
             
             // Send the request and get the weather information
-////            Metar metar = Weather.getMetar(station, 5000);
-//
-//            // Build the answer
-//            Message reply = new Message();
-//            reply.setTo(message.getFrom());
-//            reply.setFrom(message.getTo());
-//            reply.setType(message.getType());
-//            reply.setThread(message.getThread());
-//
-//            // Append the discovered information if something was found
-//            if (metar != null) {
-//                StringBuilder sb = new StringBuilder();
-//                sb.append("station id : " + metar.getStationID());
-//                sb.append("\rwind dir   : " + metar.getWindDirection() + " degrees");
-//                sb.append("\rwind speed : " + metar.getWindSpeedInMPH() + " mph, " +
-//                                                     metar.getWindSpeedInKnots() + " knots");
-//                if (!metar.getVisibilityLessThan()) {
-//                    sb.append("\rvisibility : " + metar.getVisibility() + " mile(s)");
-//                } else {
-//                    sb.append("\rvisibility : < " + metar.getVisibility() + " mile(s)");
-//                }
-//
-//                sb.append("\rpressure   : " + metar.getPressure() + " in Hg");
-//                sb.append("\rtemperaturePrecise: " +
-//                                   metar.getTemperaturePreciseInCelsius() + " C, " +
-//                                   metar.getTemperaturePreciseInFahrenheit() + " F");
-//                sb.append("\rtemperature: " +
-//                                   metar.getTemperatureInCelsius() + " C, " +
-//                                   metar.getTemperatureInFahrenheit() + " F");
-//                sb.append("\rtemperatureMostPrecise: " +
-//                                   metar.getTemperatureMostPreciseInCelsius() + " C, " +
-//                                   metar.getTemperatureMostPreciseInFahrenheit() + " F");
-//                reply.setBody(sb.toString());
-//            }
-//            else {
-//                // Answer that the requested station id does not exist
-//                reply.setBody("Unknown station ID");
-//            }
-//
-//            // Send the response to the sender of the request
-//            try {
-//                ComponentManagerFactory.getComponentManager().sendPacket(this, reply);
-//            } catch (ComponentException e) {
-//                ComponentManagerFactory.getComponentManager().getLog().error(e);
-//            }
+            ////            Metar metar = Weather.getMetar(station, 5000);
+            //
+            //            // Build the answer
+            //            Message reply = new Message();
+            //            reply.setTo(message.getFrom());
+            //            reply.setFrom(message.getTo());
+            //            reply.setType(message.getType());
+            //            reply.setThread(message.getThread());
+            //
+            //            // Append the discovered information if something was found
+            //            if (metar != null) {
+            //                StringBuilder sb = new StringBuilder();
+            //                sb.append("station id : " + metar.getStationID());
+            //                sb.append("\rwind dir   : " + metar.getWindDirection() + " degrees");
+            //                sb.append("\rwind speed : " + metar.getWindSpeedInMPH() + " mph, " +
+            //                                                     metar.getWindSpeedInKnots() + " knots");
+            //                if (!metar.getVisibilityLessThan()) {
+            //                    sb.append("\rvisibility : " + metar.getVisibility() + " mile(s)");
+            //                } else {
+            //                    sb.append("\rvisibility : < " + metar.getVisibility() + " mile(s)");
+            //                }
+            //
+            //                sb.append("\rpressure   : " + metar.getPressure() + " in Hg");
+            //                sb.append("\rtemperaturePrecise: " +
+            //                                   metar.getTemperaturePreciseInCelsius() + " C, " +
+            //                                   metar.getTemperaturePreciseInFahrenheit() + " F");
+            //                sb.append("\rtemperature: " +
+            //                                   metar.getTemperatureInCelsius() + " C, " +
+            //                                   metar.getTemperatureInFahrenheit() + " F");
+            //                sb.append("\rtemperatureMostPrecise: " +
+            //                                   metar.getTemperatureMostPreciseInCelsius() + " C, " +
+            //                                   metar.getTemperatureMostPreciseInFahrenheit() + " F");
+            //                reply.setBody(sb.toString());
+            //            }
+            //            else {
+            //                // Answer that the requested station id does not exist
+            //                reply.setBody("Unknown station ID");
+            //            }
+            //
+            //            // Send the response to the sender of the request
+            //            try {
+            //                ComponentManagerFactory.getComponentManager().sendPacket(this, reply);
+            //            } catch (ComponentException e) {
+            //                ComponentManagerFactory.getComponentManager().getLog().error(e);
+            //            }
         }
     }
-
+    
+    
     public void initialize(JID jid, ComponentManager componentManager) {
-    	System.out.println("SCYHubComponent.initialize()");
-    	initModules();
+        logger.debug("SCYHubComponent.initialize()");
+        initModules();
     }
-
+    
+    
     private void initModules() {
-		//data sync
-    	  try {
-              
-              dataSyncModule = DataSyncModuleFactory.getDataSyncModule(DataSyncModuleFactory.LOCAL_STYLE);
-              // add listner in order to get callbacks on stuff that's happening
-              dataSyncModule.addDataSyncListener(new IDataSyncListener(){
-			
-				@Override
-				public void handleDataSyncEvent(IDataSyncEvent e) {
-					// ComponentManagerFactory.getComponentManager().sendPacket(this, packet)
-				      ISyncMessage syncMessage = e.getSyncMessage();
-				      if (syncMessage.getFrom() != null) {            
-				            Date date = new java.util.Date(System.currentTimeMillis());
-				            java.sql.Timestamp ts = new java.sql.Timestamp(date.getTime());
-				            System.out.println("SCYHubComponent.initModules()");
-				      } 
-					
-				}
-			});
-              //create new session
-              String HARD_CODED_TOOL_NAME = "eu.scy.scyhub";
-              String HARD_CODED_USER_NAME = "thomasd";
-              dataSyncSession = dataSyncModule.createSession(HARD_CODED_TOOL_NAME, HARD_CODED_USER_NAME);
-          } catch (DataSyncException e) {
-              e.printStackTrace();
-          }        
-	}
-
-	public void start() {
-    	System.out.println("SCYHubComponent.start()");
+        //data sync
+        try {
+            dataSyncModule = DataSyncModuleFactory.getDataSyncModule(DataSyncModuleFactory.LOCAL_STYLE);
+            // add listner in order to get callbacks on stuff that's happening
+            dataSyncModule.addDataSyncListener(new IDataSyncListener(){
+                
+                @Override
+                public void handleDataSyncEvent(IDataSyncEvent event) {
+                    // ComponentManagerFactory.getComponentManager().sendPacket(this, packet)
+                    ISyncMessage syncMessage = event.getSyncMessage();
+                    if (syncMessage.getFrom() != null) {            
+                        Date date = new java.util.Date(System.currentTimeMillis());
+                        java.sql.Timestamp ts = new java.sql.Timestamp(date.getTime());
+                        logger.debug("SCYHubComponent.initModules()");
+                    } 
+                    
+                }
+            });
+            //create new session
+            String HARD_CODED_TOOL_NAME = "eu.scy.scyhub";
+            String HARD_CODED_USER_NAME = "thomasd";
+            dataSyncSession = dataSyncModule.createSession(HARD_CODED_TOOL_NAME, HARD_CODED_USER_NAME);
+        } catch (DataSyncException exeption) {
+            logger.error("Something was messed up during creation of dataSyncModule: ");
+            exeption.printStackTrace();
+        }        
     }
-
+    
+    
+    public void start() {
+        logger.debug("SCYHubComponent.start()");
+    }
+    
+    
     public void shutdown() {
-    	System.out.println("SCYHubComponent.shutdown()");
+        logger.debug("SCYHubComponent.shutdown()");
     }
 }
---
+++ trunk/sources/modules/server/scy-hub/src/test/java/eu/scy/SCYHubTestCase.java
@@ -0,0 +1,66 @@
+package eu.scy;
+
+import static org.junit.Assert.*;
+
+import org.apache.log4j.Logger;
+import org.jivesoftware.smack.packet.Message;
+import org.xmpp.packet.Packet;
+
+import eu.scy.communications.message.ISyncMessage;
+import eu.scy.communications.message.impl.DataSyncPacketExtension;
+import eu.scy.communications.message.impl.SyncMessage;
+import eu.scy.datasync.impl.SyncMessageTestCase;
+import eu.scy.scyhub.SCYHubComponent;
+
+
+public class SCYHubTestCase {
+
+	private final Logger logger = Logger.getLogger(SCYHubTestCase.class.getName());
+	
+	private final String TEST_CONTENT = "This is the content, but there isn't much.";
+	private final String TEST_EVENT = "HEY HEY HEY";
+	private final String TEST_TOOL_ID = "eu.scy.test." + SyncMessageTestCase.class.getName();
+	private final String TEST_TOOL_SESSION_ID = "1234567890";
+	private final String TEST_FROM = "passerby@wiki.intermedia.uio.no";
+	private final String TEST_PERSISTENCE_ID = "123";
+
+	
+	public SCYHubTestCase() {
+	}
+
+	
+	private ISyncMessage getTestSyncMessage() {
+		return SyncMessage.createSyncMessage(TEST_TOOL_SESSION_ID,
+				TEST_TOOL_ID, TEST_FROM, TEST_CONTENT, TEST_EVENT, TEST_PERSISTENCE_ID,
+				SyncMessage.DEFAULT_MESSAGE_EXPIRATION_TIME);
+	}
+	
+
+	private Message getMessage() {
+		// test convert from ScyMessage to xmpp message
+		ISyncMessage syncMessage = getTestSyncMessage();
+		assertNotNull(syncMessage);
+		
+		Message message = ((SyncMessage) syncMessage).convertToXMPPMessage();
+		message.setTo("to@to");
+		message.setFrom("from@from");
+		return message;
+	}
+	
+	
+//TODO: fix this test, we have a confliect between xmpp (serverside) and jive packets (
+//	@org.junit.Test
+//	public void testDataSyncMessageRouter(){
+//		Message jiveMessage = getMessage();
+//		assertNotNull(jiveMessage);
+//		SCYHubComponent scyHubComponent = new SCYHubComponent();
+//		scyHubComponent.initialize(null, null);
+//
+//		org.xmpp.packet.Message xmppMessage = new org.xmpp.packet.Message();
+//		xmppMessage.setTo(jiveMessage.getTo());
+//		xmppMessage.setFrom(jiveMessage.getFrom());
+//		xmppMessage.addExtension(jiveMessage.getExtension(DataSyncPacketExtension.NAMESPACE, DataSyncPacketExtension.ELEMENT_NAME));
+//		scyHubComponent.processPacket((Packet) xmppMessage);
+//	}
+
+}
