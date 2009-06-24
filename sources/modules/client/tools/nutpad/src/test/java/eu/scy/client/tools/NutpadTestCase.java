6a24b03078065798a48d633c82d5cacd9332bbec
thomas.drevon <thomas.drevon@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-06-24 13:15:56 +0000

    added a test

---
+++ sources/modules/client/tools/nutpad/src/test/java/eu/scy/client/tools/NutpadTestCase.java
@@ -0,0 +1,54 @@
+package eu.scy.client.tools;
+
+import roolo.elo.api.IMetadataKey;
+import eu.scy.communications.message.ISyncMessage;
+import eu.scy.communications.message.impl.SyncMessage;
+import eu.scy.communications.message.impl.SyncMessageTestCase;
+import eu.scy.datasync.CommunicationProperties;
+import eu.scy.datasync.api.DataSyncException;
+import eu.scy.datasync.api.IDataSyncModule;
+import eu.scy.datasync.api.session.IDataSyncSession;
+import eu.scy.datasync.client.IDataSyncService;
+import eu.scy.datasync.impl.factory.DataSyncModuleFactory;
+import eu.scy.toolbroker.ToolBrokerImpl;
+
+
+
+public class NutpadTestCase {
+
+    private static final String TEST_TOOL_SESSION_ID = "1234567890";
+    private static final String TEST_TOOL_ID = "eu.scy.test." + NutpadTestCase.class.getName();
+    private static final String TEST_FROM = "passerby@wiki.intermedia.uio.no";
+    private static final String TEST_CONTENT = "This is the content, but there isn't much.";
+    private static final String TEST_EVENT = "test event";
+    private static final String TEST_PERSISTENCE_ID = "1239999999";
+
+    private IDataSyncSession dataSyncSession;
+    private ToolBrokerImpl<IMetadataKey> tbi;
+    private IDataSyncService dataSyncService;
+    CommunicationProperties props;
+    
+    public NutpadTestCase() {        
+    }
+    
+    private void init() {
+        IDataSyncModule dataSyncModule = null;
+        try {
+            dataSyncModule = DataSyncModuleFactory.getDataSyncModule(DataSyncModuleFactory.LOCAL_STYLE);
+        } catch (DataSyncException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        dataSyncSession = dataSyncModule.createSession(TEST_TOOL_ID, TEST_FROM);
+        tbi = new ToolBrokerImpl<IMetadataKey>();
+        dataSyncService = tbi.getDataSyncService();
+        props = new CommunicationProperties();
+    }
+    
+    @org.junit.Test
+    public void testSendMessage() {
+        init();
+        ISyncMessage syncMessage = SyncMessage.createSyncMessage(TEST_TOOL_SESSION_ID, TEST_TOOL_ID, TEST_FROM, TEST_CONTENT, TEST_EVENT, null, SyncMessage.DEFAULT_MESSAGE_EXPIRATION_TIME);
+        dataSyncService.sendMessage((SyncMessage) syncMessage);
+    }
+}
