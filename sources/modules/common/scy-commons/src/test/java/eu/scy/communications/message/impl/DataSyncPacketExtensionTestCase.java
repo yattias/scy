61708eabede8e8cbe1ee780694820e92319ededf
thomas.drevon <thomas.drevon@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-06-24 13:18:45 +0000

    fixed packet conversion

--- sources/modules/common/scy-commons/src/test/java/eu/scy/communications/message/impl/DataSyncPacketExtensionTestCase.java
+++ sources/modules/common/scy-commons/src/test/java/eu/scy/communications/message/impl/DataSyncPacketExtensionTestCase.java
@@ -6,6 +6,7 @@ import junit.framework.JUnit4TestAdapter;
 import junit.framework.Test;
 
 import org.apache.log4j.Logger;
+import org.dom4j.Element;
 import org.hibernate.validator.AssertTrue;
 import org.jivesoftware.smack.packet.Message;
 
@@ -36,7 +37,7 @@ public class DataSyncPacketExtensionTestCase {
     
     
     @org.junit.Test
-    public void testDataSyncPacketExtensionToXml() {        
+    public void testDataSyncPacketExtensionToXml() {
         ISyncMessage syncMessage = getTestSyncMessage();
         assertNotNull(syncMessage);
         DataSyncPacketExtension dspe = new DataSyncPacketExtension(syncMessage);
@@ -45,6 +46,28 @@ public class DataSyncPacketExtensionTestCase {
         assertEquals(true, packetExtensionXml.contains(TEST_EVENT));
     }
     
+    
+    @org.junit.Test
+    public void testConvertFromXmppPacketExtension() {        
+        org.xmpp.packet.PacketExtension xmppPacketExtension = new org.xmpp.packet.PacketExtension(DataSyncPacketExtension.ELEMENT_NAME, DataSyncPacketExtension.NAMESPACE);
+        assertNotNull(xmppPacketExtension);
+
+        Element peRoot = xmppPacketExtension.getElement();
+        peRoot.addElement(DataSyncPacketExtension.TOOL_SESSION_ID).addText(TEST_TOOL_SESSION_ID);
+        peRoot.addElement(DataSyncPacketExtension.TOOL_ID).addText(TEST_TOOL_ID);
+        peRoot.addElement(DataSyncPacketExtension.FROM).addText(TEST_FROM);
+        peRoot.addElement(DataSyncPacketExtension.CONTENT).addText(TEST_CONTENT);
+        peRoot.addElement(DataSyncPacketExtension.EVENT).addText(TEST_EVENT);
+        peRoot.addElement(DataSyncPacketExtension.PERSISTENCE_ID).addText("");
+        peRoot.addElement(DataSyncPacketExtension.EXPIRATION).addText(String.valueOf(SyncMessage.DEFAULT_MESSAGE_EXPIRATION_TIME));
+        
+        assertNotNull(peRoot);
+        assertEquals(true, peRoot.asXML().contains(TEST_CONTENT));
+        DataSyncPacketExtension dspe = DataSyncPacketExtension.convertFromXmppPacketExtension(xmppPacketExtension);
+        assertNotNull(dspe);
+        assertEquals(true, dspe.toXML().contains(TEST_CONTENT));
+    }
+
     
     @org.junit.Test
     public void testDataSyncPacketExtensionToPojo() {        
