e5d40789b95840f9ae3f514f77a3df1e611cc468
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-07-03 08:35:19 +0000

    update to the data sync service

--- trunk/sources/modules/common/tool-broker-api/src/main/java/eu/scy/datasync/client/IDataSyncService.java
+++ trunk/sources/modules/common/tool-broker-api/src/main/java/eu/scy/datasync/client/IDataSyncService.java
@@ -1,6 +1,7 @@
 package eu.scy.datasync.client;
 
-import eu.scy.communications.message.impl.SyncMessage;
+import eu.scy.communications.datasync.event.IDataSyncListener;
+import eu.scy.communications.message.ISyncMessage;
 
 /**
  * Interface for the data sync service
@@ -10,6 +11,26 @@ import eu.scy.communications.message.impl.SyncMessage;
  */
 public interface IDataSyncService {
 
-    void sendMessage(SyncMessage syncMessage);
+   /**
+    * sends a sync message to the server
+    * 
+    * @param syncMessage
+    */
+   public void sendMessage(ISyncMessage syncMessage);
+
+   /**
+    * creates a new session
+    * 
+    * @param toolName
+    * @param userName
+    */
+   public void createSession(String toolName, String userName);
+
+   /**
+    * adds a data sync listener
+    * 
+    * @param iDataSyncListener
+    */
+   public void addDataSyncListener(IDataSyncListener iDataSyncListener);
 
 }
