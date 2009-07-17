c7722a8bb90d5c92e61a7b422b565827ccc3308e
st.weinbrenner@arcor.de <st.weinbrenner@arcor.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-07-17 11:37:21 +0000

    moved getTupleSpace() to the next lower level

--- trunk/sources/modules/common/agent-api/src/main/java/eu/scy/agents/api/IAgent.java
+++ trunk/sources/modules/common/agent-api/src/main/java/eu/scy/agents/api/IAgent.java
@@ -1,7 +1,5 @@
 package eu.scy.agents.api;
 
-import info.collide.sqlspaces.client.TupleSpace;
-
 /**
  * A common interface for all agent types.
  * 
@@ -9,22 +7,16 @@ import info.collide.sqlspaces.client.TupleSpace;
  */
 public interface IAgent {
 
-	/**
-	 * Get the name of the agent.
-	 */
-	public String getName();
+    /**
+     * Get the name of the agent.
+     */
+    public String getName();
 
-	/**
-	 * Get an instance of the tuplespace.
-	 * 
-	 * @return The global instance of the tuple space.
-	 */
-	public TupleSpace getTupleSpace();
+    /*
+     * Get an interface to the persistent storage for the agents.
+     * 
+     * @return The global persisent storage facility.
+     */
+    // public IPersistentStorage getPersistentStorage();
 
-	/*
-	 * Get an interface to the persistent storage for the agents.
-	 * 
-	 * @return The global persisent storage facility.
-	 */
-	// public IPersistentStorage getPersistentStorage();
 }
