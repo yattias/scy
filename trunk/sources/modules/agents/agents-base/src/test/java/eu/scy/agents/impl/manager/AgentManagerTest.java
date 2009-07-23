fdbd182d3cdbb3f0a09384b58f24fa22cca6b70d
janengler80 <janengler80@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-07-23 09:18:29 +0000

    improved tests new Tests: alive & identify

--- trunk/sources/modules/agents/agents-base/src/test/java/eu/scy/agents/impl/manager/AgentManagerTest.java
+++ trunk/sources/modules/agents/agents-base/src/test/java/eu/scy/agents/impl/manager/AgentManagerTest.java
@@ -1,62 +1,99 @@
 package eu.scy.agents.impl.manager;
 
 import static org.junit.Assert.assertTrue;
+import info.collide.sqlspaces.client.TupleSpace;
 import info.collide.sqlspaces.commons.Configuration;
+import info.collide.sqlspaces.commons.Field;
+import info.collide.sqlspaces.commons.Tuple;
+import info.collide.sqlspaces.commons.TupleSpaceException;
 import info.collide.sqlspaces.server.Server;
+
+import java.rmi.dgc.VMID;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import eu.scy.agents.api.AgentLifecycleException;
-import eu.scy.agents.impl.AbstractThreadedAgent;
+import eu.scy.agents.api.IThreadedAgent;
+import eu.scy.agents.impl.AgentProtocol;
 import eu.scy.agents.impl.ThreadedAgentMock;
 
 public class AgentManagerTest {
 
-	private static AbstractThreadedAgent mockAgent;
-	private static AgentManager agentManager;
+    private static AgentManager agentManager;
 
-	@BeforeClass
-	public static void setUp() {
-		if (!Server.isRunning()) {
-			Configuration.getConfiguration().setSSLEnabled(false);
-			Server.startServer();
-		}
+    private static final int TS_PORT = 2525;
 
-		agentManager = new AgentManager("localhost", 2525);
-	}
+    private static final String TS_HOST = "localhost";
 
-	@AfterClass
-	public static void tearDown() {
-		agentManager.dispose();
-		Server.stopServer();
-	}
+    private static final boolean standalone = false;
 
-	@Test
-	public void testStartStopAgent() throws InterruptedException, AgentLifecycleException {
-		String id=agentManager.startAgent(ThreadedAgentMock.NAME, null);
-		// agentManager.startAgent(ThreadedAgentMock.NAME);
-		Thread.sleep(5000);
+    @BeforeClass
+    public static void setUp() {
 
-		assertTrue("Agent not started", mockAgent.isRunning());
-		agentManager.stopAgent(ThreadedAgentMock.NAME, id);
+        if (!Server.isRunning() && standalone) {
+            Configuration.getConfiguration().setSSLEnabled(false);
+            Server.startServer();
+        }
 
-		Thread.sleep(1000);
+        agentManager = new AgentManager(TS_HOST, TS_PORT);
+    }
 
-		assertTrue("agent not stopped", !mockAgent.isRunning());
-	}
-	// @Test
-	// public void testSuspendResumeAgent() throws InterruptedException {
-	// agentManager.suspendAgent(ThreadedAgentMock.NAME);
-	//
-	// Thread.sleep(1000);
-	//
-	// assertTrue("Agent not suspended", mockAgent.isSuspended());
-	// agentManager.resumeAgent(ThreadedAgentMock.NAME);
-	//
-	// Thread.sleep(1000);
-	//
-	// assertTrue("agent not resumed", !mockAgent.isSuspended());
-	// }
+    @AfterClass
+    public static void tearDown() {
+        agentManager.cleanUp();
+        if (standalone) {
+            Server.stopServer();
+        }
+    }
+
+    @Test
+    public void testStartStopAgent() throws InterruptedException, AgentLifecycleException {
+        IThreadedAgent agent = agentManager.startAgent(ThreadedAgentMock.NAME, null);
+        // agentManager.startAgent(ThreadedAgentMock.NAME);
+        Thread.sleep(5000);
+
+        assertTrue("Agent not started", agent.isRunning());
+        agentManager.stopAgent(agent.getId());
+        System.out.println("AgentID(testStartStopAgent) : " + agent.getId());
+        Thread.sleep(1000);
+
+        assertTrue("agent not stopped", !agent.isRunning());
+    }
+
+    @Test
+    public void testIdentify() throws AgentLifecycleException, InterruptedException, TupleSpaceException {
+        IThreadedAgent agent = agentManager.startAgent(ThreadedAgentMock.NAME, null);
+        Thread.sleep(1000);
+        assertTrue("Agent not started", agent.isRunning());
+        TupleSpace ts = new TupleSpace(TS_HOST, TS_PORT, AgentProtocol.COMMAND_SPACE_NAME);
+        VMID vmid = new VMID();
+        String queryId = vmid.toString();
+        Tuple requestIdent = (AgentProtocol.getIdentifyTuple(agent.getId(), agent.getName(), vmid));
+        requestIdent.setExpiration(3*1000);
+        ts.write(requestIdent);
+        Tuple tuple = ts.waitToTake(new Tuple(AgentProtocol.RESPONSE, queryId, agent.getId(), agent.getName(), AgentProtocol.MESSAGE_IDENTIFY, Field.createWildCardField()));
+        assertTrue("No ident-tuple taken...", tuple != null);
+        agentManager.stopAgent(agent.getId());
+        System.out.println("AgentID(testIdentify) : " + agent.getId());
+        Thread.sleep(1000);
+        assertTrue("agent not stopped", !agent.isRunning());
+    }
+
+    @Test
+    public void testAlive() throws AgentLifecycleException, InterruptedException, TupleSpaceException {
+        IThreadedAgent agent = agentManager.startAgent(ThreadedAgentMock.NAME, null);
+        Thread.sleep(1000);
+        assertTrue("Agent not started", agent.isRunning());
+        TupleSpace ts = new TupleSpace(TS_HOST, TS_PORT, AgentProtocol.COMMAND_SPACE_NAME);
+        Tuple t = new Tuple(AgentProtocol.COMMAND_LINE, String.class, agent.getId(), agent.getName(), AgentProtocol.ALIVE);
+        Tuple aliveTuple = ts.read(t);
+        assertTrue("No alive-tuple taken...", aliveTuple != null);
+        agentManager.stopAgent(agent.getId());
+        System.out.println("AgentID(testAlive) : " + agent.getId());
+        Thread.sleep(1000);
+        assertTrue("agent not stopped", !agent.isRunning());
+    }
+
 }
