c64949f735d552f7beb1efb8319704292eece2cf
janengler80 <janengler80@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-07-23 13:15:40 +0000

    removed Sysos added two new tests

--- sources/modules/agents/agents-base/src/test/java/eu/scy/agents/impl/manager/AgentManagerTest.java
+++ sources/modules/agents/agents-base/src/test/java/eu/scy/agents/impl/manager/AgentManagerTest.java
@@ -14,6 +14,8 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.mchange.util.AssertException;
+
 import eu.scy.agents.api.AgentLifecycleException;
 import eu.scy.agents.api.IThreadedAgent;
 import eu.scy.agents.impl.AgentProtocol;
@@ -55,8 +57,7 @@ public class AgentManagerTest {
         Thread.sleep(5000);
 
         assertTrue("Agent not started", agent.isRunning());
-        agentManager.stopAgent(agent.getId());
-        System.out.println("AgentID(testStartStopAgent) : " + agent.getId());
+        agentManager.stopAgent(agent);
         Thread.sleep(1000);
 
         assertTrue("agent not stopped", !agent.isRunning());
@@ -71,12 +72,11 @@ public class AgentManagerTest {
         VMID vmid = new VMID();
         String queryId = vmid.toString();
         Tuple requestIdent = (AgentProtocol.getIdentifyTuple(agent.getId(), agent.getName(), vmid));
-        requestIdent.setExpiration(3*1000);
+        requestIdent.setExpiration(3 * 1000);
         ts.write(requestIdent);
         Tuple tuple = ts.waitToTake(new Tuple(AgentProtocol.RESPONSE, queryId, agent.getId(), agent.getName(), AgentProtocol.MESSAGE_IDENTIFY, Field.createWildCardField()));
         assertTrue("No ident-tuple taken...", tuple != null);
-        agentManager.stopAgent(agent.getId());
-        System.out.println("AgentID(testIdentify) : " + agent.getId());
+        agentManager.stopAgent(agent);
         Thread.sleep(1000);
         assertTrue("agent not stopped", !agent.isRunning());
     }
@@ -90,10 +90,57 @@ public class AgentManagerTest {
         Tuple t = new Tuple(AgentProtocol.COMMAND_LINE, String.class, agent.getId(), agent.getName(), AgentProtocol.ALIVE);
         Tuple aliveTuple = ts.read(t);
         assertTrue("No alive-tuple taken...", aliveTuple != null);
-        agentManager.stopAgent(agent.getId());
-        System.out.println("AgentID(testAlive) : " + agent.getId());
+        agentManager.stopAgent(agent);
         Thread.sleep(1000);
         assertTrue("agent not stopped", !agent.isRunning());
+    }
+
+    @Test
+    public void testKill() throws AgentLifecycleException, InterruptedException, TupleSpaceException {
+        IThreadedAgent agent = agentManager.startAgent(ThreadedAgentMock.NAME, null);
+        Thread.sleep(1000);
+        assertTrue("Agent not started", agent.isRunning());
+        agent.kill();
+        TupleSpace ts = new TupleSpace(TS_HOST, TS_PORT, AgentProtocol.COMMAND_SPACE_NAME);
+        Tuple t = new Tuple(AgentProtocol.COMMAND_LINE, String.class, agent.getId(), agent.getName(), AgentProtocol.ALIVE);
+        Thread.sleep(AgentProtocol.ALIVE_INTERVAL*2);
+        Tuple aliveTuple = ts.read(t);
+        assertTrue("Agent not killed", aliveTuple == null);
+
+    }
+    @Test
+    public void testAgentMaps() throws AgentLifecycleException, InterruptedException {
+        IThreadedAgent agent1 = agentManager.startAgent(ThreadedAgentMock.NAME, null);
+        assertTrue("Agent1 not started", agent1.isRunning());
+        IThreadedAgent agent2 = agentManager.startAgent(ThreadedAgentMock.NAME, null);
+        assertTrue("Agent2 not started", agent2.isRunning());
+        IThreadedAgent agent3 = agentManager.startAgent(ThreadedAgentMock.NAME, null);
+        assertTrue("Agent3 not started", agent3.isRunning());
+        IThreadedAgent agent4 = agentManager.startAgent(ThreadedAgentMock.NAME, null);
+        assertTrue("Agent4 not started", agent4.isRunning());
+        IThreadedAgent agent5 = agentManager.startAgent(ThreadedAgentMock.NAME, null);
+        assertTrue("Agent5 not started", agent5.isRunning());
+        IThreadedAgent agent6 = agentManager.startAgent(ThreadedAgentMock.NAME, null);
+        assertTrue("Agent6 not started", agent6.isRunning());
+        IThreadedAgent agent7 = agentManager.startAgent(ThreadedAgentMock.NAME, null);
+        assertTrue("Agent7 not started", agent7.isRunning());
+        assertTrue(7==agentManager.getAgentsIdMap().size());
+        Thread.sleep(100);
+        agentManager.stopAgent(agent1);
+        agentManager.stopAgent(agent2);
+        agentManager.stopAgent(agent3);
+        agentManager.stopAgent(agent4);
+        agentManager.stopAgent(agent5);
+        agentManager.stopAgent(agent6);
+        agentManager.stopAgent(agent7);
+        Thread.sleep(1000);
+        assertTrue(0==agentManager.getAgentsIdMap().size());
+        assertTrue(7==agentManager.getOldAgentsMap().size());
+        Thread.sleep(AgentProtocol.ALIVE_INTERVAL*2);
+        assertTrue(0==agentManager.getAgentsIdMap().size());
+        assertTrue(0==agentManager.getOldAgentsMap().size());
+        
+        
     }
 
 }
