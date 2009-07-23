f35fbbf8dba75eeb4c9ef4e28f417f9e0884a8b1
janengler80 <janengler80@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-07-23 14:04:35 +0000

    Added some comments

--- sources/modules/agents/agents-base/src/main/java/eu/scy/agents/impl/manager/AgentManager.java
+++ sources/modules/agents/agents-base/src/main/java/eu/scy/agents/impl/manager/AgentManager.java
@@ -4,23 +4,44 @@ import info.collide.sqlspaces.client.TupleSpace;
 import info.collide.sqlspaces.commons.Callback;
 import info.collide.sqlspaces.commons.Tuple;
 import info.collide.sqlspaces.commons.TupleSpaceException;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.rmi.dgc.VMID;
 import java.util.HashMap;
 import java.util.Map;
-
 import roolo.api.IRepository;
 import roolo.elo.api.IELO;
 import roolo.elo.api.IMetadataKey;
 import roolo.elo.api.IMetadataTypeManager;
-
 import eu.scy.agents.api.AgentLifecycleException;
 import eu.scy.agents.api.IRepositoryAgent;
 import eu.scy.agents.api.IThreadedAgent;
 import eu.scy.agents.impl.AgentProtocol;
 
+/**
+ * This class provides a facility to manage the agents inside SCY. 
+ * It is used to start/stop/kill single or multiple agents of the 
+ * same or different kind.
+ * 
+ * There are some conventions regarding the agents which can be
+ * managed using this manager: - All manageable agents must be an 
+ * implementation of {@link IThreadedAgent}.
+ * 
+ * - All manageable agents must implement a constructor like
+ *   MyAgent(Map<String, Object> map) This maps is usually used 
+ *   to set some properties. If you do not need properties, 
+ *   you can ignore the map exept for the entry "id" which contains 
+ *   the uniqueID (VMID as String) of the single instances. 
+ * - The whole inter-agent-communication is handled using
+ *   the SQLSpaces (http://weinbrenner.collide.info/sqlspaces-site/) 
+ *   and follows following convention:
+ *   ("query"|"response"|"agentCommand"), queryID [String], agentID [String], agentName [String], serviceName [String], ....(Payload)
+ * 
+ * @author Florian Schulz
+ * @author Jan Engler
+ * @author Stefan Weinbrenner
+ * 
+ */
 public class AgentManager implements Callback {
 
     private TupleSpace tupleSpace;
@@ -39,6 +60,19 @@ public class AgentManager implements Callback {
     // TODO: inject via spring magic
     private IMetadataTypeManager<IMetadataKey> manager;
 
+    /**
+     * This constructor is used to start an agent manager. 
+     * Please make sure that only one instance is running 
+     * on one SQLSpace. We intentionally used no Singleton Pattern
+     * or Lazy creation to provide the possibility to have 
+     * several managers running parallel, but you have to know 
+     * what you are doing.
+     * 
+     * @param host
+     *            The host of the SQLSpaces server (e.g. localhost)
+     * @param port
+     *            The port of the SQLSpaces server (e.g. 2525)
+     */
     public AgentManager(String host, int port) {
         agentAlive = new HashMap<String, Long>();
         agentIdMap = new HashMap<String, IThreadedAgent>();
@@ -63,6 +97,9 @@ public class AgentManager implements Callback {
         Runtime.getRuntime().addShutdownHook(new Thread(r));
     }
 
+    /*
+     * This method kills all agents and tidy up the hole manager
+     */
     protected void cleanUp() {
         for (IThreadedAgent agent : agentIdMap.values()) {
             try {
@@ -77,19 +114,32 @@ public class AgentManager implements Callback {
             } catch (AgentLifecycleException e) {
                 e.printStackTrace();
             }
-    
+
         }
         agentIdMap.clear();
         oldAgents.clear();
-        //TODO Exception?
-//        try {
-//            tupleSpace.disconnect();
-//        } catch (TupleSpaceException e) {
-//            e.printStackTrace();
-//        }
+        startParameters.clear();
+        // TODO Exception?
+        // try {
+        // tupleSpace.disconnect();
+        // } catch (TupleSpaceException e) {
+        // e.printStackTrace();
+        // }
 
     }
 
+    /**
+     * This methods sends a stop-Tuple to the passed {@link IThreadedAgent}.
+     * The agent itself have to make sure that it is stopped correctly.
+     * 
+     * After sent the tuple, the agent is taken from the list of actual
+     * agents ({@code agentIdMap} and a reference is stored in the {@code oldAgents}-Map
+     * This is to make sure that if an alive-tuple with the id of an 
+     * already killed agent is received (via callback) this agent can be
+     * referenced and killed the "hard way" ({@code Thread.stop()})
+     * @param agent The agent to send the stop-tuple
+     * @return The name of the agent you have forced to stop.
+     */
     public String stopAgent(IThreadedAgent agent) {
         try {
             tupleSpace.write(AgentProtocol.getStopTuple(agent.getId(), agent.getName(), new VMID()));
@@ -100,17 +150,43 @@ public class AgentManager implements Callback {
             throw new RuntimeException(e);
         }
     }
-
+    /**
+     * This methods sends a stop-Tuple to the passed agentId({@link VMID} as String).
+     * The agent itself have to make sure that it is stopped correctly.
+     * 
+     * After sent the tuple, the agent is taken from the list of actual
+     * agents ({@code agentIdMap} and a reference is stored in the {@code oldAgents}-Map
+     * This is to make sure that if an alive-tuple with the id of an 
+     * already killed agent is received (via callback) this agent can be
+     * referenced and killed the "hard way" ({@code Thread.stop()})
+     * @param agentId The agent's id to send the stop-tuple
+     * @return The name of the agent you have forced to stop.
+     */
     public String stopAgent(String agentId) {
-	return stopAgent(agentIdMap.get(agentId));
+        return stopAgent(agentIdMap.get(agentId));
     }
-  
 
+    /**
+     * This method is used to start an agent using this manager.
+     * To start this {@link IThreadedAgent} you have to pass the
+     * (fully qualified) name (e.g. eu.scy.agents.impl.ThreadedAgentMock) 
+     * which can be accessed using the classpath. You can pass a {@link Map}
+     * with properties to the agent. If you pass {@code null} instead, a new
+     * HashMap with only the instance ID of this agent will be created.
+     * 
+     * @param name The (fully qualified, classpath accessible) Name of the new Agent
+     * @param params A {@link Map} with properties. Can be {@code null}
+     * @return The new (just started) {@link IThreadedAgent}
+     * @throws AgentLifecycleException If something went wrong during the creation
+     *         of the new agent (e.g. in the process of reflection) this
+     *         Exception is thrown
+     */
     public IThreadedAgent startAgent(String name, Map<String, Object> params) throws AgentLifecycleException {
         try {
             Class<?> c = Class.forName(name);
             Constructor<?> con = c.getConstructor(Map.class);
             String agentId = new VMID().toString();
+            //if the params are null a new HashMap have to be created
             if (params == null) {
                 params = new HashMap<String, Object>();
             }
@@ -118,11 +194,15 @@ public class AgentManager implements Callback {
             IThreadedAgent agent = (IThreadedAgent) con.newInstance(params);
             agentIdMap.put(agentId, agent);
             startParameters.put(agentId, params);
+            //There are special kinds of agents which are treated from here
+            //For instance a IRepositoryAgent needs references to a
+            //repository and a MetaDataTypeManager
             if (agent instanceof IRepositoryAgent) {
                 IRepositoryAgent ira = (IRepositoryAgent) agent;
                 ira.setMetadataTypeManager(manager);
                 ira.setRepository(repository);
             }
+            //The agent is started
             agent.start();
             return agent;
         } catch (ClassNotFoundException e) {
@@ -141,7 +221,14 @@ public class AgentManager implements Callback {
             throw new AgentLifecycleException("Error during instantiation of agent " + name + "! Could not be started!", e);
         }
     }
-
+   /**
+    * This Method uses the "hard-way" to stop an agent.
+    * If an agent is still alive although it has been forced to stop
+    * the Thread in which this agent lives will be stopped by Thread.stop()
+    * @param id The uniqueID ({@link VMID} as String) of the agent
+    * @throws AgentLifecycleException If something went wrong during the killing
+    *         of this agent this exception is thrown.
+    */
     public void killAgent(String id) throws AgentLifecycleException {
         IThreadedAgent agentToKill = agentIdMap.get(id);
         agentToKill.kill();
@@ -169,9 +256,10 @@ public class AgentManager implements Callback {
                 String agentId = (String) beforeTuple.getField(2).getValue();
                 if (oldAgents.containsKey(agentId)) {
                     oldAgents.remove(agentId);
+                    startParameters.remove(agentId);
                 } else {
                     try {
-                    restartAgent(agentId);
+                        restartAgent(agentId);
                     } catch (AgentLifecycleException e) {
                         e.printStackTrace();
                     }
@@ -179,18 +267,35 @@ public class AgentManager implements Callback {
             }
         }
     }
-
+    /*
+     * This Method is used internally to restart an agent which should be
+     * alive but isn't.
+     * We use the kill method to stop this agent the "hard way" to make
+     * sure that (a potential) zomie-instance is shut down.
+     * After that we start an agent using his parameter.
+     */
     private void restartAgent(String agentId) throws AgentLifecycleException {
         IThreadedAgent agentToRestart = agentIdMap.get(agentId);
         agentToRestart.kill();
         Map<String, Object> params = startParameters.get(agentId);
         startAgent(agentId, params);
     }
-
-    public Map<String, IThreadedAgent> getOldAgentsMap(){
+    /**
+     * This method is just for testing purposes.
+     * In a later version this methods will be deleted.
+     * @return A {@link Map} with the old agents in it.
+     */
+    @Deprecated
+    public Map<String, IThreadedAgent> getOldAgentsMap() {
         return oldAgents;
     }
-    public Map<String, IThreadedAgent> getAgentsIdMap(){
+    /**
+     * This method is just for testing purposes.
+     * In a later version this methods will be deleted.
+     * @return A {@link Map} with the actual agents in it.
+     */
+    @Deprecated
+    public Map<String, IThreadedAgent> getAgentsIdMap() {
         return agentIdMap;
     }
 
