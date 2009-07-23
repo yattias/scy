1ffe49a582e345939345bf281c293d384e20a71c
janengler80 <janengler80@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-07-23 09:14:01 +0000

    Exchanged stop()-method by kill()-method. this method is used to shutdown an agent by using the Thread.stop()-method (the hard way)

--- trunk/sources/modules/common/agent-api/src/main/java/eu/scy/agents/api/IThreadedAgent.java
+++ trunk/sources/modules/common/agent-api/src/main/java/eu/scy/agents/api/IThreadedAgent.java
@@ -2,12 +2,12 @@ package eu.scy.agents.api;
 
 public interface IThreadedAgent extends IAgent, Runnable {
 
-	public void start() throws AgentLifecycleException;
+    public void start() throws AgentLifecycleException;
 
-	public void stop() throws AgentLifecycleException;
+    public void kill() throws AgentLifecycleException;
 
-	public boolean isRunning();
+    public boolean isRunning();
 
-	public boolean isStopped();
+    public boolean isStopped();
 
 }
