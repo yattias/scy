b5bc74c3cd81989ec9c10f26e42c26b5c9aa91e6
janengler80 <janengler80@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-07-24 09:20:02 +0000

    uncommented interesting stuff

--- trunk/sources/modules/agents/agents-base/src/test/java/eu/scy/agents/impl/ThreadedAgentMock.java
+++ trunk/sources/modules/agents/agents-base/src/test/java/eu/scy/agents/impl/ThreadedAgentMock.java
@@ -3,74 +3,77 @@
  */
 package eu.scy.agents.impl;
 
-import java.util.Map;
-
 import info.collide.sqlspaces.commons.Tuple;
 import info.collide.sqlspaces.commons.TupleSpaceException;
 
+import java.util.Map;
+
 public class ThreadedAgentMock extends AbstractThreadedAgent {
 
-	private boolean first = true;
-	private boolean updated = false;
+    private boolean first = true;
 
-	public boolean isFirst() {
-		return first;
-	}
+    private boolean updated = false;
+    
+    public boolean isFirst() {
+        return first;
+    }
 
-	public void setFirst(boolean first) {
-		this.first = first;
-	}
+    public void setFirst(boolean first) {
+        this.first = first;
+    }
 
-	public boolean isUpdated() {
-		return updated;
-	}
+    public boolean isUpdated() {
+        return updated;
+    }
 
-	public void setUpdated(boolean updated) {
-		this.updated = updated;
-	}
+    public void setUpdated(boolean updated) {
+        this.updated = updated;
+    }
 
-	public static final String NAME = "eu.scy.agents.impl.ThreadedAgentMock";
-	private int runCount;
+    public static final String NAME = "eu.scy.agents.impl.ThreadedAgentMock";
 
-	public ThreadedAgentMock(@SuppressWarnings("unused") Map<String, Object> map) {
-		super(NAME, (String) map.get("id"));
-		runCount = 0;
-	}
+    private int runCount;
 
-	@Override
-	public void doRun() throws TupleSpaceException {
-		while (status == Status.Running) {
-			sendAliveUpdate();
-			try {
+    public ThreadedAgentMock(Map<String, Object> map) {
+        super(NAME, (String) map.get("id"));
+        runCount = 0;
+    }
+
+    @Override
+    public void doRun() throws TupleSpaceException {
+        while (status == Status.Running) {
+            
+            sendAliveUpdate();
+            try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            // Tuple triggerTuple = getTupleSpace().waitToTake(
-            // new Tuple(NAME, String.class, Long.class), 5000);
-            // // TODO: interpret tuple ...
-            // runCount++;
-		}
-	}
+            @SuppressWarnings("unused")
+            Tuple triggerTuple = getTupleSpace().waitToTake(new Tuple(NAME, String.class, Long.class), 5000);
+            // TODO: interpret tuple ...
+            runCount++;
+        }
+    }
 
-	public int getRunCount() {
-		return runCount;
-	}
+    public int getRunCount() {
+        return runCount;
+    }
 
-	@Override
-	protected void doStop() {
+    @Override
+    protected void doStop() {
 		// do nothing
-	}
+    }
 
-	// TODO think about it
-	@Override
-	public boolean isStopped() {
-		return status == Status.Stopping;
-	}
+    // TODO think about it
+    @Override
+    public boolean isStopped() {
+        return status == Status.Stopping;
+    }
 
     @Override
     protected Tuple getIdentifyTuple(String queryId) {
-        Tuple t = new Tuple(AgentProtocol.RESPONSE, queryId,this.getId(),this.getName(),AgentProtocol.MESSAGE_IDENTIFY, "This is just a test");
+        Tuple t = new Tuple(AgentProtocol.RESPONSE, queryId, this.getId(), this.getName(), AgentProtocol.MESSAGE_IDENTIFY, "This is just a test");
         return t;
     }
 
