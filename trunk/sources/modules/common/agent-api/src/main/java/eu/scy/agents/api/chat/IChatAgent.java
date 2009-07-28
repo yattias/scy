51a4df149af7e5965c54655c9416ec3659daa93b
hanbei@gmx.de <hanbei@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-07-28 13:57:17 +0000

    more javadoc for the agents

--- trunk/sources/modules/common/agent-api/src/main/java/eu/scy/agents/api/chat/IChatAgent.java
+++ trunk/sources/modules/common/agent-api/src/main/java/eu/scy/agents/api/chat/IChatAgent.java
@@ -2,8 +2,14 @@ package eu.scy.agents.api.chat;
 
 import eu.scy.agents.api.IAgent;
 
+/**
+ * Processes chat messages. TODO define more.
+ * 
+ * @author Florian Schulz
+ * 
+ */
 public interface IChatAgent extends IAgent {
-    
-    public void doSomethingWithChatMessage(String message);
-    
+
+	public void doSomethingWithChatMessage(String message);
+
 }
