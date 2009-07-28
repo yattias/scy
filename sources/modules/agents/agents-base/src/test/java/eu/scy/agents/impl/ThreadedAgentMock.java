77cbaa2b62f670341ef0a978aa0111a9d476e94c
janengler80 <janengler80@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-07-28 13:15:52 +0000

    corrected name (to fully qualified) identify action

--- sources/modules/agents/agents-base/src/test/java/eu/scy/agents/impl/ThreadedAgentMock.java
+++ sources/modules/agents/agents-base/src/test/java/eu/scy/agents/impl/ThreadedAgentMock.java
@@ -10,29 +10,13 @@ import java.util.Map;
 
 public class ThreadedAgentMock extends AbstractThreadedAgent {
 
-    private boolean first = true;
-
-    private boolean updated = false;
-    
-    public boolean isFirst() {
-        return first;
-    }
-
-    public void setFirst(boolean first) {
-        this.first = first;
-    }
-
-    public boolean isUpdated() {
-        return updated;
-    }
-
-    public void setUpdated(boolean updated) {
-        this.updated = updated;
-    }
-
     public static final String NAME = "eu.scy.agents.impl.ThreadedAgentMock";
 
+    private boolean first = true;
+
     private int runCount;
+
+    private boolean updated = false;
 
     public ThreadedAgentMock(Map<String, Object> map) {
         super(NAME, (String) map.get("id"));
@@ -42,27 +26,37 @@ public class ThreadedAgentMock extends AbstractThreadedAgent {
     @Override
     public void doRun() throws TupleSpaceException {
         while (status == Status.Running) {
-            
+
             sendAliveUpdate();
             try {
-                Thread.sleep(1000);
+                Thread.sleep(5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            @SuppressWarnings("unused")
-            Tuple triggerTuple = getTupleSpace().waitToTake(new Tuple(NAME, String.class, Long.class), 5000);
+            // @SuppressWarnings("unused")
+            // Tuple triggerTuple = getTupleSpace().waitToTake(new Tuple(NAME, String.class, Long.class), 5000);
             // TODO: interpret tuple ...
-            runCount++;
+            // runCount++;
         }
+    }
+
+    @Override
+    protected void doStop() {
+        //Do nothing
+    }
+
+    @Override
+    protected Tuple getIdentifyTuple(String queryId) {
+        Tuple t = new Tuple(AgentProtocol.RESPONSE, queryId, this.getId(), this.getName(), AgentProtocol.MESSAGE_IDENTIFY, "This is just a test");
+        return t;
     }
 
     public int getRunCount() {
         return runCount;
     }
 
-    @Override
-    protected void doStop() {
-		// do nothing
+    public boolean isFirst() {
+        return first;
     }
 
     // TODO think about it
@@ -71,10 +65,16 @@ public class ThreadedAgentMock extends AbstractThreadedAgent {
         return status == Status.Stopping;
     }
 
-    @Override
-    protected Tuple getIdentifyTuple(String queryId) {
-        Tuple t = new Tuple(AgentProtocol.RESPONSE, queryId, this.getId(), this.getName(), AgentProtocol.MESSAGE_IDENTIFY, "This is just a test");
-        return t;
+    public boolean isUpdated() {
+        return updated;
+    }
+
+    public void setFirst(boolean first) {
+        this.first = first;
+    }
+
+    public void setUpdated(boolean updated) {
+        this.updated = updated;
     }
 
 }
