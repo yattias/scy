3a6507076a6cfd6a6f3078fb06eafb9322122a9b
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-08-14 13:22:06 +0000

    ActionLogging - the hibernate way

---
+++ trunk/sources/modules/server/action-logging-hibernate/src/main/java/eu/scy/actionlogging/ActionLoggerHibernate.java
@@ -0,0 +1,18 @@
+package eu.scy.actionlogging;
+
+import eu.scy.actionlogging.api.IActionLogger;
+import eu.scy.actionlogging.api.IAction;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 14.aug.2009
+ * Time: 10:52:17
+ * To change this template use File | Settings | File Templates.
+ */
+public class ActionLoggerHibernate implements IActionLogger {
+    @Override
+    public void log(String username, String source, IAction action) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
