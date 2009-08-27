f0af8d26e8acae09d9bb8d93d7fc3ba052f2e467
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-08-27 10:24:03 +0000

    SCYPlanner - new model interfaces

---
+++ trunk/sources/modules/client/tools/scy-planner/src/main/java/eu/scy/scyplanner/model/IPedagogicalPlan.java
@@ -0,0 +1,11 @@
+package eu.scy.scyplanner.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 27.aug.2009
+ * Time: 12:11:25
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IPedagogicalPlan {
+}
