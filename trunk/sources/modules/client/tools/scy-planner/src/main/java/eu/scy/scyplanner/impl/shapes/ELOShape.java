0d7aa609b08fa36efbf0700356c911c6ed861cb8
larschla <larschla@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-09-03 11:37:07 +0000

    Added ELO and LAS to diagram

--- trunk/sources/modules/client/tools/scy-planner/src/main/java/eu/scy/scyplanner/impl/shapes/ELOShape.java
+++ trunk/sources/modules/client/tools/scy-planner/src/main/java/eu/scy/scyplanner/impl/shapes/ELOShape.java
@@ -4,6 +4,8 @@ import eu.scy.scymapper.api.shapes.INodeShape;
 
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.AffineTransform;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,17 +24,20 @@ public class ELOShape extends INodeShape {
         double x = center.x + (bounds.width-2)/2 * Math.cos(angle);
         double y = center.y + (bounds.height-2)/2 * Math.sin(angle);
         return new Point((int)x,(int)y);
+        
     }
 
     @Override
     public void paint(Graphics g, Rectangle bounds) {
         Graphics2D g2 = (Graphics2D) g.create();
         switch (getMode()) {
-            case DRAW:
-                g2.draw(new Ellipse2D.Float(0, 0, bounds.width - 1, bounds.height - 1));
+            case DRAW:                
+                g2.transform(AffineTransform.getRotateInstance((-Math.PI/2)/2,bounds.width/2,bounds.height/2));
+                g2.draw(new Rectangle2D.Float(0, 0, bounds.width - 1, bounds.height -1));
                 break;
             case FILL:
-                g2.fill(new Ellipse2D.Float(0, 0, bounds.width - 1, bounds.height - 1));
+                g2.transform(AffineTransform.getRotateInstance((-Math.PI/2)/2,bounds.width/2,bounds.height/2));
+                g2.fill(new Rectangle2D.Float(0, 0, bounds.width - 1, bounds.height -1));
         }
     }
 }
