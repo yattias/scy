f7e94ad4af83830152766bf132cb7579ccbefabb
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-09-10 09:33:51 +0000

    Created  initial interfaces for pedagogical plan

---
+++ trunk/sources/modules/common/scy-model-api/src/main/java/eu/scy/core/model/pedagogicalplan/LearningActivitySpace.java
@@ -0,0 +1,15 @@
+package eu.scy.core.model.pedagogicalplan;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 10.sep.2009
+ * Time: 11:20:42
+ * To change this template use File | Settings | File Templates.
+ */
+public interface LearningActivitySpace extends BaseObject{
+
+    public String getDescription();
+    public void setDescription(String description); 
+
+}
