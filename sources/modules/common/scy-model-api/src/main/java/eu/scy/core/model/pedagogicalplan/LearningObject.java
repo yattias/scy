410751f81ca048d0221dbc7b2604728017c20258
henrik <henrik@enovate.no>
2009-09-10 09:33:51 +0000

    Created  initial interfaces for pedagogical plan

---
+++ sources/modules/common/scy-model-api/src/main/java/eu/scy/core/model/pedagogicalplan/LearningObject.java
@@ -0,0 +1,11 @@
+package eu.scy.core.model.pedagogicalplan;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 10.sep.2009
+ * Time: 11:25:42
+ * To change this template use File | Settings | File Templates.
+ */
+public interface LearningObject {
+}
