4f0922dad7890a705d8e3236356f0c516b168511
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-09-10 11:02:16 +0000

    Refined hierarchy

---
+++ trunk/sources/modules/common/scy-model-api/src/main/java/eu/scy/core/model/pedagogicalplan/Tool.java
@@ -0,0 +1,11 @@
+package eu.scy.core.model.pedagogicalplan;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 10.sep.2009
+ * Time: 12:42:57
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Tool extends BaseObject{
+}
