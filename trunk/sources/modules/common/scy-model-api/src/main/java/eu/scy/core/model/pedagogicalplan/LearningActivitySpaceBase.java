fe96f52b2d70e071c42fbdb30fa9f15615e6127e
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-09-11 09:36:32 +0000

    LASB extends BaseObject

--- trunk/sources/modules/common/scy-model-api/src/main/java/eu/scy/core/model/pedagogicalplan/LearningActivitySpaceBase.java
+++ trunk/sources/modules/common/scy-model-api/src/main/java/eu/scy/core/model/pedagogicalplan/LearningActivitySpaceBase.java
@@ -7,7 +7,7 @@ package eu.scy.core.model.pedagogicalplan;
  * Time: 11:05:16
  * To change this template use File | Settings | File Templates.
  */
-public interface LearningActivitySpaceBase {
+public interface LearningActivitySpaceBase extends BaseObject {
 
     public String getDescription();
     public void setDescription(String description);
