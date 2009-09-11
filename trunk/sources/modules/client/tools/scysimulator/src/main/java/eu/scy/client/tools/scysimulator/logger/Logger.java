f64947414b94e440a949694b09e3e924aa552f19
larsbollen <larsbollen@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-09-11 09:54:52 +0000

    adapted SCYSimulator to new logging api

--- trunk/sources/modules/client/tools/scysimulator/src/main/java/eu/scy/client/tools/scysimulator/logger/Logger.java
+++ trunk/sources/modules/client/tools/scysimulator/src/main/java/eu/scy/client/tools/scysimulator/logger/Logger.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 import javax.swing.Timer;
 
-import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
 
 import roolo.elo.api.IMetadataKey;
@@ -17,6 +16,7 @@ import sqv.data.DataAgent;
 import sqv.data.DataServer;
 import sqv.data.IDataClient;
 import eu.scy.actionlogging.api.IAction;
+import eu.scy.actionlogging.api.ContextConstants;
 import eu.scy.actionlogging.api.IActionLogger;
 import eu.scy.actionlogging.logger.Action;
 import eu.scy.elo.contenttype.dataset.DataSetRow;
@@ -24,225 +24,236 @@ import eu.scy.toolbroker.ToolBrokerImpl;
 
 public class Logger implements ActionListener, IDataClient {
 
-    private static final String HOST = "localhost";
+	private String username;
 
-    private static final int PORT = 2525;
+	private String toolname;
 
-    private static final String COMMAND_SPACE = "command";
+	private String missionname;
 
-    private String username;
+	private DataServer dataServer;
 
-    private String toolname;
+	private ArrayList<Double> oldInputValues;
 
-    private String missionname;
+	private ArrayList<Double> oldOutputValues;
 
-    private XMLOutputter outputter;
+	private ArrayList<ModelVariable> inputVariables;
 
-    private DataServer dataServer;
+	private ArrayList<ModelVariable> outputVariables;
 
-    private ArrayList<Double> oldInputValues;
+	private IAction action;
 
-    private ArrayList<Double> oldOutputValues;
+	private IAction lastInputVariableValueChangedAction;
 
-    private ArrayList<ModelVariable> inputVariables;
+	private IAction lastOutputVariableValueChangedAction;
 
-    private ArrayList<ModelVariable> outputVariables;
+	private Timer inputVariableTimer;
 
-    private IAction action;
+	private Timer outputVariableTimer;
 
-    private IAction lastInputVariableValueChangedAction;
+	private IAction firstInputVariableValueChangedAction;
 
-    private IAction lastOutputVariableValueChangedAction;
+	private IAction firstOutputVariableValueChangedAction;
 
-    private Timer inputVariableTimer;
+	private IActionLogger actionLogger;
 
-    private Timer outputVariableTimer;
+	private XMLOutputter xmlOutputter;
 
-    private IAction firstInputVariableValueChangedAction;
+	public Logger(DataServer dataServer) {
+		this.dataServer = dataServer;
+		xmlOutputter = new XMLOutputter();
+		// TODO: these properties have to be fetched from the SCY environment
+		username = System.getProperty("user.name");
+		missionname = "mission 1";
+		toolname = "scysimulator";
+		DataAgent dataAgent = new BasicDataAgent(this, dataServer);
+		// find input variables
+		inputVariables = getVariables(ModelVariable.VK_INPUT);
+		outputVariables = getVariables(ModelVariable.VK_OUTPUT, "Mtot");
+		// store the values to find the changed value later
+		storeOldValues();
+		// subscribe to variables
+		dataAgent.add(inputVariables);
+		dataAgent.add(outputVariables);
+		// register
 
-    private IAction firstOutputVariableValueChangedAction;
+		dataAgent.register();
+		inputVariableTimer = new Timer(500, new ActionListener() {
 
-    private IActionLogger actionLogger;
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				IAction action = createBasicAction("value_changed");
+				action.addAttribute("name", lastInputVariableValueChangedAction
+						.getAttribute("name"));
+				action.addAttribute("oldValue",
+						firstInputVariableValueChangedAction
+								.getAttribute("oldValue"));
+				action.addAttribute("newValue",
+						lastInputVariableValueChangedAction
+								.getAttribute("newValue"));
+				write(action);
+			}
+		});
+		inputVariableTimer.setRepeats(false);
+		outputVariableTimer = new Timer(500, new ActionListener() {
 
-    public Logger(DataServer dataServer) {
-        this.dataServer = dataServer;
-        // TODO: these properties have to be fetched from the SCY environment
-        username = System.getProperty("user.name");
-        missionname = "mission 1";
-        toolname = "scysimulator";
-        outputter = new XMLOutputter(Format.getPrettyFormat());
-        DataAgent dataAgent = new BasicDataAgent(this, dataServer);
-        // find input variables
-        inputVariables = getVariables(ModelVariable.VK_INPUT);
-        outputVariables = getVariables(ModelVariable.VK_OUTPUT, "Mtot");
-        // store the values to find the changed value later
-        storeOldValues();
-        // subscribe to variables
-        dataAgent.add(inputVariables);
-        dataAgent.add(outputVariables);
-        // register
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				IAction action = createBasicAction("value_changed");
+				action.addAttribute("name",
+						lastOutputVariableValueChangedAction
+								.getAttribute("name"));
+				action.addAttribute("oldValue",
+						firstOutputVariableValueChangedAction
+								.getAttribute("oldValue"));
+				action.addAttribute("newValue",
+						lastOutputVariableValueChangedAction
+								.getAttribute("newValue"));
+				write(action);
+			}
+		});
+		outputVariableTimer.setRepeats(false);
 
-        dataAgent.register();
-        inputVariableTimer = new Timer(500, new ActionListener() {
+		ToolBrokerImpl<IMetadataKey> tbi = new ToolBrokerImpl<IMetadataKey>();
+		actionLogger = tbi.getActionLogger();
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                IAction action = createBasicAction("value_changed");
-                action.addAttribute("name", lastInputVariableValueChangedAction.getAttribute("name"));
-                action.addAttribute("oldValue", firstInputVariableValueChangedAction.getAttribute("oldValue"));
-                action.addAttribute("newValue", lastInputVariableValueChangedAction.getAttribute("newValue"));
-                write(action);
-            }
-        });
-        inputVariableTimer.setRepeats(false);
-        outputVariableTimer = new Timer(500, new ActionListener() {
+	}
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                IAction action = createBasicAction("value_changed");
-                action.addAttribute("name", lastOutputVariableValueChangedAction.getAttribute("name"));
-                action.addAttribute("oldValue", firstOutputVariableValueChangedAction.getAttribute("oldValue"));
-                action.addAttribute("newValue", lastOutputVariableValueChangedAction.getAttribute("newValue"));
-                write(action);
-            }
-        });
-        outputVariableTimer.setRepeats(false);
+	private ArrayList<ModelVariable> getVariables(int mv) {
+		ArrayList<ModelVariable> variables = new ArrayList<ModelVariable>();
+		for (ModelVariable variable : dataServer.getVariables("")) {
+			if (variable.getKind() == mv) {
+				System.out.println("Logger added variable: "
+						+ variable.getName() + " type: " + variable.getKind());
+				variables.add(variable);
+			}
+		}
+		return variables;
+	}
 
-        ToolBrokerImpl<IMetadataKey> tbi = new ToolBrokerImpl<IMetadataKey>();
-        actionLogger = tbi.getActionLogger();
+	private ArrayList<ModelVariable> getVariables(int mv, String name) {
+		ArrayList<ModelVariable> variables = new ArrayList<ModelVariable>();
+		for (ModelVariable variable : dataServer.getVariables("")) {
+			if (variable.getKind() == mv && variable.getName().equals(name)) {
+				System.out.println("Logger added variable: "
+						+ variable.getName() + " type: " + variable.getKind());
+				variables.add(variable);
+			}
+		}
+		return variables;
+	}
 
-    }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("ScyDynamics.Logger actionPerformed.");
+	}
 
-    private ArrayList<ModelVariable> getVariables(int mv) {
-        ArrayList<ModelVariable> variables = new ArrayList<ModelVariable>();
-        for (ModelVariable variable : dataServer.getVariables("")) {
-            if (variable.getKind() == mv) {
-                System.out.println("Logger added variable: " + variable.getName() + " type: " + variable.getKind());
-                variables.add(variable);
-            }
-        }
-        return variables;
-    }
+	@Override
+	public void updateClient() {
+		for (int i = 0; i < inputVariables.size(); i++) {
+			if (inputVariables.get(i).getValue() != oldInputValues.get(i)) {
+				logValueChanged(inputVariables.get(i).getName(), oldInputValues
+						.get(i), inputVariables.get(i).getValue());
+			}
+		}
+		for (int i = 0; i < outputVariables.size(); i++) {
+			if (outputVariables.get(i).getValue() != oldOutputValues.get(i)) {
+				logValueChanged(outputVariables.get(i).getName(),
+						oldOutputValues.get(i), outputVariables.get(i)
+								.getValue());
+			}
+		}
+		storeOldValues();
+	}
 
-    private ArrayList<ModelVariable> getVariables(int mv, String name) {
-        ArrayList<ModelVariable> variables = new ArrayList<ModelVariable>();
-        for (ModelVariable variable : dataServer.getVariables("")) {
-            if (variable.getKind() == mv && variable.getName().equals(name)) {
-                System.out.println("Logger added variable: " + variable.getName() + " type: " + variable.getKind());
-                variables.add(variable);
-            }
-        }
-        return variables;
-    }
+	private void logValueChanged(String name, double oldValue, double newValue) {
+		action = createBasicAction("value_changed");
+		action.addAttribute("name", name);
+		action.addAttribute("oldValue", oldValue + "");
+		action.addAttribute("newValue", newValue + "");
+		for (ModelVariable inVar : inputVariables) {
+			if (inVar.getName().equals(name)) {
+				if (!inputVariableTimer.isRunning()) {
+					firstInputVariableValueChangedAction = action;
+				}
+				lastInputVariableValueChangedAction = action;
+				inputVariableTimer.restart();
+				break;
+			}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        System.out.println("ScyDynamics.Logger actionPerformed.");
-    }
+		}
+		for (ModelVariable outVar : outputVariables) {
+			if (outVar.getName().equals(name)) {
+				if (!outputVariableTimer.isRunning()) {
+					firstOutputVariableValueChangedAction = action;
+				}
+				lastOutputVariableValueChangedAction = action;
+				outputVariableTimer.restart();
+				break;
+			}
+		}
+	}
 
-    @Override
-    public void updateClient() {
-        for (int i = 0; i < inputVariables.size(); i++) {
-            if (inputVariables.get(i).getValue() != oldInputValues.get(i)) {
-                logValueChanged(inputVariables.get(i).getName(), oldInputValues.get(i), inputVariables.get(i).getValue());
-            }
-        }
-        for (int i = 0; i < outputVariables.size(); i++) {
-            if (outputVariables.get(i).getValue() != oldOutputValues.get(i)) {
-                logValueChanged(outputVariables.get(i).getName(), oldOutputValues.get(i), outputVariables.get(i).getValue());
-            }
-        }
-        storeOldValues();
-    }
+	private void storeOldValues() {
+		oldInputValues = new ArrayList<Double>();
+		oldOutputValues = new ArrayList<Double>();
+		for (ModelVariable invar : inputVariables) {
+			oldInputValues.add(invar.getValue());
+		}
+		for (ModelVariable outvar : outputVariables) {
+			oldOutputValues.add(outvar.getValue());
+		}
+	}
 
-    private void logValueChanged(String name, double oldValue, double newValue) {
-        action = createBasicAction("value_changed");
-        action.addAttribute("name", name);
-        action.addAttribute("oldValue", oldValue + "");
-        action.addAttribute("newValue", newValue + "");
-        for (ModelVariable inVar : inputVariables) {
-            if (inVar.getName().equals(name)) {
-                if (!inputVariableTimer.isRunning()) {
-                    firstInputVariableValueChangedAction = action;
-                }
-                lastInputVariableValueChangedAction = action;
-                inputVariableTimer.restart();
-                break;
-            }
+	public IAction createBasicAction(String type) {
+		IAction action = new Action(type, username);
+		action.addContext(ContextConstants.tool, toolname);
+		action.addContext(ContextConstants.mission, missionname);
+		return action;
+	}
 
-        }
-        for (ModelVariable outVar : outputVariables) {
-            if (outVar.getName().equals(name)) {
-                if (!outputVariableTimer.isRunning()) {
-                    firstOutputVariableValueChangedAction = action;
-                }
-                lastOutputVariableValueChangedAction = action;
-                outputVariableTimer.restart();
-                break;
-            }
-        }
-    }
+	public void logAddRow(DataSetRow newRow) {
+		action = createBasicAction("add_row");
+		action.addAttribute("dataRow", xmlOutputter
+				.outputString(newRow.toXML()));
+		write(action);
+	}
 
-    private void storeOldValues() {
-        oldInputValues = new ArrayList<Double>();
-        oldOutputValues = new ArrayList<Double>();
-        for (ModelVariable invar : inputVariables) {
-            oldInputValues.add(invar.getValue());
-        }
-        for (ModelVariable outvar : outputVariables) {
-            oldOutputValues.add(outvar.getValue());
-        }
-    }
+	public void logSelectedVariables(List<ModelVariable> selectedVariables) {
+		action = createBasicAction("variables_selected");
+		String selection = new String();
+		for (ModelVariable var : selectedVariables) {
+			selection = selection + var.getName() + ", ";
+		}
+		if (selection.length() > 2) {
+			selection = selection.substring(0, selection.length() - 2);
+		}
+		action.addAttribute("selected_variables", selection);
+		write(action);
+	}
 
-    public IAction createBasicAction(String type) {
-        IAction action = new Action(type, username);
-        action.addContext("tool", toolname);
-        action.addContext("mission", missionname);
-        return action;
-    }
+	private void write(IAction action) {
+		actionLogger.log(username, toolname, action);
+		// outputter.output(action.getXML(), System.out);
+	}
 
-    public void logAddRow(DataSetRow newRow) {
-        action = createBasicAction("add_row");
-        action.addAttribute("dataRow", "", newRow.toXML());
-        write(action);
-    }
+	public void toolStarted() {
+		IAction loginAction = createBasicAction("tool started");
+		write(loginAction);
+	}
 
-    public void logSelectedVariables(List<ModelVariable> selectedVariables) {
-        action = createBasicAction("variables_selected");
-        String selection = new String();
-        for (ModelVariable var : selectedVariables) {
-            selection = selection + var.getName() + ", ";
-        }
-        if (selection.length() > 2) {
-            selection = selection.substring(0, selection.length() - 2);
-        }
-        action.addAttribute("selected_variables", selection);
-        write(action);
-    }
+	public void toolStopped() {
+		IAction loginAction = createBasicAction("tool stopped");
+		write(loginAction);
+	}
 
-    private void write(IAction action) {
-        actionLogger.log(username, toolname, action);
-        // outputter.output(action.getXML(), System.out);
-    }
+	public void focusGained() {
+		IAction focusGained = createBasicAction("focus gained");
+		write(focusGained);
+	}
 
-    public void toolStarted() {
-        IAction loginAction = createBasicAction("tool started");
-        write(loginAction);
-    }
-    public void toolStopped() {
-        IAction loginAction = createBasicAction("tool stopped");
-        write(loginAction);
-    }
-    
-    public void focusGained() {
-        IAction focusGained = createBasicAction("focus gained");
-        write(focusGained);
-    }
-    
-    public void focusLost() {
-        IAction focusLost = createBasicAction("focus lost");
-        write(focusLost);
-    }
-    
+	public void focusLost() {
+		IAction focusLost = createBasicAction("focus lost");
+		write(focusLost);
+	}
 
 }
