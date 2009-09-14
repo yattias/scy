f8e4bc9709c0a203831f017a084a45cf206cc413
latinum1@gmx.de <latinum1@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-09-14 08:58:08 +0000

    Initial import. This is the webservice belonging to the Webbrowsing Tool (Firefox Extension). See Wiki for more information

---
+++ trunk/sources/modules/server/webbrowsingtoolELOSaver/src/main/java/eu/scy/webbrowsingtoolelosaver/config/Config.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eu.scy.webbrowsingtoolelosaver.config;
+
+//import eu.scy.client.desktop.scydesktop.elofactory.RegisterWindowContentCreators;
+//import eu.scy.client.desktop.scydesktop.missionmap.MissionModelCreator;
+import roolo.api.IExtensionManager;
+import roolo.api.IRepository;
+import roolo.elo.api.IELOFactory;
+import roolo.elo.api.IMetadataKey;
+import roolo.elo.api.IMetadataTypeManager;
+
+/**
+ *
+ * @author sikkenj
+ */
+public interface Config {
+
+   public IRepository getRepository();
+   public IExtensionManager getExtensionManager();
+   public IMetadataTypeManager getMetadataTypeManager();
+   public IELOFactory getEloFactory();
+
+   public IMetadataKey getTitleKey();
+
+}
