f630513a153c76ab9bb674cd4d1f381ce7433348
larsbollen <larsbollen@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-09-29 09:46:48 +0000

    added standalone applet version

---
+++ sources/modules/client/tools/scysimulator/src/main/java/eu/scy/client/tools/scysimulator/SCYSimulatorStandaloneApplet.java
@@ -0,0 +1,136 @@
+package eu.scy.client.tools.scysimulator;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.swing.JApplet;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import sqv.SimQuestViewer;
+
+public class SCYSimulatorStandaloneApplet extends JApplet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6054788019795975680L;
+	private SimQuestViewer sqv;
+	private DataCollector dataCollector;
+
+	public void init() {	
+		JPanel simquestPanel = new JPanel();
+		createSimQuestViewer();
+		sqv.setContainer(simquestPanel);
+		dataCollector = null;
+		EloSimQuestWrapper eloSimQuestWrapper = new EloSimQuestWrapper();
+
+		try {
+			sqv.run();
+
+			simquestPanel.setLayout(new BorderLayout());
+			sqv.getInterfacePanel().setMinimumSize(
+					new Dimension(450, 450));
+			simquestPanel.add(sqv.getInterfacePanel(),
+					BorderLayout.CENTER);
+
+			dataCollector = new DataCollector(sqv);
+			simquestPanel.add(dataCollector, BorderLayout.SOUTH);
+			simquestPanel.add(new StandAloneMenu(dataCollector), BorderLayout.NORTH);
+
+		} catch (java.lang.Exception e) {
+			System.out
+			.println("SimQuestNode.createSimQuestNode(). exception caught:");
+			e.printStackTrace();
+
+			JTextArea info = new JTextArea(4, 42);
+			info.append(e.getMessage());
+			simquestPanel.add(info);
+		}
+		
+		this.getContentPane().add(simquestPanel);		
+	}
+	
+	public void injectSimConfig(String simConfig) {
+		dataCollector.setSimConfig(simConfig);
+	}
+
+	private void createSimQuestViewer() {
+		{
+			 /**
+			  * Run as secure (default) or unsecure applet.
+			  */
+			 boolean m_hasSecurity = false;
+			
+			 //Check the secure parameter
+			 String security = getParameter("security");
+			 if (security != null)
+			 {
+				 m_hasSecurity = Boolean.valueOf(security);
+			 }
+			 
+			 //Create SimQuestViewer instance
+			 sqv = new SimQuestViewer(m_hasSecurity);
+			 
+			 //Check of obligatory file parameter
+			 String file = getParameter("file");
+			 if (file == null)
+			 {
+				 System.err.println("No valid file parameter specified exiting");
+				 System.exit(ERROR);
+			 }
+
+			 //Validate the file URI
+			 try 
+			 {
+				 //Fix for spaces...
+				 file = file.replace(" ", "%20");
+				 
+				 URI fileURI = new URI(file);
+				
+				if ( fileURI.isAbsolute() == false)
+				{
+					//Append the code base and try again
+					fileURI = new URI(getCodeBase() + file);
+					
+					if ( fileURI.isAbsolute() == true)
+					{
+						file = fileURI.toString();
+					}
+					else
+					{
+					    System.err.println("No valid file URI could be created");
+					    System.exit(ERROR);
+					}
+				}
+				
+				 //Set the file (URI)
+				 sqv.setFile(fileURI);
+				 System.out.println("File : " + file);
+			 } 
+			 catch (URISyntaxException e) 
+			 {
+				e.printStackTrace();
+			    System.err.println("No valid file URI could be created");
+			    System.exit(ERROR);
+			 }
+			 
+			 //Check for optional scy-server parameter
+			String  scyServer = getParameter("scy-server"); 
+	      	if (scyServer != null)
+	      	{
+	      			sqv.setScyServer(scyServer);
+	      	}
+
+			//Check for optional user parameter
+			String user = getParameter("user"); 
+	      	if (user != null)
+	      	{
+	      		sqv.setUser(user);
+	      	}
+
+	      	sqv.createFrame(false);	      	
+	    }
+	}
+}
