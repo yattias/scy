2b798d16f98f5889ffc5c3248a634ad1ce358c2d
jakob.sikken <jakob.sikken@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-10-11 18:25:33 +0000

    Added part of code for retrieving the mission map from the spring config file.

---
+++ sources/modules/client/desktop/scy-desktop/src/main/java/eu/scy/client/desktop/scydesktop/tools/corner/missionmap/MissionAnchor.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eu.scy.client.desktop.scydesktop.tools.corner.missionmap;
+
+import java.awt.Color;
+import java.net.URI;
+import java.util.List;
+
+/**
+ *
+ * @author sikken
+ */
+public interface MissionAnchor {
+
+   public URI getEloUri();
+   public String getTitle();
+   public char getIconCharacter();
+   public Color getColor();
+   public float getXPosition();
+   public float getYPosition();
+   public boolean isExisting();
+   public List<MissionAnchor> getNextMissionAnchors();
+   public List<String> getRelationNames();
+}
