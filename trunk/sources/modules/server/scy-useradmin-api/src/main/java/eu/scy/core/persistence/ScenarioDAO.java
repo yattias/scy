9bb81c96f861725cf4e1aa72924d85f9c2bbcc39
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-10-14 19:24:03 +0000

    Added ScenarioDAO

---
+++ trunk/sources/modules/server/scy-useradmin-api/src/main/java/eu/scy/core/persistence/ScenarioDAO.java
@@ -0,0 +1,18 @@
+package eu.scy.core.persistence;
+
+import eu.scy.core.model.pedagogicalplan.Scenario;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 14.okt.2009
+ * Time: 21:15:31
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ScenarioDAO extends SCYBaseDAO{
+
+    public List<Scenario> getScenarios();
+
+}
