df2359e14c682df3b466d134d80f001c6c18c609
hanbei <hanbei@gmx.de>
2009-10-20 13:55:49 +0000

    reimplemented as processing agent

--- sources/modules/agents/agents-impl/src/test/java/eu/scy/agents/topics/TopicDetectorTest.java
+++ sources/modules/agents/agents-impl/src/test/java/eu/scy/agents/topics/TopicDetectorTest.java
@@ -1,10 +1,15 @@
 package eu.scy.agents.topics;
 
 import static org.junit.Assert.assertEquals;
+import info.collide.sqlspaces.commons.Tuple;
+import info.collide.sqlspaces.commons.TupleSpaceException;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.net.URI;
+import java.rmi.dgc.VMID;
+import java.util.HashMap;
 import java.util.List;
 
 import org.junit.AfterClass;
@@ -13,6 +18,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import roolo.elo.api.IELO;
+import roolo.elo.api.IMetadata;
 import roolo.elo.api.metadata.CoreRooloMetadataKeyIds;
 import roolo.elo.content.BasicContent;
 import cc.mallet.topics.TopicModelParameter;
@@ -22,36 +28,50 @@ import eu.scy.agents.roolo.elo.helper.ELOFiller;
 
 public class TopicDetectorTest extends AbstractTestFixture {
 
-	private TopicDetector topicDetectorAgent;
+	// private TopicDetector topicDetectorAgent;
 
 	private IELO elo;
+
+	private URI eloURI;
 
 	// #0 9(0.8) 2(0.1502394136378272) 3(0.04976058636217276)
 	private static final String TEXT = "divided different took reference types electrical standard ceilings examined pyronometer night radiation temperature house ducts resultant air problems times dealers expect savings remained washed admittedly setback located intervals average energy evening unless summer rest houses house mentioned time home styles apparent identical condensation temperatures ice chris house strengthen rating summer increased thermostat cloudy house days setforward thermostat totaling 64 reach trends shaped lead argonfilled question houses success basement focuses partly time surface mentioned point savings apparent discussing cavity nottingham set early insulating insulation house time report setforward house early completely storm common trials condensation feels surface indoor test says volume";
 
 	@BeforeClass
-	public static void beforeAll() {
+	public static void startTS() {
 		startTupleSpaceServer();
 	}
 
 	@AfterClass
-	public static void afterAll() {
+	public static void stopTS() {
 		stopTupleSpaceServer();
 	}
 
 	@Before
 	public void setUp() throws Exception {
 		super.setUp();
-		initModel();
 
-		topicDetectorAgent = new TopicDetector("co2_scy_english");
-		topicDetectorAgent.setMetadataTypeManager(typeManager);
+		initModel();
+		HashMap<String, Object> params = new HashMap<String, Object>();
+		params.put("id", new VMID());
+		params.put(TopicDetector.MODEL_NAME, "co2_scy_english");
+		agentMap.put("eu.scy.agents.topics.TopicDetector", params);
+		startAgentFramework(agentMap);
+
+		// topicDetectorAgent = new TopicDetector("co2_scy_english");
+		// topicDetectorAgent.setMetadataTypeManager(typeManager);
 
 		elo = createNewElo();
 		ELOFiller eloFiller = new ELOFiller(elo, typeManager);
 		eloFiller.fillValue(CoreRooloMetadataKeyIds.TECHNICAL_FORMAT.getId(),
 				"scy/text");
+		eloFiller.fillValue(CoreRooloMetadataKeyIds.TITLE.getId(), "testELO");
 		elo.setContent(new BasicContent(TEXT.getBytes()));
+		IMetadata data = repository.addNewELO(elo);
+		eloURI = (URI) data.getMetadataValueContainer(
+				typeManager.getMetadataKey(CoreRooloMetadataKeyIds.IDENTIFIER
+						.getId())).getValue();
+		System.out.println("EloURI " + eloURI);
 	}
 
 	private void initModel() {
@@ -73,9 +93,15 @@ public class TopicDetectorTest extends AbstractTestFixture {
 
 	@SuppressWarnings("unchecked")
 	@Test
-	public void testProcessElo() {
-		topicDetectorAgent.processElo(elo);
+	public void testProcessElo() throws TupleSpaceException,
+			InterruptedException {
+		// topicDetectorAgent.processElo(elo);
 
+		getTupleSpace().write(new Tuple("topicDetector", eloURI.toString()));
+
+		Thread.sleep(5000);
+
+		elo = repository.retrieveELO(eloURI);
 		List<String> topicScores = (List<String>) elo
 				.getMetadata()
 				.getMetadataValueContainer(
@@ -85,25 +111,25 @@ public class TopicDetectorTest extends AbstractTestFixture {
 		assertEquals(10, topicScores.size());
 		assertEquals("wrong probability for topic 0", 0.0017892133644281931,
 				getTopicScore(topicScores.get(0)), 0.01);
-		assertEquals("wrong probability for topic 1", 0.002575589897297382, getTopicScore(topicScores.get(1)),
-				0.01);
-		assertEquals("wrong probability for topic 4", 0.00227468953178241, getTopicScore(topicScores.get(4)),
-				0.01);
-		assertEquals("wrong probability for topic 5", 0.0016823702862740107, getTopicScore(topicScores.get(5)),
-				0.01);
-		assertEquals("wrong probability for topic 6", 0.001945861865589766, getTopicScore(topicScores.get(6)),
-				0.01);
-		assertEquals("wrong probability for topic 7", 0.002655118753113757, getTopicScore(topicScores.get(7)),
-				0.01);
-		assertEquals("wrong probability for topic 8", 0.0019143937827241963, getTopicScore(topicScores.get(8)),
-				0.01);
+		assertEquals("wrong probability for topic 1", 0.002575589897297382,
+				getTopicScore(topicScores.get(1)), 0.01);
+		assertEquals("wrong probability for topic 4", 0.00227468953178241,
+				getTopicScore(topicScores.get(4)), 0.01);
+		assertEquals("wrong probability for topic 5", 0.0016823702862740107,
+				getTopicScore(topicScores.get(5)), 0.01);
+		assertEquals("wrong probability for topic 6", 0.001945861865589766,
+				getTopicScore(topicScores.get(6)), 0.01);
+		assertEquals("wrong probability for topic 7", 0.002655118753113757,
+				getTopicScore(topicScores.get(7)), 0.01);
+		assertEquals("wrong probability for topic 8", 0.0019143937827241963,
+				getTopicScore(topicScores.get(8)), 0.01);
 
-		assertEquals("wrong probability for topic 2", 0.1603676990866432, getTopicScore(topicScores.get(2)),
-				0.03);
-		assertEquals("wrong probability for topic 3", 0.05110776312074621, getTopicScore(topicScores.get(3)),
-				0.03);
-		assertEquals("wrong probability for topic 9", 0.7885358343528651, getTopicScore(topicScores.get(9)),
-				0.03);
+		assertEquals("wrong probability for topic 2", 0.1603676990866432,
+				getTopicScore(topicScores.get(2)), 0.03);
+		assertEquals("wrong probability for topic 3", 0.05110776312074621,
+				getTopicScore(topicScores.get(3)), 0.03);
+		assertEquals("wrong probability for topic 9", 0.7885358343528651,
+				getTopicScore(topicScores.get(9)), 0.03);
 	}
 
 	// public static void main(String[] args) throws IOException,
