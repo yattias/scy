a0f35469d02f6c745e6a8c6d1b2fb3599be8ce37
latinum1 <latinum1@gmx.de>
2009-11-04 11:11:39 +0000

    Changed interchange format for communication with the webservice (other JSON fields) Added views for opening and preview ELOs.

--- sources/external/webbrowsingtool/src/main/eu/scy/webbrowsingtool/defaults/preferences/highlighter.js
+++ sources/external/webbrowsingtool/src/main/eu/scy/webbrowsingtool/defaults/preferences/highlighter.js
@@ -3,8 +3,8 @@ pref("extensions.highlighter.intpref", 0);
 pref("extensions.highlighter.stringpref", "A string");
 pref("extensions.highlighter.username", "username");
 pref("extensions.highlighter.password", "password");
-pref("extensions.highlighter.address", "http://localhost:8080/webbrowsingtoolELOSaver/webresources/saveELO");
-pref("extensions.highlighter.defaultaddress", "http://localhost:8080/webbrowsingtoolELOSaver/webresources/saveELO");
+pref("extensions.highlighter.address", "http://localhost:8080/webbrowsingtoolELOSaver/webresources");
+pref("extensions.highlighter.defaultaddress", "http://localhost:8080/webbrowsingtoolELOSaver/webresources");
 pref("extensions.highlighter.port", "8080");
 pref("extensions.highlighter.usedefaultaddress", false);
 // See http://kb.mozillazine.org/Localize_extension_descriptions
