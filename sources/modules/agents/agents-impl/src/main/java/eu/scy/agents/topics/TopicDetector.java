904c821f9590591ca3d45b818a6355658641c5fc
hanbei <hanbei@gmx.de>
2009-11-04 10:27:26 +0000

    RESOLVED - issue SCY-326: Split topicdetector in topic detection component and metadata annotator component http://www.intermedia.uio.no/jira/browse/SCY-326

--- sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/topics/TopicDetector.java
+++ sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/topics/TopicDetector.java
@@ -6,15 +6,8 @@ import info.collide.sqlspaces.commons.TupleSpaceException;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Map;
 
-import roolo.api.IRepository;
-import roolo.elo.api.IELO;
-import roolo.elo.api.IMetadataTypeManager;
-import roolo.elo.api.IMetadataValueContainer;
-import roolo.elo.api.metadata.CoreRooloMetadataKeyIds;
 import cc.mallet.topics.TopicModelAnnotator;
 import cc.mallet.topics.TopicModelParameter;
 import de.fhg.iais.kd.tm.obwious.base.featurecarrier.Document;
@@ -22,39 +15,28 @@ import de.fhg.iais.kd.tm.obwious.base.featurecarrier.Features;
 import de.fhg.iais.kd.tm.obwious.operator.ObjectIdentifiers;
 import de.fhg.iais.kd.tm.obwious.operator.Operator;
 import eu.scy.agents.api.AgentLifecycleException;
-import eu.scy.agents.api.IRepositoryAgent;
-import eu.scy.agents.impl.AbstractProcessingAgent;
+import eu.scy.agents.impl.AbstractRequestAgent;
 import eu.scy.agents.impl.AgentProtocol;
 import eu.scy.agents.impl.PersistentStorage;
 
 /**
- * Detects topics in a text ELOs. Intended to be a before agent.
+ * ("topicDetector":String, <QueryID>:String, <ModelName>:String, <Text>:String)
+ * -> ("topicDetector":String, <QueryID>:String,
+ * <topicModelScore>:byte[](HashMap<Integer,Double>))
  * 
- * ("topicDetector":String, <ELOUri>:String) -> ("topicDetector":String,
- * <ELOUri>:String, <topicModelScore>:byte[](HashMap<Integer,Double>))
- * 
- * @author Florian Schulz
+ * @author fschulz_2
  * 
  */
-public class TopicDetector extends AbstractProcessingAgent implements
-		IRepositoryAgent {
+public class TopicDetector extends AbstractRequestAgent {
 
 	static final String KEY_TOPIC_SCORES = "topicScores";
+	static final String NAME = "eu.scy.agents.topics.TopicDetector";
 
-	private static final String NAME = "eu.scy.agents.topics.TopicDetector";
-
-	public static final String MODEL_NAME = "TopicModelName";
-
-	private TopicModelAnnotator model;
 	private PersistentStorage agentDatabase;
-	private String modelName;
-
-	private IMetadataTypeManager metadataTypeManager;
-
-	private IRepository repository;
+	private Tuple activationTuple;
 
 	public TopicDetector(Map<String, Object> params) {
-		super(NAME, (String) params.get("id"));
+		super(NAME, params);
 		if (params.containsKey("tsHost")) {
 			host = (String) params.get("tsHost");
 		}
@@ -62,46 +44,31 @@ public class TopicDetector extends AbstractProcessingAgent implements
 			port = (Integer) params.get("tsPort");
 		}
 		agentDatabase = new PersistentStorage();
-		modelName = (String) params.get(MODEL_NAME);
+		activationTuple = new Tuple("topicDetector", String.class,
+				String.class, String.class);
 	}
 
 	@Override
-	public void doRun() throws TupleSpaceException, AgentLifecycleException {
+	protected void doRun() throws TupleSpaceException, AgentLifecycleException {
 		while (status == Status.Running) {
-			Tuple tuple = getTupleSpace().waitToTake(getTemplateTuple(),
+			Tuple tuple = getTupleSpace().waitToTake(activationTuple,
 					AgentProtocol.ALIVE_INTERVAL);
 			if (tuple != null) {
-				String uri = "";
+				String queryID = (String) tuple.getField(1).getValue();
+				String modelName = (String) tuple.getField(2).getValue();
+				String text = (String) tuple.getField(3).getValue();
+				Map<Integer, Double> topicScores = getTopicScores(modelName,
+						text);
 				try {
-					uri = (String) tuple.getField(1).getValue();
-					IELO elo = repository.retrieveELO(new URI(uri));
-					if (isValidType(elo)) {
-						Document doc = convertEloToDocument(elo);
-						Map<Integer, Double> topicScores = getTopicScores(doc);
-						addTopicMetadata(elo, topicScores);
-						repository.addMetadata(elo.getUri(), elo.getMetadata());
-						sendTopicsToTS(elo, topicScores);
-					}
-				} catch (URISyntaxException e) {
-					throw new AgentLifecycleException("malformed uri: " + uri,
-							e);
+					sendTopicsToTS(queryID, topicScores);
 				} catch (IOException e) {
 					throw new AgentLifecycleException("Could not send tuple", e);
 				}
-			} else {
-				sendAliveUpdate();
 			}
 		}
 	}
 
-	/**
-	 * Sends ("topicDetector":String, <ELOUri>:String,
-	 * topicModelScores:byte[](HashMap<Integer,Double>))
-	 * 
-	 * @throws IOException
-	 * @throws TupleSpaceException
-	 */
-	private void sendTopicsToTS(IELO elo, Map<Integer, Double> topicScores)
+	private void sendTopicsToTS(String queryId, Map<Integer, Double> topicScores)
 			throws IOException, TupleSpaceException {
 
 		ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
@@ -110,57 +77,9 @@ public class TopicDetector extends AbstractProcessingAgent implements
 		out.writeObject(topicScores);
 		bytesOut.toByteArray();
 
-		Tuple topicsDetectedTuple = new Tuple("topicDetector", elo.getUri()
+		Tuple topicsDetectedTuple = new Tuple("topicDetector", queryId
 				.toString(), bytesOut.toByteArray());
 		getTupleSpace().write(topicsDetectedTuple);
-	}
-
-	private Tuple getTemplateTuple() {
-		return new Tuple("topicDetector", String.class);
-	}
-
-	private boolean isValidType(IELO elo) {
-		IMetadataValueContainer type = elo
-				.getMetadata()
-				.getMetadataValueContainer(
-						metadataTypeManager
-								.getMetadataKey(CoreRooloMetadataKeyIds.TECHNICAL_FORMAT
-										.getId()));
-		if ("scy/text".equals(type.getValue())) {
-			return true;
-		}
-		return false;
-	}
-
-	private Map<Integer, Double> getTopicScores(Document doc) {
-		model = new TopicModelAnnotator((TopicModelParameter) agentDatabase
-				.get(modelName));
-		Operator assignTopicScores = new AssignTopicScores()
-				.getOperator("Main");
-		assignTopicScores.setInputParameter(ObjectIdentifiers.DOCUMENT, doc);
-		assignTopicScores.setInputParameter(ObjectIdentifiers.MODEL, model);
-		assignTopicScores.run();
-
-		Map<Integer, Double> topicScores = doc.getFeature(KEY_TOPIC_SCORES);
-		return topicScores;
-	}
-
-	private void addTopicMetadata(IELO elo, Map<Integer, Double> topicScores) {
-		IMetadataValueContainer topicScoresContainer = elo.getMetadata()
-				.getMetadataValueContainer(
-						metadataTypeManager.getMetadataKey(KEY_TOPIC_SCORES));
-		for (Integer topicId : topicScores.keySet()) {
-			Double topicProbability = topicScores.get(topicId);
-			String value = topicId + ":" + topicProbability;
-			topicScoresContainer.addValue(value);
-		}
-	}
-
-	private Document convertEloToDocument(IELO elo) {
-		Document document = new Document("tmp");
-		String text = new String(elo.getContent().getBytes());
-		document.setFeature(Features.TEXT, text);
-		return document;
 	}
 
 	@Override
@@ -179,13 +98,18 @@ public class TopicDetector extends AbstractProcessingAgent implements
 		return status == Status.Stopping;
 	}
 
-	@Override
-	public void setMetadataTypeManager(IMetadataTypeManager manager) {
-		metadataTypeManager = manager;
-	}
+	private Map<Integer, Double> getTopicScores(String modelName, String text) {
+		Document doc = new Document("text");
+		doc.setFeature(Features.TEXT, text);
+		TopicModelAnnotator model = new TopicModelAnnotator(
+				(TopicModelParameter) agentDatabase.get(modelName));
+		Operator assignTopicScores = new AssignTopicScores()
+				.getOperator("Main");
+		assignTopicScores.setInputParameter(ObjectIdentifiers.DOCUMENT, doc);
+		assignTopicScores.setInputParameter(ObjectIdentifiers.MODEL, model);
+		assignTopicScores.run();
 
-	@Override
-	public void setRepository(IRepository rep) {
-		repository = rep;
+		Map<Integer, Double> topicScores = doc.getFeature(KEY_TOPIC_SCORES);
+		return topicScores;
 	}
 }
