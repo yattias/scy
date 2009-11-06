75811ef401a56d91685153739a352a3d6bbfa561
henrik <henrik@enovate.no>
2009-11-06 05:30:35 +0000

    Initial commit

---
+++ sources/modules/server/pedagogical-plan-service-impl/src/main/java/eu/scy/server/pedagogicalplan/PedagogicalPlanServiceImpl.java
@@ -0,0 +1,35 @@
+package eu.scy.server.pedagogicalplan;
+
+import eu.scy.core.ScenarioService;
+import eu.scy.core.model.pedagogicalplan.PedagogicalPlan;
+import eu.scy.core.model.pedagogicalplan.Scenario;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 06.nov.2009
+ * Time: 05:51:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class PedagogicalPlanServiceImpl implements PedagogicalPlanService {
+
+    private ScenarioService scenarioService = null;
+
+    public List<Scenario> getScenarios() {
+        return getScenarioService().getScenarios();
+    }
+
+    public List<PedagogicalPlan> getPedagogicalPlans() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public ScenarioService getScenarioService() {
+        return scenarioService;
+    }
+
+    public void setScenarioService(ScenarioService scenarioService) {
+        this.scenarioService = scenarioService;
+    }
+}
