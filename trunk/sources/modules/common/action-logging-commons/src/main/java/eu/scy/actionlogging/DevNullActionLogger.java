7aa22bc779857583748d739471c0abff6cb22a09
larsbollen <larsbollen@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-11-06 13:34:35 +0000

    initial commit of some helpful loggers

---
+++ trunk/sources/modules/common/action-logging-commons/src/main/java/eu/scy/actionlogging/DevNullActionLogger.java
@@ -0,0 +1,18 @@
+package eu.scy.actionlogging;
+
+import eu.scy.actionlogging.api.IAction;
+import eu.scy.actionlogging.api.IActionLogger;
+
+/**
+ * This IActionLogger simply accepts any IAction and does nothing with it.
+ * @author lars
+ *
+ */
+public class DevNullActionLogger implements IActionLogger {
+
+	@Override
+	public void log(String username, String source, IAction action) {
+		// doing absolutely nothing here		
+	}
+	
+}
