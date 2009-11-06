a92de7c2e203cc2f8e904d282eefa30e0526b51c
henrik <henrik@enovate.no>
2009-11-06 05:09:46 +0000

    Added pedagogical-plan-service-api

---
+++ sources/modules/common/pedagogical-plan-service-api/src/main/java/eu/scy/server/pedagogicalplan/PedagogicalPlanService.java
@@ -0,0 +1,11 @@
+package eu.scy.server.pedagogicalplan;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 06.nov.2009
+ * Time: 05:47:26
+ * To change this template use File | Settings | File Templates.
+ */
+public interface PedagogicalPlanService {
+}
