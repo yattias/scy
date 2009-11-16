7bfd4348b81cafd92d496b338fef47f8d315f305
hanbei@gmx.de <hanbei@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-11-16 12:55:19 +0000

    PersistentStorage uses right TS

--- trunk/sources/modules/agents/agents-base/src/main/java/eu/scy/agents/impl/PersistentStorage.java
+++ trunk/sources/modules/agents/agents-base/src/main/java/eu/scy/agents/impl/PersistentStorage.java
@@ -23,8 +23,8 @@ import eu.scy.agents.api.IPersistentStorage;
  */
 public class PersistentStorage implements IPersistentStorage {
 
-	private static final String TSHOST = "localhost";
-	// private static final String TSHOST = "scy.collide.info";
+	// private static final String TSHOST = "localhost";
+	private static final String TSHOST = "scy.collide.info";
 	private static final int TSPORT = 2525;
 	private static final String PERSISTENT_STORAGE = "persistent_storage_1_0";
 	private TupleSpace tupleSpace;
