07e055e49d06478ae64cd00325b804d0e0852a26
latinum1 <latinum1@gmx.de>
2009-11-16 16:44:18 +0000

    First draft of the eloImporter

---
+++ sources/modules/client/desktop/scy-desktop/src/main/java/eu/scy/client/desktop/scydesktop/tools/content/eloImporter/EloImporterModel.java
@@ -0,0 +1,86 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eu.scy.client.desktop.scydesktop.tools.content.eloImporter;
+
+import java.io.File;
+
+/**
+ *
+ * @author Sven
+ */
+public class EloImporterModel {
+
+    private File file;
+    private String fileEncoded;
+    private String filename;
+    private String description;
+    private String title;
+
+    public EloImporterModel(){
+        this(null,"","","","");
+    }
+
+    public EloImporterModel(File file, String filename, String description, String title, String fileEncoded) {
+        this.file = file;
+        this.filename = filename;
+        this.description = description;
+        this.title = title;
+        this.fileEncoded=fileEncoded;
+    }
+
+    public void updateModel(File file, String filename, String description, String title, String fileEncoded) {
+        this.file = file;
+        this.filename = filename;
+        this.description = description;
+        this.title = title;
+        this.fileEncoded=fileEncoded;
+    }
+
+    public String getFileEncoded() {
+        return fileEncoded;
+    }
+
+    public void setFileEncoded(String fileEncoded) {
+        this.fileEncoded = fileEncoded;
+    }
+
+    
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+
+}
