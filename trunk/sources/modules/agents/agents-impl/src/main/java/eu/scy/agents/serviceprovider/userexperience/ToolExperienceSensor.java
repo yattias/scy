54e9b55bf1925e0da543006a3ed55b5229ff4666
janengler80 <janengler80@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-11-17 08:51:22 +0000

    cleaned up exchanged syso's to logging

--- trunk/sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/serviceprovider/userexperience/ToolExperienceSensor.java
+++ trunk/sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/serviceprovider/userexperience/ToolExperienceSensor.java
@@ -40,6 +40,7 @@ import eu.scy.agents.supervisor.SupervisingAgent;
 
 public class ToolExperienceSensor extends AbstractThreadedAgent implements ActionListener {
 
+    // TODO gather that info from tbi?
     private static final String ACTION_SPACE = "actionSpace";
 
     private static final String SENSOR_SPACE = "sensorSpace";
@@ -51,12 +52,6 @@ public class ToolExperienceSensor extends AbstractThreadedAgent implements Actio
     private TupleSpace sensorSpace;
 
     private HashMap<String, UserToolExperienceModel> userModels;
-
-    private HashMap<String, Long> userOnlineTimeMap;
-
-    private HashMap<String, String> sessionUserMap;
-
-    private HashMap<String, Long> sessionWithFocusMap;
 
     private List<Integer> callbacks;
 
@@ -72,12 +67,8 @@ public class ToolExperienceSensor extends AbstractThreadedAgent implements Actio
 
     private List<Action> actionQueue;
 
-    private long processActionAttempts;
-
-
     public ToolExperienceSensor(Map<String, Object> map) {
         super("eu.scy.agents.serviceprovider.userexperience.ToolExperienceServiceProvider", (String) map.get("id"), "localhost", 2525);
-        processActionAttempts = 0L;
         try {
             sensorSpace = new TupleSpace(new User(getName()), host, port, false, false, SENSOR_SPACE);
             actionSpace = new TupleSpace(new User(getName()), host, port, false, false, ACTION_SPACE);
@@ -96,13 +87,10 @@ public class ToolExperienceSensor extends AbstractThreadedAgent implements Actio
         initLogger();
         actionQueue = new Vector<Action>();
         userModels = new HashMap<String, UserToolExperienceModel>();
-        userOnlineTimeMap = new HashMap<String, Long>();
-        sessionUserMap = new HashMap<String, String>();
-        sessionWithFocusMap = new HashMap<String, Long>();
         callbacks = new ArrayList<Integer>();
         startedInitialization();
         SessionCallback cb = new SessionCallback();
-        Tuple tupleTemplate = new Tuple(String.class, long.class, String.class, String.class, String.class,String.class, String.class, String.class);
+        Tuple tupleTemplate = new Tuple(String.class, long.class, String.class, String.class, String.class, String.class, String.class, String.class);
         callbacks.add(actionSpace.eventRegister(Command.WRITE, tupleTemplate, cb, false));
         rebuildFromSpace();
         finishedInitialization();
@@ -117,7 +105,6 @@ public class ToolExperienceSensor extends AbstractThreadedAgent implements Actio
         long lastActionTime = 0L;
         for (Tuple tuple : userExpTuples) {
             String userName = (String) tuple.getField(1).getValue();
-            // long lastAction = (Long) tuple.getField(2).getValue();
             String tool = (String) tuple.getField(3).getValue();
             long expTime = (Long) tuple.getField(4).getValue();
             long lastModificationTimestamp = tuple.getLastModificationTimestamp();
@@ -137,7 +124,7 @@ public class ToolExperienceSensor extends AbstractThreadedAgent implements Actio
         timeStampField.setLowerBound(lastActionTime + 1l);
         Tuple tupleTemplate = new Tuple(new Field(String.class), timeStampField, new Field(String.class), new Field(String.class), new Field(String.class), new Field(String.class), new Field(String.class), new Field(String.class));
         Tuple[] actionTuples = actionSpace.readAll(tupleTemplate);
-        System.out.println("Have to process: " + actionTuples.length + " Tuples.");
+        logger.log(Level.FINE, "Have to process: " + actionTuples.length + " tuples.");
         Arrays.sort(actionTuples, new Comparator<Tuple>() {
 
             @Override
@@ -157,11 +144,11 @@ public class ToolExperienceSensor extends AbstractThreadedAgent implements Actio
             processAction(action, false);
         }
         for (UserToolExperienceModel m : userModels.values()) {
-            System.out.println("Now after rebuilding:");
+            logger.log(Level.FINE, "Now after rebuilding:");
             Set<String> toolsList = m.getToolsList();
             for (String string : toolsList) {
 
-                System.out.println(m.getUserName() + " has exp in Tool: " + string + " of " + m.getExperience(string));
+                logger.log(Level.FINE, m.getUserName() + " has exp in Tool: " + string + " of " + m.getExperience(string));
             }
 
         }
@@ -180,58 +167,44 @@ public class ToolExperienceSensor extends AbstractThreadedAgent implements Actio
     }
 
     private synchronized void processAction(Action a, boolean queued) throws ParseException {
-        processActionAttempts++;
-        System.out.println("Process Action Attempts: " + processActionAttempts);
-        logger.log(Level.FINE, "Action to process occured, queed-flag is set to " + queued);
+        logger.log(Level.FINE, "Action to process occured, queued-flag is set to " + queued);
         if (!initializing && queued) {
             actionQueue.add(a);
             return;
         }
         if (a.getType().equals("tool started")) {
             String sessionid = a.getContext(ContextConstants.session);
-            String user = a.getUser();
             UserToolExperienceModel userModel = userModels.get(a.getUser());
             if (userModel == null) {
                 userModel = new UserToolExperienceModel(a.getUser(), sensorSpace);
                 userModels.put(a.getUser(), userModel);
-                System.out.println("new usermodel for " + a.getUser() + " created");
+                logger.log(Level.FINE, "new usermodel for " + a.getUser() + " created");
             }
-            sessionUserMap.put(sessionid, user);
-            System.out.println("Tool started with user: " + a.getUser() + "and SessionID: " + sessionid);
+            logger.log(Level.FINE, "Tool started with user: " + a.getUser() + " and SessionID: " + sessionid);
         } else if (a.getType().equals("tool stopped")) {
             String sessionid = a.getContext(ContextConstants.session);
-            sessionUserMap.remove(sessionid);
             UserToolExperienceModel exp = userModels.get(a.getUser());
             exp.setToolInactive(a.getContext(ContextConstants.tool), TimeFormatHelper.getInstance().getISO8601AsLong(a.getTime()));
-            System.out.println("Tool stopped with user: " + a.getUser() + "and SessionID: " + sessionid);
+            logger.log(Level.FINE, "Tool stopped with user: " + a.getUser() + " and SessionID: " + sessionid);
 
         } else if (a.getType().equals("focus gained")) {
             String sessionid = a.getContext(ContextConstants.session);
             long focusTime = TimeFormatHelper.getInstance().getISO8601AsLong(a.getTime());
-            sessionWithFocusMap.put(sessionid, focusTime);
             UserToolExperienceModel exp = userModels.get(a.getUser());
             exp.setActiveTool(a.getContext(ContextConstants.tool), focusTime);
-            System.out.println("Focus gained with user: " + a.getUser() + "and SessionID: " + sessionid);
+            logger.log(Level.FINE, "Focus gained with user: " + a.getUser() + " and SessionID: " + sessionid);
         } else if (a.getType().equals("focus lost")) {
             String sessionid = a.getContext(ContextConstants.session);
-            // TODO fix
-            long focusStartTime = sessionWithFocusMap.remove(sessionid);
             long focusEndTime = TimeFormatHelper.getInstance().getISO8601AsLong(a.getTime());
-            Long onlineTime = userOnlineTimeMap.get(a.getUser());
-            if (onlineTime == null) {
-                onlineTime = 0L;
-            }
-            onlineTime += focusEndTime - focusStartTime;
-            userOnlineTimeMap.put(a.getUser(), onlineTime);
             UserToolExperienceModel exp = userModels.get(a.getUser());
             exp.setToolInactive(a.getContext(ContextConstants.tool), focusEndTime);
-            System.out.println("Focus lost with user: " + a.getUser() + "and SessionID: " + sessionid);
+            logger.log(Level.FINE, "Focus lost with user: " + a.getUser() + " and SessionID: " + sessionid);
         }
     }
 
     @Override
     protected void doRun() throws TupleSpaceException, AgentLifecycleException {
-        
+
         while (status == Status.Running) {
             sendAliveUpdate();
             try {
@@ -261,7 +234,7 @@ public class ToolExperienceSensor extends AbstractThreadedAgent implements Actio
 
     @Override
     protected Tuple getIdentifyTuple(String queryId) {
-        // TODO Insert ident here
+        // TODO This agent is just a sensor and therefore it don't need to identify....
         return null;
     }
 
@@ -292,7 +265,6 @@ public class ToolExperienceSensor extends AbstractThreadedAgent implements Actio
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // logger.log(Level.FINE, "actionPerformed");
         HashMap<String, UserToolExperienceModel> clone = (HashMap<String, UserToolExperienceModel>) userModels.clone();
         for (Entry<String, UserToolExperienceModel> userModel : clone.entrySet()) {
             UserToolExperienceModel user = userModel.getValue();
