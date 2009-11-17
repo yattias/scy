75556e1003b1afae40179b4a02119e3c4b38edbd
janengler80 <janengler80@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-11-17 14:41:14 +0000

    changed due to changes in IAction added toString method

--- sources/modules/common/action-logging-commons/src/main/java/eu/scy/actionlogging/Action.java
+++ sources/modules/common/action-logging-commons/src/main/java/eu/scy/actionlogging/Action.java
@@ -10,152 +10,182 @@ import eu.scy.actionlogging.api.IContext;
 import eu.scy.core.model.impl.ScyBaseObject;
 
 public class Action extends ScyBaseObject implements IAction {
-	
-	public static final String PATH = "action";
-	
-	/** Maybe we want to have a real user object!? */
-	private String user;
-	
-	/** Time in ISO8601 format */
-	private String time;
-	
-	/** The type of the action */
-	private String type;
-	
-	/** Action context */
-	private IContext context;
-	
-	private String dataType;
-	
-	private String data;
-	
-	private Map<String, String> attributes;
-	
+
+    public static final String PATH = "action";
+
+    /** Maybe we want to have a real user object!? */
+    private String user;
+
+    /** Time in ISO8601 format */
+    private String time;
+
+    /** Time in millis after 1970 */
+    private long timeInMillis;
+
+    /** The type of the action */
+    private String type;
+
+    /** Action context */
+    private IContext context;
+
+    private String dataType;
+
+    private String data;
+
+    private Map<String, String> attributes;
+
     public Action() {
-    	setId(UUID.randomUUID().toString());
-    	time = TimeFormatHelper.getInstance().getCurrentTimeMillisAsISO8601(); 	
-    	attributes = new HashMap<String, String>();
+        setId(UUID.randomUUID().toString());
+        time = TimeFormatHelper.getInstance().getCurrentTimeMillisAsISO8601();
+        attributes = new HashMap<String, String>();
     }
-     
+
+    @Override
+    public String toString() {
+        return "Action [attributes=" + attributes + ", context=" + context + ", data=" + data + ", dataType=" + dataType + ", time=" + time + ", type=" + type + ", user=" + user + "]";
+    }
+
     @Override
     public void setContext(IContext context) {
-    	this.context = context;
+        this.context = context;
     }
-    
+
     @Override
     public IContext getContext() {
-    	return context;
+        return context;
     }
-    
+
     @Override
     public void addContext(ContextConstants constant, String value) {
-    	if(context == null) {
-    		context = new Context();
-    	}
-    	context.set(constant, value);
+        if (context == null) {
+            context = new Context();
+        }
+        context.set(constant, value);
     }
-    
+
     @Override
-	public String getContext(ContextConstants constant) {
-		return context.get(constant);
-	}
-    
-	@Override
-    public void addAttribute(String name, String value) {	
-		attributes.put(name, value);
+    public String getContext(ContextConstants constant) {
+        return context.get(constant);
     }
-	
-    @Override
-	public String getAttribute(String key) {
-    	return attributes.get(key);
-	}
-    
-	/**
-	 * @return the user
-	 */
-    @Override
-	public String getUser() {
-		return user;
-	}
 
-	/**
-	 * @param user the user to set
-	 */
     @Override
-	public void setUser(String user) {
-		this.user = user;
-	}
+    public void addAttribute(String name, String value) {
+        attributes.put(name, value);
+    }
 
-	/**
-	 * @return the time
-	 */
     @Override
-	public String getTime() {
-		return time;
-	}
+    public String getAttribute(String key) {
+        return attributes.get(key);
+    }
 
-	/**
-	 * @param time the time to set
-	 */
+    /**
+     * @return the user
+     */
     @Override
-	public void setTime(String time) {
-		this.time = time;
-	}
+    public String getUser() {
+        return user;
+    }
 
-	/**
-	 * @return the type
-	 */
+    /**
+     * @param user
+     *            the user to set
+     */
     @Override
-	public String getType() {
-		return type;
-	}
+    public void setUser(String user) {
+        this.user = user;
+    }
 
-	/**
-	 * @param type the type to set
-	 */
+    /**
+     * @return the time
+     */
     @Override
-	public void setType(String type) {
-		this.type = type;
-	}
+    @Deprecated
+    public String getTime() {
+        return time;
+    }
 
-	/**
-	 * @return the dataType
-	 */
+    /**
+     * @param time
+     *            the time to set
+     */
     @Override
-	public String getDataType() {
-		return dataType;
-	}
+    @Deprecated
+    public void setTime(String time) {
+        this.time = time;
+    }
 
-	/**
-	 * @param dataType the dataType to set
-	 */
+    /**
+     * @return the time in milliseconds from 1970
+     */
+    public long getTimeInMillis() {
+        return this.timeInMillis;
+    }
+
+    /**
+     * @param timeInMillis
+     *            the time in milliseconds to set
+     */
+    public void setTimeInMillis(long timeInMillis) {
+        this.timeInMillis = timeInMillis;
+    }
+
+    /**
+     * @return the type
+     */
     @Override
-	public void setDataType(String dataType) {
-		this.dataType = dataType;
-	}
+    public String getType() {
+        return type;
+    }
 
-	/**
-	 * @return the data
-	 */
+    /**
+     * @param type
+     *            the type to set
+     */
     @Override
-	public String getData() {
-		return data;
-	}
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	/**
-	 * @param data the data to set
-	 */
+    /**
+     * @return the dataType
+     */
     @Override
-	public void setData(String data) {
-		this.data = data;
-	}
+    public String getDataType() {
+        return dataType;
+    }
 
-	/**
-	 * @return the attributes
-	 */
-	@Override
-	public Map<String, String> getAttributes() {
-		return attributes;
-	}
-    
+    /**
+     * @param dataType
+     *            the dataType to set
+     */
+    @Override
+    public void setDataType(String dataType) {
+        this.dataType = dataType;
+    }
+
+    /**
+     * @return the data
+     */
+    @Override
+    public String getData() {
+        return data;
+    }
+
+    /**
+     * @param data
+     *            the data to set
+     */
+    @Override
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    /**
+     * @return the attributes
+     */
+    @Override
+    public Map<String, String> getAttributes() {
+        return attributes;
+    }
+
 }
