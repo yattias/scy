d0a9737c4e2e25488cb2e8e25dccc355cfb4ce54
janengler80 <janengler80@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-11-18 09:32:45 +0000

    Now with logging Now working Now prettier Now better

--- sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/sensors/userexperience/UserToolExperienceModel.java
+++ sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/sensors/userexperience/UserToolExperienceModel.java
@@ -7,15 +7,23 @@ import info.collide.sqlspaces.commons.TupleSpaceException;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public class UserToolExperienceModel {
+
+    private static final String USER_EXP = "user_exp";
 
     private String userName;
 
-    private HashMap<String, Long> toolTimeMap;
+    private Map<String, Long> toolTimeMap;
 
     private String activeTool;
 
@@ -24,10 +32,14 @@ public class UserToolExperienceModel {
     private Lock lock = new ReentrantLock();
 
     private TupleSpace sensorSpace;
+    private Map<String, TupleID> toolTIDMap;
 
-    private HashMap<String, TupleID> toolTIDMap;
+    private static final Level DEBUGLEVEL = Level.FINE;
+
+    private static final Logger logger = Logger.getLogger(UserToolExperienceModel.class.getName());
 
     public UserToolExperienceModel(String userName, TupleSpace sensorSpace) {
+        initLogger();
         this.userName = userName;
         this.sensorSpace = sensorSpace;
         toolTimeMap = new HashMap<String, Long>();
@@ -59,9 +71,9 @@ public class UserToolExperienceModel {
             long timeToAdd = endTime - startTime;
             Long oldTime = toolTimeMap.get(tool);
             if (oldTime == null) {
-                System.out.println("Tool " + tool + " not yet in Map...will add it now");
+                logger.log(Level.FINER, "[UserToolExperienceModel for User " + getUserName() + " and active Tool " + activeTool + "] [setToolInactive] Tool " + tool + " not yet in Map...will add it now");
                 oldTime = 0l;
-                Tuple t = new Tuple("user_exp", this.getUserName(), endTime, tool, oldTime);
+                Tuple t = new Tuple(USER_EXP, this.getUserName(), endTime, tool, oldTime);
                 TupleID id = null;
                 try {
                     id = sensorSpace.write(t);
@@ -71,59 +83,54 @@ public class UserToolExperienceModel {
                 }
 
             }
-            System.out.println("[INACTIVE] Old time is "+oldTime);
             long newTime = oldTime + timeToAdd;
+            logger.log(Level.FINER, "[UserToolExperienceModel for User " + getUserName() + " and active Tool " + activeTool + "] [setToolInactive] TimeToAdd: " + timeToAdd);
             toolTimeMap.put(tool, newTime);
             TupleID tupleID = toolTIDMap.get(tool);
             if (tupleID != null) {
                 try {
-                    sensorSpace.update(tupleID, new Tuple("user_exp", this.getUserName(), endTime, tool, newTime));
+                    sensorSpace.update(tupleID, new Tuple(USER_EXP, this.getUserName(), endTime, tool, newTime));
                 } catch (TupleSpaceException e) {
                     e.printStackTrace();
                 }
             } else {
-                System.out.println("[setToolInactive]: TupleID not found!");
+                logger.log(Level.WARNING, "[UserToolExperienceModel for User " + getUserName() + " and active Tool " + activeTool + "] [setToolInactive]: TupleID not found!");
             }
-            System.out.println("");
-            System.out.println("[INACTIVE]User " + this.getUserName() + " has now onlineTime for Tool " + activeTool + " of " + toolTimeMap.get(activeTool));
-            System.out.println("");
+            logger.log(Level.FINER, "[UserToolExperienceModel for User " + getUserName() + " and active Tool " + activeTool + "] [setToolInactive] User has now onlineTime for Tool " + activeTool + " of " + toolTimeMap.get(activeTool));
             activeTool = null;
 
         } else {
-            System.err.println("Fatal! The Tool " + tool + " that should be closed isn't open! The open Tool is " + activeTool);
+            logger.log(Level.SEVERE, "[UserToolExperienceModel for User " + getUserName() + " and active Tool " + activeTool + "] [setToolInactive] Fatal! The Tool " + tool + " that should be closed isn't open! The open Tool is " + activeTool);
         }
         lock.unlock();
-        System.out.println("User " + getUserName() + " with tool " + tool + " has exp of " + toolTimeMap.get(tool));
+        logger.log(Level.FINER, "[UserToolExperienceModel for User " + getUserName() + " and active Tool " + activeTool + "] [setToolInactive] User with tool " + tool + " has experience (in MS) of " + toolTimeMap.get(tool));
     }
 
-    public void updateActiveToolExperience(long intervall, long lastActionTime) {
+    public void updateActiveToolExperience(long intervall, long updateTime) {
         lock.lock();
         if (activeTool != null) {
             Long oldTime = toolTimeMap.get(activeTool);
             if (oldTime == null) {
-                System.out.println("Tool " + activeTool + " not yet in Map...will add it now");
+                logger.log(Level.FINER, "[UserToolExperienceModel for User " + getUserName() + " and active Tool " + activeTool + "] Tool " + activeTool + " [updateActiveToolExperience] Not yet in Map...will add it now");
                 oldTime = 0l;
             }
-            long newTime = oldTime + intervall;
+            long newTime = oldTime + (updateTime - startTime);
             toolTimeMap.put(activeTool, newTime);
             TupleID tupleID = toolTIDMap.get(activeTool);
             if (tupleID != null) {
                 try {
-                    sensorSpace.update(tupleID, new Tuple("user_exp", this.getUserName(), lastActionTime, activeTool, newTime));
+                    sensorSpace.update(tupleID, new Tuple(USER_EXP, this.getUserName(), updateTime, activeTool, newTime));
                 } catch (TupleSpaceException e) {
                     e.printStackTrace();
                 }
             } else {
-                System.out.println("[updateActiveToolExperience]: TupleID not found!");
+                logger.log(Level.WARNING, "[UserToolExperienceModel for User " + getUserName() + " and active Tool " + activeTool + "] Tool " + activeTool + " [updateActiveToolExperience] TupleID not found!");
             }
-            startTime += intervall;
-            System.out.println("");
-            System.out.println("[UPDATE]User " + this.getUserName() + " has now onlineTime for Tool " + activeTool + " of " + toolTimeMap.get(activeTool));
-            System.out.println("");
+            startTime = updateTime;
+            logger.log(Level.FINER, "[UserToolExperienceModel for User " + getUserName() + " and active Tool " + activeTool + "] Tool " + activeTool + " [updateActiveToolExperience] User " + this.getUserName() + " has now onlineTime for Tool " + activeTool + " of " + toolTimeMap.get(activeTool));
         } else {
-            System.out.println("no active tool found...for user " + this.getUserName());
-            HashMap<String, TupleID> clone = (HashMap<String, TupleID>) toolTIDMap.clone();
-            Collection<TupleID> values = clone.values();
+            logger.log(Level.FINER, "[UserToolExperienceModel for User " + getUserName() + " and active Tool " + activeTool + "] Tool " + activeTool + " [updateActiveToolExperience] User " + this.getUserName() + " no active tool found...for user " + this.getUserName());
+            Collection<TupleID> values = toolTIDMap.values();
             for (TupleID tupleID : values) {
                 try {
                     Tuple readTupleById = sensorSpace.readTupleById(tupleID);
@@ -142,5 +149,14 @@ public class UserToolExperienceModel {
 
     public void setToolTID(String tool, TupleID tid) {
         toolTIDMap.put(tool, tid);
+    }
+
+    private void initLogger() {
+        ConsoleHandler cH = new ConsoleHandler();
+        SimpleFormatter sF = new SimpleFormatter();
+        cH.setFormatter(sF);
+        cH.setLevel(DEBUGLEVEL);
+        logger.setLevel(DEBUGLEVEL);
+        logger.addHandler(cH);
     }
 }
