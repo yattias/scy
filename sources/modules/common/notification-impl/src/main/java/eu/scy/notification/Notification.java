36a4634e5a049b404cd71cbe4c12f90a2e639916
janengler80 <janengler80@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-11-25 11:01:09 +0000

    changes due to the new API

--- sources/modules/common/notification-impl/src/main/java/eu/scy/notification/Notification.java
+++ sources/modules/common/notification-impl/src/main/java/eu/scy/notification/Notification.java
@@ -1,84 +1,89 @@
 package eu.scy.notification;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.util.Properties;
 
 import eu.scy.notification.api.INotification;
 
+public class Notification implements INotification {
 
-public class Notification implements INotification
-{
-	private Properties properties = new Properties();
-	
-	
-	public Notification() {
-	}
-	
-	/**
-	 * creates a new notification from XML
-	 * @param xml	
-	 */
-	public Notification(String xml) {
-		setPropertiesFromXML(xml);
-	}
-	
-	/**
-	 * returns the properties object
-	 */
-	public Properties getProperties()
-	{
-		return properties;
-	}
+    private long timestamp;
 
-	/**
-	 * sets properties from XML,
-	 * used in the cunstructor
-	 * @param xml
-	 */
-	public void setPropertiesFromXML(String xml)
-	{
-		InputStream in = new ByteArrayInputStream(xml.getBytes());
-		try {
-			properties.loadFromXML(in);
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * returns XML
-	 */
-	public String getXML()
-	{
-		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		try {
-			properties.storeToXML(os, null);
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		return os.toString();
-	}
+    private Properties properties;
 
-	/**
-	 * adds a key<>value pair
-	 * @param key
-	 * @param value
-	 */
-	public void addProperty(String key, String value) {
-		properties.setProperty(key, value);
-		
-	}
+    private String receiver;
 
-	/**
-	 * @param key
-	 * @return value of 'key'
-	 */
-	public String getProperty(String key) {
-		return properties.getProperty(key);
-	}
+    private String sender;
+
+    public Notification() {
+        properties = new Properties();
+    }
+
+    /**
+     * creates a new notification from XML
+     * 
+     * @param xml
+     */
+    public Notification(String sender, String receiver, long timestamp, Properties props) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.timestamp = timestamp;
+        properties = (props == null) ? new Properties() : props;
+    }
+
+    /**
+     * returns the properties object
+     */
+    public Properties getProperties() {
+        return properties;
+    }
+
+    /**
+     * adds a key<>value pair
+     * 
+     * @param key
+     * @param value
+     */
+    public void addProperty(String key, String value) {
+        properties.setProperty(key, value);
+
+    }
+
+    /**
+     * @param key
+     * @return value of 'key'
+     */
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    @Override
+    public String getReceiver() {
+        return receiver;
+    }
+
+    @Override
+    public String getSender() {
+        return sender;
+    }
+
+    @Override
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    @Override
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    @Override
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
 
 }
