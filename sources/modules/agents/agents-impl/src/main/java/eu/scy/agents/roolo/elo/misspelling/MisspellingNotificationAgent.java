a672599aae7f5e1cc425553f6e03be99d5003c3c
janengler80 <janengler80@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-11-25 10:52:31 +0000

    AGAIN: removed inheritance to the AbstractCommunicationAgent (now AbstractThreadedAgent) commented out some notification stuff

--- sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/roolo/elo/misspelling/MisspellingNotificationAgent.java
+++ sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/roolo/elo/misspelling/MisspellingNotificationAgent.java
@@ -1,11 +1,12 @@
 package eu.scy.agents.roolo.elo.misspelling;
 
-import java.util.Map;
-
 import info.collide.sqlspaces.commons.Tuple;
 import info.collide.sqlspaces.commons.TupleSpaceException;
+
+import java.util.Map;
+
 import eu.scy.agents.api.AgentLifecycleException;
-import eu.scy.agents.impl.AbstractCommunicationAgent;
+import eu.scy.agents.impl.AbstractThreadedAgent;
 import eu.scy.agents.impl.AgentProtocol;
 import eu.scy.notification.Notification;
 import eu.scy.notification.api.INotification;
@@ -18,7 +19,7 @@ import eu.scy.notification.api.INotification;
  * @author Florian Schulz
  * 
  */
-public class MisspellingNotificationAgent extends AbstractCommunicationAgent {
+public class MisspellingNotificationAgent extends AbstractThreadedAgent {
 
 	private static final String NOTIFY_ABOUT_MISSPELLINGS_NAME = "NotifyAboutMisspellings";
 	private boolean stopped;
@@ -46,11 +47,11 @@ public class MisspellingNotificationAgent extends AbstractCommunicationAgent {
 						.getValue();
 				String user = (String) trigger.getField(4).getValue();
 
-				INotification notification = new Notification();
-				notification.addProperty("errors", "" + numberOfErrors);
-				notification.addProperty("target", "misspellings");
-				notification.addProperty("eloURI", uri);
-				getNotificationSender().send(user, "textpad", notification);
+//				INotification notification = new Notification();
+//				notification.addProperty("errors", "" + numberOfErrors);
+//				notification.addProperty("target", "misspellings");
+//				notification.addProperty("eloURI", uri);
+//				getNotificationSender().send(user, "textpad", notification);
 			} catch (TupleSpaceException e) {
 				stop();
 			}
