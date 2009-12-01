a1dc760c5412a8649772bf25fb190697e84610f5
adam.giemza <adam.giemza@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-12-01 13:08:53 +0000

    Implemented Notificator as SCYHubModule

--- trunk/sources/modules/server/notification-module/src/main/java/eu/scy/server/notification/Notificator.java
+++ trunk/sources/modules/server/notification-module/src/main/java/eu/scy/server/notification/Notificator.java
@@ -11,13 +11,32 @@ import info.collide.sqlspaces.commons.Callback.Command;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
+import org.xmpp.component.Component;
+import org.xmpp.packet.Message;
+import org.xmpp.packet.Packet;
+
+import eu.scy.common.configuration.Configuration;
+import eu.scy.common.packetextension.SCYPacketTransformer;
+import eu.scy.commons.whack.WhacketExtension;
 import eu.scy.notification.Notification;
+import eu.scy.notification.NotificationPacketTransformer;
 import eu.scy.notification.api.INotification;
+import eu.scy.scyhub.SCYHubModule;
 
-public class Notificator {
+public class Notificator extends SCYHubModule{
 
-    private TupleSpace commandSpace;
+    protected Notificator(Component scyhub, SCYPacketTransformer transformer) {
+		super(scyhub, transformer);
+		try {
+			init();
+		} catch (TupleSpaceException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private TupleSpace commandSpace;
 
     private String COMMAND_SPACE = "command";
 
@@ -25,17 +44,11 @@ public class Notificator {
 
     private static final String NOTIFICATION = "notification";
 
-    public Notificator(String host, int port) {
-        try {
-            commandSpace = new TupleSpace(new User(Notificator.class.getName()), host, port, false, false, COMMAND_SPACE);
-            init();
-        } catch (TupleSpaceException e) {
-            e.printStackTrace();
-        }
-
-    }
-
     private void init() throws TupleSpaceException {
+    	String host = Configuration.getInstance().getSqlSpacesServerHost();
+    	int port = Configuration.getInstance().getSqlSpacesServerPort();
+    	
+    	commandSpace = new TupleSpace(new User(Notificator.class.getName()), host, port, false, false, COMMAND_SPACE);
         callbacks = new ArrayList<Integer>();
         SessionCallback cb = new SessionCallback();
         Tuple tupleTemplate = new Tuple(NOTIFICATION, Field.createWildCardField());
@@ -54,13 +67,13 @@ public class Notificator {
     private synchronized void processNotification(Tuple notificationTuple) {
         // "notification":String, <User>:String, <Receiver>:String, <Sender>:String, <Mission>:String, <Session>:String, <Key=Value>:String*)
         String uniqueID =(String) notificationTuple.getField(1).getValue();
-        String userName = (String) notificationTuple.getField(2).getValue();
-        String receiver = (String) notificationTuple.getField(3).getValue();
-        String sender = (String) notificationTuple.getField(4).getValue();
-        String mission = (String) notificationTuple.getField(5).getValue();
-        String session = (String) notificationTuple.getField(6).getValue();
-        Map<String,String> props = new HashMap<String,String>();
-        for (int i = 7; i < notificationTuple.getNumberOfFields(); i++) {
+        String userName = (String) notificationTuple.getField(1).getValue();
+        String receiver = (String) notificationTuple.getField(2).getValue();
+        String sender = (String) notificationTuple.getField(3).getValue();
+        String mission = (String) notificationTuple.getField(4).getValue();
+        String session = (String) notificationTuple.getField(5).getValue();
+        Map<String, String> props = new HashMap<String, String>();
+        for (int i = 6; i < notificationTuple.getNumberOfFields(); i++) {
             String keyValue = (String) notificationTuple.getField(i).getValue();
             int index = keyValue.indexOf('=');
             String key = keyValue.substring(0, index - 1);
@@ -70,7 +83,18 @@ public class Notificator {
         }
         INotification notification = new Notification(uniqueID,sender, receiver, notificationTuple.getCreationTimestamp(), mission, session, props);
 
-        // TODO send via xmpp
-
+        // Create a message to send the notification with receiver
+        Message message = new Message();
+        message.setTo(receiver);
+        message.setFrom("scyhub.scy.collide.info");
+        // Attach the extension
+        message.addExtension(new WhacketExtension(new NotificationPacketTransformer(notification)));
+        // Send the message to receiver
+        send(message);        
     }
+
+	@Override
+	protected void process(Packet packet, WhacketExtension extension) {
+		// TODO noting to process as we will only send (at the moment)
+	}
 }
