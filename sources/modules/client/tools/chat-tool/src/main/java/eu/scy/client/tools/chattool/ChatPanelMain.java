37847826f92c224e9302bf0aa35498d63e628741
adam.giemza <adam.giemza@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-12-04 20:17:31 +0000

    removed initialization of awareness service as this will already be done inside the TB

--- sources/modules/client/tools/chat-tool/src/main/java/eu/scy/client/tools/chattool/ChatPanelMain.java
+++ sources/modules/client/tools/chat-tool/src/main/java/eu/scy/client/tools/chattool/ChatPanelMain.java
@@ -192,7 +192,6 @@ public class ChatPanelMain extends JPanel {
 		
 		ToolBrokerImpl tbi = new ToolBrokerImpl("senders11@scy.intermedia.uio.no", "senders11");
 		IAwarenessService aService = tbi.getAwarenessService();
-		aService.init(tbi.getConnection("senders11@scy.intermedia.uio.no", "senders11"));
 		
 		cmp = new ChatPanelMain(aService);
 		frame.getContentPane().add(cmp);
