f647e8ea43137dc46454d2419c6b2c4c45faaac8
janengler80 <janengler80@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-12-04 12:44:50 +0000

    Changes due to TBI

--- sources/modules/client/tools/scysimulator/src/main/java/eu/scy/client/tools/scysimulator/DatasetSandbox.java
+++ sources/modules/client/tools/scysimulator/src/main/java/eu/scy/client/tools/scysimulator/DatasetSandbox.java
@@ -1,5 +1,11 @@
 package eu.scy.client.tools.scysimulator;
 
+import java.util.Iterator;
+import java.util.Locale;
+
+import org.jivesoftware.smack.XMPPConnection;
+
+import roolo.elo.JDomStringConversion;
 import eu.scy.client.common.datasync.IDataSyncService;
 import eu.scy.client.common.datasync.ISyncListener;
 import eu.scy.client.common.datasync.ISyncSession;
@@ -7,77 +13,61 @@ import eu.scy.collaborationservice.CollaborationServiceException;
 import eu.scy.common.datasync.ISyncObject;
 import eu.scy.common.datasync.SyncObject;
 import eu.scy.elo.contenttype.dataset.DataSetRow;
-import eu.scy.toolbroker.ToolBrokerImpl;
-
-import java.util.Iterator;
-import java.util.Locale;
-import org.jivesoftware.smack.XMPPConnection;
-import roolo.elo.JDomStringConversion;
+import eu.scy.toolbrokerapi.ToolBrokerAPI;
 
 /**
- *
+ * 
  * @author lars
  */
 public class DatasetSandbox implements ISyncListener {
-    private DataCollector datacollector;
-    private String TOOL_NAME = "scysimulator";
-    private String USER_NAME = "obama";
-    private String PASS = "obama";
-    private IDataSyncService datasync;
-	ISyncSession currentSession = null;
-	private XMPPConnection conn;
 
-    DatasetSandbox(DataCollector datacollector) throws CollaborationServiceException {
+    private DataCollector datacollector;
+
+    private String TOOL_NAME = "scysimulator";
+
+    private String USER_NAME = "obama";
+
+    private String PASS = "obama";
+
+    private IDataSyncService datasync;
+
+    ISyncSession currentSession = null;
+
+    private XMPPConnection conn;
+
+    private ToolBrokerAPI tbi;
+
+    DatasetSandbox(DataCollector datacollector, ToolBrokerAPI tbi) throws CollaborationServiceException {
         this.datacollector = datacollector;
+        this.tbi = tbi;
         initCollaborationService();
     }
 
     public void clear() {
-    	for (ISyncObject syncObject : currentSession.getAllSyncObjects()) {
-    		currentSession.removeSyncObject(syncObject);
-    	}
+        for (ISyncObject syncObject : currentSession.getAllSyncObjects()) {
+            currentSession.removeSyncObject(syncObject);
+        }
     }
 
     private void initCollaborationService() throws CollaborationServiceException {
-    	// TODO the datasync instance will be soon delivered by the SCY-lab via TBI
-    	/*ConnectionConfiguration config;
-        config = new ConnectionConfiguration("scy.collide.info", 5222);
-		conn = new XMPPConnection(config);
-		try {
-			conn.connect();
-			conn.login(USER_NAME, PASS);
-			conn.addConnectionListener(new ConnectionListener() {		
-				public void reconnectionSuccessful() {}
-				public void reconnectionFailed(Exception arg0) {}
-				public void reconnectingIn(int arg0) {}
-				public void connectionClosedOnError(Exception arg0) {}
-				public void connectionClosed() {}
-			});
-			
-			datasync = new DataSyncService();
-			datasync.init(conn);
-			
-		} catch (XMPPException e) {
-			e.printStackTrace();
-		}		*/
-    	ToolBrokerImpl tbi = new ToolBrokerImpl(USER_NAME, PASS);
-    	datasync = tbi.getDataSyncService();
+        // TODO the datasync instance will be soon delivered by the SCY-lab via TBI
+        datasync = tbi.getDataSyncService();
     }
-    
-	public void disconnect() {
-		currentSession.removeSyncListener(this);
-		conn.disconnect();
-	}
-    
+
+    public void disconnect() {
+        currentSession.removeSyncListener(this);
+        conn.disconnect();
+    }
+
     public ISyncSession createSession() {
-    	try {
-    		currentSession = datasync.createSession(this);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		sendHeaderMessage();
-		sendDataRows();
-		return currentSession;
+        try {
+            currentSession = datasync.createSession(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        sendHeaderMessage();
+        sendDataRows();
+        return currentSession;
     }
 
     private void sendDataRows() {
@@ -89,30 +79,29 @@ public class DatasetSandbox implements ISyncListener {
     }
 
     public void sendDataSetRow(DataSetRow row) {
-    	ISyncObject syncObject = new SyncObject(this.TOOL_NAME, this.USER_NAME);
-    	syncObject.setProperty("type", "datasetrow");
-    	syncObject.setProperty("datasetrow", new JDomStringConversion().xmlToString(row.toXML()));
-    	currentSession.addSyncObject(syncObject);
+        ISyncObject syncObject = new SyncObject(this.TOOL_NAME, this.USER_NAME);
+        syncObject.setProperty("type", "datasetrow");
+        syncObject.setProperty("datasetrow", new JDomStringConversion().xmlToString(row.toXML()));
+        currentSession.addSyncObject(syncObject);
     }
 
     public void sendHeaderMessage() {
-    	ISyncObject syncObject = new SyncObject(this.TOOL_NAME, this.USER_NAME);
-    	syncObject.setProperty("type", "datasetheader");
-    	syncObject.setProperty("datasetheader", new JDomStringConversion().xmlToString(datacollector.getDataSet().getHeader(Locale.ENGLISH).toXML()));
-    	currentSession.addSyncObject(syncObject);
+        ISyncObject syncObject = new SyncObject(this.TOOL_NAME, this.USER_NAME);
+        syncObject.setProperty("type", "datasetheader");
+        syncObject.setProperty("datasetheader", new JDomStringConversion().xmlToString(datacollector.getDataSet().getHeader(Locale.ENGLISH).toXML()));
+        currentSession.addSyncObject(syncObject);
     }
 
-    /* These ISyncListener methods are empty because SCYSimulator is only sending action,
-     * but not reacting.
-     *  
+    /*
+     * These ISyncListener methods are empty because SCYSimulator is only sending action, but not reacting.
      */
-	@Override
-	public void syncObjectAdded(ISyncObject syncObject) {}
+    @Override
+    public void syncObjectAdded(ISyncObject syncObject) {}
 
-	@Override
-	public void syncObjectChanged(ISyncObject syncObject) {}
+    @Override
+    public void syncObjectChanged(ISyncObject syncObject) {}
 
-	@Override
-	public void syncObjectRemoved(ISyncObject syncObject) {}
+    @Override
+    public void syncObjectRemoved(ISyncObject syncObject) {}
 
 }
