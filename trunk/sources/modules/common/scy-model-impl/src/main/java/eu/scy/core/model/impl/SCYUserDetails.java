bde41e891e3781cb122b28319e08d692fa37d4bf
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-12-18 06:00:50 +0000

    Updated user properties (impl)

--- trunk/sources/modules/common/scy-model-impl/src/main/java/eu/scy/core/model/impl/SCYUserDetails.java
+++ trunk/sources/modules/common/scy-model-impl/src/main/java/eu/scy/core/model/impl/SCYUserDetails.java
@@ -1,5 +1,9 @@
 package eu.scy.core.model.impl;
 
+import eu.scy.core.model.UserDetails;
+
+import javax.persistence.*;
+
 
 /**
  * Created by IntelliJ IDEA.
@@ -8,10 +12,101 @@ package eu.scy.core.model.impl;
  * Time: 12:23:59
  * Overridden details class in order to add SCY specific user fields
  */
-@Deprecated
-//Entity (name = "eu.scy.core.model.impl.SCYUserDetails")
-public class SCYUserDetails /*extends StudentUserDetails */{
+@Entity(name = "eu.scy.core.model.impl.SCYUserDetails")
+@Table(name="user_details")
+public class SCYUserDetails implements UserDetails/*extends StudentUserDetails */{
+
+    private Long id;
+
+    private String username;
+    private String password;
+    private boolean accountNotExpired = Boolean.TRUE;
+    private boolean accoundNotLocked = Boolean.TRUE;
+    private boolean credentialsNotExpired = Boolean.TRUE;
+    private boolean enabled = Boolean.TRUE;
 
 
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Column(name="account_not_expired")
+    public boolean isAccountNotExpired() {
+        return accountNotExpired;
+    }
+
+    public void setAccountNotExpired(boolean accountNotExpired) {
+        this.accountNotExpired = accountNotExpired;
+    }
+
+    @Column (name="account_not_locked")
+    public boolean isAccoundNotLocked() {
+        return accoundNotLocked;
+    }
+
+    public void setAccoundNotLocked(boolean accoundNotLocked) {
+        this.accoundNotLocked = accoundNotLocked;
+    }
+
+    @Column (name="credentials_not_expired")
+    public boolean isCredentialsNotExpired() {
+        return credentialsNotExpired;
+    }
+
+    public void setCredentialsNotExpired(boolean credentialsNotExpired) {
+        this.credentialsNotExpired = credentialsNotExpired;
+    }
+
+
+    @Column (name="enabled")
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SCYUserDetails)) return false;
+
+        SCYUserDetails that = (SCYUserDetails) o;
+
+        if (!password.equals(that.password)) return false;
+        if (!username.equals(that.username)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username.hashCode();
+        result = 31 * result + password.hashCode();
+        return result;
+    }
 }
--- trunk/sources/modules/common/scy-model-impl/src/main/java/eu/scy/core/model/impl/SCYUserImpl.java
+++ trunk/sources/modules/common/scy-model-impl/src/main/java/eu/scy/core/model/impl/SCYUserImpl.java
@@ -13,9 +13,55 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 
-//Entity(name = "SCYUserImpl")
-//Table(name = "users")
+@Entity
+@Table(name = "users")
 //org.hibernate.annotations.Proxy(proxyClass = User.class)
-public class SCYUserImpl /*implements User */{
+public class SCYUserImpl implements User {
 
+    private long id;
+
+
+    private UserDetails userDetails;
+
+    @Override
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    @OneToOne(cascade = CascadeType.ALL, targetEntity = SCYUserDetails.class)
+    @JoinColumn(name = "user_details_fk", nullable = false, unique = true)
+    public UserDetails getUserDetails() {
+        return userDetails;
+    }
+
+    public void setUserDetails(SCYUserDetails userDetails) {
+        this.userDetails = (UserDetails) userDetails;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SCYUserImpl)) return false;
+
+        SCYUserImpl scyUser = (SCYUserImpl) o;
+
+        if (id != scyUser.id) return false;
+        if (!userDetails.equals(scyUser.userDetails)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + userDetails.hashCode();
+        return result;
+    }
 }
