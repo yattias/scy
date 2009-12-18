5b4c2641ac2063a36035dcc62cede77adcd92e03
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-12-18 10:45:10 +0000

    this a branch that uses Multi User Chat functionality

--- sources/modules/client/client-common/data-sync-api/src/main/java/eu/scy/communications/message/IMessageTranslator.java
+++
@@ -1,8 +0,0 @@
-package eu.scy.communications.message;
-
-public interface IMessageTranslator {
-    
-    public abstract Object getObject(IScyMessage scyMessage);    
-    public abstract IScyMessage getScyMessage(Object object);
-    
-}
--- sources/modules/client/misc/elo-importer/src/main/java/eu/scy/eloimporter/gui/PanelFactory.java
+++
@@ -1,9 +0,0 @@
-package eu.scy.eloimporter.gui;
-
-import javax.swing.JPanel;
-
-public interface PanelFactory {
-
-	public JPanel create();
-
-}
