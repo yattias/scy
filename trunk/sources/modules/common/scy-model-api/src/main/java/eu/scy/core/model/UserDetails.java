fde2a6c38b44cd9f5568c90bb62ec962e250b05c
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-12-18 06:00:07 +0000

    Updated user api

---
+++ trunk/sources/modules/common/scy-model-api/src/main/java/eu/scy/core/model/UserDetails.java
@@ -0,0 +1,11 @@
+package eu.scy.core.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 18.des.2009
+ * Time: 06:18:38
+ * To change this template use File | Settings | File Templates.
+ */
+public interface UserDetails {
+}
