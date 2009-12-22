b24545abb8a5780d9c27ef9d916153badee98285
st.weinbrenner@arcor.de <st.weinbrenner@arcor.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2009-12-22 13:06:56 +0000

    removed unused import

--- trunk/sources/modules/agents/agents-base/src/main/java/eu/scy/agents/impl/AbstractThreadedAgent.java
+++ trunk/sources/modules/agents/agents-base/src/main/java/eu/scy/agents/impl/AbstractThreadedAgent.java
@@ -1,5 +1,6 @@
 package eu.scy.agents.impl;
 
+import info.collide.sqlspaces.client.TupleSpace;
 import info.collide.sqlspaces.commons.Callback;
 import info.collide.sqlspaces.commons.Tuple;
 import info.collide.sqlspaces.commons.TupleID;
