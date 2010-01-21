2dfbd87a3b92816bdc3949f0b845336592b81ae0
hanbei <hanbei@gmx.de>
2010-01-21 15:56:40 +0000

    made persistent storage update already present tuples

--- sources/modules/agents/agents-base/src/main/java/eu/scy/agents/impl/PersistentStorage.java
+++ sources/modules/agents/agents-base/src/main/java/eu/scy/agents/impl/PersistentStorage.java
@@ -3,6 +3,7 @@ package eu.scy.agents.impl;
 import info.collide.sqlspaces.client.TupleSpace;
 import info.collide.sqlspaces.commons.Field;
 import info.collide.sqlspaces.commons.Tuple;
+import info.collide.sqlspaces.commons.TupleID;
 import info.collide.sqlspaces.commons.TupleSpaceException;
 import info.collide.sqlspaces.commons.User;
 
@@ -11,15 +12,14 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.HashMap;
 
 import eu.scy.agents.api.IPersistentStorage;
 
 /**
- * The implementation of {@link IPersistentStorage}. Saves things in the
- * TupleSpace.
+ * The implementation of {@link IPersistentStorage}. Saves things in the TupleSpace.
  * 
  * @author Florian Schulz
- * 
  */
 public class PersistentStorage implements IPersistentStorage {
 
@@ -28,6 +28,8 @@ public class PersistentStorage implements IPersistentStorage {
 	private static final int TSPORT = 2525;
 	private static final String PERSISTENT_STORAGE = "persistent_storage_1_0";
 	private TupleSpace tupleSpace;
+
+	private HashMap<String, TupleID> key2TupleId;
 
 	/**
 	 * Create a new {@link PersistentStorage} instance.
@@ -39,19 +41,18 @@ public class PersistentStorage implements IPersistentStorage {
 	public PersistentStorage(String host, int port) {
 		try {
 			// TODO Configuration
-			tupleSpace = new TupleSpace(new User(PERSISTENT_STORAGE), host,
-					port, PERSISTENT_STORAGE);
+			tupleSpace = new TupleSpace(new User(PERSISTENT_STORAGE), host, port, PERSISTENT_STORAGE);
 		} catch (TupleSpaceException e) {
 			e.printStackTrace();
 		}
+		key2TupleId = new HashMap<String, TupleID>();
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public <T> T get(String key) {
 		try {
-			Tuple t = tupleSpace.read(new Tuple(PERSISTENT_STORAGE, key, Field
-					.createWildCardField()));
+			Tuple t = tupleSpace.read(new Tuple(PERSISTENT_STORAGE, key, Field.createWildCardField()));
 			byte[] byteArray = (byte[]) t.getField(2).getValue();
 			ByteArrayInputStream byteIn = new ByteArrayInputStream(byteArray);
 			ObjectInputStream objectIn = new ObjectInputStream(byteIn);
@@ -68,17 +69,45 @@ public class PersistentStorage implements IPersistentStorage {
 
 	@Override
 	public <T> void put(String key, T value) {
-		try {
-			ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
-			ObjectOutputStream objectOut = new ObjectOutputStream(byteArray);
-			objectOut.writeObject(value);
-			objectOut.close();
-			tupleSpace.write(new Tuple(PERSISTENT_STORAGE, key, byteArray
-					.toByteArray()));
-		} catch (TupleSpaceException e) {
-			throw new RuntimeException(e);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
+		if (!key2TupleId.containsKey(key)) {
+			try {
+				ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
+				ObjectOutputStream objectOut = new ObjectOutputStream(byteArray);
+				objectOut.writeObject(value);
+				objectOut.close();
+				TupleID id = tupleSpace.write(new Tuple(PERSISTENT_STORAGE, key, byteArray.toByteArray()));
+				key2TupleId.put(key, id);
+			} catch (TupleSpaceException e) {
+				throw new RuntimeException(e);
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		} else {
+			try {
+				TupleID tupleId = key2TupleId.get(key);
+				ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
+				ObjectOutputStream objectOut;
+				objectOut = new ObjectOutputStream(byteArray);
+				objectOut.writeObject(value);
+				objectOut.close();
+				tupleSpace.update(tupleId, new Tuple(PERSISTENT_STORAGE, key, byteArray.toByteArray()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (TupleSpaceException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+	public void remove(String key) {
+		if (key2TupleId.containsKey(key)) {
+			TupleID tupleId = key2TupleId.get(key);
+			try {
+				tupleSpace.takeTupleById(tupleId);
+			} catch (TupleSpaceException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
