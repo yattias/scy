1d81de955a779a9d386e9ae5426d880c2c436ff9
janengler80 <janengler80@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-01-22 11:49:44 +0000

    new version of classifier

--- sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/sensors/behaviourclassifier/BehavioralModel.java
+++ sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/sensors/behaviourclassifier/BehavioralModel.java
@@ -1,6 +1,12 @@
 package eu.scy.agents.sensors.behaviourclassifier;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.rmi.dgc.VMID;
+import java.util.List;
+import java.util.Vector;
+
+import javax.swing.Timer;
 
 import info.collide.sqlspaces.client.TupleSpace;
 import info.collide.sqlspaces.commons.Tuple;
@@ -8,10 +14,15 @@ import info.collide.sqlspaces.commons.TupleSpaceException;
 
 public class BehavioralModel {
 
-    public enum Notification {
-        LEVEL1,
-        LEVEL2,
-        LEVEL3;
+    private static final int NOTIFICATION_DELAY = 10 * 1000;
+
+    public enum SCAFFOLD {
+        VOTAT,
+        INC_CHANGE,
+        EXTREME_VALUES,
+        CONFIRM_HYPO,
+        IDENT_HYPO,
+        SHOWBUTTON;
     }
 
     private int userExp;
@@ -30,6 +41,12 @@ public class BehavioralModel {
 
     private String mission;
 
+    private boolean showButtonSent = false;
+
+    private List<SCAFFOLD> sentScaffolds;
+
+    private Timer timer;
+
     public BehavioralModel(String name, String tool, String mission, String session, int canonical, int votat, int userExp, TupleSpace commandSpace) {
         this.name = name;
         this.tool = tool;
@@ -39,6 +56,7 @@ public class BehavioralModel {
         this.commandSpace = commandSpace;
         this.mission = mission;
         this.session = session;
+        sentScaffolds = new Vector<SCAFFOLD>();
     }
 
     public int getUserExp() {
@@ -78,21 +96,36 @@ public class BehavioralModel {
 
     private void checkForSystematicBehaviour() {
         // Simple ruleset :externalize
-        if (votat < 30 && canonical < 30 && userExp > 60) {
-            sendNotification(Notification.LEVEL1);
-        } else if (votat < 20 && canonical < 20 && userExp > 60) {
-            sendNotification(Notification.LEVEL2);
-        } else if (votat < 10 && canonical < 10 && userExp > 60) {
-            sendNotification(Notification.LEVEL3);
+        if (userExp >= 20){
+            sendNotification(SCAFFOLD.VOTAT);
+            showButtonSent = true;
         }
+//        if (votat < 30 && canonical < 30 && userExp > 30) {
+//            sendNotification(SCAFFOLD.VOTAT);
+//        } else if (votat < 20 && canonical < 20 && userExp > 30) {
+//            sendNotification(SCAFFOLD.INC_CHANGE);
+//        } else if (votat < 10 && canonical < 10 && userExp > 30) {
+//            sendNotification(SCAFFOLD.EXTREME_VALUES);
+//        }
     }
 
-    private void sendNotification(Notification level) {
-        Tuple notificationTuple = new Tuple("notification", new VMID().toString(), name, tool, "ScySimBehaviorClassifier", mission, session, "type=scaffold","level=" + level.name());
-        try {
-            commandSpace.write(notificationTuple);
-        } catch (TupleSpaceException e) {
-            e.printStackTrace();
+    private void sendNotification(final SCAFFOLD level) {
+        if (!sentScaffolds.contains(level)) {
+            sentScaffolds.add(level);
+            timer = new Timer(NOTIFICATION_DELAY, new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    sentScaffolds.remove(level);
+                }
+            });
+            timer.setRepeats(false);
+            timer.start();
+            Tuple notificationTuple = new Tuple("notification", new VMID().toString(), name, tool, "ScySimBehaviorClassifier", mission, session, "type=scaffold", "level=" + level.name());
+            try {
+                commandSpace.write(notificationTuple);
+            } catch (TupleSpaceException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
