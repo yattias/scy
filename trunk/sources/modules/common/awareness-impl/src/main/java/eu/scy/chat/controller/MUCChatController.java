f2c558931a47cccc92d2aa6bb2c9346b27bac6e2
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-01-27 16:12:36 +0000

    added more invoker

--- trunk/sources/modules/common/awareness-impl/src/main/java/eu/scy/chat/controller/MUCChatController.java
+++ trunk/sources/modules/common/awareness-impl/src/main/java/eu/scy/chat/controller/MUCChatController.java
@@ -31,8 +31,8 @@ import eu.scy.presence.IPresenceEvent;
 
 public class MUCChatController implements ChatController {
 
-	private static final Logger logger = Logger.getLogger(MUCChatController.class
-			.getName());
+	private static final Logger logger = Logger
+			.getLogger(MUCChatController.class.getName());
 	private DefaultListModel buddyListModel = new DefaultListModel();
 	private IAwarenessService awarenessService;
 	private String ELOUri;
@@ -40,36 +40,38 @@ public class MUCChatController implements ChatController {
 
 	public MUCChatController(IAwarenessService awarenessService, String ELOUri) {
 		logger.debug("MUC ChatController: starting ... ");
-		logger.debug("MUC ChatController: awarenessService.isConnected(): "+ awarenessService.isConnected());
+		logger.debug("MUC ChatController: awarenessService.isConnected(): "
+				+ awarenessService.isConnected());
 		this.ELOUri = ELOUri;
 		this.awarenessService = awarenessService;
 	}
-	
-	
+
 	public String getCurrentUser() {
 		String user = getAwarenessService().getConnection().getUser();
-		if( user != null)
+		if (user != null)
 			return StringUtils.parseName(user);
 		return "NO ONE";
 	}
-	
-	public void sendMessage(final String ELOUri, final String message){
+
+	public void sendMessage(final String ELOUri, final String message) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					awarenessService.sendMUCMessage(ELOUri, message);
 				} catch (AwarenessServiceException e) {
-					logger.error("ChatController: sendMessageMUC: AwarenessServiceException for ELOUri:" + ELOUri + " " + e);
+					logger
+							.error("ChatController: sendMessageMUC: AwarenessServiceException for ELOUri:"
+									+ ELOUri + " " + e);
 				}
 			}
 		});
-		
+
 	}
-	
+
 	public void addBuddy(final AwarenessUser user) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				buddyListModel.addElement(user);	
+				buddyListModel.addElement(user);
 			}
 		});
 	}
@@ -78,121 +80,168 @@ public class MUCChatController implements ChatController {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				buddyListModel.removeElement(user);
-				//kick that fucker away call handler
+				// kick that fucker away call handler
 			}
 		});
 	}
 
-	
 	public void registerChatArea(final JTextArea chatArea) {
-		
-		awarenessService.addAwarenessMessageListener(new IAwarenessMessageListener() {
-			@Override
-			public void handleAwarenessMessageEvent(final IAwarenessEvent awarenessEvent) {
-			
-				System.out.println("calling message event");
-				
-					SwingUtilities.invokeLater(new Runnable() {
-						public void run() {
-							
-							String oldText = chatArea.getText();
-							List<IAwarenessUser> users = new ArrayList<IAwarenessUser>();
-							
-							if(awarenessEvent.getMessage() != null) {
-								chatArea.setText(oldText + awarenessEvent.getUser().getNickName() + ": " + awarenessEvent.getMessage() + "\n");	
-								logger.debug("message sent from: " + awarenessEvent.getUser().getNickName() + " message: " + awarenessEvent.getMessage());
-							}	
-							
-						}
-					});
-			
-			}
-		});
-		
-		
-		awarenessService.addAwarenessPresenceListener(new IAwarenessPresenceListener() {		
-			@Override
-			public void handleAwarenessPresenceEvent(final IAwarePresenceEvent e) {
-				logger.debug("registerChatArea: handleAwarenessPresenceEvent: smthg happened...: " + e.getUser().getNickName()+" : "+e.getUser().getPresence());				
-				
-				SwingUtilities.invokeLater(new Runnable() {
-					public void run() {
-						
-				
-				IAwarenessUser awarenessUser = e.getUser();
-				
-				boolean isFound = false;
-				IAwarenessUser iau;
-				for(int i=0; i<buddyListModel.getSize(); i++) {
-					iau = (IAwarenessUser) buddyListModel.elementAt(i);
-					logger.debug("registerChatArea: handleAwarenessPresenceEvent: " + iau.getNickName());	
-					if(iau.getNickName().equals(awarenessUser.getNickName())) {
-						((IAwarenessUser) buddyListModel.elementAt(i)).setPresence(awarenessUser.getPresence());
-						isFound = true;
-					}
-				}
+
+		awarenessService
+				.addAwarenessMessageListener(new IAwarenessMessageListener() {
+					@Override
+					public void handleAwarenessMessageEvent(
+							final IAwarenessEvent awarenessEvent) {
+
+						System.out.println("calling message event");
+
+						SwingUtilities.invokeLater(new Runnable() {
+							public void run() {
+
+								String oldText = chatArea.getText();
+								List<IAwarenessUser> users = new ArrayList<IAwarenessUser>();
+
+								if (awarenessEvent.getMessage() != null) {
+									chatArea.setText(oldText
+											+ awarenessEvent.getUser()
+													.getNickName() + ": "
+											+ awarenessEvent.getMessage()
+											+ "\n");
+									logger.debug("message sent from: "
+											+ awarenessEvent.getUser()
+													.getNickName()
+											+ " message: "
+											+ awarenessEvent.getMessage());
+								}
+
+							}
+						});
+
 					}
 				});
-				
-			}
-		});
-		
-		awarenessService.addAwarenessRosterListener(new IAwarenessRosterListener() {
-			
-			@Override
-			public void handleAwarenessRosterEvent(IAwarenessRosterEvent e) {
-				Collection<String> addresses = e.getAddresses();
-				for (String address : addresses) {
-					IAwarenessUser a = new AwarenessUser();
-					a.setNickName(address);
-					if(e.getMessage().equals(IAwarenessRosterEvent.ADD)){
-						
-						int indexOfBuddy = getIndexOfBuddy(a);
-						if( indexOfBuddy > -1) {
-							IAwarenessUser elementAt = (IAwarenessUser) buddyListModel.elementAt(indexOfBuddy);
-							elementAt.setPresence(IPresenceEvent.AVAILABLE);
-							buddyListModel.remove(indexOfBuddy);
-							buddyListModel.add(indexOfBuddy, elementAt);
-						} else {
-							a.setPresence(IPresenceEvent.AVAILABLE);
-							buddyListModel.addElement(a);
-						}
-						
-					} else if(e.getMessage().equals(IAwarenessRosterEvent.REMOVE)) {
-						
-						int indexOfBuddy = getIndexOfBuddy(a);
-						if( indexOfBuddy > -1) {
-							IAwarenessUser elementAt = (IAwarenessUser) buddyListModel.elementAt(indexOfBuddy);
-							elementAt.setPresence(IPresenceEvent.UNAVAILABLE);
-							buddyListModel.remove(indexOfBuddy);
-							buddyListModel.add(indexOfBuddy, elementAt);
-						} else {
-							a.setPresence(IPresenceEvent.UNAVAILABLE);
-							buddyListModel.addElement(a);
-						}
+
+		awarenessService
+				.addAwarenessPresenceListener(new IAwarenessPresenceListener() {
+					@Override
+					public void handleAwarenessPresenceEvent(
+							final IAwarePresenceEvent e) {
+						logger
+								.debug("registerChatArea: handleAwarenessPresenceEvent: smthg happened...: "
+										+ e.getUser().getNickName()
+										+ " : "
+										+ e.getUser().getPresence());
+
+						SwingUtilities.invokeLater(new Runnable() {
+							public void run() {
+
+								IAwarenessUser awarenessUser = e.getUser();
+
+								boolean isFound = false;
+								IAwarenessUser iau;
+								for (int i = 0; i < buddyListModel.getSize(); i++) {
+									iau = (IAwarenessUser) buddyListModel
+											.elementAt(i);
+									logger
+											.debug("registerChatArea: handleAwarenessPresenceEvent: "
+													+ iau.getNickName());
+									if (iau.getNickName().equals(
+											awarenessUser.getNickName())) {
+										((IAwarenessUser) buddyListModel
+												.elementAt(i))
+												.setPresence(awarenessUser
+														.getPresence());
+										isFound = true;
+									}
+								}
+							}
+						});
+
 					}
-				}
-				
-			}
-		});
-		
+				});
+
+		awarenessService
+				.addAwarenessRosterListener(new IAwarenessRosterListener() {
+
+					@Override
+					public void handleAwarenessRosterEvent(
+							IAwarenessRosterEvent e) {
+						Collection<String> addresses = e.getAddresses();
+						for (String address : addresses) {
+							final IAwarenessUser a = new AwarenessUser();
+							a.setNickName(address);
+							if (e.getMessage()
+									.equals(IAwarenessRosterEvent.ADD)) {
+
+								SwingUtilities.invokeLater(new Runnable() {
+									@Override
+									public void run() {
+
+										int indexOfBuddy = getIndexOfBuddy(a);
+										if (indexOfBuddy > -1) {
+											IAwarenessUser elementAt = (IAwarenessUser) buddyListModel
+													.elementAt(indexOfBuddy);
+											elementAt
+													.setPresence(IPresenceEvent.AVAILABLE);
+											buddyListModel.remove(indexOfBuddy);
+											buddyListModel.add(indexOfBuddy,
+													elementAt);
+										} else {
+											a
+													.setPresence(IPresenceEvent.AVAILABLE);
+											buddyListModel.addElement(a);
+										}
+
+									}
+								});
+
+							} else if (e.getMessage().equals(
+									IAwarenessRosterEvent.REMOVE)) {
+
+								SwingUtilities.invokeLater(new Runnable() {
+									@Override
+									public void run() {
+
+										int indexOfBuddy = getIndexOfBuddy(a);
+										if (indexOfBuddy > -1) {
+											IAwarenessUser elementAt = (IAwarenessUser) buddyListModel
+													.elementAt(indexOfBuddy);
+											elementAt
+													.setPresence(IPresenceEvent.UNAVAILABLE);
+											buddyListModel.remove(indexOfBuddy);
+											buddyListModel.add(indexOfBuddy,
+													elementAt);
+										} else {
+											a
+													.setPresence(IPresenceEvent.UNAVAILABLE);
+											buddyListModel.addElement(a);
+										}
+									}
+								});
+							}
+						}
+
+					}
+				});
+
 	}
-	
+
 	protected int getIndexOfBuddy(IAwarenessUser awarenessUser) {
 		Enumeration<?> elements = buddyListModel.elements();
 		while (elements.hasMoreElements()) {
-			  IAwarenessUser auser =  (IAwarenessUser) elements.nextElement();
-			  
-			  if( auser.getNickName() != null && auser.getNickName().equals(awarenessUser.getNickName())) {
-				  return buddyListModel.indexOf(auser);
-			  }
+			IAwarenessUser auser = (IAwarenessUser) elements.nextElement();
+
+			if (auser.getNickName() != null
+					&& auser.getNickName().equals(awarenessUser.getNickName())) {
+				return buddyListModel.indexOf(auser);
+			}
 		}
 		return -1;
 	}
-	
-	
+
 	public void connectToRoom() {
-		logger.debug("ChatController: Joining room with ELOUri: "+ getELOUri());
+		logger
+				.debug("ChatController: Joining room with ELOUri: "
+						+ getELOUri());
 		try {
 			this.getAwarenessService().joinMUCRoom(this.ELOUri);
 		} catch (AwarenessServiceException e) {
@@ -204,7 +253,6 @@ public class MUCChatController implements ChatController {
 		this.awarenessService = awarenessService;
 	}
 
-
 	public IAwarenessService getAwarenessService() {
 		return awarenessService;
 	}
@@ -213,7 +261,6 @@ public class MUCChatController implements ChatController {
 		ELOUri = eLOUri;
 	}
 
-	
 	public String getELOUri() {
 		return ELOUri;
 	}
@@ -226,8 +273,7 @@ public class MUCChatController implements ChatController {
 		return buddyListModel;
 	}
 
-	
 	public void sendMessage(IAwarenessUser recipient, String message) {
-		//for OOO chat
+		// for OOO chat
 	}
 }
