52d7be782980c924a03dc44994a5df866328fb38
hanbei <hanbei@gmx.de>
2010-02-05 10:15:13 +0000

    Implemented new ActionLog triggered ELOSave Agent

---
+++ sources/modules/agents/agents-base/src/main/java/eu/scy/agents/impl/AbstractELOSavedAgent.java
@@ -0,0 +1,96 @@
+package eu.scy.agents.impl;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import info.collide.sqlspaces.commons.Tuple;
+import info.collide.sqlspaces.commons.TupleSpaceException;
+import eu.scy.actionlogging.ActionTupleTransformer;
+import eu.scy.actionlogging.api.IAction;
+import eu.scy.agents.api.AgentLifecycleException;
+
+public abstract class AbstractELOSavedAgent extends AbstractThreadedAgent {
+
+	private static final String ELO_SAVED_ACTION = "elo_saved";
+	private static final String ACTION = "action";
+
+	private int listenerId;
+
+	// ("action":String, <ID>:String, <Timestamp>:long, elo_saved:String, <User>:String, <Tool>:String,
+	// <Mission>:String, <Session>:String, <Key=Value>:String*)
+	private Tuple eloSavedTupleTemplate = new Tuple(ACTION, String.class, Long.class, ELO_SAVED_ACTION, String.class,
+			String.class, String.class, String.class, String.class, String.class);
+
+	private static final Logger logger = Logger.getLogger(AbstractELOSavedAgent.class.getName());
+
+	protected AbstractELOSavedAgent(String name, String id) {
+		super(name, id);
+		initTSListener();
+	}
+
+	public AbstractELOSavedAgent(String name, String id, String tsHost, int tsPort) {
+		super(name, id, tsHost, tsPort);
+		initTSListener();
+	}
+
+	private void initTSListener() {
+		try {
+			listenerId = getActionSpace().eventRegister(Command.WRITE, eloSavedTupleTemplate, this, true);
+			logger.log(Level.FINEST, "Callback registered");
+		} catch (TupleSpaceException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	protected void doRun() throws TupleSpaceException, AgentLifecycleException, InterruptedException {
+		while (status == Status.Running) {
+			sendAliveUpdate();
+			Thread.sleep(AgentProtocol.ALIVE_INTERVAL - 1000);
+		}
+	}
+
+	@Override
+	protected void doStop() {
+		status = Status.Stopping;
+		if (listenerId != 0) {
+			try {
+				getActionSpace().eventDeRegister(listenerId);
+			} catch (TupleSpaceException e) {
+				logger.log(Level.SEVERE, e.getMessage());
+			}
+		}
+		logger.log(Level.FINE, name + " stopped");
+	}
+
+	@Override
+	protected Tuple getIdentifyTuple(String queryId) {
+		return null;
+	}
+
+	@Override
+	public boolean isStopped() {
+		return status == Status.Stopping;
+	}
+
+	@Override
+	public void call(Command command, int seq, Tuple afterTuple, Tuple beforeTuple) {
+		System.err.println("Processing tuple");
+		if (listenerId != seq) {
+			// If a callback arrives here that wasn't registered from this class it is passed to the
+			// AbstractThreadedAgent.
+			logger.log(Level.FINEST, "Callback passed to Superclass.");
+			super.call(command, seq, afterTuple, beforeTuple);
+			return;
+		}
+		IAction action = ActionTupleTransformer.getActionFromTuple(afterTuple);
+		if (!ELO_SAVED_ACTION.equals(action.getType())) {
+			logger.warning("Trying to process action log that does not match elo_saved signature. Type: "
+					+ action.getType());
+		} else {
+			processELOSavedAction(action);
+		}
+	}
+
+	protected abstract void processELOSavedAction(IAction action);
+}
