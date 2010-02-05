402d065135016606d14a1dee1786e71d5fa6fc76
hanbei <hanbei@gmx.de>
2010-02-05 10:18:07 +0000

    changed Agents to use new AbstractELOSavedAgent

---
+++ sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/ActionLogConstants.java
@@ -0,0 +1,12 @@
+package eu.scy.agents;
+
+public final class ActionLogConstants {
+
+	public static final String ELO_TYPE = "type";
+	public static final String ELO_URI = "elouri";
+
+	private ActionLogConstants() {
+		// do nothing
+	}
+
+}
--- sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/topics/TextForTopicSaved.java
+++ sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/topics/TextForTopicSaved.java
@@ -3,53 +3,52 @@ package eu.scy.agents.topics;
 import info.collide.sqlspaces.commons.Tuple;
 import info.collide.sqlspaces.commons.TupleSpaceException;
 
-import java.net.URI;
-import java.rmi.dgc.VMID;
+import java.util.Map;
 
-import roolo.elo.api.IELO;
-import roolo.elo.api.IMetadataValueContainer;
-import roolo.elo.api.metadata.CoreRooloMetadataKeyIds;
-import eu.scy.agents.impl.elo.AbstractELOAgent;
+import eu.scy.actionlogging.api.IAction;
+import eu.scy.agents.ActionLogConstants;
+import eu.scy.agents.impl.AbstractELOSavedAgent;
 
-public class TextForTopicSaved extends AbstractELOAgent {
+public class TextForTopicSaved extends AbstractELOSavedAgent {
 
-	private static final String NAME = "eu.scy.agents.topics.TextForTopicSaved";
+	public static final String NAME = TextForTopicSaved.class.getName();
 
-	public TextForTopicSaved() {
-		super(NAME, new VMID().toString());
-	}
-
-	public TextForTopicSaved(String tsHost, int tsPort) {
-		super(NAME, new VMID().toString(), tsHost, tsPort);
-	}
-
-	@Override
-	public void processElo(IELO elo) {
-		if (elo != null) {
-			if (isValidType(elo)) {
-				URI eloUri = elo.getUri();
-				try {
-					getCommandSpace().write(
-							new Tuple(TopicAgents.TOPIC_DETECTOR, eloUri
-									.toString()));
-				} catch (TupleSpaceException e) {
-					e.printStackTrace();
-				}
-			}
+	public TextForTopicSaved(Map<String, Object> params) {
+		super(NAME, (String) params.get("id"));
+		if (params.containsKey("tsHost")) {
+			host = (String) params.get("tsHost");
+		}
+		if (params.containsKey("tsPort")) {
+			port = (Integer) params.get("tsPort");
 		}
 	}
 
-	private boolean isValidType(IELO elo) {
-		IMetadataValueContainer type = elo
-				.getMetadata()
-				.getMetadataValueContainer(
-						metadataTypeManager
-								.getMetadataKey(CoreRooloMetadataKeyIds.TECHNICAL_FORMAT
-										.getId()));
-		if ("scy/text".equals(type.getValue())) {
+	private boolean isValidType(String eloType) {
+		if ("scy/text".equals(eloType)) {
 			return true;
 		}
 		return false;
 	}
 
+	@Override
+	protected void processELOSavedAction(IAction action) {
+		String eloUri = action.getAttribute(ActionLogConstants.ELO_URI);
+		String eloType = action.getAttribute(ActionLogConstants.ELO_TYPE);
+		System.out.println(eloUri);
+		System.out.println(eloType);
+		if (isValidType(eloType)) {
+			writeELOSavedTuple(eloUri);
+		}
+	}
+
+	private void writeELOSavedTuple(String eloUri) {
+		System.out.println("writing tuple");
+		try {
+			getCommandSpace().write(new Tuple(TopicAgents.TOPIC_DETECTOR, eloUri));
+		} catch (TupleSpaceException e) {
+			System.out.println("error");
+			e.printStackTrace();
+		}
+		System.out.println("tuple written");
+	}
 }
