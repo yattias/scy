7cbc7031decc12b6ca59a72c455785dd04a0056e
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-02-08 12:13:02 +0000

    Added SCYAuthor

---
+++ trunk/sources/modules/server/scy-author/src/main/java/eu/scy/server/controllers/ScyIndexController.java
@@ -0,0 +1,22 @@
+package eu.scy.server.controllers;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 08.feb.2010
+ * Time: 10:26:12
+ * To change this template use File | Settings | File Templates.
+ */
+public class ScyIndexController extends AbstractController {
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView modelAndView = new ModelAndView();
+        return modelAndView;
+    }
+}
---
+++ trunk/sources/modules/server/scy-author/src/main/resources/webapp/WEB-INF/jsp/scy/app/scyauthor/ScyAuthorIndex.jsp
@@ -0,0 +1,1 @@
+scyauthor!          Is here foreva! ooh
