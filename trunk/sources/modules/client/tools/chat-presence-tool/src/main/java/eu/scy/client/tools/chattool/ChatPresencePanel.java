4a3fb8b3ff77ad4d3c0d408fbd894707cf7c78a0
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-02-09 09:46:34 +0000

    removed logger

--- trunk/sources/modules/client/tools/chat-presence-tool/src/main/java/eu/scy/client/tools/chattool/ChatPresencePanel.java
+++ trunk/sources/modules/client/tools/chat-presence-tool/src/main/java/eu/scy/client/tools/chattool/ChatPresencePanel.java
@@ -22,69 +22,71 @@ import eu.scy.chat.controller.ChatController;
 
 /**
  * @author jeremyt
- *
  */
 public class ChatPresencePanel extends JPanel {
 
-	private static final long serialVersionUID = 1L;
-	private static final Logger logger = Logger.getLogger(ChatPresencePanel.class.getName());
+    private static final long serialVersionUID = 1L;
+    private static final Logger logger = Logger.getLogger(ChatPresencePanel.class.getName());
 
-	protected JList buddyList;
-	static ChatPresencePanel cmp;
-	JXTitledPanel chatAreaPanel;
-	private ChatController chatController;
-	private DefaultListModel model = new DefaultListModel();
-	private JScrollPane buddyListScroll;
-
-	
-	public ChatPresencePanel(ChatController mucChatController) {
-		this.chatController = mucChatController;
-		//updateModel();
-		logger.debug("ChatPresencePanel: starting ... ");
-		logger.debug("ChatPresencePanel: awareness: awarenessService.isConnected(): "+ this.chatController.getAwarenessService().isConnected());
-		initGUI();
-		
-	}
-	
-	
-	protected void initGUI() {
-		this.setLayout(new MigLayout("insets 0 0 0 0, wrap 1"));
-		this.setBackground(Color.white);
-		this.add(createBuddyListPanel(), "align center,grow");
-	}
-
-	protected JPanel createBuddyListPanel() {
-		//JPanel buddyPanel = new JPanel(new MigLayout("insets 0 0 0 0,wrap 1"));
-		JPanel buddyPanel = new JPanel();
-		buddyPanel.setBackground(Color.white);
-		//buddyPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		buddyList = new JList(chatController.getBuddyListModel());
-		buddyList.setBorder(BorderFactory.createEmptyBorder());
-		buddyList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
-		buddyList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		buddyList.setVisibleRowCount(1);
-		buddyList.setCellRenderer(new BuddyListRenderer());
-		//updateModel();
-
-		buddyListScroll = new JScrollPane(buddyList);
-		buddyListScroll.setBorder(BorderFactory.createEmptyBorder());
-		buddyListScroll.setPreferredSize(new Dimension(200, 75));
-
-		buddyPanel.add(buddyListScroll);
-		return buddyPanel;
-	}
-	
-	public void resizeChat(int newWidth, int newHeight) {
-		this.buddyListScroll.setPreferredSize(new Dimension(newWidth, newHeight));
-	}
-
-	public void setChatController(ChatController chatController) {
-		this.chatController = chatController;
-	}
+    protected JList buddyList;
+    static ChatPresencePanel cmp;
+    JXTitledPanel chatAreaPanel;
+    private ChatController chatController;
+    private DefaultListModel model = new DefaultListModel();
+    private JScrollPane buddyListScroll;
 
 
-	public ChatController getChatController() {
-		return chatController;
-	}
-	
+    public ChatPresencePanel(ChatController mucChatController) {
+        this.chatController = mucChatController;
+        //updateModel();
+        if (logger != null) {
+            logger.debug("ChatPresencePanel: starting ... ");
+            logger.debug("ChatPresencePanel: awareness: awarenessService.isConnected(): " + this.chatController.getAwarenessService().isConnected());
+        }
+
+        initGUI();
+
+    }
+
+
+    protected void initGUI() {
+        this.setLayout(new MigLayout("insets 0 0 0 0, wrap 1"));
+        this.setBackground(Color.white);
+        this.add(createBuddyListPanel(), "align center,grow");
+    }
+
+    protected JPanel createBuddyListPanel() {
+        //JPanel buddyPanel = new JPanel(new MigLayout("insets 0 0 0 0,wrap 1"));
+        JPanel buddyPanel = new JPanel();
+        buddyPanel.setBackground(Color.white);
+        //buddyPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        buddyList = new JList(chatController.getBuddyListModel());
+        buddyList.setBorder(BorderFactory.createEmptyBorder());
+        buddyList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+        buddyList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        buddyList.setVisibleRowCount(1);
+        buddyList.setCellRenderer(new BuddyListRenderer());
+        //updateModel();
+
+        buddyListScroll = new JScrollPane(buddyList);
+        buddyListScroll.setBorder(BorderFactory.createEmptyBorder());
+        buddyListScroll.setPreferredSize(new Dimension(200, 75));
+
+        buddyPanel.add(buddyListScroll);
+        return buddyPanel;
+    }
+
+    public void resizeChat(int newWidth, int newHeight) {
+        this.buddyListScroll.setPreferredSize(new Dimension(newWidth, newHeight));
+    }
+
+    public void setChatController(ChatController chatController) {
+        this.chatController = chatController;
+    }
+
+
+    public ChatController getChatController() {
+        return chatController;
+    }
+
 }
