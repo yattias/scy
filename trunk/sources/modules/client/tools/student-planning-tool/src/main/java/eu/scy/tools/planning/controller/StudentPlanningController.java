45081db56effd0143629e1de9b7971690b7d3c2f
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-02-09 11:33:30 +0000

    new stuff

---
+++ trunk/sources/modules/client/tools/student-planning-tool/src/main/java/eu/scy/tools/planning/controller/StudentPlanningController.java
@@ -0,0 +1,152 @@
+package eu.scy.tools.planning.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.jdesktop.swingx.JXTaskPane;
+import org.jdesktop.swingx.JXTaskPaneContainer;
+import org.springframework.remoting.httpinvoker.HttpInvokerProxyFactoryBean;
+
+import eu.scy.core.model.impl.ScyBaseObject;
+import eu.scy.core.model.student.StudentPlanELO;
+import eu.scy.core.model.student.StudentPlannedActivity;
+import eu.scy.server.pedagogicalplan.PedagogicalPlanService;
+import eu.scy.server.pedagogicalplan.StudentPedagogicalPlanService;
+import eu.scy.toolbroker.ToolBrokerImpl;
+
+public class StudentPlanningController {
+
+	private static Logger log = Logger
+			.getLogger("StudentPlanningController.class");
+
+	private StudentPedagogicalPlanService studentPedagogicalPlanService;
+	// private UserDAO userDAOHibernate;
+	private PedagogicalPlanService pedagogicalPlanService;
+	private List<StudentPlannedActivity> studentPlannedActivities;
+	private StudentPlanELO studentPlanELO;
+	private List<JXTaskPane> taskPanes = new ArrayList<JXTaskPane>();
+	private List<JXTaskPaneContainer> taskContainers = new ArrayList<JXTaskPaneContainer>();
+
+	public StudentPlanningController() {
+
+	}
+
+	public StudentPlanningController(ToolBrokerImpl toolbroker) {
+		log.info("Starting Student Planning Controller...");
+		studentPedagogicalPlanService = getStudentPlanService();
+
+		if (studentPedagogicalPlanService == null)
+			throw new RuntimeException("STUDENT SERVICE IS NULL!!!! LAME");
+
+		// get the user from the tool broker
+
+		log.info("fetching student plans");
+		List<StudentPlanELO> studentPlans = studentPedagogicalPlanService
+				.getStudentPlans("wiwoo");
+
+		//if( studentPlans.size() > 0 ) {
+		 setStudentPlanELO(studentPlans.get(0));
+         System.out.println("PLAN:" + getStudentPlanELO());
+         
+		for (StudentPlanELO studentPlanELO : studentPlans) {
+			this.dumpStudentPlan(studentPlanELO);
+		}
+		//}
+
+		// pedagogicalPlanService = toolbroker.getPedagogicalPlanService();
+		// pedagogicalPlanService.getPedagogicalPlan(mission, scenario);
+		// how do i get the current ped plan if dont know what mission i am
+		// currently logined in as. QUESTION, how do i figure out which mission
+		// am in from the tool broker? or in general. because if
+		// getPedagogicalPlan is accessable from toolbroker and it needs a
+		// mission how do i get it?
+		// do i get the ped plan that student is currently on and find the
+		// studentPlanELO that is assocaiated with it?
+		// PedagogicalPlan pedagogicalPlan =
+		// pedagogicalPlanService.getPedagogicalPlan(null, null);
+
+		log.info("fetch done......");
+
+	}
+
+	public void addTaskPane(JXTaskPane taskPane) {
+		taskPanes.add(taskPane);
+	}
+
+	public void removeTaskPane(JXTaskPane taskPane) {
+		taskPanes.remove(taskPane);
+	}
+
+	public void collapseAllExcept(String name) {
+		for (JXTaskPane tpane : taskPanes) {
+			if (!tpane.getName().equals(name)) {
+				tpane.setCollapsed(true);
+			}
+
+		}
+	}
+	public StudentPedagogicalPlanService getStudentPlanService() {
+
+		// service =
+		// getWithUrl("http://localhost:8080/server-external-components/remoting/studentPlan-httpinvoker");
+		if (studentPedagogicalPlanService == null)
+			studentPedagogicalPlanService = getWithUrl("http://scy.collide.info:8080/extcomp/remoting/studentPlan-httpinvoker");
+
+		return studentPedagogicalPlanService;
+
+	}
+
+	private StudentPedagogicalPlanService getWithUrl(String url) {
+		HttpInvokerProxyFactoryBean fb = new HttpInvokerProxyFactoryBean();
+		fb.setServiceInterface(StudentPedagogicalPlanService.class);
+		fb.setServiceUrl(url);
+		fb.afterPropertiesSet();
+		return (StudentPedagogicalPlanService) fb.getObject();
+	}
+
+	public List<StudentPlannedActivity> getStudentPlannedActivities() {
+		return studentPlanELO.getStudentPlannedActivities();
+	}
+
+	public void dumpStudentPlan(StudentPlanELO studentPlan) {
+		System.out.println("Dumping student plan");
+		System.out.println("id:" + studentPlan.getId());
+		System.out.println("description:" + studentPlan.getDescription());
+		System.out.println("name " + studentPlan.getName());
+		System.out.println("number of activities: "
+				+ studentPlan.getStudentPlannedActivities().size());
+		for (StudentPlannedActivity activity : studentPlan
+				.getStudentPlannedActivities()) {
+			dumpStudentActivity(activity);
+		}
+	}
+
+	public void dumpStudentActivity(StudentPlannedActivity studentActivity) {
+		System.out.println("Dumping activity");
+		System.out.println("id: " + studentActivity.getId());
+		// System.out.println("start date: " +
+		// studentActivity.getStartDate().toString());
+		// System.out.println("end date: " +
+		// studentActivity.getEndDate().toString());
+		// System.out.println("name: " + studentActivity.getName());
+		// System.out.println("note: " + studentActivity.getNote());
+		// System.out.println("members: " +
+		// studentActivity.getMembers().toString());
+		System.out.println("anchor elo: "
+				+ studentActivity.getAssoicatedELO().toString());
+
+	}
+
+	public void setStudentPlanELO(StudentPlanELO studentPlanELO) {
+		this.studentPlanELO = studentPlanELO;
+	}
+
+	public StudentPlanELO getStudentPlanELO() {
+		return studentPlanELO;
+	}
+
+	public void saveStudentActivity(StudentPlannedActivity studenPlannedActivity) {
+		this.studentPedagogicalPlanService.save((ScyBaseObject) studenPlannedActivity);
+	}
+}
