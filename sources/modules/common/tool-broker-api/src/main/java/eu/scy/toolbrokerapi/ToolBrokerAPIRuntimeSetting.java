428de985f1ed91199afd79157b6a19dc071ccc69
jakob.sikken <jakob.sikken@gmail.com>
2010-02-15 17:07:07 +0000

    Added ToolBrokerAPIRuntimeSetting interface.

---
+++ sources/modules/common/tool-broker-api/src/main/java/eu/scy/toolbrokerapi/ToolBrokerAPIRuntimeSetting.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eu.scy.toolbrokerapi;
+
+/**
+ *
+ * @author sikken
+ */
+public interface ToolBrokerAPIRuntimeSetting {
+
+   public void setMissionId(String missionId);
+}
