e394c1f39f3c16eda0cb6913696d8f2584b160a9
jt151077 <jt151077@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-02-25 12:18:31 +0000

    fixed resizing

--- trunk/sources/modules/client/tools/student-planning-tool/src/test/java/eu/scy/tools/planning/test/SampleLASTestClient.java
+++ trunk/sources/modules/client/tools/student-planning-tool/src/test/java/eu/scy/tools/planning/test/SampleLASTestClient.java
@@ -1,5 +1,8 @@
 package eu.scy.tools.planning.test;
 
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
@@ -15,6 +18,10 @@ public class SampleLASTestClient {
 	/** simple main driver for this class */
 	public static void main(String[] args) {
 		
+		 StudentPlanningController spc = new StudentPlanningController(null);
+	     final StudentPlanningTool studentPlanningToolMain = new StudentPlanningTool(spc);
+	     final JComponent createStudentPlanningPanel = studentPlanningToolMain.createStudentPlanningPanel();
+	     
 //		 StudentPlanningController spc = new StudentPlanningController(null);
 //	      StudentPlanningTool studentPlanningToolMain = new StudentPlanningTool(spc);
 //	      studentPlanningToolMain.launchInFrame()
@@ -24,15 +31,42 @@ public class SampleLASTestClient {
 	    
 	    	JFrame frame = new JFrame("Planning Tool");
 			frame.setLayout(new MigLayout("insets 0 0 0 0"));
+			frame.addComponentListener(new ComponentListener() {
+				
+				@Override
+				public void componentShown(ComponentEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void componentResized(ComponentEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void componentMoved(ComponentEvent e) {
+					// TODO Auto-generated method stub
+					System.out.println(e.getComponent().getClass().getName() + " --- Resized ");     
+					JFrame jf = (JFrame) e.getSource();
+					studentPlanningToolMain.resizeSPT(jf.getWidth(),jf.getHeight());
+				}
+				
+				@Override
+				public void componentHidden(ComponentEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+			});
 //			JScrollPane scrollPane = new JScrollPane(doInit());
 //			
 //			scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 //			scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 			
-			 StudentPlanningController spc = new StudentPlanningController(null);
-		      StudentPlanningTool studentPlanningToolMain = new StudentPlanningTool(spc);
+			
 		      
-		      JComponent createStudentPlanningPanel = studentPlanningToolMain.createStudentPlanningPanel();
+		      
 			frame.add(createStudentPlanningPanel);
 			//frame.add(studentPlanningToolMain.createDragPanel());
 			frame.setPreferredSize(createStudentPlanningPanel.getPreferredSize());
