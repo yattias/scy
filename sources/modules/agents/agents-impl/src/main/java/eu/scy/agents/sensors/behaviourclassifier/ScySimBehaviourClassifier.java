b61c049ac64e7e886c3e6d1ce3f313a20ad70935
janengler80 <janengler80@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-03-03 00:46:50 +0000

    Only if {Votat|UserExp|Canonical}-value changes something happens

--- sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/sensors/behaviourclassifier/ScySimBehaviourClassifier.java
+++ sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/sensors/behaviourclassifier/ScySimBehaviourClassifier.java
@@ -21,125 +21,137 @@ import eu.scy.agents.impl.AgentProtocol;
 
 public class ScySimBehaviourClassifier extends AbstractThreadedAgent implements Callback {
 
-	private TupleSpace commandSpace;
+    private TupleSpace commandSpace;
 
-	private static final Level DEBUGLEVEL = Level.FINE;
+    private static final Level DEBUGLEVEL = Level.FINE;
 
-	private static final Logger logger = Logger.getLogger(ScySimBehaviourClassifier.class.getName());
+    private static final Logger logger = Logger.getLogger(ScySimBehaviourClassifier.class.getName());
 
-	private boolean isStopped;
+    private boolean isStopped;
 
-	private static final Tuple USER_EXP_TEMPLATE = new Tuple("user_exp", Field.createWildCardField());
+    private static final Tuple USER_EXP_TEMPLATE = new Tuple("user_exp", Field.createWildCardField());
 
-	private static final Tuple VOTAT_TEMPLATE = new Tuple("votat", Field.createWildCardField());
+    private static final Tuple VOTAT_TEMPLATE = new Tuple("votat", Field.createWildCardField());
 
-	private static final Tuple CANONICAL_TEMPLATE = new Tuple("inc_change", Field.createWildCardField());
+    private static final Tuple CANONICAL_TEMPLATE = new Tuple("inc_change", Field.createWildCardField());
 
-	private Map<String, BehavioralModel> userModels;
+    private Map<String, BehavioralModel> userModels;
 
-	private int userExpSeq;
+    private int userExpSeq;
 
-	private int votatSeq;
+    private int votatSeq;
 
-	private int canoSeq;
+    private int canoSeq;
 
-	private static final double MAX_EXP_TIME = 1 * 60 * 60 * 1000;
+    private static final double MAX_EXP_TIME = 1 * 60 * 60 * 1000;
 
-	public ScySimBehaviourClassifier(Map<String, Object> map) {
-		super("eu.scy.agents.serviceprovider.userexperience.ScySimBehaviourClassifier", (String) map
-				.get(AgentProtocol.PARAM_AGENT_ID), (String) map.get(AgentProtocol.TS_HOST), (Integer) map
-				.get(AgentProtocol.TS_PORT));
-		try {
-			commandSpace = new TupleSpace(new User(getSimpleName()), host, port, false, false,
-					AgentProtocol.COMMAND_SPACE_NAME);
-			userExpSeq = commandSpace.eventRegister(Command.WRITE, USER_EXP_TEMPLATE, this, true);
-			votatSeq = commandSpace.eventRegister(Command.WRITE, VOTAT_TEMPLATE, this, true);
-			canoSeq = commandSpace.eventRegister(Command.WRITE, CANONICAL_TEMPLATE, this, true);
-			userModels = new HashMap<String, BehavioralModel>();
-			initLogger();
-		} catch (TupleSpaceException e) {
-			e.printStackTrace();
-		}
-	}
+    private int lastVotat;
 
-	@Override
-	protected void doRun() throws TupleSpaceException, AgentLifecycleException, InterruptedException {
-		while (status == Status.Running) {
-			sendAliveUpdate();
-			Thread.sleep(5000);
-		}
+    private int lastCanonical;
 
-	}
+    private int lastUserExp;
 
-	@Override
-	protected void doStop() {
-		status = Status.Stopping;
-		try {
-			commandSpace.disconnect();
-		} catch (TupleSpaceException e) {
-			e.printStackTrace();
-		}
-		isStopped = true;
-	}
+    public ScySimBehaviourClassifier(Map<String, Object> map) {
+        super("eu.scy.agents.serviceprovider.userexperience.ScySimBehaviourClassifier", (String) map.get(AgentProtocol.PARAM_AGENT_ID), (String) map.get(AgentProtocol.TS_HOST), (Integer) map.get(AgentProtocol.TS_PORT));
+        try {
+            commandSpace = new TupleSpace(new User(getSimpleName()), host, port, false, false, AgentProtocol.COMMAND_SPACE_NAME);
+            userExpSeq = commandSpace.eventRegister(Command.WRITE, USER_EXP_TEMPLATE, this, true);
+            votatSeq = commandSpace.eventRegister(Command.WRITE, VOTAT_TEMPLATE, this, true);
+            canoSeq = commandSpace.eventRegister(Command.WRITE, CANONICAL_TEMPLATE, this, true);
+            userModels = new HashMap<String, BehavioralModel>();
+            initLogger();
+        } catch (TupleSpaceException e) {
+            e.printStackTrace();
+        }
+    }
 
-	@Override
-	protected Tuple getIdentifyTuple(String queryId) {
-		// This agent is just a sensor and therefore it don't need to identify....
-		return null;
-	}
+    @Override
+    protected void doRun() throws TupleSpaceException, AgentLifecycleException, InterruptedException {
+        while (status == Status.Running) {
+            sendAliveUpdate();
+            Thread.sleep(5000);
+        }
 
-	@Override
-	public boolean isStopped() {
-		return isStopped;
-	}
+    }
 
-	private void initLogger() {
-		ConsoleHandler cH = new ConsoleHandler();
-		SimpleFormatter sF = new SimpleFormatter();
-		cH.setFormatter(sF);
-		cH.setLevel(DEBUGLEVEL);
-		logger.setLevel(DEBUGLEVEL);
-		logger.addHandler(cH);
-	}
+    @Override
+    protected void doStop() {
+        status = Status.Stopping;
+        try {
+            commandSpace.disconnect();
+        } catch (TupleSpaceException e) {
+            e.printStackTrace();
+        }
+        isStopped = true;
+    }
 
-	public BehavioralModel getModel(String user, String tool, String session, String mission) {
-		BehavioralModel model = userModels.get(user + "/" + tool + "/" + mission + "/" + session);
-		if (model == null) {
-			model = new BehavioralModel(user, tool, mission, session, 0, 0, 0, commandSpace);
-			userModels.put(user + "/" + tool + "/" + mission + "/" + session, model);
-			logger.log(Level.FINE, "New Model for " + user + " with tool " + tool + " created...");
-		}
-		return model;
+    @Override
+    protected Tuple getIdentifyTuple(String queryId) {
+        // This agent is just a sensor and therefore it don't need to identify....
+        return null;
+    }
 
-	}
+    @Override
+    public boolean isStopped() {
+        return isStopped;
+    }
 
-	@Override
-	public void call(Command cmd, int seqnum, Tuple afterTuple, Tuple beforeTuple) {
-		String user = afterTuple.getField(1).getValue().toString();
-		String tool = afterTuple.getField(2).getValue().toString();
-		String mission = afterTuple.getField(3).getValue().toString();
-		String session = afterTuple.getField(4).getValue().toString();
-		BehavioralModel model = null;
-		if (seqnum == votatSeq) {
-			int newVotat = ((Double) afterTuple.getField(6).getValue()).intValue();
-			model = getModel(user, tool, mission, session);
-			model.updateVotat(newVotat);
+    private void initLogger() {
+        ConsoleHandler cH = new ConsoleHandler();
+        SimpleFormatter sF = new SimpleFormatter();
+        cH.setFormatter(sF);
+        cH.setLevel(DEBUGLEVEL);
+        logger.setLevel(DEBUGLEVEL);
+        logger.addHandler(cH);
+    }
 
-		} else if (seqnum == userExpSeq) {
-			int newUserExp = ((Long) afterTuple.getField(6).getValue()).intValue();
-			int l = (int) (newUserExp / MAX_EXP_TIME * 100);
-			l = Math.min(l, 100);
-			model = getModel(user, tool, mission, session);
-			model.updateUserExp(l);
+    public BehavioralModel getModel(String user, String tool, String session, String mission) {
+        BehavioralModel model = userModels.get(user + "/" + tool + "/" + mission + "/" + session);
+        if (model == null) {
+            model = new BehavioralModel(user, tool, mission, session, 0, 0, 0, commandSpace);
+            userModels.put(user + "/" + tool + "/" + mission + "/" + session, model);
+            logger.log(Level.FINE, "New Model for " + user + " with tool " + tool + " created...");
+        }
+        return model;
 
-		} else if (seqnum == canoSeq) {
-			int newCanonical = ((Double) afterTuple.getField(6).getValue()).intValue();
-			model = getModel(user, tool, mission, session);
-			model.updateCanonical(newCanonical);
+    }
 
-		} else {
-			System.err.println("Callback without registered seqnum arrived!");
-		}
-	}
+    @Override
+    public void call(Command cmd, int seqnum, Tuple afterTuple, Tuple beforeTuple) {
+        String user = afterTuple.getField(1).getValue().toString();
+        String tool = afterTuple.getField(2).getValue().toString();
+        String mission = afterTuple.getField(3).getValue().toString();
+        String session = afterTuple.getField(4).getValue().toString();
+        BehavioralModel model = null;
+        if (seqnum == votatSeq) {
+            int newVotat = ((Double) afterTuple.getField(6).getValue()).intValue();
+            if (newVotat != lastVotat) {
+                model = getModel(user, tool, mission, session);
+                model.updateVotat(newVotat);
+            }
+            lastVotat = newVotat;
+
+        } else if (seqnum == userExpSeq) {
+            int newUserExp = ((Long) afterTuple.getField(6).getValue()).intValue();
+            if (newUserExp != lastUserExp) {
+                int l = (int) (newUserExp / MAX_EXP_TIME * 100);
+                l = Math.min(l, 100);
+                model = getModel(user, tool, mission, session);
+                model.updateUserExp(l);
+            }
+            lastUserExp = newUserExp;
+
+        } else if (seqnum == canoSeq) {
+            int newCanonical = ((Double) afterTuple.getField(6).getValue()).intValue();
+            if (newCanonical != lastCanonical) {
+                model = getModel(user, tool, mission, session);
+                model.updateCanonical(newCanonical);
+            }
+            lastCanonical = newCanonical;
+
+        } else {
+            System.err.println("Callback without registered seqnum arrived!");
+        }
+    }
 
 }
