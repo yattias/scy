a7350e1bf013aa2e5f539d135e4d5a09e74acd1c
hanbei <hanbei@gmx.de>
2010-03-03 08:34:32 +0000

    injecting metadataTypeManager and repository in PAF

--- sources/modules/server/scy-server-utils/src/main/java/eu/scy/server/externalcomponents/agentframework/AgentFrameworkComponent.java
+++ sources/modules/server/scy-server-utils/src/main/java/eu/scy/server/externalcomponents/agentframework/AgentFrameworkComponent.java
@@ -6,6 +6,9 @@ import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
 
+import roolo.api.IRepository;
+import roolo.elo.api.IMetadataTypeManager;
+
 import eu.scy.agents.api.AgentLifecycleException;
 import eu.scy.agents.impl.AgentStartConfiguration;
 import eu.scy.agents.impl.manager.AgentManager;
@@ -14,58 +17,76 @@ import eu.scy.server.externalcomponents.IExternalComponent;
 
 public class AgentFrameworkComponent implements IExternalComponent {
 
-    private Logger log = Logger.getLogger("AgentFrameworkComponent.class");
+	private Logger log = Logger.getLogger("AgentFrameworkComponent.class");
 
-    private AgentManager agentFramework;
+	private AgentManager agentFramework;
 
-    private Set<AgentStartConfiguration> initialAgents;
+	private Set<AgentStartConfiguration> initialAgents;
 
-    @Override
-    public void startComponent() throws ExternalComponentFailedException {
-        log.info("Initializing AGENT FRAMEWORK");
+	private IRepository repository;
 
-        String tsHost = "localhost"; // will this ever be something different?
-        int tsPort = Configuration.getConfiguration().getNonSSLPort();
-        agentFramework = new AgentManager(tsHost, tsPort);
-        agentFramework.setRepository(null); // TODO: inject via spring
-        agentFramework.setMetadataTypeManager(null); // TODO: inject via spring
+	private IMetadataTypeManager metadataTypeManager;
 
-        for (AgentStartConfiguration conf : initialAgents) {
-            Map<String, Object> params = conf.getProperties();
-            if (!params.containsKey("tsHost")) {
-                params.put("tsHost", tsHost);
-            }
-            if (!params.containsKey("tsPort")) {
-                params.put("tsPort", tsPort);
-            }
-            try {
-                log.info("Starting agent '" + conf.getClassName() + "'");
-                agentFramework.startAgent(conf.getClassName(), params);
-            } catch (AgentLifecycleException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+	@Override
+	public void startComponent() throws ExternalComponentFailedException {
+		log.info("Initializing AGENT FRAMEWORK");
 
-    @Override
-    public void stopComponent() throws ExternalComponentFailedException {
-        log.info("-----> STOPPING: AGENT FRAMEWORK");
-        agentFramework.cleanUp();
-    }
+		String tsHost = "localhost"; // will this ever be something different?
+		int tsPort = Configuration.getConfiguration().getNonSSLPort();
+		agentFramework = new AgentManager(tsHost, tsPort);
+		agentFramework.setRepository(repository);
+		agentFramework.setMetadataTypeManager(metadataTypeManager);
 
-    /**
-     * @return the initialAgents
-     */
-    public Set<AgentStartConfiguration> getInitialAgents() {
-        return initialAgents;
-    }
+		for (AgentStartConfiguration conf : initialAgents) {
+			Map<String, Object> params = conf.getProperties();
+			if (!params.containsKey("tsHost")) {
+				params.put("tsHost", tsHost);
+			}
+			if (!params.containsKey("tsPort")) {
+				params.put("tsPort", tsPort);
+			}
+			try {
+				log.info("Starting agent '" + conf.getClassName() + "'");
+				agentFramework.startAgent(conf.getClassName(), params);
+			} catch (AgentLifecycleException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-    /**
-     * @param initialAgents
-     *            the initialAgents to set
-     */
-    public void setInitialAgents(Set<AgentStartConfiguration> initialAgents) {
-        this.initialAgents = initialAgents;
-    }
+	@Override
+	public void stopComponent() throws ExternalComponentFailedException {
+		log.info("-----> STOPPING: AGENT FRAMEWORK");
+		agentFramework.cleanUp();
+	}
 
+	/**
+	 * @return the initialAgents
+	 */
+	public Set<AgentStartConfiguration> getInitialAgents() {
+		return initialAgents;
+	}
+
+	/**
+	 * @param initialAgents the initialAgents to set
+	 */
+	public void setInitialAgents(Set<AgentStartConfiguration> initialAgents) {
+		this.initialAgents = initialAgents;
+	}
+
+	public void setRepository(IRepository repo) {
+		repository = repo;
+	}
+
+	public IMetadataTypeManager getMetadataTypeManager() {
+		return metadataTypeManager;
+	}
+
+	public void setMetadataTypeManager(IMetadataTypeManager metadataTypeManager) {
+		this.metadataTypeManager = metadataTypeManager;
+	}
+
+	public IRepository getRepository() {
+		return repository;
+	}
 }
