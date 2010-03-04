15ffda44c8cbd1f8d19bbdd74a777ba81643b532
adam.giemza <adam.giemza@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-03-04 06:43:22 +0000

    Changed buddyfier to set subscription state to both. Still issues with nicknames :(

--- trunk/sources/external/buddyfier/src/main/java/eu/scy/jabber/Buddyfier.java
+++ trunk/sources/external/buddyfier/src/main/java/eu/scy/jabber/Buddyfier.java
@@ -10,6 +10,8 @@ import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.Roster;
 import org.jivesoftware.smack.RosterEntry;
 import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.Roster.SubscriptionMode;
+import org.jivesoftware.smack.packet.Presence;
 
 /**
  * @author giemza
@@ -32,10 +34,10 @@ public class Buddyfier {
 		logger.debug("Buddyfier started!");
 		
 //		String[] users = {"adam", "alfons", "andreas", "barbara", "bjoerge", "david", "jan", "jeremy", "jakob", "lars", "marjolaine", "stefan", "ton", "tony", "wolfgang", "wouter"};
-		String[] users = {"hhh", "hillary", "adie", "lars"};
+		String[] users = {"aaa1", "aaa2", "aaa3"};
 		
-		//String host = "scy.collide.info";
-		String host = "83.168.205.138";
+		String host = "scy.collide.info";
+//		String host = "83.168.205.138";
 
 		ConnectionConfiguration config = new ConnectionConfiguration(host, 5222);
 		config.setCompressionEnabled(true);
@@ -47,19 +49,50 @@ public class Buddyfier {
         	connection = new XMPPConnection(config);
         	connection.connect();
         	logger.debug("Connected to server.");
-        	connection.login(user, user);
+        	connection.login(user, user, "buddyfier");
         	logger.debug("Logged in as " + connection.getUser());
         	
         	Roster roster = connection.getRoster();
+        	roster.setSubscriptionMode(SubscriptionMode.accept_all);
         	for (String buddy : users) {
         		if(!user.equals(buddy)) {
         			RosterEntry entry = roster.getEntry(buddy + "@" + host);
-        			if(entry != null) {
-        				roster.removeEntry(entry);
-        				logger.debug("Removed roster: " + user + " -> " + buddy);
+        			if(entry == null) {
+        				logger.debug("Creating roster: " + user + " -> " + buddy);
+        				roster.createEntry(buddy + "@" + host, buddy, null);
         			}
-        			roster.createEntry(buddy + "@" + host, buddy, null);
+        			
+        			logger.debug("Requesting subscription: " + user + " -> " + buddy);
+        			
+        			Presence presence = new Presence(Presence.Type.subscribe);
+        	        presence.setTo(buddy + "@" + host);
+        	        presence.setFrom(user + "@" + host);
+        	        connection.sendPacket(presence);
+
         			logger.debug("Created roster: " + user + " -> " + buddy);
+        		}
+			}
+        	connection.disconnect();
+		}
+        
+        for (String user : users) {
+        	connection = new XMPPConnection(config);
+        	connection.connect();
+        	logger.debug("Connected to server.");
+        	connection.login(user, user, "buddyfier");
+        	logger.debug("Logged in as " + connection.getUser());
+        	
+        	Roster roster = connection.getRoster();
+        	roster.setSubscriptionMode(SubscriptionMode.accept_all);
+        	for (String buddy : users) {
+        		if(!user.equals(buddy)) {
+        			
+        			Presence presence = new Presence(Presence.Type.subscribed);
+        	        presence.setTo(buddy + "@" + host);
+        	        presence.setFrom(user + "@" + host);
+        	        connection.sendPacket(presence);
+
+        			logger.debug("Approved subscription: " + user + " -> " + buddy);
         		}
 			}
         	connection.disconnect();
