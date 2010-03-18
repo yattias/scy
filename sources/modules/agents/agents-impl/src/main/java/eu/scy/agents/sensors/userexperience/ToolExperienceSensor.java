1538d409cdc5b9331095ad63b04249d2da4f0b60
janengler80 <janengler80@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-03-18 14:38:40 +0000

    added some timer.stop calls to prevent the TES from writing 400 tpm (tuples per minute)

--- sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/sensors/userexperience/ToolExperienceSensor.java
+++ sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/sensors/userexperience/ToolExperienceSensor.java
@@ -93,7 +93,7 @@ public class ToolExperienceSensor extends AbstractThreadedAgent implements Actio
             timer = new Timer(UPDATE_INTERVAL, this);
             timer.setRepeats(true);
             timer.start();
-        }
+        }  
     }
 
     private synchronized void rebuildFromSpace() throws TupleSpaceException, DocumentException, ParseException, IOException {
@@ -191,11 +191,13 @@ public class ToolExperienceSensor extends AbstractThreadedAgent implements Actio
                 userModel.setStarts(userModel.getStops());
             }
             logger.log(Level.FINE, "Tool started with user: " + a.getUser() + " and SessionID: " + sessionid);
+            timer.start();
         } else if (a.getType().equals("tool_closed")) {
             String sessionid = a.getContext(ContextConstants.session);
             UserToolExperienceModel exp = userModels.get(a.getUser());
             exp.setToolInactive(a.getContext(ContextConstants.tool), a.getTimeInMillis(), true);
             logger.log(Level.FINE, "Tool stopped with user: " + a.getUser() + " and SessionID: " + sessionid);
+            timer.stop();
 
         } else if (a.getType().equals("tool_gotfocus")) {
             String sessionid = a.getContext(ContextConstants.session);
@@ -208,12 +210,14 @@ public class ToolExperienceSensor extends AbstractThreadedAgent implements Actio
             }
             exp.setActiveTool(a.getContext(ContextConstants.tool), focusTime, false);
             logger.log(Level.FINE, "Focus gained with user: " + a.getUser() + " and SessionID: " + sessionid);
+            timer.start();
         } else if (a.getType().equals("tool_lostfocus")) {
             String sessionid = a.getContext(ContextConstants.session);
             long focusEndTime = a.getTimeInMillis();
             UserToolExperienceModel exp = userModels.get(a.getUser());
             exp.setToolInactive(a.getContext(ContextConstants.tool), focusEndTime, false);
             logger.log(Level.FINE, "Focus lost with user: " + a.getUser() + " and SessionID: " + sessionid);
+            timer.stop();
         }
     }
 
