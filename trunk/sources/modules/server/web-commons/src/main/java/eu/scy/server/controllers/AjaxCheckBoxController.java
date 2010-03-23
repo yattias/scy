c9c322ac1aaf907613e899e9bc7066cc0596ea19
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-03-23 12:48:37 +0000

    Added web-commons (taglibs and component pages)

---
+++ trunk/sources/modules/server/web-commons/src/main/java/eu/scy/server/controllers/AjaxCheckBoxController.java
@@ -0,0 +1,23 @@
+package eu.scy.server.controllers;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 23.mar.2010
+ * Time: 13:16:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class AjaxCheckBoxController extends AbstractController {
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView modelAndView = new ModelAndView();
+
+        return modelAndView;
+    }
+}
