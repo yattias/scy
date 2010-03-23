63674b10fcaf7924c206d77dd007f3b7f25e5c66
oyvind <oyvind@enovate.no>
2010-03-23 15:08:14 +0000

    Mission stores targetGroup and missionOutline in database

--- sources/modules/server/scy-server-utils/src/main/resources/scy/dbSchema.sql
+++ sources/modules/server/scy-server-utils/src/main/resources/scy/dbSchema.sql
@@ -147,6 +147,8 @@ CREATE TABLE `mission` (
 	`primKey` varchar(55) NOT NULL default '',
 	`name` varchar(250) default NULL,
 	`description` text,
+	`missionOutline` text,
+	`targetGroup` text,	
     `timeCreated` bigint(20) NOT NULL default '0',
 	PRIMARY KEY  (`primKey`)
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
