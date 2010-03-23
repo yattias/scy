62f740b11620f468880a0badc1098de7434da210
henrik <henrik@enovate.no>
2010-03-23 12:48:37 +0000

    Added web-commons (taglibs and component pages)

---
+++ sources/modules/server/web-commons/src/main/java/eu/scy/server/taglibs/AjaxCheckBox.java
@@ -0,0 +1,52 @@
+package eu.scy.server.taglibs;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 23.mar.2010
+ * Time: 12:59:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class AjaxCheckBox extends TagSupport {
+    
+    private String modelClass;
+    private String modelId;
+    private String property;
+
+     public int doEndTag() throws JspException {
+        try {
+            pageContext.getOut().write("Class: " +getModelClass() + " id: " + getModelId() + " property : " +getProperty());
+            pageContext.getOut().write("<input type=\"checkbox\">");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return EVAL_PAGE;
+    }
+
+    public String getModelClass() {
+        return modelClass;
+    }
+
+    public void setModelClass(String modelClass) {
+        this.modelClass = modelClass;
+    }
+
+    public String getModelId() {
+        return modelId;
+    }
+
+    public void setModelId(String modelId) {
+        this.modelId = modelId;
+    }
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+}
