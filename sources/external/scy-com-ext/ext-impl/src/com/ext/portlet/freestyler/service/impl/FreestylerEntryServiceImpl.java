1934c7447e6964bf7425755d491359f122397e29
jakob.sikken <jakob.sikken@gmail.com>
2010-04-07 19:52:06 +0000

    Created branch for UT 1004 experiment.

--- sources/external/scy-com-ext/ext-impl/src/com/ext/portlet/freestyler/service/impl/FreestylerEntryServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.ext.portlet.freestyler.service.impl;
-
-import com.ext.portlet.freestyler.service.base.FreestylerEntryServiceBaseImpl;
-
-
-public class FreestylerEntryServiceImpl extends FreestylerEntryServiceBaseImpl {
-}
