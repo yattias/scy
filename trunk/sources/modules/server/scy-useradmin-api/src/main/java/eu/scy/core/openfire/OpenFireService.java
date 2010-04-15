1daf4b847d827237f127fa5d77e643965aeae31d
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-04-15 09:30:12 +0000

    And interface really implements what should be overridden. The cowboy rides again

--- trunk/sources/modules/server/scy-useradmin-api/src/main/java/eu/scy/core/openfire/OpenFireService.java
+++ trunk/sources/modules/server/scy-useradmin-api/src/main/java/eu/scy/core/openfire/OpenFireService.java
@@ -1,5 +1,8 @@
 package eu.scy.core.openfire;
 
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Henrik
@@ -8,4 +11,14 @@ package eu.scy.core.openfire;
  * To change this template use File | Settings | File Templates.
  */
 public interface OpenFireService {
+    @Override
+    XMPPConnection getConnection(String userName1, String password1) throws XMPPException;
+
+    String getUsernameWithHost(String username);
+
+    @Override
+    String getHost();
+
+    @Override
+    void setHost(String host);
 }
