70cb11824d8e776ed088a2a696d760404b94f17d
larschla <larschla@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-04-22 08:11:56 +0000

    Time is now saved

--- sources/modules/server/web-commons/src/main/java/eu/scy/server/taglibs/AjaxTimePicker.java
+++ sources/modules/server/web-commons/src/main/java/eu/scy/server/taglibs/AjaxTimePicker.java
@@ -18,10 +18,11 @@ public class AjaxTimePicker extends AjaxBaseComponent{
         try {
             double id = Math.random() ;
             pageContext.getOut().write("<form id=\"timePickerForm" + id + "\" method=\"post\" action=\"/webapp/components/ajaxTimePicker.html\">");
-            pageContext.getOut().write("<span value=\"" + executeGetter(getModel(), getProperty()) + "\"  name=\"time\" id=\"time"+id+"\"  onChange=\"postForm('timePickerForm" + id + "');\" dojoType=\"dijit.InlineEditBox\" editor=\"dijit.form.TimeTextBox\"  required=\"true\" >" + executeGetter(getModel(), getProperty()) + "</span>");
-            pageContext.getOut().write("<input type=\"hidden\" name=\"clazz\" value=\"" + getModel().getClass().getName() + "\">");
-            pageContext.getOut().write("<input type=\"hidden\" name=\"id\" value=\"" + ((ScyBase)getModel()).getId() + "\">");
-            pageContext.getOut().write("<input type=\"hidden\" name=\"property\" value=\"" + getProperty() + "\">");
+            pageContext.getOut().write("<input type=\"hidden\" name=\"time\" id=\"timeValue"+id+"\" value=\"T"+ executeGetter(getModel(), getProperty()) +"\" />");
+            pageContext.getOut().write("<input type=\"hidden\" name=\"clazz\" value=\"" + getModel().getClass().getName() + "\"/>");
+            pageContext.getOut().write("<input type=\"hidden\" name=\"id\" value=\"" + ((ScyBase)getModel()).getId() + "\"/>");
+            pageContext.getOut().write("<input type=\"hidden\" name=\"property\" value=\"" + getProperty() + "\"/>");
+            pageContext.getOut().write("<span dojoType=\"dijit.InlineEditBox\" id=\"timeSelector"+id+"\" onChange=\"document.getElementById('timeValue"+id+"').value='T' + this.value;postForm('timePickerForm" + id + "');\" editor=\"dijit.form.TimeTextBox\"  value=\""+ executeGetter(getModel(), getProperty()) +"\" required=\"true\" />");
             pageContext.getOut().write("</form>");
         } catch(Exception e) {
             e.printStackTrace();
