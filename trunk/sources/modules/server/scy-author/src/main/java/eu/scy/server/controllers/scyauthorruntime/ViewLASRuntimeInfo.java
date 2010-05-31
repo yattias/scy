19d159de23f3ada7eca50ae9aeda5f8c9c187096
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-05-31 08:56:20 +0000

    Steffy: I removed the logging before you told me to!

---
+++ trunk/sources/modules/server/scy-author/src/main/java/eu/scy/server/controllers/scyauthorruntime/ViewLASRuntimeInfo.java
@@ -0,0 +1,21 @@
+package eu.scy.server.controllers.scyauthorruntime;
+
+import eu.scy.server.controllers.BaseController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 30.mai.2010
+ * Time: 21:30:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class ViewLASRuntimeInfo extends BaseController {
+    @Override
+    protected void handleRequest(HttpServletRequest request, HttpServletResponse response, ModelAndView modelAndView) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
---
+++ trunk/sources/modules/server/scy-author/src/main/resources/webapp/WEB-INF/jsp/scy/app/scyauthorruntime/viewLASRuntimeInfo.jsp
@@ -0,0 +1,8 @@
+<%@ include file="common-taglibs.jsp" %>
+<tiles:insertDefinition name="default-page">
+    <tiles:putAttribute name="main">
+
+        LAS RUNTIME VIEW!
+
+    </tiles:putAttribute>
+</tiles:insertDefinition>
