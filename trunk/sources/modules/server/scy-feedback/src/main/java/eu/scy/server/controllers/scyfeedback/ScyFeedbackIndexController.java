d401fd199b1b9b100abaf1e604e9b64434004b10
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-06-10 05:39:37 +0000

    Feedback added

---
+++ trunk/sources/modules/server/scy-feedback/src/main/java/eu/scy/server/controllers/scyfeedback/ScyFeedbackIndexController.java
@@ -0,0 +1,21 @@
+package eu.scy.server.controllers.scyfeedback;
+
+import eu.scy.server.controllers.BaseController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 10.jun.2010
+ * Time: 07:07:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class ScyFeedbackIndexController extends BaseController {
+    @Override
+    protected void handleRequest(HttpServletRequest request, HttpServletResponse response, ModelAndView modelAndView) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
