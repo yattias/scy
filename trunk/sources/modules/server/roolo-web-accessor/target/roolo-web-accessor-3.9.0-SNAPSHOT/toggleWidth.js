35ec5bccf74a1f50ff98d03fe98876dde55f3936
janengler80 <janengler80@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-06-21 09:17:45 +0000

    removed target dir

--- trunk/sources/modules/server/roolo-web-accessor/target/roolo-web-accessor-3.9.0-SNAPSHOT/toggleWidth.js
+++
@@ -1,9 +0,0 @@
-function toggleContent(nodeId, shortText, longText) {
-	var node = document.getElementById(nodeId).firstChild;
-  var currText = node.nodeValue;
-  if (currText.length == shortText.length) {
-	  node.nodeValue = longText;
-  } else {
-	  node.nodeValue = shortText;
-  }
-}
