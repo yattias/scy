3720806c2d5b6bff99180b6a013c554fc9df084e
jakob.sikken <jakob.sikken@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-07-26 14:32:37 +0000

    Disable the loading of the document in the background, it causes npe's for certain documents.

--- branches/JavaFX1.3_client/tools/fx-flying-saucer/src/main/java/eu/scy/client/tools/fxflyingsaucer/FlyingSaucerPanel.java
+++ branches/JavaFX1.3_client/tools/fx-flying-saucer/src/main/java/eu/scy/client/tools/fxflyingsaucer/FlyingSaucerPanel.java
@@ -258,15 +258,22 @@ public class FlyingSaucerPanel extends javax.swing.JPanel
       try
       {
          urlFieldIsTitle = true;
-         Thread t = new Thread() {
-
-                @Override
-                public void run() {
-                    browser.setDocument(url);
-                }
-
-         };
-         t.start();
+         // loading the document in the background causes some time a nullpointer exception
+         // it seems to happen only for certain documents, one such document is:
+         // http://www.scy-lab.eu/content/en/mission1/LAS_Conceptualization_design/Assignments/A_First_ideas.html
+         // the exception occurs at org.xhtmlrenderer.context.StyleReference:199
+         // for some reason org.xhtmlrenderer.css.newmatch.Matcher _matcher is null
+         //
+//         Thread t = new Thread() {
+//
+//                @Override
+//                public void run() {
+//                    browser.setDocument(url);
+//                }
+//
+//         };
+//         t.start();
+         browser.setDocument(url);
          if (!errorInPage)
          {
             isPageLoaded = true;
