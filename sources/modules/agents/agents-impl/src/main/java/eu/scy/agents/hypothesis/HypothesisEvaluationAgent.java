494c10fb3864d7e9f0bd75a109b8a3b12bf8d371
joerg.kindermann <joerg.kindermann@iais.fraunhofer.de>
2010-08-13 12:22:30 +0000

    replaced local calls to establish TupleSpace communication with functions from super class

--- sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/hypothesis/HypothesisEvaluationAgent.java
+++ sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/hypothesis/HypothesisEvaluationAgent.java
@@ -20,75 +20,78 @@ import eu.scy.agents.impl.AgentProtocol;
 
 public class HypothesisEvaluationAgent extends AbstractThreadedAgent implements Callback {
 
-    private static final String TOOL_NAME = "richtext";
+  private static final String TOOL_NAME = "richtext";
 
-    private static final String TOOL_START = "tool_start";
+  private static final String TOOL_START = "tool_start";
 
-    private TupleSpace commandSpace;
+  private static final Logger logger = Logger.getLogger(HypothesisEvaluationAgent.class.getName());
 
-    private TupleSpace actionSpace;
+  private boolean isStopped;
 
-    private static final Logger logger = Logger.getLogger(HypothesisEvaluationAgent.class.getName());
-
-    private boolean isStopped;
-
-    public HypothesisEvaluationAgent(Map<String, Object> map) {
-        super(HypothesisEvaluationAgent.class.getName(), (String) map.get(AgentProtocol.PARAM_AGENT_ID), (String) map.get(AgentProtocol.TS_HOST), (Integer) map.get(AgentProtocol.TS_PORT));
-        try {
-            commandSpace = new TupleSpace(new User(getSimpleName()), host, port, false, false, AgentProtocol.COMMAND_SPACE_NAME);
-            actionSpace = new TupleSpace(new User(getSimpleName()), host, port, false, false, AgentProtocol.ACTION_SPACE_NAME);
-            Callback hypothesisCallback = new HypothesisCallback();
-            // TODO tool_started correct
-            // TODO add Tool
-            actionSpace.eventRegister(Command.WRITE, new Tuple("action", String.class, Long.class, TOOL_START, Field.createWildCardField(),TOOL_NAME,Field.createWildCardField()), hypothesisCallback, true);
-        } catch (TupleSpaceException e) {
-            e.printStackTrace();
-        }
+  public HypothesisEvaluationAgent(Map<String, Object> map) {
+    super(HypothesisEvaluationAgent.class.getName(),
+          (String) map.get(AgentProtocol.PARAM_AGENT_ID), (String) map.get(AgentProtocol.TS_HOST),
+          (Integer) map.get(AgentProtocol.TS_PORT));
+    try {
+      // commandSpace = new TupleSpace(new User(getSimpleName()), host, port, false, false,
+      // AgentProtocol.COMMAND_SPACE_NAME);
+      // actionSpace = new TupleSpace(new User(getSimpleName()), host, port, false, false,
+      // AgentProtocol.ACTION_SPACE_NAME);
+      Callback hypothesisCallback = new HypothesisCallback();
+      // TODO tool_started correct
+      // TODO add Tool
+      getActionSpace().eventRegister(
+                                     Command.WRITE,
+                                     new Tuple("action", String.class, Long.class, TOOL_START,
+                                               Field.createWildCardField(), TOOL_NAME,
+                                               Field.createWildCardField()), hypothesisCallback,
+                                     true);
+    } catch (TupleSpaceException e) {
+      e.printStackTrace();
     }
+  }
+
+  @Override
+  protected void doRun() throws TupleSpaceException, AgentLifecycleException, InterruptedException {
+    while (status == Status.Running) {
+      sendAliveUpdate();
+      Thread.sleep(5000);
+    }
+  }
+
+  @Override
+  protected void doStop() {
+    try {
+      getActionSpace().disconnect();
+      getCommandSpace().disconnect();
+    } catch (TupleSpaceException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  protected Tuple getIdentifyTuple(String queryId) {
+    // TODO
+    return null;
+  }
+
+  @Override
+  public boolean isStopped() {
+    return isStopped;
+  }
+
+  class HypothesisCallback implements Callback {
 
     @Override
-    protected void doRun() throws TupleSpaceException, AgentLifecycleException, InterruptedException {
-        while (status == Status.Running) {
-            sendAliveUpdate();
-            Thread.sleep(5000);
-        }
+    public void call(Command cmd, int seqnum, Tuple afterTuple, Tuple beforeTuple) {
+      Action a = (Action) ActionTupleTransformer.getActionFromTuple(afterTuple);
+      String mission = a.getContext(ContextConstants.mission);
+      String session = a.getContext(ContextConstants.session);
+      String elouri = a.getContext(ContextConstants.eloURI);
+      // TODO Here the logic must be inserted ;-)
+      // Wait till saved -> then its easy but maybe we want to evaluate intermediate
+      // results....
     }
-
-    @Override
-    protected void doStop() {
-        try {
-            actionSpace.disconnect();
-            commandSpace.disconnect();
-        } catch (TupleSpaceException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    protected Tuple getIdentifyTuple(String queryId) {
-        // TODO
-        return null;
-    }
-
-    @Override
-    public boolean isStopped() {
-        return isStopped;
-    }
-
-    class HypothesisCallback implements Callback {
-
-        @Override
-        public void call(Command cmd, int seqnum, Tuple afterTuple, Tuple beforeTuple) {
-            Action a = (Action) ActionTupleTransformer.getActionFromTuple(afterTuple);
-            String mission = a.getContext(ContextConstants.mission);
-            String session = a.getContext(ContextConstants.session);
-            String elouri = a.getContext(ContextConstants.eloURI);
-            // TODO Here the logic must be inserted ;-)
-            // Wait till saved -> then its easy but maybe we want to evaluate intermediate
-            // results....
-        }
-    }
-
-    
+  }
 
 }
