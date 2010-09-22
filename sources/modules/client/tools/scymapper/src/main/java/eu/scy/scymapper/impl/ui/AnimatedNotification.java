39e99c719254737d066125d53674ce567b0584e8
janengler80 <janengler80@googlemail.com>
2010-09-22 11:54:38 +0000

    Added new FadeNotification Component

---
+++ sources/modules/client/tools/scymapper/src/main/java/eu/scy/scymapper/impl/ui/AnimatedNotification.java
@@ -0,0 +1,14 @@
+package eu.scy.scymapper.impl.ui;
+
+
+public interface AnimatedNotification {
+
+    /**
+     * This method fades the notification in, depending on the specific implementation
+     *
+     */
+    public void show();
+
+    public void hide();
+
+}
