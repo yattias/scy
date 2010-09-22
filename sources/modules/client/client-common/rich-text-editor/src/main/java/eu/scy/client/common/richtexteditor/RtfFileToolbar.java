e00d2cec73a41a012848b37fb99ab03fb328822d
kaidohallik <kaidohallik@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-09-22 10:43:09 +0000

    Changed rich text editor component: * removed PDF button (resolved SCY-799) * resolved SCY-790 "can't see if the formatting buttons are pressed or not" * improved action logging in rich text editor component (first part of SCY-889) * added open button in authorMode (second part of SCY-889)

--- sources/modules/client/client-common/rich-text-editor/src/main/java/eu/scy/client/common/richtexteditor/RtfFileToolbar.java
+++ sources/modules/client/client-common/rich-text-editor/src/main/java/eu/scy/client/common/richtexteditor/RtfFileToolbar.java
@@ -10,6 +10,7 @@ import javax.swing.SwingConstants;
 import java.util.ResourceBundle;
 import javax.jnlp.ServiceManager;
 import javax.jnlp.FileSaveService;
+import javax.jnlp.FileOpenService;
 import javax.jnlp.PrintService;
 import javax.jnlp.UnavailableServiceException;
 import org.apache.log4j.Logger;
@@ -24,14 +25,16 @@ import java.awt.print.Book;
 import java.awt.print.PrinterJob;
 import java.awt.print.PrinterException;
 import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
 import com.lowagie.text.pdf.PdfWriter;
 import com.lowagie.text.rtf.parser.RtfParser;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.nio.charset.Charset;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 import java.util.Date;
+import javax.jnlp.FileContents;
 
 /**
 * File toolbar for rich text editor component.
@@ -42,19 +45,25 @@ public class RtfFileToolbar extends JToolBar implements ActionListener {
     private ImageIcon saveIcon = new ImageIcon(this.getClass().getResource(imagesLocation+"Button_save_changes.png"));
     private ImageIcon printIcon = new ImageIcon(this.getClass().getResource(imagesLocation+"Button_print.png"));
     private ImageIcon pdfIcon = new ImageIcon(this.getClass().getResource(imagesLocation+"Button_pdf.png"));
+    private ImageIcon openIcon = new ImageIcon(this.getClass().getResource(imagesLocation+"Button_open-qt.png"));
     private ResourceBundle messages = ResourceBundle.getBundle("eu.scy.client.common.richtexteditor.RichTextEditor");
+    private JButton openButton = new JButton(openIcon);
+    private JButton saveButton = new JButton(saveIcon);
+    private JButton printButton = new JButton(printIcon);
+    private JButton pdfButton = new JButton(pdfIcon);
 
     private RichTextEditor editorPanel;
     private FileSaveService fileSaveService = null;
+    private FileOpenService fileOpenService = null;
     private PrintService printService = null;
     private JFileChooser fc = new JFileChooser();
     private boolean html;
 
-    public RtfFileToolbar(RichTextEditor richTextEditor) {
+    public RtfFileToolbar(RichTextEditor richTextEditor, boolean authoring) {
         super();
         this.editorPanel = richTextEditor;
         this.html = richTextEditor.isHTML();
-        initUI();
+        initUI(authoring);
     }
 
     public RichTextEditor getRichTextEditor() {
@@ -69,32 +78,121 @@ public class RtfFileToolbar extends JToolBar implements ActionListener {
             ":\nMessage: " + e.getMessage(), null, JOptionPane.ERROR_MESSAGE);
     }
 
-    private void initUI() {
+    private void initUI(boolean authorMode) {
         this.setOrientation(SwingConstants.HORIZONTAL);
         this.setFloatable(false);
 
-        JButton button = new JButton(saveIcon);
-        button.setActionCommand("savefile");
-        button.addActionListener(this);
-        if (html) {
-            button.setToolTipText(messages.getString("saveFile")+" html...");
-        } else {
-            button.setToolTipText(messages.getString("saveFile")+" rtf...");
+        openButton.setActionCommand("openfile");
+        openButton.addActionListener(this);
+        openButton.setToolTipText(messages.getString("open"));
+        if (authorMode) {
+            this.add(openButton);
         }
-        this.add(button);
 
-        button = new JButton(printIcon);
-        button.setActionCommand("print");
-        button.addActionListener(this);
-        button.setToolTipText(messages.getString("print"));
-        this.add(button);
+        saveButton.setActionCommand("savefile");
+        saveButton.addActionListener(this);
+        if (html) {
+            saveButton.setToolTipText(messages.getString("saveFile")+" html...");
+        } else {
+            saveButton.setToolTipText(messages.getString("saveFile")+" rtf...");
+        }
+        this.add(saveButton);
 
-        if (!html) {
-            button = new JButton(pdfIcon);
-            button.setActionCommand("pdf");
-            button.addActionListener(this);
-            button.setToolTipText(messages.getString("pdf"));
-            this.add(button);
+        printButton.setActionCommand("print");
+        printButton.addActionListener(this);
+        printButton.setToolTipText(messages.getString("print"));
+        this.add(printButton);
+
+        // no good converter from RTF to PDF - so removed PDF generation
+        if (!html && false) {
+            pdfButton.setActionCommand("pdf");
+            pdfButton.addActionListener(this);
+            pdfButton.setToolTipText(messages.getString("pdf"));
+            this.add(pdfButton);
+        }
+    }
+
+    private byte[] getBytesFromFile(File file) throws IOException {
+        InputStream is = null;
+        try{
+           is = new FileInputStream(file);
+           long length = file.length();
+           if (length > Integer.MAX_VALUE) {
+               new Exception ("Maximum filesize reached.");
+           }
+           byte[] bytes = new byte[(int) length];
+           int offset = 0;
+           int numRead = 0;
+           while (offset < bytes.length && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
+               offset += numRead;
+           }
+           if (offset < bytes.length) {
+               throw new IOException("Could not completely read file " + file.getName());
+           }
+           return bytes;
+        }
+        finally{
+          if (is!=null){
+             is.close();
+          }
+        }
+    }
+
+    private void bytesToEditor(byte[] bytes, String extension) {
+        String content = new String(bytes);
+        if (extension.equals("rtf")) {
+            editorPanel.setText(content);
+        } else {
+            editorPanel.setPlainText(content);
+        }
+    }
+
+    /** Load a file into the editing area.
+    @param is An input stream pointing to the desired file. */
+    public void loadFile(InputStream is, String extension) throws IOException {
+        byte[] buffer = new byte[1024];
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        int length;
+        while ( (length = is.read(buffer)) > 0 ) {
+            baos.write(buffer, 0, length);
+        }
+        baos.flush();
+        baos.close();
+        bytesToEditor(baos.toByteArray(), extension);
+    }
+
+    private void openFile() {
+        try {
+            if (fileOpenService==null) {
+                fileOpenService = (FileOpenService)ServiceManager.
+                    lookup("javax.jnlp.FileOpenService");
+            }
+            String[] extensions = {"rtf","txt"};
+            FileContents fileContents =
+                fileOpenService.openFileDialog(".", extensions);
+            if (fileContents!=null) {
+                String extension = "txt";
+                if (fileContents.getName().endsWith(".rtf"))
+                    extension = "rtf";
+                loadFile( fileContents.getInputStream(), extension );
+            }
+        } catch(UnavailableServiceException use) {
+            logger.info("No JNLP FileOpenService found, trying other method for opening");
+            int returnVal = fc.showOpenDialog(editorPanel);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                String extension = "txt";
+                if (fc.getSelectedFile().getName().endsWith(".rtf"))
+                    extension = "rtf";
+                try {
+                    bytesToEditor( getBytesFromFile(fc.getSelectedFile()), extension );
+                } catch(IOException ioe) {
+                    showError("openError", ioe);
+                }
+            } else {
+                logger.info("Open command cancelled by user.");
+            }
+        } catch(IOException ioe) {
+            showError("openError", ioe);
         }
     }
 
@@ -170,7 +268,11 @@ public class RtfFileToolbar extends JToolBar implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equals("savefile")) {
+        if (e.getActionCommand().equals("openfile")) {
+            openFile();
+            if (editorPanel.getRichTextEditorLogger() != null)
+                editorPanel.getRichTextEditorLogger().logFileAction(RichTextEditorLogger.OPEN);
+        } else if (e.getActionCommand().equals("savefile")) {
             String[] xtnsRtf = {"rtf"};
             String[] xtnsHtml = {"html"};
             if (html) {
