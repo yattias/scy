b78612f530050264d87f0be1bfb4600db9f9eeb1
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-10-06 12:09:23 +0000

    more changes to eporty

--- branches/BRANCH_CYPRUS_2/server/scy-author/src/main/java/eu/scy/server/controllers/ViewCreateNewPedagogicalPlanController.java
+++ branches/BRANCH_CYPRUS_2/server/scy-author/src/main/java/eu/scy/server/controllers/ViewCreateNewPedagogicalPlanController.java
@@ -1,5 +1,9 @@
 package eu.scy.server.controllers;
 
+import eu.scy.core.PedagogicalPlanPersistenceService;
+import eu.scy.core.model.impl.pedagogicalplan.PedagogicalPlanTemplateImpl;
+import eu.scy.core.model.pedagogicalplan.PedagogicalPlan;
+import eu.scy.core.model.pedagogicalplan.PedagogicalPlanTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,8 +17,25 @@ import javax.servlet.http.HttpServletResponse;
  * To change this template use File | Settings | File Templates.
  */
 public class ViewCreateNewPedagogicalPlanController extends BaseController{
+
+    private PedagogicalPlanPersistenceService pedagogicalPlanPersistenceService;
+
     @Override
     protected void handleRequest(HttpServletRequest request, HttpServletResponse response, ModelAndView modelAndView) {
-        
+        logger.info("CREATING NEW PEDAGOGICAL PLAN!");
+        PedagogicalPlanTemplate template = new PedagogicalPlanTemplateImpl();
+        template.setName("New pedagogical plan");
+        template.setDescription("A pedagogical plan");
+        getPedagogicalPlanPersistenceService().save(template);
+        PedagogicalPlan plan = getPedagogicalPlanPersistenceService().createPedagogicalPlan(template);
+        setModel(plan);
+    }
+
+    public PedagogicalPlanPersistenceService getPedagogicalPlanPersistenceService() {
+        return pedagogicalPlanPersistenceService;
+    }
+
+    public void setPedagogicalPlanPersistenceService(PedagogicalPlanPersistenceService pedagogicalPlanPersistenceService) {
+        this.pedagogicalPlanPersistenceService = pedagogicalPlanPersistenceService;
     }
 }
