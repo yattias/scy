75f0c99d8b18084de446bb27ebf8b599e0aaa43a
jakob.sikken <jakob.sikken@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-10-12 15:31:26 +0000

    Created branch for the coming UT experiment.

--- sources/external/scy-com-ext/ext-impl/src/com/ext/portlet/cart/service/impl/CartEntryLocalServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.ext.portlet.cart.service.impl;
-
-import com.ext.portlet.cart.service.base.CartEntryLocalServiceBaseImpl;
-
-
-public class CartEntryLocalServiceImpl extends CartEntryLocalServiceBaseImpl {
-}
