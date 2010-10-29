fb9286214461a47323775fd7fd051393fa855cef
jakob.sikken <jakob.sikken@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-10-29 15:03:23 +0000

    Improved the MissionModel classes.

--- sources/modules/common/mission/src/main/java/eu/scy/common/mission/MissionModel.java
+++ sources/modules/common/mission/src/main/java/eu/scy/common/mission/MissionModel.java
@@ -1,6 +1,11 @@
 package eu.scy.common.mission;
 
+import java.net.URI;
+import java.util.List;
+
 public interface MissionModel extends MissionModelEloContent
 {
-
+   public List<URI> getEloUris(boolean allElos);
+   
+   public void updateElo();
 }
