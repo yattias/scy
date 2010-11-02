810d67572673a3b1798010ac87177aeead49bee5
janengler80 <janengler80@googlemail.com>
2010-11-02 10:39:43 +0000

    added Spring stuff for collide version of scymapper

--- sources/modules/client/tools/scymapper/src/main/java/eu/scy/scymapper/impl/configuration/SCYMapperStandaloneConfig.java
+++ sources/modules/client/tools/scymapper/src/main/java/eu/scy/scymapper/impl/configuration/SCYMapperStandaloneConfig.java
@@ -4,70 +4,75 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
 
-import eu.scy.scymapper.impl.SCYMapperPanel;
-
 public class SCYMapperStandaloneConfig {
-	
+
 	public enum Help {
-		NOHELP, VOLUNTARY, CONTINUOUS;  
-		
+		NOHELP, VOLUNTARY, CONTINUOUS;
+
 	}
 
-    private final static Logger logger = Logger.getLogger(SCYMapperStandaloneConfig.class);
+	private final static Logger logger = Logger
+			.getLogger(SCYMapperStandaloneConfig.class);
 
-    private static SCYMapperStandaloneConfig instance = null;
+	private static SCYMapperStandaloneConfig instance = null;
 
-//    private static final String LOCAL_CONFIG_FILE_PATH = "src/main/java/eu/scy/scymapper/";
+	// private static final String LOCAL_CONFIG_FILE_PATH =
+	// "src/main/java/eu/scy/scymapper/";
 	private static final String LOCAL_CONFIG_FILE_PATH = "";
 
 	private static final String LOCAL_CONFIG_FILE = "scymapper.properties";
-	
+
 	private static final String KEY_HELP_MODE = "HelpMode";
-	
+
 	private static final String KEY_CONTINUOUS_HELP_WAIT_TIME = "ContinuousHelpWaitTime";
 
 	private static Properties config;
-	
+
 	private static final Help DEFAULT_HELP_MODE = Help.CONTINUOUS;
-	
+
 	private static final int DEFAULT_CON_HELP_WAIT_TIME = 180;
-	
+
+	private static final String KEY_RELATIONS = "Relations";
+
 	private SCYMapperStandaloneConfig() {
 		config = new Properties();
 		loadDefaultConfig(config);
 
 		File file = new File(LOCAL_CONFIG_FILE_PATH + LOCAL_CONFIG_FILE);
-		if(file.canRead()) {
+		if (file.canRead()) {
 			// Use config file if possible
 			Reader reader = null;
 			try {
 				reader = new FileReader(file);
 				config.load(reader);
-			} catch(IOException e) {
+			} catch (IOException e) {
 				// TODO
 			} finally {
-				try { 
-					reader.close(); 
-				} catch ( IOException e ) { 
+				try {
+					reader.close();
+				} catch (IOException e) {
 					// TODO
-				} 
+				}
 			}
 		}
 	};
-	
+
 	private static void loadDefaultConfig(Properties props) {
-		
+
 		props.setProperty(KEY_HELP_MODE, DEFAULT_HELP_MODE.name());
-		
-		props.setProperty(KEY_CONTINUOUS_HELP_WAIT_TIME, String.valueOf(DEFAULT_CON_HELP_WAIT_TIME));
+
+		props.setProperty(KEY_CONTINUOUS_HELP_WAIT_TIME,
+				String.valueOf(DEFAULT_CON_HELP_WAIT_TIME));
 	}
-	
+
 	public static SCYMapperStandaloneConfig getInstance() {
-		if(instance == null) {
+		if (instance == null) {
 			instance = new SCYMapperStandaloneConfig();
 		}
 		return instance;
@@ -76,25 +81,42 @@ public class SCYMapperStandaloneConfig {
 	public Help getHelpMode() {
 		try {
 			return Help.valueOf(config.getProperty(KEY_HELP_MODE));
-		} catch(IllegalArgumentException e) {
+		} catch (IllegalArgumentException e) {
 			return DEFAULT_HELP_MODE;
 		}
 	}
-	
+
+	public List<String> getRelations() {
+		try {
+			String[] relations = config.getProperty(KEY_RELATIONS).split(",");
+			List<String> relationsAsList = new ArrayList<String>();
+			for (String string : relations) {
+				relationsAsList.add(string.trim());
+
+			}
+			return relationsAsList;
+
+		} catch (IllegalArgumentException iae) {
+			return null;
+		}
+	}
+
 	public void setHelpMode(Help h) {
 		config.setProperty(KEY_HELP_MODE, h.toString());
 	}
 
 	public int getContinuousHelpWaitTime() {
 		try {
-			return Integer.parseInt(config.getProperty(KEY_CONTINUOUS_HELP_WAIT_TIME));
+			return Integer.parseInt(config
+					.getProperty(KEY_CONTINUOUS_HELP_WAIT_TIME));
 		} catch (NumberFormatException e) {
 			return DEFAULT_CON_HELP_WAIT_TIME;
 		}
 	}
-	
+
 	public void setContinuousHelpWaitTime(int time) {
-		config.setProperty(KEY_CONTINUOUS_HELP_WAIT_TIME, Integer.toString(time));
+		config.setProperty(KEY_CONTINUOUS_HELP_WAIT_TIME,
+				Integer.toString(time));
 	}
 
 	// Only for testing purposes
@@ -102,5 +124,10 @@ public class SCYMapperStandaloneConfig {
 		SCYMapperStandaloneConfig config = new SCYMapperStandaloneConfig();
 		System.out.println(config.getHelpMode());
 		System.out.println(config.getContinuousHelpWaitTime());
+		List<String> relations = config.getRelations();
+		System.out.println("Relations:");
+		for (String string : relations) {
+			System.out.println("/"+string+"/");
+		}
 	}
 }
