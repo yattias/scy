3598f8f45bd9f08bb0ecdead924da233659aba01
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-11-06 17:49:16 +0000

    triangles

---
+++ trunk/sources/modules/client/tools/math-tool/src/main/java/eu/scy/tools/math/shapes/IMathTriangle.java
@@ -0,0 +1,19 @@
+package eu.scy.tools.math.shapes;
+
+import java.awt.Point;
+
+public interface IMathTriangle extends IMathShape {
+
+	public Point getPointR();
+
+	public void setPointR(Point pointR);
+
+	public Point getPointQ();
+
+	public void setPointQ(Point pointQ);
+
+	public Point getPointP();
+
+	public void setPointP(Point pointP);
+
+}
