1308d09ba6f4775dd5b52a6a3e2f1188f8557392
jakob.sikken <jakob.sikken@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-11-26 11:00:01 +0000

    Added method to get all MissionRuntimeModels for a mission specification.

--- trunk/sources/modules/common/mission/src/main/java/eu/scy/common/mission/MissionManagement.java
+++ trunk/sources/modules/common/mission/src/main/java/eu/scy/common/mission/MissionManagement.java
@@ -1,5 +1,7 @@
 package eu.scy.common.mission;
 
+import java.util.List;
+
 public interface MissionManagement
 {
    public MissionRuntimeModel createMissionRuntimeModelElos(String userName);
@@ -7,5 +9,7 @@ public interface MissionManagement
    public MissionRuntimeModel getMissionRuntimeModelElosOnSpecifiaction(String userName);
 
    public MissionRuntimeModel getMissionRuntimeModel(String userName);
+   
+   public List<MissionRuntimeModel> getAllMissionRuntimeModels();
 
 }
