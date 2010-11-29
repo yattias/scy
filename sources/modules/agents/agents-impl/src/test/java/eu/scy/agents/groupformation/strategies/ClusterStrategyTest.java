4d65d48349441be866edc20439c2fa5e72fad6a2
hanbei <hanbei@gmx.de>
2010-11-29 13:46:12 +0000

    renamed test

---
+++ sources/modules/agents/agents-impl/src/test/java/eu/scy/agents/groupformation/strategies/ClusterStrategyTest.java
@@ -0,0 +1,22 @@
+package eu.scy.agents.groupformation.strategies;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ClusterStrategyTest {
+
+	private ClusterStrategy groupFormationStrategySequence;
+
+	@Before
+	public void setup() {
+		groupFormationStrategySequence = new ClusterStrategy();
+	}
+
+	@Test
+	@Ignore("needs to be reimplemented")
+	public void testFormGroup() {
+
+	}
+
+}
