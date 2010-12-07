8da206344222f1cd2dff1c38231c034a4cfe34a3
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-12-07 18:11:06 +0000

    save returns string xml

--- trunk/sources/modules/client/tools/math-tool-scy-extensions/src/main/java/eu/scy/tools/math/controller/SCYMathToolController.java
+++ trunk/sources/modules/client/tools/math-tool-scy-extensions/src/main/java/eu/scy/tools/math/controller/SCYMathToolController.java
@@ -1,9 +1,20 @@
 package eu.scy.tools.math.controller;
 
+import eu.scy.tools.math.ui.UIUtils;
+import eu.scy.tools.math.ui.panels.ShapeCanvas;
+
 public class SCYMathToolController extends MathToolController {
 
 	public SCYMathToolController() {
 		super();
 	}
 
+	@Override
+	public String save() {
+		ShapeCanvas sc = shapeCanvases.get(UIUtils._2D);
+	      
+		String xml = xstream.toXML(sc.getMathShapes());
+		
+		return xml;
+	}
 }
