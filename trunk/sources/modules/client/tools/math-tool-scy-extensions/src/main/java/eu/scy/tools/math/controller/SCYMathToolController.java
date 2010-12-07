32a77ae2cb4492b450f70232b7f40d50981d909d
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2010-12-07 21:25:07 +0000

    better extraction

--- trunk/sources/modules/client/tools/math-tool-scy-extensions/src/main/java/eu/scy/tools/math/controller/SCYMathToolController.java
+++ trunk/sources/modules/client/tools/math-tool-scy-extensions/src/main/java/eu/scy/tools/math/controller/SCYMathToolController.java
@@ -1,10 +1,14 @@
 package eu.scy.tools.math.controller;
 
+import org.apache.commons.lang.StringUtils;
+
 import eu.scy.tools.math.ui.UIUtils;
 import eu.scy.tools.math.ui.panels.ShapeCanvas;
 
 public class SCYMathToolController extends MathToolController {
 
+	private static final String MATH_TOOL_END = "</MathTool>";
+	private static final String MATH_TOOL_START = "<MathTool>";
 	public SCYMathToolController() {
 		super();
 	}
@@ -14,10 +18,17 @@ public class SCYMathToolController extends MathToolController {
 	      
 		String xml = xstream.toXML(sc.getMathShapes());
 		
-		xml = "<MathTool>" + xml + "</MathTool>";
+		xml = MATH_TOOL_START + xml + MATH_TOOL_END;
 		return xml;
 	}
 	
+	@Override
+	public void open(String xml) {
+		String removeStart = StringUtils.remove(xml, MATH_TOOL_START);
+		String removeEnd = StringUtils.remove(xml, MATH_TOOL_END);
+		
+		super.open(removeEnd);
+	}
 	public void logAction(String actionLog) { 
 		
 	}
