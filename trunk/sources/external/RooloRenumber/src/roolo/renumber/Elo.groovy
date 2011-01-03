0323d786b9c4ec62b8e06051d29a49595bdce702
jakob.sikken <jakob.sikken@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-01-03 19:39:53 +0000

    first commit of roolo renumber.

---
+++ trunk/sources/external/RooloRenumber/src/roolo/renumber/Elo.groovy
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package roolo.renumber
+
+
+/**
+ *
+ * @author sikken
+ */
+class Elo {
+   String xml
+   URI uri
+	int id
+	
+	String toString(){
+		"uri= $uri, id=$id, lenght= ${xml.length()}"
+	}
+
+}
+
