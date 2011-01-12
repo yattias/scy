4fa99698c9830f3863bcb109410471c141e4deea
stefan.weinbrenner <stefan.weinbrenner@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-01-12 13:10:29 +0000

    expanded Stemmer util class

--- sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/conceptmap/proposer/Stemmer.java
+++ sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/conceptmap/proposer/Stemmer.java
@@ -1,5 +1,14 @@
 package eu.scy.agents.conceptmap.proposer;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.tartarus.snowball.SnowballStemmer;
 import org.tartarus.snowball.ext.englishStemmer;
 import org.tartarus.snowball.ext.germanStemmer;
@@ -8,8 +17,11 @@ public class Stemmer {
 
     private static SnowballStemmer stemmer;
     
+    private static Set<String> stopwords;
+    
     static {
         stemmer = new englishStemmer();
+        readStopwords("english");
     }
 
     public synchronized static String stem(String string) {
@@ -18,25 +30,63 @@ public class Stemmer {
         return stemmer.getCurrent();
     }
 
+    private static void readStopwords(String lang) {
+        stopwords = new HashSet<String>();
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(Stemmer.class.getResourceAsStream("/" + lang + "_stopwords.txt")));
+            String buffer = null;
+            while ((buffer = br.readLine()) != null) {
+                try {
+                    stopwords.add(Stemmer.stem(buffer));
+                } catch (StringIndexOutOfBoundsException e) {
+                    e.printStackTrace();
+                }
+            }
+            br.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }        
+    }
+
     public synchronized static boolean equalStem(String s1, String s2) {
         return stemWordWise(s1).equals(stemWordWise(s2));
     }
     
-    public synchronized static String stemWordWise(String string) {
-        String temp = "";
-        // no stringbuilder, because there will be rarely any terms with words > 2
+    public synchronized static boolean equalStemUnordered(String s1, String s2) {
+        return stemWordWise(s1, true, true).equals(stemWordWise(s2, true, true));
+    }
+    
+    public synchronized static String stemWordWise(String string, boolean order, boolean removeStopwords) {
+        ArrayList<String> words = new ArrayList<String>();
         for (String l : string.toLowerCase().split(" ")) {
-            temp += Stemmer.stem(l) + " ";
+            if (!removeStopwords || !stopwords.contains(l)) {
+                words.add(Stemmer.stem(l));
+            }
+        }
+        if (order) {
+            Collections.sort(words);
+        }
+        String temp = "";
+        for (String w : words) {
+            temp += w + " ";
         }
         temp = temp.substring(0, temp.length() - 1);
         return temp;
+    }
+    
+    public synchronized static String stemWordWise(String string) {
+        return stemWordWise(string, false, false);
     }
 
     public static void setLanguage(String language) {
         if (language.equals("en")) {
             stemmer = new englishStemmer();
+            readStopwords("english");
         } else if (language.equals("de")){
             stemmer = new germanStemmer();
+            readStopwords("german");
         }
     }
 
