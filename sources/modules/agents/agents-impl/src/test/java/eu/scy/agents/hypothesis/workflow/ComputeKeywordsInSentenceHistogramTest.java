2487591675bcb072eba80f6d0a7e3542c46fae4f
joerg.kindermann <joerg.kindermann@iais.fraunhofer.de>
2011-01-17 08:58:58 +0000

    fixed sentenceSplitter bug on small texts

--- sources/modules/agents/agents-impl/src/test/java/eu/scy/agents/hypothesis/workflow/ComputeKeywordsInSentenceHistogramTest.java
+++ sources/modules/agents/agents-impl/src/test/java/eu/scy/agents/hypothesis/workflow/ComputeKeywordsInSentenceHistogramTest.java
@@ -31,61 +31,98 @@ import eu.scy.agents.util.Utilities;
 
 public class ComputeKeywordsInSentenceHistogramTest extends AbstractTestFixture {
 
-	public static final String HISTOGRAM = "histogram";
+  public static final String HISTOGRAM = "histogram";
 
-	private static final Logger logger = Logger
-			.getLogger(ComputeKeywordsInSentenceHistogramTest.class.getName());
+  private static final Logger logger = Logger.getLogger(ComputeKeywordsInSentenceHistogramTest.class.getName());
 
-	private IELO elo;
+  private IELO largeElo, smallElo, emptyElo;
 
-	private String[] keywords = { "ingredients", "nontoxic", "binder",
-			"solvent", "labels", "toxic", "chemical", "voc", "paint", "health",
-			"natural", "pigment" };
+  private String[] keywords = { "ingredients", "nontoxic", "binder", "solvent", "labels", "toxic",
+                               "chemical", "voc", "paint", "health", "natural", "pigment" };
 
-	private ArrayList<String> keywordList;
+  private ArrayList<String> keywordList;
 
-	@Before
-	public void setup() throws Exception {
-		super.setUp();
+  @Before
+  public void setup() throws Exception {
+    super.setUp();
 
-		InputStream inStream = this.getClass().getResourceAsStream(
-				"/copexExampleElo.xml");
-		String eloContent = readFile(inStream);
-		elo = createNewElo("TestCopex", EloTypes.SCY_XPROC);
-		elo.setContent(new BasicContent(eloContent));
-		keywordList = new ArrayList<String>();
-		for (int i = 0; i < keywords.length; i++) {
-			keywordList.add(keywords[i]);
-		}
-	}
+    InputStream inStream = this.getClass().getResourceAsStream("/copexExampleElo.xml");
+    String eloContent = readFile(inStream);
+    largeElo = createNewElo("TestCopex1", EloTypes.SCY_XPROC);
+    largeElo.setContent(new BasicContent(eloContent));
+    inStream = this.getClass().getResourceAsStream("/copexExampleSmallElo.xml");
+    eloContent = readFile(inStream);
+    smallElo = createNewElo("TestCopex2", EloTypes.SCY_XPROC);
+    smallElo.setContent(new BasicContent(eloContent));
+    inStream = this.getClass().getResourceAsStream("/copexExampleEmptyElo.xml");
+    eloContent = readFile(inStream);
+    emptyElo = createNewElo("TestCopex3", EloTypes.SCY_XPROC);
+    emptyElo.setContent(new BasicContent(eloContent));
+    keywordList = new ArrayList<String>();
+    for (int i = 0; i < keywords.length; i++) {
+      keywordList.add(keywords[i]);
+    }
+  }
 
-	@Override
-	@After
-	public void tearDown() {
-		try {
-			super.tearDown();
-		} catch (AgentLifecycleException e) {
-			e.printStackTrace();
-		}
-	}
+  @Override
+  @After
+  public void tearDown() {
+    try {
+      super.tearDown();
+    } catch (AgentLifecycleException e) {
+      e.printStackTrace();
+    }
+  }
 
-	@Test
-	public void testComputeKeywordsInSentenceHistogramTest() {
-		String XMLPATH = "//learner_proc/proc_hypothesis/hypothesis";
+  @Test
+  public void testComputeKeywordsInSentenceHistogramTest() {
+    String XMLPATH = "//learner_proc/proc_hypothesis/hypothesis";
 
-		String text = Utilities.getEloText(elo, XMLPATH, logger);
-		Document doc = Utilities.convertTextToDocument(text);
-		doc.setFeature(Features.WORDS, keywordList);
+    String text = Utilities.getEloText(largeElo, XMLPATH, logger);
+    Document doc = Utilities.convertTextToDocument(text);
+    doc.setFeature(Features.WORDS, keywordList);
 
-		Operator cmpHistogramOp = new EvalHypothesisWorkflow()
-				.getOperator("Main");
-		cmpHistogramOp.setInputParameter(ObjectIdentifiers.DOCUMENT, doc);
-		Container result = cmpHistogramOp.run();
-		Document docResult = (Document) result.get(ObjectIdentifiers.DOCUMENT);
-		HashMap<Integer, Integer> hist = docResult
-				.getFeature(KeywordConstants.KEYWORD_SENTENCE_HISTOGRAM);
-		String string = hist.toString();
-		assertEquals(string, "{0=16, 1=14, 2=7, 3=5, 4=1, 5=1}");
-	}
+    Operator cmpHistogramOp = new EvalHypothesisWorkflow().getOperator("Main");
+    cmpHistogramOp.setInputParameter(ObjectIdentifiers.DOCUMENT, doc);
+    Container result = cmpHistogramOp.run();
+    Document docResult = (Document) result.get(ObjectIdentifiers.DOCUMENT);
+    HashMap<Integer, Integer> hist = docResult.getFeature(KeywordConstants.KEYWORD_SENTENCE_HISTOGRAM);
+    String string = hist.toString();
+    assertEquals(string, "{0=16, 1=14, 2=7, 3=5, 4=1, 5=1}");
+  }
+
+  @Test
+  public void testSmallComputeKeywordsInSentenceHistogramTest() {
+    String XMLPATH = "//learner_proc/proc_hypothesis/hypothesis";
+
+    String text = Utilities.getEloText(smallElo, XMLPATH, logger);
+    Document doc = Utilities.convertTextToDocument(text);
+    doc.setFeature(Features.WORDS, keywordList);
+
+    Operator cmpHistogramOp = new EvalHypothesisWorkflow().getOperator("Main");
+    cmpHistogramOp.setInputParameter(ObjectIdentifiers.DOCUMENT, doc);
+    Container result = cmpHistogramOp.run();
+    Document docResult = (Document) result.get(ObjectIdentifiers.DOCUMENT);
+    HashMap<Integer, Integer> hist = docResult.getFeature(KeywordConstants.KEYWORD_SENTENCE_HISTOGRAM);
+    String string = hist.toString();
+    assertEquals(string, "{0=1}");
+  }
+  
+  @Test
+  public void testEmptyComputeKeywordsInSentenceHistogramTest() {
+    String XMLPATH = "//learner_proc/proc_hypothesis/hypothesis";
+
+    String text = Utilities.getEloText(emptyElo, XMLPATH, logger);
+    Document doc = Utilities.convertTextToDocument(text);
+    doc.setFeature(Features.WORDS, keywordList);
+
+    Operator cmpHistogramOp = new EvalHypothesisWorkflow().getOperator("Main");
+    cmpHistogramOp.setInputParameter(ObjectIdentifiers.DOCUMENT, doc);
+    Container result = cmpHistogramOp.run();
+    Document docResult = (Document) result.get(ObjectIdentifiers.DOCUMENT);
+    HashMap<Integer, Integer> hist = docResult.getFeature(KeywordConstants.KEYWORD_SENTENCE_HISTOGRAM);
+    String string = hist.toString();
+    assertEquals(string, "{0=1}");
+  }
 
 }
