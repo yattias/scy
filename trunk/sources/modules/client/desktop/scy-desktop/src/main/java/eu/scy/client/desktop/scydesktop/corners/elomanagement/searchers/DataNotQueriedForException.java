64c68d3d41c5757e54d73f787951613e2a89e4be
latinum1@gmx.de <latinum1@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-02-02 16:02:08 +0000

    missing classes

---
+++ trunk/sources/modules/client/desktop/scy-desktop/src/main/java/eu/scy/client/desktop/scydesktop/corners/elomanagement/searchers/DataNotQueriedForException.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eu.scy.client.desktop.scydesktop.corners.elomanagement.searchers;
+
+import roolo.elo.api.exceptions.RooloException;
+
+/**
+ *
+ * @author sven
+ */
+public class DataNotQueriedForException extends RooloException{
+
+    public DataNotQueriedForException() {
+    }
+
+}
