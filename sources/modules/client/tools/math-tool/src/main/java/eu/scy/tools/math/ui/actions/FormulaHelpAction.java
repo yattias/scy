8f204d4c7627905093350b820a20b2c50b379329
aperritano <aperritano@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-02-09 14:51:58 +0000

    DAMN YOU SPELL CHECK

--- sources/modules/client/tools/math-tool/src/main/java/eu/scy/tools/math/ui/actions/FormulaHelpAction.java
+++ sources/modules/client/tools/math-tool/src/main/java/eu/scy/tools/math/ui/actions/FormulaHelpAction.java
@@ -14,7 +14,7 @@ public class FormulaHelpAction extends AbstractAction {
 	private String type;
 
 	public FormulaHelpAction(String type) {
-		putValue(Action.NAME, type + " Forumla Guide");
+		putValue(Action.NAME, type + " Formula Guide");
 		putValue(Action.SHORT_DESCRIPTION, "Guide for " + type + " forumlas");
 		this.type = type;
 	}
