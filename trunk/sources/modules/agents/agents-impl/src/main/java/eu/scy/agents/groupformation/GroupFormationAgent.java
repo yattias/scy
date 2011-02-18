57c2ac07bf921c0d57b4177ae974c3b7a627adcc
hanbei@gmx.de <hanbei@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-02-18 14:39:50 +0000

    npe fixed

--- trunk/sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/groupformation/GroupFormationAgent.java
+++ trunk/sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/groupformation/GroupFormationAgent.java
@@ -10,6 +10,7 @@ import java.rmi.dgc.VMID;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -71,6 +72,7 @@ public class GroupFormationAgent extends AbstractRequestAgent implements
 		} catch (TupleSpaceException e) {
 			e.printStackTrace();
 		}
+		missionGroupsCache = new HashMap<String, GroupFormationCache>();
 	}
 
 	/* activated by action log */
