1e8ca44eda95cf043c726204f3bd69fd0c00b30f
latinum1@gmx.de <latinum1@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-02-22 09:51:33 +0000

    new version 1.4.7 - removed update keys for mozilla addon site support - removed full html page export (not supported anymore/no usecase) - minor changes

--- trunk/sources/external/webbrowsingtool/src/main/eu/scy/webbrowsingtool/defaults/preferences/highlighter.js
+++ trunk/sources/external/webbrowsingtool/src/main/eu/scy/webbrowsingtool/defaults/preferences/highlighter.js
@@ -3,8 +3,8 @@ pref("extensions.highlighter.intpref", 0);
 pref("extensions.highlighter.stringpref", "A string");
 pref("extensions.highlighter.username", "username");
 pref("extensions.highlighter.password", "password");
-pref("extensions.highlighter.address", "http://scy.collide.info:8080/roolo-ws/webresources");
-pref("extensions.highlighter.defaultaddress", "http://scy.collide.info:8080/roolo-ws/webresources");
+pref("extensions.highlighter.address", "http://scy.collide.info:8080/roolo-ws");
+pref("extensions.highlighter.defaultaddress", "http://scy.collide.info:8080/roolo-ws");
 pref("extensions.highlighter.port", "8080");
 pref("extensions.highlighter.usedefaultaddress", false);
 // See http://kb.mozillazine.org/Localize_extension_descriptions
