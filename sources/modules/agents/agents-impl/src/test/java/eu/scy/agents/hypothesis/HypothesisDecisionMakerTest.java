9ef98a1e4264cdaf7b4fe0de8d744cd698b60ee9
hanbei <hanbei@gmx.de>
2011-03-23 14:55:01 +0000

    fixed test

--- sources/modules/agents/agents-impl/src/test/java/eu/scy/agents/hypothesis/HypothesisDecisionMakerTest.java
+++ sources/modules/agents/agents-impl/src/test/java/eu/scy/agents/hypothesis/HypothesisDecisionMakerTest.java
@@ -29,111 +29,120 @@ import eu.scy.agents.impl.AgentProtocol;
 
 public class HypothesisDecisionMakerTest extends AbstractTestFixture {
 
-  private static final String ELO_TYPE = "scy/xproc";
+	private static final String ELO_TYPE = "scy/xproc";
 
-  private IELO elo;
+	private static final String MISSION = "roolo://memory/0/0/Design+a+CO2-friendly+house.scymissionspecification";
 
-  private String eloPath;
+	private IELO elo;
 
-  private HashMap<Integer, Integer> goodHistogram, badHistogram, mediumHistogram;
+	private String eloPath;
 
-  @BeforeClass
-  public static void startTS() {
-    startTupleSpaceServer();
-  }
+	private HashMap<Integer, Integer> goodHistogram, badHistogram,
+			mediumHistogram;
 
-  @AfterClass
-  public static void stopTS() {
-    stopTupleSpaceServer();
-  }
+	@BeforeClass
+	public static void startTS() {
+		startTupleSpaceServer();
+	}
 
-  @Override
-  @Before
-  public void setUp() throws Exception {
-    super.setUp();
+	@AfterClass
+	public static void stopTS() {
+		stopTupleSpaceServer();
+	}
 
-    this.initTopicModel();
-    this.initDfModel();
+	@Override
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
 
-    HashMap<String, Object> params = new HashMap<String, Object>();
-    params.put(AgentProtocol.PARAM_AGENT_ID, new VMID());
-    params.put(AgentProtocol.TS_HOST, TSHOST);
-    params.put(AgentProtocol.TS_PORT, TSPORT);
-    this.agentMap.put(HypothesisDecisionMakerAgent.NAME, params);
+		this.initTopicModel();
+		this.initDfModel();
 
-    this.startAgentFramework(this.agentMap);
+		HashMap<String, Object> params = new HashMap<String, Object>();
+		params.put(AgentProtocol.PARAM_AGENT_ID, new VMID());
+		params.put(AgentProtocol.TS_HOST, TSHOST);
+		params.put(AgentProtocol.TS_PORT, TSPORT);
+		this.agentMap.put(HypothesisDecisionMakerAgent.NAME, params);
 
-    InputStream inStream = this.getClass().getResourceAsStream("/copexExampleElo.xml");
-    String eloContent = readFile(inStream);
-    elo = createNewElo("TestCopex", ELO_TYPE);
-    elo.setContent(new BasicContent(eloContent));
-    IMetadata metadata = repository.addNewELO(elo);
-    URI eloUri = (URI) metadata.getMetadataValueContainer(this.typeManager.getMetadataKey(CoreRooloMetadataKeyIds.IDENTIFIER)).getValue();
-    this.eloPath = eloUri.toString();
+		this.startAgentFramework(this.agentMap);
 
-    System.out.println(eloUri.toString());
+		InputStream inStream = this.getClass().getResourceAsStream(
+				"/copexExampleElo.xml");
+		String eloContent = readFile(inStream);
+		elo = createNewElo("TestCopex", ELO_TYPE);
+		elo.setContent(new BasicContent(eloContent));
+		IMetadata metadata = repository.addNewELO(elo);
+		URI eloUri = (URI) metadata.getMetadataValueContainer(
+				this.typeManager
+						.getMetadataKey(CoreRooloMetadataKeyIds.IDENTIFIER))
+				.getValue();
+		this.eloPath = eloUri.toString();
 
-    goodHistogram = new HashMap<Integer, Integer>();
-    goodHistogram.put(0, 16);
-    goodHistogram.put(1, 16);
-    goodHistogram.put(2, 7);
-    goodHistogram.put(3, 3);
-    goodHistogram.put(4, 1);
-    goodHistogram.put(5, 1);
-    
-    badHistogram = new HashMap<Integer, Integer>();
-    badHistogram.put(0, 5);
-    badHistogram.put(1, 2);
+		System.out.println(eloUri.toString());
 
-    mediumHistogram = new HashMap<Integer, Integer>();
-    mediumHistogram.put(0, 2);
-    mediumHistogram.put(1, 4);
-  }
+		goodHistogram = new HashMap<Integer, Integer>();
+		goodHistogram.put(0, 16);
+		goodHistogram.put(1, 16);
+		goodHistogram.put(2, 7);
+		goodHistogram.put(3, 3);
+		goodHistogram.put(4, 1);
+		goodHistogram.put(5, 1);
 
-  @Override
-  @After
-  public void tearDown() {
-    try {
-      removeTopicModel();
-      removeDFModel();
-      this.stopAgentFrameWork();
-      super.tearDown();
-    } catch (AgentLifecycleException e) {
-      e.printStackTrace();
-    }
-  }
+		badHistogram = new HashMap<Integer, Integer>();
+		badHistogram.put(0, 5);
+		badHistogram.put(1, 2);
 
-  @Test
-  public void testRun() throws InterruptedException, TupleSpaceException, IOException {
-    Tuple response = writeTupleGetResponse(goodHistogram);
-    assertNotNull("no response received", response);
-    String message = (String) response.getField(7).getValue();
-    assertNotNull(message, "message = ok");
-    response = writeTupleGetResponse(badHistogram);
-    assertNotNull("no response received", response);
-    message = (String) response.getField(7).getValue();
-    assertNotNull(message, "message = too few keywords or text too long");
-    response = writeTupleGetResponse(mediumHistogram);
-    assertNotNull("no response received", response);
-    message = (String) response.getField(7).getValue();
-    assertNotNull(message, "message = inter-relation between keywords");
-  }
+		mediumHistogram = new HashMap<Integer, Integer>();
+		mediumHistogram.put(0, 2);
+		mediumHistogram.put(1, 4);
+	}
 
-  private Tuple writeTupleGetResponse(HashMap<Integer, Integer> histogram) throws IOException, TupleSpaceException {
-    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-    ObjectOutputStream objectOut = new ObjectOutputStream(bytes);
-    objectOut.writeObject(histogram);
-    objectOut.close();
+	@Override
+	@After
+	public void tearDown() {
+		try {
+			removeTopicModel();
+			removeDFModel();
+			this.stopAgentFrameWork();
+			super.tearDown();
+		} catch (AgentLifecycleException e) {
+			e.printStackTrace();
+		}
+	}
 
-    Tuple tuple = new Tuple(HypothesisEvaluationAgent.EVAL, "testUser", "SomeMission",
-                            "TestSession", "copex", eloPath, bytes.toByteArray());
-    getCommandSpace().write(tuple);
-    Tuple response = this.getCommandSpace().waitToTake(new Tuple(AgentProtocol.NOTIFICATION,
-                                                                 String.class, String.class,
-                                                                 String.class, String.class,
-                                                                 String.class, String.class,
-                                                                 Field.createWildCardField()),
-                                                       AgentProtocol.ALIVE_INTERVAL * 3);
-    return response;
-  }
+	@Test
+	public void testRun() throws InterruptedException, TupleSpaceException,
+			IOException {
+		Tuple response = writeTupleGetResponse(goodHistogram);
+		assertNotNull("no response received", response);
+		String message = (String) response.getField(7).getValue();
+		assertNotNull(message, "message = ok");
+		response = writeTupleGetResponse(badHistogram);
+		assertNotNull("no response received", response);
+		message = (String) response.getField(7).getValue();
+		assertNotNull(message, "message = too few keywords or text too long");
+		response = writeTupleGetResponse(mediumHistogram);
+		assertNotNull("no response received", response);
+		message = (String) response.getField(7).getValue();
+		assertNotNull(message, "message = inter-relation between keywords");
+	}
+
+	private Tuple writeTupleGetResponse(HashMap<Integer, Integer> histogram)
+			throws IOException, TupleSpaceException {
+		ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+		ObjectOutputStream objectOut = new ObjectOutputStream(bytes);
+		objectOut.writeObject(histogram);
+		objectOut.close();
+
+		Tuple tuple = new Tuple(HypothesisEvaluationAgent.EVAL, "testUser",
+				MISSION, "TestSession", "copex", eloPath,
+				bytes.toByteArray());
+		getCommandSpace().write(tuple);
+		Tuple response = this.getCommandSpace().waitToTake(
+				new Tuple(AgentProtocol.NOTIFICATION, String.class,
+						String.class, String.class, String.class, String.class,
+						String.class, Field.createWildCardField()),
+				AgentProtocol.ALIVE_INTERVAL * 3);
+		return response;
+	}
 }
