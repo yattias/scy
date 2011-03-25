de45e4c493ace246b0d11619a23ee8093e36e75c
hanbei <hanbei@gmx.de>
2011-03-25 09:30:04 +0000

    fixed wrong class bug

--- sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/util/Preprocessor.java
+++ sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/util/Preprocessor.java
@@ -8,96 +8,103 @@ import java.util.Scanner;
 import java.util.Set;
 import java.util.StringTokenizer;
 
+import cc.mallet.pipe.Pipe;
+import cc.mallet.pipe.TokenSequence2FeatureSequence;
 import cc.mallet.types.Instance;
 import cc.mallet.types.Token;
 import cc.mallet.types.TokenSequence;
 
 public class Preprocessor {
 
-  public Instance createInstanceFromTokens(String id, String[] documents) {
-    List<Token> malletTokens = new ArrayList<Token>();
-    for (String token : documents) {
-      malletTokens.add(new Token(token));
-    }
-    Instance instance = new Instance(new TokenSequence(malletTokens), "", id, id);
-    return instance;
-  }
+	public Instance createInstanceFromTokens(String id, String[] documents) {
+		List<Token> malletTokens = new ArrayList<Token>();
+		for (String token : documents) {
+			malletTokens.add(new Token(token));
+		}
+		Instance instance = new Instance(new TokenSequence(malletTokens), "",
+				id, id);
+		Pipe pipe = new TokenSequence2FeatureSequence();
+		return pipe.pipe(instance);
+	}
 
-  public String removeEmailAdresses(String document) {
-    return document.replaceAll(TMParameters.EMAIL_PATTERN, " ");
-  }
+	public String removeEmailAdresses(String document) {
+		return document.replaceAll(TMParameters.EMAIL_PATTERN, " ");
+	}
 
-  public String removeURLs(String document) {
-    return document.replaceAll(TMParameters.URL_PATTERN, " ");
-  }
+	public String removeURLs(String document) {
+		return document.replaceAll(TMParameters.URL_PATTERN, " ");
+	}
 
-  public String removeNumbers(String document) {
-    return document.replaceAll(TMParameters.NUMBER_PATTERN, " ");
-  }
+	public String removeNumbers(String document) {
+		return document.replaceAll(TMParameters.NUMBER_PATTERN, " ");
+	}
 
-  public String removeBrackets(String document) {
-    return document.replaceAll("[" + TMParameters.BRACKET_PATTERN + "]", " ");
-  }
+	public String removeBrackets(String document) {
+		return document.replaceAll("[" + TMParameters.BRACKET_PATTERN + "]",
+				" ");
+	}
 
-  public String removePunctuation(String document) {
-    return document.replaceAll("[\\|" + TMParameters.PUNCT_PATTERN + "]", " ");
-  }
+	public String removePunctuation(String document) {
+		return document.replaceAll("[\\|" + TMParameters.PUNCT_PATTERN + "]",
+				" ");
+	}
 
-  public String removeSpecialChars(String document) {
-    return document.replaceAll("[&%\\$§�\\^\\°·]", " ");
-  }
+	public String removeSpecialChars(String document) {
+		return document.replaceAll("[&%\\$§�\\^\\°·]", " ");
+	}
 
-  public String[] tokenize(String document) {
-    List<String> tokenizedDocument = new ArrayList<String>();
-    StringTokenizer tokenizer = new StringTokenizer(document);
-    while (tokenizer.hasMoreTokens()) {
-      String token = tokenizer.nextToken();
-      tokenizedDocument.add(token);
-    }
-    return tokenizedDocument.toArray(new String[0]);
-  }
+	public String[] tokenize(String document) {
+		List<String> tokenizedDocument = new ArrayList<String>();
+		StringTokenizer tokenizer = new StringTokenizer(document);
+		while (tokenizer.hasMoreTokens()) {
+			String token = tokenizer.nextToken();
+			tokenizedDocument.add(token);
+		}
+		return tokenizedDocument.toArray(new String[0]);
+	}
 
-  public String[] removeStopwords(String[] tokens, String language) {
-    Set<String> stopWords = readStopWords(language);
-    List<String> result = new ArrayList<String>();
-    for (String token : tokens) {
-      if (!stopWords.contains(token)) {
-        result.add(token);
-      }
-    }
-    return result.toArray(new String[0]);
-  }
+	public String[] removeStopwords(String[] tokens, String language) {
+		Set<String> stopWords = readStopWords(language);
+		List<String> result = new ArrayList<String>();
+		for (String token : tokens) {
+			if (!stopWords.contains(token)) {
+				result.add(token);
+			}
+		}
+		return result.toArray(new String[0]);
+	}
 
-  private Set<String> readStopWords(String language) {
-    Set<String> stopwords = new HashSet<String>();
-    InputStream inStream = getClass().getResourceAsStream("/" + language + "_stopwords.txt");
-    Scanner scanner = new Scanner(inStream).useDelimiter("\n");
+	private Set<String> readStopWords(String language) {
+		Set<String> stopwords = new HashSet<String>();
+		InputStream inStream = getClass().getResourceAsStream(
+				"/" + language + "_stopwords.txt");
+		Scanner scanner = new Scanner(inStream).useDelimiter("\n");
 
-    while (scanner.hasNext()) {
-      stopwords.add(scanner.next().trim());
-    }
-    return stopwords;
-  }
+		while (scanner.hasNext()) {
+			stopwords.add(scanner.next().trim());
+		}
+		return stopwords;
+	}
 
-  public String toLowerCase(String string) {
-    return string.toLowerCase();
-  }
+	public String toLowerCase(String string) {
+		return string.toLowerCase();
+	}
 
-  public String[] preprocessDocument(String doc) {
-    doc = removeEmailAdresses(doc);
-    doc = removeURLs(doc);
-    doc = removeNumbers(doc);
-    doc = removePunctuation(doc);
-    doc = removeBrackets(doc);
-    doc = toLowerCase(doc);
-    doc = removeSpecialChars(doc);
-    String [] tokDoc = tokenize(doc);
-    List<String> newTokens = new ArrayList<String>();
-    for (String tok : tokDoc) {
-      if (tok.matches(TMParameters.WORD_PATTERN))
-        newTokens.add(tok);
-    }
-    return newTokens.toArray(new String[0]);
-  }
+	public String[] preprocessDocument(String doc) {
+		doc = removeEmailAdresses(doc);
+		doc = removeURLs(doc);
+		doc = removeNumbers(doc);
+		doc = removePunctuation(doc);
+		doc = removeBrackets(doc);
+		doc = toLowerCase(doc);
+		doc = removeSpecialChars(doc);
+		String[] tokDoc = tokenize(doc);
+		List<String> newTokens = new ArrayList<String>();
+		for (String tok : tokDoc) {
+			if (tok.matches(TMParameters.WORD_PATTERN))
+				newTokens.add(tok);
+		}
+		return newTokens.toArray(new String[0]);
+	}
 
 }
