8057fff70bec635631c4c25a9b473a43abf0cbab
jakob.sikken <jakob.sikken@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-03-31 16:56:12 +0000

    Set the DrawerUIIndicator.

--- sources/modules/client/tools/fx-flying-saucer/src/main/java/eu/scy/client/tools/fxflyingsaucer/registration/FlyingSaucerCreator.java
+++ sources/modules/client/tools/fx-flying-saucer/src/main/java/eu/scy/client/tools/fxflyingsaucer/registration/FlyingSaucerCreator.java
@@ -2,35 +2,39 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package eu.scy.client.tools.fxflyingsaucer.registration;
 
 import eu.scy.client.desktop.scydesktop.elofactory.ScyToolCreator;
 import eu.scy.client.tools.fxflyingsaucer.EloFlyingSaucerPanel;
 import eu.scy.client.tools.fxflyingsaucer.UrlSource;
 import javax.swing.JComponent;
+import eu.scy.client.desktop.scydesktop.tools.DrawerUIIndicator;
 
 /**
  *
  * @author sikken
  */
-public class FlyingSaucerCreator implements ScyToolCreator {
+public class FlyingSaucerCreator implements ScyToolCreator
+{
+
    private UrlSource urlSource;
+   private DrawerUIIndicator drawerUIIndicator = null;
 
    public FlyingSaucerCreator()
    {
-      this(UrlSource.ELO);
+      this(UrlSource.ELO, null);
    }
 
-   public FlyingSaucerCreator(UrlSource urlSource)
+   public FlyingSaucerCreator(UrlSource urlSource, DrawerUIIndicator drawerUIIndicator)
    {
       this.urlSource = urlSource;
+      this.drawerUIIndicator = drawerUIIndicator;
    }
 
    @Override
    public JComponent createScyToolComponent(String eloType, String creatorId, boolean windowContent)
    {
-      JComponent flyingSaucer = new EloFlyingSaucerPanel(urlSource);
+      JComponent flyingSaucer = new EloFlyingSaucerPanel(urlSource, drawerUIIndicator);
       return flyingSaucer;
    }
 
@@ -39,5 +43,4 @@ public class FlyingSaucerCreator implements ScyToolCreator {
    {
       return true;
    }
-
 }
