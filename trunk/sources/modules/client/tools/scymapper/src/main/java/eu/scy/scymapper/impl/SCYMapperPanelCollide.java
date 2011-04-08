a4855fe8e10cbb7e0e9f017429f11964c9569c2d
janengler80@googlemail.com <janengler80@googlemail.com@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-04-08 13:23:25 +0000

    removed lexicon

--- trunk/sources/modules/client/tools/scymapper/src/main/java/eu/scy/scymapper/impl/SCYMapperPanelCollide.java
+++ trunk/sources/modules/client/tools/scymapper/src/main/java/eu/scy/scymapper/impl/SCYMapperPanelCollide.java
@@ -11,8 +11,6 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
-import javax.swing.JToggleButton;
-import javax.swing.SwingUtilities;
 
 import eu.scy.actionlogging.SQLSpacesActionLogger;
 import eu.scy.scymapper.api.IConceptMap;
@@ -24,7 +22,6 @@ import eu.scy.scymapper.impl.ui.FadeNotificator;
 import eu.scy.scymapper.impl.ui.Localization;
 import eu.scy.scymapper.impl.ui.Notificator;
 import eu.scy.scymapper.impl.ui.diagram.ConceptDiagramView;
-import eu.scy.scymapper.impl.ui.notification.ConceptBrowserPanel;
 import eu.scy.scymapper.impl.ui.notification.DoubleKeywordSuggestionPanel;
 import eu.scy.scymapper.impl.ui.notification.KeywordSuggestionPanelCollide;
 import eu.scy.scymapper.impl.ui.toolbar.ConceptMapToolBar;
@@ -34,21 +31,21 @@ public class SCYMapperPanelCollide extends SCYMapperPanel {
 
     private final static FadeNotificator.Position SUGGEST_KEYWORD_POSITION = FadeNotificator.Position.EAST;
 
-    private final static FadeNotificator.Position LEXICON_POSITION = FadeNotificator.Position.LOWER_LEFT_CORNER;
+//    private final static FadeNotificator.Position LEXICON_POSITION = FadeNotificator.Position.LOWER_LEFT_CORNER;
 
     private final static int REENABLE_BUTTONS_TIMER = 10;
 
-    private Notificator lexiconNotificator;
+//    private Notificator lexiconNotificator;
 
     private SCYMapperStandaloneConfig standaloneConfig;
 
-    private ConceptBrowserPanel conceptBrowserPanel;
+//    private ConceptBrowserPanel conceptBrowserPanel;
 
     private JButton requestConceptHelpButton;
 
     private JButton requestRelationHelpButton;
 
-    private JToggleButton requestLexiconButton;
+//    private JToggleButton requestLexiconButton;
 
     private Timer timer;
 
@@ -196,25 +193,25 @@ public class SCYMapperPanelCollide extends SCYMapperPanel {
                 break;
         }
 
-        requestLexiconButton = new JToggleButton(Localization.getString("Mainframe.Toolbar.RequestLexicon"));
-        requestLexiconButton.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ((ConceptMapActionLoggerCollide) actionLogger).logLexiconSwitched();
-                if (lexiconNotificator != null && lexiconNotificator.isVisible()) {
-                    lexiconNotificator.hide();
-                } else {
-                    showLexicon();
-                }
-            }
-
-        });
+//        requestLexiconButton = new JToggleButton(Localization.getString("Mainframe.Toolbar.RequestLexicon"));
+//        requestLexiconButton.addActionListener(new ActionListener() {
+//
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                ((ConceptMapActionLoggerCollide) actionLogger).logLexiconSwitched();
+//                if (lexiconNotificator != null && lexiconNotificator.isVisible()) {
+//                    lexiconNotificator.hide();
+//                } else {
+//                    showLexicon();
+//                }
+//            }
+//
+//        });
         //toolBar.add(requestLexiconButton);
 
-        conceptBrowserPanel = new ConceptBrowserPanel();
-        conceptBrowserPanel.setVisible(false);
-        add(BorderLayout.SOUTH, conceptBrowserPanel);
+//        conceptBrowserPanel = new ConceptBrowserPanel();
+//        conceptBrowserPanel.setVisible(false);
+//        add(BorderLayout.SOUTH, conceptBrowserPanel);
     }
 
     @Override
@@ -276,34 +273,34 @@ public class SCYMapperPanelCollide extends SCYMapperPanel {
     /**
      * Creates a new ConceptBrowserPanel with a new set of lexicon entries and shows it on screen.
      */
-    public void showLexicon() {
-
-        conceptBrowserPanel = new ConceptBrowserPanel();
-        conceptBrowserPanel.showFirstEntries();
-
-    	conceptBrowserPanel.setSize(250, 300);
-    	conceptBrowserPanel.setBorder(BorderFactory.createLineBorder(Color.darkGray, 1));
-    	conceptBrowserPanel.setBackground(Color.WHITE);
-
-        if (lexiconNotificator != null) {
-            lexiconNotificator.hide();
-        }
-
-        lexiconNotificator = createNotificator(this, conceptBrowserPanel, LEXICON_POSITION, 0, 0);
-
-        JButton close = new JButton(Localization.getString("Mainframe.Input.Close"));
-        close.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                requestLexiconButton.setSelected(false);
-                lexiconNotificator.hide();
-            }
-        });
-        conceptBrowserPanel.add(BorderLayout.SOUTH, close);
-
-        lexiconNotificator.show();
-    }
+//    public void showLexicon() {
+//
+//        conceptBrowserPanel = new ConceptBrowserPanel();
+//        conceptBrowserPanel.showFirstEntries();
+//
+//    	conceptBrowserPanel.setSize(250, 300);
+//    	conceptBrowserPanel.setBorder(BorderFactory.createLineBorder(Color.darkGray, 1));
+//    	conceptBrowserPanel.setBackground(Color.WHITE);
+//
+//        if (lexiconNotificator != null) {
+//            lexiconNotificator.hide();
+//        }
+//
+//        lexiconNotificator = createNotificator(this, conceptBrowserPanel, LEXICON_POSITION, 0, 0);
+//
+//        JButton close = new JButton(Localization.getString("Mainframe.Input.Close"));
+//        close.addActionListener(new ActionListener() {
+//
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                requestLexiconButton.setSelected(false);
+//                lexiconNotificator.hide();
+//            }
+//        });
+//        conceptBrowserPanel.add(BorderLayout.SOUTH, close);
+//
+//        lexiconNotificator.show();
+//    }
 
     protected void requestConceptHelp() {
         actionLogger.logRequestConceptHelp();
