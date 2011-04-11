109f188c460400218f012f1900a14281d2ed6842
stefan.weinbrenner <stefan.weinbrenner@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-04-11 13:47:53 +0000

    added elouri to notification tuple

--- sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/conceptmap/enrich/CMEnricherAgent.java
+++ sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/conceptmap/enrich/CMEnricherAgent.java
@@ -89,7 +89,7 @@ public class CMEnricherAgent extends AbstractThreadedAgent {
     }
 
     private void sendFeeback(String id, String elouri, String user, String mission, String session, String type, String... proposals) throws TupleSpaceException {
-        Tuple notificationTuple = new Tuple("notification", id, user, "scymapper", "cmenricher agent", mission, session, "proposal_type=" + type);
+        Tuple notificationTuple = new Tuple("notification", id, user, elouri, "cmenricher agent", mission, session, "proposal_type=" + type);
         for (String proposal : proposals) {
             if (proposal.startsWith(type)) {
                 notificationTuple.add(proposal);
