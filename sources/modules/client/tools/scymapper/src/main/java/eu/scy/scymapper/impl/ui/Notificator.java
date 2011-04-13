6921b39e2ee2890ec38b3e7409cecae70379d1bf
janengler80 <janengler80@googlemail.com>
2011-04-13 14:06:19 +0000

    added hideNow() method

--- sources/modules/client/tools/scymapper/src/main/java/eu/scy/scymapper/impl/ui/Notificator.java
+++ sources/modules/client/tools/scymapper/src/main/java/eu/scy/scymapper/impl/ui/Notificator.java
@@ -4,9 +4,11 @@ package eu.scy.scymapper.impl.ui;
 public interface Notificator {
 
     public void show();
-    
+
     public void hide();
-    
+
     public boolean isVisible();
-    
+
+    public void hideNow();
+
 }
