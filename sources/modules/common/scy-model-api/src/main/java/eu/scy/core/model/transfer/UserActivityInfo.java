2e20e723fbf5f68e0635166b980f27f136516d09
henrik <henrik@enovate.no>
2011-05-18 20:17:06 +0000

    added useractivityinfo

---
+++ sources/modules/common/scy-model-api/src/main/java/eu/scy/core/model/transfer/UserActivityInfo.java
@@ -0,0 +1,30 @@
+package eu.scy.core.model.transfer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 18.mai.2011
+ * Time: 13:33:59
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserActivityInfo {
+
+    private String userName;
+    private String language;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
