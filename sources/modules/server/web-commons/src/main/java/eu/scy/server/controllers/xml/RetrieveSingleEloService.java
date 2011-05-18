94c69cb9a0ef477b34e1379929db0a70440f646f
henrik <henrik@enovate.no>
2011-05-18 00:20:17 +0000

    Fixed single elo loading. Also fixed the bug with all feedback being listed up on every elo

--- sources/modules/server/web-commons/src/main/java/eu/scy/server/controllers/xml/RetrieveSingleEloService.java
+++ sources/modules/server/web-commons/src/main/java/eu/scy/server/controllers/xml/RetrieveSingleEloService.java
@@ -1,5 +1,6 @@
 package eu.scy.server.controllers.xml;
 
+import eu.scy.common.mission.MissionRuntimeElo;
 import eu.scy.common.scyelo.ScyElo;
 import eu.scy.core.model.transfer.NewestElos;
 import eu.scy.core.model.transfer.ServiceMessage;
@@ -9,6 +10,7 @@ import eu.scy.core.roolo.MissionELOService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URLDecoder;
 import java.util.List;
 
@@ -25,40 +27,26 @@ public class RetrieveSingleEloService extends XMLStreamerController{
 
     @Override
     protected Object getObjectToStream(HttpServletRequest request, HttpServletResponse httpServletResponse) {
-        String eloURI = request.getParameter("singleELO");
-        String missionURI = request.getParameter("missionURI");
-
         try {
+            String eloURI = request.getParameter("singleELO");
+            String missionURI = request.getParameter("missionURI");
+
+
+            URI missionRuntimeURI = new URI(missionURI);
+            MissionRuntimeElo missionRuntimeElo = MissionRuntimeElo.loadLastVersionElo(missionRuntimeURI, getMissionELOService());
+
             URI eloUri = new URI(eloURI);
 
             ScyElo scyElo = ScyElo.loadLastVersionElo(eloUri, getMissionELOService());
-            TransferElo transferElo = new TransferElo(scyElo);
+            TransferElo returnElo = getMissionELOService().getTransferElo(scyElo);
+            if(returnElo != null) return returnElo;
 
-
-            NewestElos newestElos = getMissionELOService().getNewestElosForFeedback(null, null);
-            List transferElos = newestElos.getElos();
-            for (int i = 0; i < transferElos.size(); i++) {
-                TransferElo elo = (TransferElo) transferElos.get(i);
-                if(elo.getUri().equals(elo.getUri())) {
-                    String feedbackURK = elo.getFeedbackEloURI();
-                    feedbackURK = URLDecoder.decode(feedbackURK, "UTF-8");
-                    logger.info("FEEDBACK URK: " + feedbackURK);
-                    if(feedbackURK != null) {
-                        URI feedbackURI = new URI(feedbackURK);
-                        ScyElo scyFeedbackElo = ScyElo.loadLastVersionElo(feedbackURI, getMissionELOService());
-                        transferElo.setFeedbackELO(scyFeedbackElo);
-                    }
-                }
-            }
-
-            return transferElo;
         } catch (Exception e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            logger.error(e.getMessage(), e);
         }
 
         ServiceMessage sm = new ServiceMessage();
         sm.setMessage("This wasn't exactly how we envisioned it - unclearException occurred.");
-
         return sm;
 
 
