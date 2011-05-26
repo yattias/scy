57c167541b965397284daff7ecc523245423ce32
hanbei@gmx.de <hanbei@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-05-26 12:48:03 +0000

    fixed a test for the NotificationAgent

--- trunk/sources/modules/agents/agents-impl/src/test/java/eu/scy/agents/authoring/NotifyUserAgentTest.java
+++ trunk/sources/modules/agents/agents-impl/src/test/java/eu/scy/agents/authoring/NotifyUserAgentTest.java
@@ -1,72 +1,71 @@
 package eu.scy.agents.authoring;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import eu.scy.agents.AbstractTestFixture;
+import eu.scy.agents.api.AgentLifecycleException;
+import eu.scy.agents.impl.AgentProtocol;
+import info.collide.sqlspaces.commons.Field;
 import info.collide.sqlspaces.commons.Tuple;
 import info.collide.sqlspaces.commons.TupleSpaceException;
-
-import java.rmi.dgc.VMID;
-import java.util.HashMap;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import eu.scy.agents.AbstractTestFixture;
-import eu.scy.agents.api.AgentLifecycleException;
-import eu.scy.agents.impl.AgentProtocol;
+import java.rmi.dgc.VMID;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class NotifyUserAgentTest extends AbstractTestFixture {
 
-	@BeforeClass
-	public static void startTS() {
-		startTupleSpaceServer();
-	}
+    @BeforeClass
+    public static void startTS() {
+        startTupleSpaceServer();
+    }
 
-	@AfterClass
-	public static void stopTS() {
-		stopTupleSpaceServer();
-	}
+    @AfterClass
+    public static void stopTS() {
+        stopTupleSpaceServer();
+    }
 
-	@Override
-	@Before
-	public void setUp() throws Exception {
-		super.setUp();
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
 
-		HashMap<String, Object> params = new HashMap<String, Object>();
-		params.put(AgentProtocol.PARAM_AGENT_ID, new VMID());
-		params.put(AgentProtocol.TS_HOST, TSHOST);
-		params.put(AgentProtocol.TS_PORT, TSPORT);
-		this.agentMap.put(NotifyUserAgent.NAME, params);
-		this.startAgentFramework(this.agentMap);
-	}
+        HashMap<String, Object> params = new HashMap<String, Object>();
+        params.put(AgentProtocol.PARAM_AGENT_ID, new VMID());
+        params.put(AgentProtocol.TS_HOST, TSHOST);
+        params.put(AgentProtocol.TS_PORT, TSPORT);
+        this.agentMap.put(NotifyUserAgent.NAME, params);
+        this.startAgentFramework(this.agentMap);
+    }
 
-	@Override
-	@After
-	public void tearDown() {
-		try {
-			this.stopAgentFrameWork();
-			super.tearDown();
-		} catch (AgentLifecycleException e) {
-			e.printStackTrace();
-		}
-	}
+    @Override
+    @After
+    public void tearDown() {
+        try {
+            this.stopAgentFrameWork();
+            super.tearDown();
+        } catch (AgentLifecycleException e) {
+            e.printStackTrace();
+        }
+    }
 
-	@Test
-	public void testRun() throws TupleSpaceException {
-		this.getCommandSpace().write(
-				new Tuple(NotifyUserAgent.SEND_NOTIFICATION, "FromTeacher",
-						"ToUser", "InMission1", "STFU"));
+    @Test
+    public void testRun() throws TupleSpaceException, InterruptedException {
+        this.getCommandSpace().write(
+                new Tuple(NotifyUserAgent.SEND_NOTIFICATION, "FromTeacher",
+                        "ToUser", "InMission1", "STFU"));
 
-		Tuple notificationTuple = this.getCommandSpace().waitToTake(
-				new Tuple(AgentProtocol.NOTIFICATION, String.class, "ToUser",
-						"growl", "FromTeacher", "InMission1", String.class,
-						"type=message_notification", String.class),
-				AgentProtocol.ALIVE_INTERVAL);
+        Thread.sleep(AgentProtocol.SECOND * 5);
 
-		assertNotNull("no notification received", notificationTuple);
-		assertEquals("STFU", notificationTuple.getField(8).getValue());
-	}
+        Tuple notificationTuple = this.getCommandSpace().waitToTake(
+                new Tuple(AgentProtocol.NOTIFICATION, Field.createWildCardField()), AgentProtocol.ALIVE_INTERVAL);
+
+        assertNotNull("no notification received", notificationTuple);
+        assertEquals("text=STFU", notificationTuple.getField(7).getValue());
+    }
 }
