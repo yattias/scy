c81653be27652e03b1d070166880d1390829f29d
latinum1@gmx.de <latinum1@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-05-27 08:11:38 +0000

    added Javadoc

--- trunk/sources/modules/common/tool-broker-api/src/main/java/eu/scy/collaboration/api/CollaborationStartable.java
+++ trunk/sources/modules/common/tool-broker-api/src/main/java/eu/scy/collaboration/api/CollaborationStartable.java
@@ -1,15 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package eu.scy.collaboration.api;
 
+import eu.scy.client.common.datasync.ISyncSession;
+
 /**
- *
+ * A tool implementing this interface should be collaborative. 
+ * Scy-Lab can now determine if collaboration is possible and if the collaboration should be initiated via Drag and Drop
+ * the acceptDrop and canAcceptDrop methods from ScyTool interface should be implemented in a useful way by the tool.
  * @author Sven
  */
 public interface CollaborationStartable {
 
+    /**
+     * This method is called when collaboration is initiated. 
+     * A useful implementation would be creating an {@link ISyncSession} and register listeners on it (e.g. {@link ISyncSession#addCollaboratorStatusListener(eu.scy.client.common.datasync.CollaboratorStatusListener)}).
+     * @param mucid The mucId for the ISyncSession
+     */
     public void startCollaboration(String mucid);
 }
