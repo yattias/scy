c60821d8f48a6455a0c1b11f6469833b22c33975
hanbei@gmx.de <hanbei@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-06-08 15:20:07 +0000

    fixed mission model loading and added scy/richtext in functional role hypothesis

--- trunk/sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/hypothesis/HypothesisEvaluationAgent.java
+++ trunk/sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/hypothesis/HypothesisEvaluationAgent.java
@@ -1,7 +1,29 @@
 package eu.scy.agents.hypothesis;
 
+import de.fhg.iais.kd.tm.obwious.base.featurecarrier.Document;
+import de.fhg.iais.kd.tm.obwious.base.featurecarrier.Features;
+import de.fhg.iais.kd.tm.obwious.operator.ObjectIdentifiers;
+import de.fhg.iais.kd.tm.obwious.operator.Operator;
+import de.fhg.iais.kd.tm.obwious.type.Container;
+import eu.scy.agents.api.AgentLifecycleException;
+import eu.scy.agents.api.IRepositoryAgent;
+import eu.scy.agents.hypothesis.workflow.EvalHypothesisWorkflow;
+import eu.scy.agents.impl.AbstractELOSavedAgent;
+import eu.scy.agents.impl.AgentProtocol;
+import eu.scy.agents.impl.AgentRooloServiceImpl;
+import eu.scy.agents.impl.EloTypes;
+import eu.scy.agents.keywords.ExtractKeyphrasesAgent;
+import eu.scy.agents.keywords.ExtractTopicModelKeywordsAgent;
+import eu.scy.agents.keywords.KeywordConstants;
+import eu.scy.agents.keywords.OntologyKeywordsAgent;
+import eu.scy.agents.util.Utilities;
+import eu.scy.common.scyelo.EloFunctionalRole;
+import eu.scy.common.scyelo.ScyElo;
 import info.collide.sqlspaces.commons.Tuple;
 import info.collide.sqlspaces.commons.TupleSpaceException;
+import org.apache.log4j.Logger;
+import roolo.api.IRepository;
+import roolo.elo.api.IMetadataTypeManager;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -16,31 +38,9 @@ import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-import org.apache.log4j.Logger;
-
-import roolo.api.IRepository;
-import roolo.elo.api.IELO;
-import roolo.elo.api.IMetadataTypeManager;
-import de.fhg.iais.kd.tm.obwious.base.featurecarrier.Document;
-import de.fhg.iais.kd.tm.obwious.base.featurecarrier.Features;
-import de.fhg.iais.kd.tm.obwious.operator.ObjectIdentifiers;
-import de.fhg.iais.kd.tm.obwious.operator.Operator;
-import de.fhg.iais.kd.tm.obwious.type.Container;
-import eu.scy.agents.api.AgentLifecycleException;
-import eu.scy.agents.api.IRepositoryAgent;
-import eu.scy.agents.hypothesis.workflow.EvalHypothesisWorkflow;
-import eu.scy.agents.impl.AbstractELOSavedAgent;
-import eu.scy.agents.impl.AgentProtocol;
-import eu.scy.agents.impl.EloTypes;
-import eu.scy.agents.keywords.ExtractKeyphrasesAgent;
-import eu.scy.agents.keywords.ExtractTopicModelKeywordsAgent;
-import eu.scy.agents.keywords.KeywordConstants;
-import eu.scy.agents.keywords.OntologyKeywordsAgent;
-import eu.scy.agents.util.Utilities;
-
 /**
  * @author Jörg Kindermann
- * 
+ *         <p/>
  *         Workflow: extract Keywords based only on topic models and ontology
  *         (don't use the meta-data keywords, because they also contain key
  *         words found by the TFIDF workflow AND keywords from other parts of
@@ -50,163 +50,177 @@ import eu.scy.agents.util.Utilities;
  *         sentences which contain 0,1,2... keywords.
  */
 public class HypothesisEvaluationAgent extends AbstractELOSavedAgent implements
-		IRepositoryAgent {
+        IRepositoryAgent {
 
-	public static final String NAME = HypothesisEvaluationAgent.class.getName();
+    public static final String NAME = HypothesisEvaluationAgent.class.getName();
 
-	public static final Object EVAL = "EvalHypothesis";
-	private static String XMLPATH = "//learner_proc/proc_hypothesis/hypothesis";
+    public static final Object EVAL = "EvalHypothesis";
+    private static String XMLPATH = "//learner_proc/proc_hypothesis/hypothesis";
 
-	private static final Logger logger = Logger
-			.getLogger(HypothesisEvaluationAgent.class.getName());
+    private static final Logger logger = Logger
+            .getLogger(HypothesisEvaluationAgent.class.getName());
 
-	private boolean isStopped;
+    private boolean isStopped;
 
-	private IRepository repository;
+    private AgentRooloServiceImpl rooloServices;
 
-	private IMetadataTypeManager metadataTypeManager;
+    public HypothesisEvaluationAgent(Map<String, Object> map) {
+        super(NAME, (String) map.get(AgentProtocol.PARAM_AGENT_ID),
+                (String) map.get(AgentProtocol.TS_HOST), (Integer) map
+                        .get(AgentProtocol.TS_PORT));
+        rooloServices = new AgentRooloServiceImpl();
+    }
 
-	public HypothesisEvaluationAgent(Map<String, Object> map) {
-		super(NAME, (String) map.get(AgentProtocol.PARAM_AGENT_ID),
-				(String) map.get(AgentProtocol.TS_HOST), (Integer) map
-						.get(AgentProtocol.TS_PORT));
-	}
+    @Override
+    protected void doRun() throws TupleSpaceException, AgentLifecycleException,
+            InterruptedException {
+        while (status == Status.Running) {
+            sendAliveUpdate();
+            Thread.sleep(5000);
+        }
+    }
 
-	@Override
-	protected void doRun() throws TupleSpaceException, AgentLifecycleException,
-			InterruptedException {
-		while (status == Status.Running) {
-			sendAliveUpdate();
-			Thread.sleep(5000);
-		}
-	}
+    @Override
+    protected void doStop() {
+        try {
+            getActionSpace().disconnect();
+            getCommandSpace().disconnect();
+        } catch (TupleSpaceException e) {
+            e.printStackTrace();
+        }
+    }
 
-	@Override
-	protected void doStop() {
-		try {
-			getActionSpace().disconnect();
-			getCommandSpace().disconnect();
-		} catch (TupleSpaceException e) {
-			e.printStackTrace();
-		}
-	}
+    @Override
+    protected Tuple getIdentifyTuple(String queryId) {
+        // TODO
+        return null;
+    }
 
-	@Override
-	protected Tuple getIdentifyTuple(String queryId) {
-		// TODO
-		return null;
-	}
+    @Override
+    public boolean isStopped() {
+        return isStopped;
+    }
 
-	@Override
-	public boolean isStopped() {
-		return isStopped;
-	}
+    @Override
+    public void processELOSavedAction(String actionId, String user,
+                                      long timeInMillis, String tool, String mission, String session,
+                                      String eloUri, String eloType) {
+        try {
+            if (eloType == null) {
+                logger.warn(eloUri + " has no type");
+            }
+            mission = getSession().getMission(user).getName();
+            URI eloURI = new URI(eloUri);
+            AgentRooloServiceImpl rooloServices = new AgentRooloServiceImpl();
+            ScyElo elo = ScyElo.loadElo(URI.create(eloUri), rooloServices);
 
-	@Override
-	public void processELOSavedAction(String actionId, String user,
-			long timeInMillis, String tool, String mission, String session,
-			String eloUri, String eloType) {
-		try {
-			if (eloType == null) {
-				logger.warn(eloUri + " has no type");
-			}
-			if (!EloTypes.SCY_XPROC.equals(eloType)) {
-				return;
-			}
-			URI eloURI = new URI(eloUri);
-			IELO elo = repository.retrieveELO(eloURI);
-			String text = Utilities.getEloText(elo, XMLPATH, logger);
-			Set<String> topicKeywords = callKeywordsAgent(
-					ExtractTopicModelKeywordsAgent.EXTRACT_TOPIC_MODEL_KEYWORDS,
-					text, mission, AgentProtocol.MINUTE * 3);
-			logger.debug("found in tm keywords: " + topicKeywords);
-			Set<String> ontologyKeywords = callKeywordsAgent(
-					OntologyKeywordsAgent.EXTRACT_ONTOLOGY_KEYWORDS, text,
-					mission, AgentProtocol.MINUTE);
-			logger.debug("found in ont keywords: " + ontologyKeywords);
-			Set<String> keyPharses = callKeywordsAgent(
-					ExtractKeyphrasesAgent.EXTRACT_KEYPHRASES, text, mission,
-					AgentProtocol.MINUTE * 3);
-			logger.debug("found in keyphrases: " + keyPharses);
+            if (!isCorrectEloType(elo)) {
+                return;
+            }
 
-			Set<String> keywords = new HashSet<String>();
-			keywords.addAll(topicKeywords);
-			keywords.addAll(ontologyKeywords);
-			keywords.addAll(keyPharses);
-			logger.info("found keywords: " + keywords);
+            String text = Utilities.getEloText(elo.getElo(), XMLPATH, logger);
+            Set<String> topicKeywords = callKeywordsAgent(
+                    ExtractTopicModelKeywordsAgent.EXTRACT_TOPIC_MODEL_KEYWORDS,
+                    text, mission, AgentProtocol.MINUTE * 3);
+            logger.debug("found in tm keywords: " + topicKeywords);
+            Set<String> ontologyKeywords = callKeywordsAgent(
+                    OntologyKeywordsAgent.EXTRACT_ONTOLOGY_KEYWORDS, text,
+                    mission, AgentProtocol.MINUTE);
+            logger.debug("found in ont keywords: " + ontologyKeywords);
+            Set<String> keyPharses = callKeywordsAgent(
+                    ExtractKeyphrasesAgent.EXTRACT_KEYPHRASES, text, mission,
+                    AgentProtocol.MINUTE * 3);
+            logger.debug("found in keyphrases: " + keyPharses);
 
-			// make OBWIOUS document and process in workflow:
-			Document document = Utilities.convertTextToDocument(text);
-			ArrayList<String> kwList = new ArrayList<String>(keywords);
-			document.setFeature(Features.WORDS, kwList);
-			Operator cmpHistogramOp = new EvalHypothesisWorkflow()
-					.getOperator("Main");
-			cmpHistogramOp.setInputParameter(ObjectIdentifiers.DOCUMENT,
-					document);
-			Container result = cmpHistogramOp.run();
-			Document docResult = (Document) result
-					.get(ObjectIdentifiers.DOCUMENT);
-			HashMap<Integer, Integer> hist = docResult
-					.getFeature(KeywordConstants.KEYWORD_SENTENCE_HISTOGRAM);
+            Set<String> keywords = new HashSet<String>();
+            keywords.addAll(topicKeywords);
+            keywords.addAll(ontologyKeywords);
+            keywords.addAll(keyPharses);
+            logger.info("found keywords: " + keywords);
 
-			// addSentenceHistogramToMetadata(elo, hist);
+            // make OBWIOUS document and process in workflow:
+            Document document = Utilities.convertTextToDocument(text);
+            ArrayList<String> kwList = new ArrayList<String>(keywords);
+            document.setFeature(Features.WORDS, kwList);
+            Operator cmpHistogramOp = new EvalHypothesisWorkflow()
+                    .getOperator("Main");
+            cmpHistogramOp.setInputParameter(ObjectIdentifiers.DOCUMENT,
+                    document);
+            Container result = cmpHistogramOp.run();
+            Document docResult = (Document) result
+                    .get(ObjectIdentifiers.DOCUMENT);
+            HashMap<Integer, Integer> hist = docResult
+                    .getFeature(KeywordConstants.KEYWORD_SENTENCE_HISTOGRAM);
 
-			// write HashMap histogram as a ByteArray object
-			ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
-			ObjectOutputStream out = new ObjectOutputStream(bytesOut);
-			out.writeObject(hist);
-			// write a tuple with Byte version of histogram to trigger the
-			// HypothesisEvaluation decision
-			// maker
-			Tuple activateDecisionMakerTuple = new Tuple(EVAL, user, mission,
-					session, tool, eloUri, bytesOut.toByteArray());
-			getCommandSpace().write(activateDecisionMakerTuple);
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (TupleSpaceException e) {
-			e.printStackTrace();
-		}
+            // addSentenceHistogramToMetadata(elo, hist);
 
-	}
+            // write HashMap histogram as a ByteArray object
+            ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
+            ObjectOutputStream out = new ObjectOutputStream(bytesOut);
+            out.writeObject(hist);
+            // write a tuple with Byte version of histogram to trigger the
+            // HypothesisEvaluation decision
+            // maker
+            Tuple activateDecisionMakerTuple = new Tuple(EVAL, user, mission,
+                    session, tool, eloUri, bytesOut.toByteArray());
+            getCommandSpace().write(activateDecisionMakerTuple);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TupleSpaceException e) {
+            e.printStackTrace();
+        }
 
-	@Override
-	public void setMetadataTypeManager(IMetadataTypeManager manager) {
-		metadataTypeManager = manager;
-	}
+    }
 
-	@Override
-	public void setRepository(IRepository rep) {
-		// injects RoOLO repository to provide access to ELOs
-		repository = rep;
-	}
+    private boolean isCorrectEloType(ScyElo elo) {
+        String technicalFormat = elo.getTechnicalFormat();
+        if (EloTypes.SCY_XPROC.equals(technicalFormat)) {
+            return true;
+        } else if (EloTypes.SCY_RICHTEXT.equals(technicalFormat)) {
+            if(elo.getFunctionalRole() == EloFunctionalRole.HYPOTHESIS) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	// TODO: extract this method because it is a duplicate from
-	// ExtractKeywordsAgent
-	private Set<String> callKeywordsAgent(String agent, String text,
-			String mission, int waitTime) {
-		String queryId = new VMID().toString();
-		Set<String> result = new HashSet<String>();
-		try {
-			getCommandSpace().write(
-					new Tuple(agent, AgentProtocol.QUERY, queryId, text,
-							mission, "en"));
-			Tuple response = getCommandSpace().waitToTake(
-					new Tuple(agent, AgentProtocol.RESPONSE, queryId,
-							String.class), waitTime);
-			if (response == null) {
-				return result;
-			}
-			String keywordString = (String) response.getField(3).getValue();
-			StringTokenizer tokenizer = new StringTokenizer(keywordString, ";");
-			while (tokenizer.hasMoreTokens()) {
-				result.add(tokenizer.nextToken());
-			}
-		} catch (TupleSpaceException e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
+    @Override
+    public void setMetadataTypeManager(IMetadataTypeManager manager) {
+        rooloServices.setMetadataTypeManager(manager);
+    }
+
+    @Override
+    public void setRepository(IRepository rep) {
+        rooloServices.setRepository(rep);
+    }
+
+    // TODO: extract this method because it is a duplicate from
+    // ExtractKeywordsAgent
+    private Set<String> callKeywordsAgent(String agent, String text,
+                                          String mission, int waitTime) {
+        String queryId = new VMID().toString();
+        Set<String> result = new HashSet<String>();
+        try {
+            getCommandSpace().write(
+                    new Tuple(agent, AgentProtocol.QUERY, queryId, text,
+                            mission, "en"));
+            Tuple response = getCommandSpace().waitToTake(
+                    new Tuple(agent, AgentProtocol.RESPONSE, queryId,
+                            String.class), waitTime);
+            if (response == null) {
+                return result;
+            }
+            String keywordString = (String) response.getField(3).getValue();
+            StringTokenizer tokenizer = new StringTokenizer(keywordString, ";");
+            while (tokenizer.hasMoreTokens()) {
+                result.add(tokenizer.nextToken());
+            }
+        } catch (TupleSpaceException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 
 }
