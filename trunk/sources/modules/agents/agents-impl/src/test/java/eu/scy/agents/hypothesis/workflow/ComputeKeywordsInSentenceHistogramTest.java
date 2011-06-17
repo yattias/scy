96b7d14ce973b3fdf3ea462f1add30946a7ec695
joerg.kindermann@iais.fraunhofer.de <joerg.kindermann@iais.fraunhofer.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-06-17 14:17:38 +0000

    fixed unit tests

--- trunk/sources/modules/agents/agents-impl/src/test/java/eu/scy/agents/hypothesis/workflow/ComputeKeywordsInSentenceHistogramTest.java
+++ trunk/sources/modules/agents/agents-impl/src/test/java/eu/scy/agents/hypothesis/workflow/ComputeKeywordsInSentenceHistogramTest.java
@@ -47,7 +47,7 @@ public class ComputeKeywordsInSentenceHistogramTest extends AbstractTestFixture 
   public void setup() throws Exception {
     super.setUp();
 
-    InputStream inStream = this.getClass().getResourceAsStream("/conceptMap1.xml");
+    InputStream inStream = this.getClass().getResourceAsStream("/copexExampleElo.xml");
     String eloContent = readFile(inStream);
     largeElo = createNewElo("TestCopex1", EloTypes.SCY_XPROC);
     largeElo.setContent(new BasicContent(eloContent));
