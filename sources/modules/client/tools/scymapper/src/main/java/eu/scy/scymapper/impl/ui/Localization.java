a932bed3721876bf5cc2aaee7a452c3a8017f39e
jakob.sikken <jakob.sikken@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-06-22 12:16:43 +0000

    Moved and renamed the properties files of scymapper.

--- sources/modules/client/tools/scymapper/src/main/java/eu/scy/scymapper/impl/ui/Localization.java
+++ sources/modules/client/tools/scymapper/src/main/java/eu/scy/scymapper/impl/ui/Localization.java
@@ -4,7 +4,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Localization {
-	private static final String BUNDLE_NAME = "eu.scy.scymapper.localization";
+	private static final String BUNDLE_NAME = "languages.scymapper";
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
 			.getBundle(BUNDLE_NAME);
