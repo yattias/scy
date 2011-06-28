5939156e77ae0ed8f8bd0fe02403ec363630f822
jakob.sikken <jakob.sikken@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-06-28 09:03:34 +0000

    Updated the language property files of most tools

---
+++ sources/modules/client/client-common/scy-i18n/src/test/java/eu/scy/client/common/scyi18n/Scy18nTests.java
@@ -0,0 +1,16 @@
+package eu.scy.client.common.scyi18n;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( { 
+   LocalUriReplacementTest.class, 
+   ResourceBundleWrapperTest.class,
+   UriLocalizerTest.class })
+         
+         
+public class Scy18nTests
+{
+
+}
