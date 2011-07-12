c8a869092646f149de845262501d2d0a0dcb4ea2
jakob.sikken <jakob.sikken@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-07-12 14:12:20 +0000

    Added a separate showAndLogException method.

--- sources/modules/client/desktop/desktop-utils/src/main/java/eu/scy/client/desktop/desktoputils/ExceptionCatcher.java
+++ sources/modules/client/desktop/desktop-utils/src/main/java/eu/scy/client/desktop/desktoputils/ExceptionCatcher.java
@@ -13,6 +13,7 @@ import org.apache.log4j.Logger;
 
 public class ExceptionCatcher implements Thread.UncaughtExceptionHandler
 {
+
    private final static Logger logger = Logger.getLogger(ExceptionCatcher.class);
    private String appName = "unknown";
    private boolean quit = false;
@@ -43,47 +44,56 @@ public class ExceptionCatcher implements Thread.UncaughtExceptionHandler
    @Override
    public void uncaughtException(Thread t, Throwable e)
    {
-      logger.error("An uncatched exception occurred in thread " + t.getName(), e);
-      System.err.println("An uncatched exception occurred at " + new Date() + " in thread " + t.getName());
-      e.printStackTrace(System.err);
-      final ResourceBundleWrapper resourceBundleWrapper = new ResourceBundleWrapper(this);
-      if (resourceBundleWrapper==null){
-         logger.error("can't report uncatched exception, because I can't find ResourceBundleWrapper");
-         return;
-      }
-      final String npeMessage = resourceBundleWrapper.getString("ExceptionCatcher.nullPointerException");
-      String errorMessage = getThrowableMessage(e,npeMessage);
-      StringBuilder exceptionMessage = new StringBuilder(errorMessage);
-      Throwable cause = e.getCause();
-      while (cause!=null){
-         exceptionMessage.append("\n");
-         exceptionMessage.append(resourceBundleWrapper.getString("ExceptionCatcher.causedBy"));
-         exceptionMessage.append(" ");
-         exceptionMessage.append(getThrowableMessage(cause,npeMessage));
-         cause = e.getCause();
-      }
-      JOptionPane.showMessageDialog(null,resourceBundleWrapper.getString("ExceptionCatcher.message")+ "\n" +
-         resourceBundleWrapper.getString("ExceptionCatcher.application") + " " + appName + "\n" +
-         resourceBundleWrapper.getString("ExceptionCatcher.thread") + " " + t.getName() + "\n" +
-         resourceBundleWrapper.getString("ExceptionCatcher.exceptionMessage") + " " + exceptionMessage.toString() + "\n" +
-         resourceBundleWrapper.getString("ExceptionCatcher.loggedAction"),
-         resourceBundleWrapper.getString("ExceptionCatcher.title"),
-         JOptionPane.ERROR_MESSAGE);
-//      JOptionPane.showMessageDialog(null, "An uncatched exception occurred:\nApplication: " + appName + "\nThread: " + t.getName() + "\nMessage: " + e.getMessage() + "\nThe exception has been logged.", "An uncatched exception occurred",
-//         JOptionPane.ERROR_MESSAGE);
+      showAndLogUncaughtException(t, e);
       if (quit)
       {
          System.exit(1);
       }
    }
 
-   private String getThrowableMessage(Throwable e, String npeMessage){
-      if (e!=null){
+   public void showAndLogUncaughtException(Thread t, Throwable e)
+   {
+      logger.error("An uncatched exception occurred in thread " + t.getName(), e);
+      System.err.println("An uncatched exception occurred at " + new Date() + " in thread " + t.getName());
+      e.printStackTrace(System.err);
+      final ResourceBundleWrapper resourceBundleWrapper = new ResourceBundleWrapper(this);
+      if (resourceBundleWrapper == null)
+      {
+         logger.error("can't report uncatched exception, because I can't find ResourceBundleWrapper");
+         return;
+      }
+      final String npeMessage = resourceBundleWrapper.getString("ExceptionCatcher.nullPointerException");
+      String errorMessage = getThrowableMessage(e, npeMessage);
+      StringBuilder exceptionMessage = new StringBuilder(errorMessage);
+      Throwable cause = e.getCause();
+      while (cause != null)
+      {
+         exceptionMessage.append("\n");
+         exceptionMessage.append(resourceBundleWrapper.getString("ExceptionCatcher.causedBy"));
+         exceptionMessage.append(" ");
+         exceptionMessage.append(getThrowableMessage(cause, npeMessage));
+         cause = e.getCause();
+      }
+      JOptionPane.showMessageDialog(null, resourceBundleWrapper.getString("ExceptionCatcher.message") + "\n"
+         + resourceBundleWrapper.getString("ExceptionCatcher.application") + " " + appName + "\n"
+         + resourceBundleWrapper.getString("ExceptionCatcher.thread") + " " + t.getName() + "\n"
+         + resourceBundleWrapper.getString("ExceptionCatcher.exceptionMessage") + " " + exceptionMessage.toString() + "\n"
+         + resourceBundleWrapper.getString("ExceptionCatcher.loggedAction"),
+         resourceBundleWrapper.getString("ExceptionCatcher.title"),
+         JOptionPane.ERROR_MESSAGE);
+   }
+
+   private String getThrowableMessage(Throwable e, String npeMessage)
+   {
+      if (e != null)
+      {
          final String message = e.getMessage();
-         if (message!=null){
+         if (message != null)
+         {
             return message;
          }
-         else{
+         else
+         {
             return npeMessage;
          }
       }
