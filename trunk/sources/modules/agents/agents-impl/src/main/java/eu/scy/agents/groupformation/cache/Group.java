a4553c3103c80665631abc20580d46b1c8a1dbce
hanbei@gmx.de <hanbei@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-07-29 15:01:47 +0000

    - introduced a group class - allows assignemnt of latecomers (not yet fully implemented)

---
+++ trunk/sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/groupformation/cache/Group.java
@@ -0,0 +1,64 @@
+package eu.scy.agents.groupformation.cache;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/** @author fschulz */
+public class Group implements Iterable<String> {
+
+    private static final Group EMPTY_GROUP = new Group();
+
+    private Set<String> group;
+    private Object data;
+
+    public Group() {
+        group = new LinkedHashSet<String>();
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        return group.iterator();
+    }
+
+    public boolean isEmpty() {
+        return group.isEmpty();
+    }
+
+    public void remove(String userToRemove) {
+        group.remove(userToRemove);
+    }
+
+    public int size() {
+        return group.size();
+    }
+
+    public void add(String user) {
+        group.add(user);
+    }
+
+    public boolean contains(String user) {
+        return group.contains(user);
+    }
+
+    public static Group emptyGroup() {
+        return EMPTY_GROUP;
+    }
+
+    public Set<String> asSet() {
+        return Collections.unmodifiableSet(group);
+    }
+
+    public void clear() {
+        group.clear();
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Object getData() {
+        return data;
+    }
+}
