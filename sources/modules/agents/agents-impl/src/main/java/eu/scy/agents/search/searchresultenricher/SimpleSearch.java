5d5a5c58b8fb95c31c6681bedd1d506d8177e1f2
christopher.krueger.83 <christopher.krueger.83@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-07-29 08:27:07 +0000

    Small query generation fix and code cleanup

--- sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/search/searchresultenricher/SimpleSearch.java
+++ sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/search/searchresultenricher/SimpleSearch.java
@@ -1,40 +1,70 @@
 package eu.scy.agents.search.searchresultenricher;
 
-import java.rmi.dgc.VMID;
+import java.util.Locale;
 
-import eu.scy.agents.impl.AgentProtocol;
-import info.collide.sqlspaces.client.TupleSpace;
-import info.collide.sqlspaces.commons.Tuple;
-import info.collide.sqlspaces.commons.TupleSpaceException;
-import info.collide.sqlspaces.commons.User;
+//import eu.scy.agents.impl.AgentProtocol;
+//import info.collide.sqlspaces.client.TupleSpace;
+//import info.collide.sqlspaces.commons.Field;
+//import info.collide.sqlspaces.commons.Tuple;
+//import info.collide.sqlspaces.commons.TupleSpaceException;
+//import info.collide.sqlspaces.commons.User;
 
 public class SimpleSearch {
 
-	/*
+    /*
 	 * Just for testing purposes
 	 */
 	public static void main(String[] args) {
 		
-		TupleSpace actionSpace; 
-		try {
+		Locale locale = new Locale("en");
+		for(Locale loc : Locale.getAvailableLocales()) {
+			System.out.println(loc.getCountry() + " : " + loc.getLanguage() + " : " + loc.getVariant());
+//			if(locale.getLanguage().equals(loc.getLanguage())) {
+//				System.out.println(loc.getLanguage());
+//			}
+		}
+		
+//		TupleSpace sessionSpace; 
+//		try {
 			
 //			actionSpace = new TupleSpace(new User("test"), "localhost", 2525, false, false, AgentProtocol.ACTION_SPACE_NAME);
-			actionSpace = new TupleSpace(new User("test"), "scy.collide.info", 2525, false, false, AgentProtocol.ACTION_SPACE_NAME);
+//			sessionSpace = new TupleSpace(new User("test"), "scy.collide.info", 2525, false, false, AgentProtocol.SESSION_SPACE_NAME);
 
 		    // ("action":String, <ID>:String, <Timestamp>:long, <Type>:String, <User>:String, <Tool>:String,
 			// <Mission>:String, <Session>:String, <ELOUri>:String, 
 			// <Key=Value>:String*)
-			Tuple t1 = new Tuple(
-					"action", new VMID().toString(), System.currentTimeMillis(), "search-query", "krueger", "scy-simple-search", 
-					"n/a", "n/a",	"n/a", 
-					"query=experiment", "search_results=");
-			actionSpace.write(t1);
+//			Tuple t1 = new Tuple(
+//					"action", new VMID().toString(), System.currentTimeMillis(), "search-query", "krueger", "scy-simple-search", 
+//					"n/a", "n/a",	"n/a", 
+//					"query=experiment", "search_results=");
+//			actionSpace.write(t1);
+			
+//			Tuple t = new Tuple(String.class, String.class, Integer.class, String.class);
+//			Tuple t = new Tuple("language", String.class, String.class);
+//			Tuple[] response = sessionSpace.readAll(t);
+//			for (Tuple tu : response) {
+//				System.out.println(tu);
+//			}
+//			for(Field field : response.getFields()){
+//				System.out.println(field.getValue().toString());
+//			}
+			
+			
+//			for(Tuple st : sessionSpace.readAll(t, 50)) {
+//				System.out.println(st);
+////				for(Field f : st.getFields()) {
+////					System.out.println(f.getValue().toString());
+////				}
+////				System.out.println(st.getFields().toString());
+////				System.out.println(st.toString());
+//			}
+			
 
-			actionSpace.disconnect();
-		} catch (TupleSpaceException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//			sessionSpace.disconnect();
+//		} catch (TupleSpaceException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 
 	}
 }
