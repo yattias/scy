a42fd0b4c38ddd8b81c3dbef4f66e8a17b9e7b4b
hanbei@gmx.de <hanbei@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-08-12 09:04:38 +0000

    fixes in workflowrecording agent

--- trunk/sources/modules/common/agent-commons/src/main/java/eu/scy/agents/impl/parameter/AgentParameterAPIImpl2.java
+++ trunk/sources/modules/common/agent-commons/src/main/java/eu/scy/agents/impl/parameter/AgentParameterAPIImpl2.java
@@ -1,9 +1,5 @@
 package eu.scy.agents.impl.parameter;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
-
 import eu.scy.agents.api.parameter.AgentParameter;
 import eu.scy.agents.api.parameter.AgentParameterAPI;
 import eu.scy.common.mission.MissionRuntimeElo;
@@ -13,104 +9,92 @@ import eu.scy.common.mission.RuntimeSettingsElo;
 import eu.scy.common.mission.RuntimeSettingsEloContent;
 import eu.scy.common.scyelo.RooloServices;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
 public class AgentParameterAPIImpl2 implements AgentParameterAPI {
 
-	private RooloServices rooloService;
-	private AgentParameterAPI agentParameterAPI;
+    private RooloServices rooloService;
+    private AgentParameterAPI agentParameterAPI;
 
-	public AgentParameterAPIImpl2(RooloServices rooloService,
-			AgentParameterAPI agentParameterAPI) {
-		this.rooloService = rooloService;
-		this.agentParameterAPI = agentParameterAPI;
-	}
+    public AgentParameterAPIImpl2(RooloServices rooloService,
+                                  AgentParameterAPI agentParameterAPI) {
+        this.rooloService = rooloService;
+        this.agentParameterAPI = agentParameterAPI;
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public <T> T getParameter(String agentId, AgentParameter parameter) {
-		String missionURI = parameter.getMission();
-		try {
-			RuntimeSettingsElo runtimeSettingsElo = getRuntimeSettingsElo(missionURI);
-			RuntimeSettingsEloContent runtimeSettingsEloContent = runtimeSettingsElo
-					.getTypedContent();
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T getParameter(String agentId, AgentParameter parameter) {
+        agentParameterAPI.getParameter(agentId, parameter);
+        String missionURI = parameter.getMission();
+        try {
+            RuntimeSettingsElo runtimeSettingsElo = getRuntimeSettingsElo(missionURI);
+            RuntimeSettingsEloContent runtimeSettingsEloContent = runtimeSettingsElo
+                    .getTypedContent();
 
-			RuntimeSettingKey runtimeSettingKey = new RuntimeSettingKey(agentId
-					+ parameter.getParameterName(), parameter.getLas(),
-					parameter.getELOUri());
+            RuntimeSettingKey runtimeSettingKey = new RuntimeSettingKey(agentId
+                    + parameter.getParameterName(), parameter.getLas(),
+                    parameter.getELOUri());
 
-			String setting = runtimeSettingsEloContent
-					.getSetting(runtimeSettingKey);
-			return (T) setting;
-		} catch (URISyntaxException e) {
-			throw new RuntimeException(
-					"the uri of the mission settings is wrong ", e);
-		}
+            String setting = runtimeSettingsEloContent
+                    .getSetting(runtimeSettingKey);
+            return (T) setting;
+        } catch ( URISyntaxException e ) {
+            throw new RuntimeException(
+                    "the uri of the mission settings is wrong ", e);
+        }
 
-	}
+    }
 
-	public RuntimeSettingsElo getRuntimeSettingsElo(String missionURI)
-			throws URISyntaxException {
-		URI runtimeSettingsEloUri = getMissionSettingsELO(missionURI);
-		RuntimeSettingsElo runtimeSettingsElo = RuntimeSettingsElo
-				.loadLastVersionElo(runtimeSettingsEloUri, rooloService);
-		return runtimeSettingsElo;
-	}
+    private RuntimeSettingsElo getRuntimeSettingsElo(String missionURI)
+            throws URISyntaxException {
+        URI runtimeSettingsEloUri = getMissionSettingsELO(missionURI);
+        RuntimeSettingsElo runtimeSettingsElo = RuntimeSettingsElo
+                .loadLastVersionElo(runtimeSettingsEloUri, rooloService);
+        return runtimeSettingsElo;
+    }
 
-	private URI getMissionSettingsELO(String missionURI)
-			throws URISyntaxException {
-		MissionRuntimeElo loadElo = MissionRuntimeElo.loadElo(new URI(
-				missionURI), rooloService);
-		URI runtimeSettingsEloUri = loadElo.getTypedContent()
-				.getRuntimeSettingsEloUri();
-		return runtimeSettingsEloUri;
-	}
+    private URI getMissionSettingsELO(String missionURI)
+            throws URISyntaxException {
+        MissionRuntimeElo loadElo = MissionRuntimeElo.loadElo(new URI(
+                missionURI), rooloService);
+        URI runtimeSettingsEloUri = loadElo.getTypedContent()
+                .getRuntimeSettingsEloUri();
+        return runtimeSettingsEloUri;
+    }
 
-	@Override
-	public List<String> listAgentParameter(String agentId) {
-		List<String> listAgentParameter = agentParameterAPI
-				.listAgentParameter(agentId);
-		return listAgentParameter;
-	}
+    @Override
+    public List<String> listAgentParameter(String agentId) {
+        List<String> listAgentParameter = agentParameterAPI
+                .listAgentParameter(agentId);
+        return listAgentParameter;
+    }
 
-	@Override
-	public <T> void setParameter(String agentId, AgentParameter parameter) {
-		String missionURI = parameter.getMission();
-		RuntimeSettingsElo runtimeSettingsElo;
-		try {
-			runtimeSettingsElo = getRuntimeSettingsElo(missionURI);
+    @Override
+    public <T> void setParameter(String agentId, AgentParameter parameter) {
+        agentParameterAPI.setParameter(agentId, parameter);
+        String missionURI = parameter.getMission();
+        RuntimeSettingsElo runtimeSettingsElo;
+        try {
+            runtimeSettingsElo = getRuntimeSettingsElo(missionURI);
 
-			RuntimeSettingsEloContent runtimeSettingsEloContent = runtimeSettingsElo
-					.getTypedContent();
+            RuntimeSettingsEloContent runtimeSettingsEloContent = runtimeSettingsElo
+                    .getTypedContent();
 
-			RuntimeSetting runtimeSetting = new RuntimeSetting();
-			runtimeSetting.setKey(new RuntimeSettingKey(agentId
-					+ parameter.getParameterName(), parameter.getLas(),
-					parameter.getELOUri()));
-			runtimeSetting.setValue((String) parameter.getParameterValue());
+            RuntimeSetting runtimeSetting = new RuntimeSetting();
+            runtimeSetting.setKey(new RuntimeSettingKey(agentId
+                    + parameter.getParameterName(), parameter.getLas(),
+                    parameter.getELOUri()));
+            runtimeSetting.setValue((String) parameter.getParameterValue());
 
-			runtimeSettingsEloContent.addSetting(runtimeSetting);
-			runtimeSettingsElo.updateElo();
-		} catch (URISyntaxException e) {
-			throw new RuntimeException(
-					"the uri of the mission settings is wrong ", e);
-		}
-	}
+            runtimeSettingsEloContent.addSetting(runtimeSetting);
+            runtimeSettingsElo.updateElo();
+        } catch ( URISyntaxException e ) {
+            throw new RuntimeException(
+                    "the uri of the mission settings is wrong ", e);
+        }
+    }
 
-	// final IMetadataKey missionRunningKey = tbi.getMetaDataTypeManager()
-	// .getMetadataKey(ScyRooloMetadataKeyIds.MISSION_RUNNING);
-	// final IMetadataKey technicalFormatKey = tbi.getMetaDataTypeManager()
-	// .getMetadataKey(CoreRooloMetadataKeyIds.TECHNICAL_FORMAT);
-	// IQuery missionSpecificationQuery = new BasicMetadataQuery(
-	// technicalFormatKey, BasicSearchOperations.EQUALS,
-	// MissionEloType.MISSION_SPECIFICATIOM.getType(), null);
-	// List<ISearchResult> missionSpecificationResults = tbi.getRepository()
-	// .search(missionSpecificationQuery);
-	// IQuery missionRuntimeQuery = new BasicMetadataQuery(technicalFormatKey,
-	// BasicSearchOperations.EQUALS, MissionEloType.MISSION_RUNTIME
-	// .getType(), null);
-	// IQuery titleQuery = new BasicMetadataQuery(missionRunningKey,
-	// BasicSearchOperations.EQUALS, userName, null);
-	// IQuery myMissionRuntimeQuery = new AndQuery(missionRuntimeQuery,
-	// titleQuery);
-	// List<ISearchResult> missionRuntimeResults = tbi.getRepository().search(
-	// myMissionRuntimeQuery);
 }
