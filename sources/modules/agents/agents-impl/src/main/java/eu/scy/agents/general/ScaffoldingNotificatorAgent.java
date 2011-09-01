88210b12170601ab501a42fe06247cdf116e38d6
hanbei <hanbei@gmx.de>
2011-09-01 07:51:10 +0000

    changes for lars

--- sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/general/ScaffoldingNotificatorAgent.java
+++ sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/general/ScaffoldingNotificatorAgent.java
@@ -1,26 +1,45 @@
 package eu.scy.agents.general;
 
+import eu.scy.actionlogging.ActionTupleTransformer;
+import eu.scy.actionlogging.api.ContextConstants;
 import eu.scy.actionlogging.api.IAction;
 import eu.scy.agents.api.AgentLifecycleException;
 import eu.scy.agents.api.parameter.AgentParameter;
 import eu.scy.agents.impl.AbstractThreadedAgent;
+import eu.scy.agents.impl.ActionConstants;
 import eu.scy.agents.impl.AgentProtocol;
+import info.collide.sqlspaces.commons.Field;
 import info.collide.sqlspaces.commons.Tuple;
 import info.collide.sqlspaces.commons.TupleSpaceException;
 import org.apache.log4j.Logger;
 
 import java.rmi.dgc.VMID;
 import java.util.Map;
+import java.util.Set;
 
+/**
+ * TODO Get all users for mission. Send notification for all users.
+ */
 public class ScaffoldingNotificatorAgent extends AbstractThreadedAgent {
 
     public static final String GLOBAL_SCAFFOLDING_LEVEL = "globalScaffoldingLevel";
     public static final String NAME = ScaffoldingNotificatorAgent.class.getName();
 
     private static final Logger LOGGER = Logger.getLogger(NAME);
+    private int loginSeq;
 
     public ScaffoldingNotificatorAgent(Map<String, Object> params) {
         super(NAME, (String) params.get(AgentProtocol.PARAM_AGENT_ID));
+        try {
+            this.loginSeq = this.getActionSpace()
+                    .eventRegister(
+                            Command.WRITE,
+                            new Tuple(ActionConstants.ACTION, String.class,
+                                    Long.class, ActionConstants.ACTION_LOG_IN,
+                                    Field.createWildCardField()), this, true);
+        } catch (TupleSpaceException e) {
+            LOGGER.warn("Couldn't register listener for log in actions.", e);
+        }
     }
 
     @Override
@@ -59,23 +78,47 @@ public class ScaffoldingNotificatorAgent extends AbstractThreadedAgent {
     @Override
     protected void parametersChanged(AgentParameter agentParameter) {
         if (GLOBAL_SCAFFOLDING_LEVEL.equals(agentParameter.getParameterName())) {
-            Tuple notificationTuple = new Tuple();
-            notificationTuple.add(AgentProtocol.NOTIFICATION);
-            notificationTuple.add(new VMID().toString());
-            notificationTuple.add(IAction.NOT_AVAILABLE);
-            notificationTuple.add(IAction.NOT_AVAILABLE);
-            notificationTuple.add(NAME);
-            notificationTuple.add(agentParameter.getMission());
-            notificationTuple.add(IAction.NOT_AVAILABLE);
-            notificationTuple.add("type=scaffold");
-            notificationTuple.add("level=" + agentParameter.getParameterValue());
+            String mission = agentParameter.getMission();
+            Set<String> usersInMission = getSession().getUsersInMission(mission);
 
-            try {
-                getCommandSpace().write(notificationTuple);
-            } catch (TupleSpaceException e) {
-                LOGGER.warn(e.getMessage(), e);
+            for (String user : usersInMission) {
+                sendScaffoldLevelNotification(agentParameter.getParameterValue(), mission, user);
             }
+        }
+
+
+    }
+
+    private <T> void sendScaffoldLevelNotification(T parameterValue, String mission, String user) {
+        Tuple notificationTuple = new Tuple();
+        notificationTuple.add(AgentProtocol.NOTIFICATION);
+        notificationTuple.add(new VMID().toString());
+        notificationTuple.add(user);
+        notificationTuple.add(IAction.NOT_AVAILABLE);
+        notificationTuple.add(NAME);
+        notificationTuple.add(mission);
+        notificationTuple.add(IAction.NOT_AVAILABLE);
+        notificationTuple.add("type=scaffold");
+        notificationTuple.add("level=" + parameterValue);
+
+        try {
+            getCommandSpace().write(notificationTuple);
+        } catch (TupleSpaceException e) {
+            LOGGER.warn(e.getMessage(), e);
         }
     }
 
+    @Override
+    public void call(Command command, int seq, Tuple afterTuple, Tuple beforeTuple) {
+        if (seq != loginSeq) {
+            logger.debug("Callback passed to Superclass.");
+            super.call(command, seq, afterTuple, beforeTuple);
+            return;
+        }
+
+        IAction action = ActionTupleTransformer.getActionFromTuple(afterTuple);
+
+        sendScaffoldLevelNotification(configuration.getParameter(GLOBAL_SCAFFOLDING_LEVEL),
+                action.getContext(ContextConstants.mission), action.getUser());
+    }
 }
