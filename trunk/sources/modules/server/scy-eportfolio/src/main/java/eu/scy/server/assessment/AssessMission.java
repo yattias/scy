516099d53f5d97e7c997af6e18ff07c1b8a4763f
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-09-05 01:03:54 +0000

    Storing portflio as assessed after assessment

---
+++ trunk/sources/modules/server/scy-eportfolio/src/main/java/eu/scy/server/assessment/AssessMission.java
@@ -0,0 +1,95 @@
+package eu.scy.server.assessment;
+
+import eu.scy.common.mission.MissionRuntimeElo;
+import eu.scy.core.XMLTransferObjectService;
+import eu.scy.core.model.transfer.MissionReflectionQuestionAnswer;
+import eu.scy.core.model.transfer.Portfolio;
+import eu.scy.core.roolo.MissionELOService;
+import eu.scy.core.roolo.PedagogicalPlanELOService;
+import eu.scy.core.roolo.PortfolioELOService;
+import eu.scy.core.roolo.RuntimeELOService;
+import eu.scy.server.controllers.BaseController;
+import eu.scy.server.util.TransferObjectMapService;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 05.sep.2011
+ * Time: 02:20:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class AssessMission extends BaseController {
+
+    private PortfolioELOService portfolioELOService;
+    private RuntimeELOService runtimeELOService;
+    private MissionELOService missionELOService;
+    private XMLTransferObjectService xmlTransferObjectService;
+    private PedagogicalPlanELOService pedagogicalPlanELOService;
+    private TransferObjectMapService transferObjectMapService;
+
+
+    @Override
+    protected void handleRequest(HttpServletRequest request, HttpServletResponse response, ModelAndView modelAndView) {
+        URI missionRuntimeURI = getURI(request.getParameter("missionRuntimeURI"));
+        MissionRuntimeElo missionRuntimeElo = MissionRuntimeElo.loadLastVersionElo(missionRuntimeURI, getMissionELOService());
+        Portfolio portfolio = getMissionELOService().getPortfolio(missionRuntimeElo, getCurrentUserName(request));
+
+        List<MissionReflectionQuestionAnswer> missionReflectionQuestionAnswers= portfolio.getMissionReflectionQuestionAnswers();
+        modelAndView.addObject("missionReflectionQuestionAnswers", missionReflectionQuestionAnswers);
+        modelAndView.addObject("missionRuntimeURI", getEncodedUri(missionRuntimeElo.getUri().toString()));
+    }
+
+    public PortfolioELOService getPortfolioELOService() {
+        return portfolioELOService;
+    }
+
+    public void setPortfolioELOService(PortfolioELOService portfolioELOService) {
+        this.portfolioELOService = portfolioELOService;
+    }
+
+    public RuntimeELOService getRuntimeELOService() {
+        return runtimeELOService;
+    }
+
+    public void setRuntimeELOService(RuntimeELOService runtimeELOService) {
+        this.runtimeELOService = runtimeELOService;
+    }
+
+    public MissionELOService getMissionELOService() {
+        return missionELOService;
+    }
+
+    public void setMissionELOService(MissionELOService missionELOService) {
+        this.missionELOService = missionELOService;
+    }
+
+    public XMLTransferObjectService getXmlTransferObjectService() {
+        return xmlTransferObjectService;
+    }
+
+    public void setXmlTransferObjectService(XMLTransferObjectService xmlTransferObjectService) {
+        this.xmlTransferObjectService = xmlTransferObjectService;
+    }
+
+    public PedagogicalPlanELOService getPedagogicalPlanELOService() {
+        return pedagogicalPlanELOService;
+    }
+
+    public void setPedagogicalPlanELOService(PedagogicalPlanELOService pedagogicalPlanELOService) {
+        this.pedagogicalPlanELOService = pedagogicalPlanELOService;
+    }
+
+    public TransferObjectMapService getTransferObjectMapService() {
+        return transferObjectMapService;
+    }
+
+    public void setTransferObjectMapService(TransferObjectMapService transferObjectMapService) {
+        this.transferObjectMapService = transferObjectMapService;
+    }
+}
