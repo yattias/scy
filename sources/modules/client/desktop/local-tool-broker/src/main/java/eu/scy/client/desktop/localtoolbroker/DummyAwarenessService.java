6801a500dd71687aa38eb9a9d52fdd87b93e1cf2
latinum1 <latinum1@gmx.de>
2011-09-09 13:19:36 +0000

    added some more dummy semantics

--- sources/modules/client/desktop/local-tool-broker/src/main/java/eu/scy/client/desktop/localtoolbroker/DummyAwarenessService.java
+++ sources/modules/client/desktop/local-tool-broker/src/main/java/eu/scy/client/desktop/localtoolbroker/DummyAwarenessService.java
@@ -17,178 +17,155 @@ import org.jivesoftware.smackx.muc.MultiUserChat;
  *
  * @author sikken
  */
-public class DummyAwarenessService implements IAwarenessService  {
+public class DummyAwarenessService implements IAwarenessService {
 
-   @Override
-   public void addAwarenessMessageListener(IAwarenessMessageListener awarenessMessageListener)
-   {
-   }
+    private List<IAwarenessUser> buddies;
 
-   @Override
-   public void addAwarenessPresenceListener(IAwarenessPresenceListener awarenessPresenceListener)
-   {
-   }
+    public DummyAwarenessService() {
+        super();
+        buddies = new ArrayList<IAwarenessUser>();
+        buddies.add(new MockAwarenessUser());
+    }
 
-   @Override
-   public void addAwarenessRosterListener(IAwarenessRosterListener awarenessListListener)
-   {
-   }
+    @Override
+    public void addAwarenessMessageListener(IAwarenessMessageListener awarenessMessageListener) {
+    }
 
-   @Override
-   public void addBuddy(String buddy) throws AwarenessServiceException
-   {
-   }
+    @Override
+    public void addAwarenessPresenceListener(IAwarenessPresenceListener awarenessPresenceListener) {
+    }
 
-   @Override
-   public void addChatToolListener(IChatPresenceToolListener listener)
-   {
-   }
+    @Override
+    public void addAwarenessRosterListener(IAwarenessRosterListener awarenessListListener) {
+    }
 
-   @Override
-   public void addPresenceToolListener(IChatPresenceToolListener listener)
-   {
-   }
+    @Override
+    public void addBuddy(String buddy) throws AwarenessServiceException {
+    }
 
-   @Override
-   public void disconnect()
-   {
-   }
+    @Override
+    public void addChatToolListener(IChatPresenceToolListener listener) {
+    }
 
-   @Override
-   public List<IAwarenessUser> getBuddies() throws AwarenessServiceException
-   {
-      return new ArrayList<IAwarenessUser>();
-   }
+    @Override
+    public void addPresenceToolListener(IChatPresenceToolListener listener) {
+    }
 
-   @Override
-   public void init(Connection connection)
-   {
-   }
+    @Override
+    public void disconnect() {
+    }
 
-   @Override
-   public boolean isConnected()
-   {
-      return false;
-   }
+    @Override
+    public List<IAwarenessUser> getBuddies() throws AwarenessServiceException {
+        System.out.println("++++++++++ getBuddies() on DummyAwarenessService called! ++++++++++++");
+        return buddies;
+    }
 
-   @Override
-   public void removeAwarenessPresenceListener(IAwarenessPresenceListener awarenessPresenceListener)
-   {
-   }
+    @Override
+    public void init(Connection connection) {
+    }
 
-   @Override
-   public void removeBuddy(String buddy) throws AwarenessServiceException
-   {
-   }
+    @Override
+    public boolean isConnected() {
+        return false;
+    }
+
+    @Override
+    public void removeAwarenessPresenceListener(IAwarenessPresenceListener awarenessPresenceListener) {
+    }
+
+    @Override
+    public void removeBuddy(String buddy) throws AwarenessServiceException {
+    }
 
 //   @Override
 //   public void sendMessage(String recipient, String message) throws AwarenessServiceException
 //   {
 //   }
+    @Override
+    public void setPresence(String presence) throws AwarenessServiceException {
+    }
 
-   @Override
-   public void setPresence(String presence) throws AwarenessServiceException
-   {
-   }
+    @Override
+    public void setStatus(String status) throws AwarenessServiceException {
+    }
 
-   @Override
-   public void setStatus(String status) throws AwarenessServiceException
-   {
-   }
+    @Override
+    public void updateChatTool(List<IAwarenessUser> users) {
+    }
 
-   @Override
-   public void updateChatTool(List<IAwarenessUser> users)
-   {
-   }
+    @Override
+    public void updatePresenceTool(List<IAwarenessUser> users) {
+    }
 
-   @Override
-   public void updatePresenceTool(List<IAwarenessUser> users)
-   {
-   }
+    @Override
+    public Connection getConnection() {
+        return null;
+    }
 
-   @Override
-   public Connection getConnection()
-   {
-      return null;
-   }
+    @Override
+    public void sendMUCMessage(String ELOUri, String message) throws AwarenessServiceException {
+    }
 
-   @Override
-   public void sendMUCMessage(String ELOUri, String message) throws AwarenessServiceException
-   {
-   }
-
-   @Override
-   public void joinMUCRoom(String ELOUri)
-   {
-   }
+    @Override
+    public void joinMUCRoom(String ELOUri) {
+    }
 
 //   @Override
 //   public List<IAwarenessUser> getChatBuddies(String ELOUri)
 //   {
 //      return new ArrayList<IAwarenessUser>();
 //   }
+    @Override
+    public void addBuddyToMUC(IAwarenessUser buddy, String ELOUri) {
+    }
 
-   @Override
-   public void addBuddyToMUC(IAwarenessUser buddy, String ELOUri)
-   {
-   }
+    @Override
+    public void removeBuddyFromMUC(IAwarenessUser buddy, String ELOUri) {
+    }
 
-   @Override
-   public void removeBuddyFromMUC(IAwarenessUser buddy, String ELOUri)
-   {
-   }
+    @Override
+    public boolean hasJoinedRoom(String ELOUri, String user) {
+        return false;
+    }
 
-   @Override
-   public boolean hasJoinedRoom(String ELOUri, String user)
-   {
-      return false;
-   }
+    @Override
+    public boolean doesRoomExist(String ELOUri) {
+        return true;
+    }
 
-   @Override
-   public boolean doesRoomExist(String ELOUri)
-   {
-      return true;
-   }
+    @Override
+    public void destoryMUCRoom(String ELOUri) {
+    }
 
-   @Override
-   public void destoryMUCRoom(String ELOUri)
-   {
-   }
+    @Override
+    public void setMUCConferenceExtension(String CONFERENCE_EXT) {
+    }
 
-   @Override
-   public void setMUCConferenceExtension(String CONFERENCE_EXT)
-   {
-   }
+    @Override
+    public String getMUCConferenceExtension() {
+        return "ext";
+    }
 
-   @Override
-   public String getMUCConferenceExtension()
-   {
-      return "ext";
-   }
+    @Override
+    public MultiUserChat getMultiUserChat(String ELOUri) {
+        return null;
+    }
 
-   @Override
-   public MultiUserChat getMultiUserChat(String ELOUri)
-   {
-      return null;
-   }
+    @Override
+    public void sendMessage(IAwarenessUser recipient, String message) throws AwarenessServiceException {
+    }
 
-   @Override
-   public void sendMessage(IAwarenessUser recipient, String message) throws AwarenessServiceException
-   {
-   }
-
-   @Override
-   public List<IAwarenessUser> getMUCBuddies(String ELOUri) throws AwarenessServiceException
-   {
-      return new ArrayList<IAwarenessUser>();
-   }
+    @Override
+    public List<IAwarenessUser> getMUCBuddies(String ELOUri) throws AwarenessServiceException {
+        return new ArrayList<IAwarenessUser>();
+    }
 
     @Override
     public void addInvitationListener(IAwarenessInvitationListener invitationListener) {
     }
-    
+
     @Override
     public void removeInvitationListener(IAwarenessInvitationListener invitationListener) {
     }
-
 }
