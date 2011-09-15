fecca5f54839d0820d08dd8901fc34e787b5fa7e
jakob.sikken <jakob.sikken@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-09-15 17:24:54 +0000

    Added ActionLoggedEvent listeners.

---
+++ trunk/sources/modules/common/action-logging-api/src/main/java/eu/scy/actionlogging/api/ActionLoggedEventListener.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eu.scy.actionlogging.api;
+
+/**
+ *
+ * @author SikkenJ
+ */
+public interface ActionLoggedEventListener {
+
+   public void actionLogged(ActionLoggedEvent actionLoggedEvent);
+}
