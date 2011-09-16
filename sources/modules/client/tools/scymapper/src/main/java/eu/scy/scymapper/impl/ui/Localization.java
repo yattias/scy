a06032ba04be4598c3c32acb33d58c3c9b2371a3
larsbollen <larsbollen@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-09-16 15:49:32 +0000

    scymapper's  i18n fixed

--- sources/modules/client/tools/scymapper/src/main/java/eu/scy/scymapper/impl/ui/Localization.java
+++ sources/modules/client/tools/scymapper/src/main/java/eu/scy/scymapper/impl/ui/Localization.java
@@ -1,22 +1,55 @@
 package eu.scy.scymapper.impl.ui;
 
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
+import eu.scy.client.common.scyi18n.ResourceBundleWrapper;
+import java.util.logging.Logger;
 
+/**
+ *
+ * @author lars
+ *
+ * changed completely to make use of scy-i18n
+ */
 public class Localization {
-	private static final String BUNDLE_NAME = "languages.scymapper";
 
-	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
-			.getBundle(BUNDLE_NAME);
+	private static Localization instance = null;
+	private static ResourceBundleWrapper bundle = null;
+	private final static Logger debugLogger = Logger.getLogger(Localization.class.getName());
 
+	// singleton pattern
 	private Localization() {
+		try {
+			bundle = new ResourceBundleWrapper("scymapper");
+		} catch (Exception ex) {
+			debugLogger.warning(ex.toString());
+		}
+	}
+
+	private static Localization getInstance() {
+		if (instance == null) {
+			instance = new Localization();
+		}
+		return instance;
 	}
 
 	public static String getString(String key) {
-		try {
-			return RESOURCE_BUNDLE.getString(key);
-		} catch (MissingResourceException e) {
-			return '!' + key + '!';
-		}
+		return getInstance().bundle.getString(key);
 	}
+
+// ye old stuff...
+
+//	private static final String BUNDLE_NAME = "languages.scymapper";
+//
+//	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
+//			.getBundle(BUNDLE_NAME);
+//
+//	private Localization() {
+//	}
+//
+//	public static String getString(String key) {
+//		try {
+//			return RESOURCE_BUNDLE.getString(key);
+//		} catch (MissingResourceException e) {
+//			return '!' + key + '!';
+//		}
+//	}
 }
