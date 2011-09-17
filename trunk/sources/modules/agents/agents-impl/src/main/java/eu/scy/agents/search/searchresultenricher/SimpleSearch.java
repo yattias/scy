baaf7977e5e2933265cfbfaa54bc19176f1b43ee
christopher.krueger.83 <christopher.krueger.83@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-09-17 12:37:10 +0000

    Commented out usage of ontology agent to make it work Some minor changes to the search handling Removed System.out.println() by logger

--- trunk/sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/search/searchresultenricher/SimpleSearch.java
+++ trunk/sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/search/searchresultenricher/SimpleSearch.java
@@ -2,8 +2,6 @@ package eu.scy.agents.search.searchresultenricher;
 
 import eu.scy.agents.impl.AgentProtocol;
 import info.collide.sqlspaces.client.TupleSpace;
-import info.collide.sqlspaces.commons.Field;
-import info.collide.sqlspaces.commons.Tuple;
 import info.collide.sqlspaces.commons.TupleSpaceException;
 import info.collide.sqlspaces.commons.User;
 
@@ -14,41 +12,43 @@ public class SimpleSearch {
 	 */
 	public static void main(String[] args) {
 		
-		TupleSpace sessionSpace; 
-//		TupleSpace actionSpace;
+//		TupleSpace sessionSpace; 
+		TupleSpace actionSpace;
+//		TupleSpace commandSpace;
 
 		try {
-//			actionSpace = new TupleSpace(new User("test"), "localhost", 2525, false, false, AgentProtocol.ACTION_SPACE_NAME);
+//			sessionSpace = new TupleSpace(new User("test"), "scy.collide.info", 2525, false, false, AgentProtocol.SESSION_SPACE_NAME);
+			actionSpace = new TupleSpace(new User("test"), "scy.collide.info", 2525, false, false, AgentProtocol.ACTION_SPACE_NAME);
+//			commandSpace = new TupleSpace(new User("test"), "scy.collide.info", 2525, false, false, AgentProtocol.COMMAND_SPACE_NAME);
 			
+			// perform a simple search
 		    // ("action":String, <ID>:String, <Timestamp>:long, <Type>:String, <User>:String, <Tool>:String,
 			// <Mission>:String, <Session>:String, <ELOUri>:String, 
 			// <Key=Value>:String*)
 //			Tuple t1 = new Tuple(
 //					"action", new VMID().toString(), System.currentTimeMillis(), "search-query", "krueger", "scy-simple-search", 
 //					"n/a", "n/a",	"n/a", 
-//					"query=experiment", "search_results=");
+//					"query=contents:\"experimant\"", "search_results=" + INSERT XML DATA HERE);
 //			actionSpace.write(t1);
 
+			// get a simple serach response
+//			String uniqueID = new VMID().toString();
+//			Tuple requestTuple = new Tuple(uniqueID, "roolo-agent", "search", "contents:\"experiment\"");
+//			commandSpace.write(requestTuple);
+//
+//			Tuple responseTuple = commandSpace.waitToTake(new Tuple(uniqueID, "roolo-response", 
+//					Field.createWildCardField()), 3000);
+//			if (responseTuple != null) {
+//				String xmlResults = responseTuple.getField(2).getValue().toString();
+//				System.out.println(xmlResults);
+//			}
 			
-			sessionSpace = new TupleSpace(new User("test"), "scy.collide.info", 2525, false, false, AgentProtocol.SESSION_SPACE_NAME);
-			Tuple t = new Tuple("language", String.class, String.class);
-			for (Tuple tu : sessionSpace.readAll(t)) {
-				System.out.println(tu);
-			}
-			
-			for(Tuple st : sessionSpace.readAll(t, 50)) {
-				System.out.println(st);
-				for(Field f : st.getFields()) {
-					System.out.println(f.getValue().toString());
-				}
-				System.out.println(st.getFields().toString());
-				System.out.println(st.toString());
-			}
-
-			sessionSpace.disconnect();
-//			actionSpace.disconnect();
+//			sessionSpace.disconnect();
+			actionSpace.disconnect();
+//			commandSpace.disconnect();
 		} catch (TupleSpaceException e) {
 			e.printStackTrace();
 		}
 	}
+	
 }
