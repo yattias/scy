cfcf61485411ceb398a317b73f5cff96ef5c262a
henrik@enovate.no <henrik@enovate.no@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-09-26 01:31:05 +0000

    Added the DNA-tool

---
+++ trunk/sources/modules/server/scy-eportfolio/src/main/java/eu/scy/server/eportfolio/DNAIndex.java
@@ -0,0 +1,21 @@
+package eu.scy.server.eportfolio;
+
+import eu.scy.server.controllers.BaseController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Henrik
+ * Date: 26.sep.2011
+ * Time: 03:25:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class DNAIndex extends BaseController {
+    @Override
+    protected void handleRequest(HttpServletRequest request, HttpServletResponse response, ModelAndView modelAndView) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
