9dc681db6bc77e589689d900bc1a769bd4deac6f
hanbei@gmx.de <hanbei@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-10-04 15:18:12 +0000

    and the estonian eco mission

--- trunk/sources/modules/common/agent-commons/src/main/java/eu/scy/agents/Mission.java
+++ trunk/sources/modules/common/agent-commons/src/main/java/eu/scy/agents/Mission.java
@@ -3,34 +3,37 @@ package eu.scy.agents;
 
 public enum Mission {
 
-    MISSION1("http://www.scy.eu/co2house#", "Design a CO2 friendly house", "mission1"),
-    MISSION1_UT("http://www.scy.eu/co2house#", "Design a CO2 friendly house (UT version)", "mission1"),
-    MISSION1_OSLO("http://www.scy.eu/co2house#", "Design a CO2 friendly house (Oslo version)", "mission1"),
+    MISSION1("http://www.scy.eu/co2house#", "Design a CO2 friendly house", "mission1", "co2_2"),
+    MISSION1_UT("http://www.scy.eu/co2house#", "Design a CO2 friendly house (UT version)", "mission1", "co2_2"),
+    MISSION1_OSLO("http://www.scy.eu/co2house#", "Design a CO2 friendly house (Oslo version)", "mission1", "co2_2"),
 
-    MISSION2("http://www.scy.eu/ecosystem#", "ECO mission", "mission2"),
+    MISSION2("http://www.scy.eu/ecosystem#", "ECO mission", "mission2", "eco"),
+    MISSION2_ESTONIAN("http://www.scy.eu/ecosystem#", "Ökoloogiamissioon", "mission2", "eco"),
 
-    MISSION3("http://www.scy.eu/pizza#", "A Healthy Pizza", "mission3"),
-    MISSION3_DUTCH("http://www.scy.eu/pizza#", "Een gezonde pizza", "mission3"),
-    MISSION3_GREEK("http://www.scy.eu/pizza#", "Υγιεινή πίτσα", "mission3"),
+    MISSION3("http://www.scy.eu/pizza#", "A Healthy Pizza", "mission3", "pizza2"),
+    MISSION3_DUTCH("http://www.scy.eu/pizza#", "Een gezonde pizza", "mission3", "pizza2"),
+    MISSION3_GREEK("http://www.scy.eu/pizza#", "Υγιεινή πίτσα", "mission3", "pizza2"),
 
-    MISSION4("http://www.scy.eu/dna#", "Forensic mission", "mission4"),
-    MISSION4_FRENCH("http://www.scy.eu/dna#", "Le laboratoire de police scientifique", "mission4"),
-    GMO("http://www.scy.eu/dna#", "GMO mission", "mission4"),
+    MISSION4("http://www.scy.eu/dna#", "Forensic mission", "mission4", "forensic"),
+    MISSION4_FRENCH("http://www.scy.eu/dna#", "Le laboratoire de police scientifique", "mission4", "forensic"),
+    GMO("http://www.scy.eu/dna#", "GMO mission", "mission4", "forensic"),
 
-    MISSION5("http://www.scy.eu/dna#", "Run for your life!", "mission5"),
+    MISSION5("http://www.scy.eu/dna#", "Run for your life!", "mission5", "mini1"),
 
-    UNKNOWN_MISSION("", "", "");
+    UNKNOWN_MISSION("", "", "", "");
 
     private String namespace;
 
     private String name;
 
     private String id;
+    private String missionId;
 
-    private Mission(String namespace, String name, String id) {
+    private Mission(String namespace, String name, String id, String missionId) {
         this.name = name;
         this.namespace = namespace;
         this.id = id;
+        this.missionId = missionId;
     }
 
     public String getName() {
@@ -45,6 +48,10 @@ public enum Mission {
         return id;
     }
 
+    public String getMissionId() {
+        return missionId;
+    }
+
     public static Mission getForName(String name) {
         for (Mission mo : Mission.values()) {
             if (mo.getName().equalsIgnoreCase(name)) {
@@ -57,6 +64,15 @@ public enum Mission {
     public static Mission getForId(String id) {
         for (Mission mo : Mission.values()) {
             if (mo.getId().equals(id)) {
+                return mo;
+            }
+        }
+        return Mission.UNKNOWN_MISSION;
+    }
+
+    public static Mission getForMissionId(String missionId) {
+        for (Mission mo : Mission.values()) {
+            if (mo.getMissionId().equals(missionId)) {
                 return mo;
             }
         }
