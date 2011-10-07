5cfe67e3ac866f37c7476f547ecff9ae0d60ed74
lubos.steskal <lubos.steskal@gmail.com>
2011-10-07 02:32:47 +0000

    LPV connecting to SQLSpaces as standalone JAR

---
+++ sources/external/LPV/src/AllDataComparator.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+
+class AllDataComparator implements Comparator<String[]> {
+
+	@Override
+	public int compare(String[] arg0, String[] arg1) {
+		if(Long.parseLong(arg0[0])<Long.parseLong(arg1[0])) return -1;
+		if(Long.parseLong(arg0[0])>Long.parseLong(arg1[0])) return 1;
+		return 0;
+	}
+
+}
