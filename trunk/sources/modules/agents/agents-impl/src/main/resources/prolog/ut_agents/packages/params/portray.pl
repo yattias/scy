de59f8b1acdeb84d9e240b8bfde6bf34fc67697c
a.a.anjewierden@utwente.nl <a.a.anjewierden@utwente.nl@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-10-12 12:55:27 +0000

    Delete ut_agents

--- trunk/sources/modules/agents/agents-impl/src/main/resources/prolog/ut_agents/packages/params/portray.pl
+++
@@ -1,9 +0,0 @@
-:- multifile
-	user:portray/1.
-
-user:portray(element(E,As,C)) :- !,
-	(   C = []
-	->  format('element(~w, ~q, [])~n', [E,As])
-	;   format('element(~w, ~q, [...,...])~n', [E,As])
-	).
-
