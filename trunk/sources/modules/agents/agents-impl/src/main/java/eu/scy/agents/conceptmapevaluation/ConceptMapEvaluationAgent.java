36c4057a8dc6b779cf5a99e4b8e763823d477d5e
stefan.weinbrenner@googlemail.com <stefan.weinbrenner@googlemail.com@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-10-12 13:45:18 +0000

    adapted CME agent wrapper to new path of pl file

--- trunk/sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/conceptmapevaluation/ConceptMapEvaluationAgent.java
+++ trunk/sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/conceptmapevaluation/ConceptMapEvaluationAgent.java
@@ -8,7 +8,7 @@ import eu.scy.agents.impl.PrologAgent;
 public class ConceptMapEvaluationAgent extends PrologAgent {
 
     public ConceptMapEvaluationAgent(Map<String, Object> map) {
-        super(ConceptMapEvaluationAgent.class.getName(), (String) map.get(AgentProtocol.PARAM_AGENT_ID), "ut_agents/applications/ic/agents/concept_map_evaluation/cme_agent.pl", "cme_agent_start([server(local)])");
+        super(ConceptMapEvaluationAgent.class.getName(), (String) map.get(AgentProtocol.PARAM_AGENT_ID), "cme_agent/applications/ic/agents/cme_agent/cme_agent.pl", "cme_agent_start([server(local)])");
     }
 
 }
