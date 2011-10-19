088e8b769f877d6ded1a726150178e52c12fa979
hanbei@gmx.de <hanbei@gmx.de@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-10-19 15:37:01 +0000

    guards against empty search results elos have authors without context

--- trunk/sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/groupformation/strategies/features/CMapFeatureExtractor.java
+++ trunk/sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/groupformation/strategies/features/CMapFeatureExtractor.java
@@ -1,17 +1,5 @@
 package eu.scy.agents.groupformation.strategies.features;
 
-import java.io.IOException;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
-
-import org.jaxen.JaxenException;
-import org.jdom.JDOMException;
-
-import roolo.elo.api.IContent;
-import roolo.elo.api.IELO;
 import de.fhg.iais.kd.tm.elo.CmapImporter;
 import de.fhg.iais.kd.tm.graphmatching.editdistance.EditCostFunction;
 import de.fhg.iais.kd.tm.graphmatching.editdistance.EditDistanceApproximator;
@@ -20,29 +8,45 @@ import de.fhg.iais.kd.tm.graphmatching.graph.Graph;
 import de.fhg.iais.kd.tm.graphmatching.graph.Vertex;
 import eu.scy.agents.impl.EloTypes;
 import eu.scy.common.scyelo.ScyElo;
+import org.jaxen.JaxenException;
+import org.jdom.JDOMException;
+import roolo.elo.api.IContent;
+import roolo.elo.api.IELO;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class CMapFeatureExtractor extends AbstractFeatureExtractor {
-	
-	   @Override
-	    public Map<String, double[]> getFeatures(Set<String> availableUsers, String mission, IELO elo) {
-	      Map<String, double[]> results = new LinkedHashMap<String, double[]>();
-	      for (String user : availableUsers) {
-	          
-	          results.put(user, this.getFeatures(user, mission, elo, retrieveEloFromRepository(user, EloTypes.SCY_MAPPING)));
-	      }
-	      return results;
-	  }
-	
+
+    @Override
+    public Map<String, double[]> getFeatures(Set<String> availableUsers, String mission, IELO elo) {
+        Map<String, double[]> results = new LinkedHashMap<String, double[]>();
+        for ( String user : availableUsers ) {
+
+            IELO userElo = retrieveEloFromRepository(user, EloTypes.SCY_MAPPING);
+            if ( userElo == null ) {
+                results.put(user, new double[]{0.0, 0.0, 0.0, 0.0});
+            } else {
+                results.put(user, this.getFeatures(user, mission, elo, userElo));
+            }
+        }
+        return results;
+    }
+
     public double[] getFeatures(String user, String mission, IELO referenceElo, IELO userElo) {
-//      TODO: extract conversion of reference ELO to graph to calling function to speed up the computation
+        //      TODO: extract conversion of reference ELO to graph to calling function to speed up the computation
         double refDist = 0.0, nOfNodes = 0.0, nOfLinks = 0.0, nOfLabels = 0.0;
         try {
             IContent content = referenceElo.getContent();
             String contentText = content.getXmlString(); // TODO: find out why imageData tag is
-                                                            // sometimes not accepted by
-                                                            // org.jaxen.XPath
-                                                            // theory 1: IELO.getXml does not work here
-                                                            // (why?)
+            // sometimes not accepted by
+            // org.jaxen.XPath
+            // theory 1: IELO.getXml does not work here
+            // (why?)
             // contentText = contentText.replaceAll("<imageData>.*</imageData>", "");
             Graph rg = CmapImporter.convertCmap(contentText);
 
@@ -53,60 +57,59 @@ public class CMapFeatureExtractor extends AbstractFeatureExtractor {
             Graph g = CmapImporter.convertCmap(contentText);
             EditCostFunction cost = EditCostFunction.simpleEditCost;
 
-//          exact calculation of the edit distance is too slow!
-//          refDist = EditDistanceCalculator.calcDistance(rg, g, cost);
+            //          exact calculation of the edit distance is too slow!
+            //          refDist = EditDistanceCalculator.calcDistance(rg, g, cost);
             refDist = EditDistanceApproximator.calcDistance(rg, g, cost);
             nOfNodes = (double) g.getVertexes().size();
             nOfLinks = (double) g.getEdges().size();
 
             // count non-empty labels
             nOfLabels = (double) this.countLabels(g);
-        } catch (JaxenException e) {
+        } catch ( JaxenException e ) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        } catch (JDOMException e) {
+        } catch ( JDOMException e ) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        } catch (IOException e) {
+        } catch ( IOException e ) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
-        return new double[] { refDist, nOfNodes, nOfLinks, nOfLabels };
+        return new double[]{refDist, nOfNodes, nOfLinks, nOfLabels};
     }
 
-	/**
-	 * @param g count number of non-empty edge and vertex labels
-	 */
-	private int countLabels(Graph g) {
-		int numOfLabels = 0;
-		// process vertices to extract labels
-		Collection<Vertex> v = g.getVertexes();
-		Iterator<Vertex> vIt = v.iterator();
-		while (vIt.hasNext()) {
-			Vertex vx = (Vertex) vIt.next();
-			if (!vx.getLabel().matches("")) {
-				numOfLabels++;
-			}
-		}
-		// process edges to extract labels
-		Collection<Edge> e = g.getEdges();
-		Iterator<Edge> eIt = e.iterator();
-		while (eIt.hasNext()) {
-			Edge ex = (Edge) eIt.next();
-			if (!ex.getLabel().matches("")) {
-				numOfLabels++;
-			}
-		}
-		return numOfLabels;
-	}
+    /** @param g count number of non-empty edge and vertex labels */
+    private int countLabels(Graph g) {
+        int numOfLabels = 0;
+        // process vertices to extract labels
+        Collection<Vertex> v = g.getVertexes();
+        Iterator<Vertex> vIt = v.iterator();
+        while ( vIt.hasNext() ) {
+            Vertex vx = (Vertex) vIt.next();
+            if ( !vx.getLabel().matches("") ) {
+                numOfLabels++;
+            }
+        }
+        // process edges to extract labels
+        Collection<Edge> e = g.getEdges();
+        Iterator<Edge> eIt = e.iterator();
+        while ( eIt.hasNext() ) {
+            Edge ex = (Edge) eIt.next();
+            if ( !ex.getLabel().matches("") ) {
+                numOfLabels++;
+            }
+        }
+        return numOfLabels;
+    }
 
-	@Override
-	public boolean canRun(IELO elo) {
+    @Override
+    public boolean canRun(IELO elo) {
         String technicalFormat = new ScyElo(elo, repository).getTechnicalFormat();
-        if (EloTypes.SCY_MAPPING.equals(technicalFormat)) {
+        if ( EloTypes.SCY_MAPPING.equals(technicalFormat) ) {
             return true;
         }
-        return false;	}
+        return false;
+    }
 
 }
