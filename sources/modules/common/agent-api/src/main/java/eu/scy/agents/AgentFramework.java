43566672a16f7e441048afe4d400dfa11b601db3
stefan.weinbrenner <stefan.weinbrenner@googlemail.com>
2011-10-24 12:26:52 +0000

    removed dependency to sqlspaces-server from agent-api

--- sources/modules/common/agent-api/src/main/java/eu/scy/agents/AgentFramework.java
+++ sources/modules/common/agent-api/src/main/java/eu/scy/agents/AgentFramework.java
@@ -1,25 +1,13 @@
 package eu.scy.agents;
 
 import info.collide.sqlspaces.commons.Configuration;
-import info.collide.sqlspaces.commons.Configuration.Database;
-import info.collide.sqlspaces.server.Server;
 import eu.scy.agents.api.AgentLifecycleException;
 
 public class AgentFramework {
 
-    //if a server should be started (true) or you use your own (false)
-    private static final boolean standalone = true;
-
     public static void main(String[] args) {
 
         Configuration conf = Configuration.getConfiguration();
-        if (standalone) {
-            conf.setDbType(Database.HSQL);
-            conf.setWebEnabled(false);
-            conf.setWebServicesEnabled(false);
-            conf.setSSLEnabled(false);
-            Server.startServer();
-        }
         try {
             AgentManager am = new AgentManager("localhost", conf.getNonSSLPort());
             for (String s : args) {
@@ -27,9 +15,6 @@ public class AgentFramework {
             }
         } catch (AgentLifecycleException e) {
             e.printStackTrace();
-            if (standalone) {
-                Server.stopServer();
-            }
         }
     }
 }
