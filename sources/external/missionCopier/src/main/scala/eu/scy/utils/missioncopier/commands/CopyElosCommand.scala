c9502121b648bf1d9a6af913738407b793ddafd8
jakob.sikken <jakob.sikken@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2011-12-07 12:41:46 +0000

    Added a query option to the elo copy command Made the TimeRetrieveElosCommand work

--- sources/external/missionCopier/src/main/scala/eu/scy/utils/missioncopier/commands/CopyElosCommand.scala
+++ sources/external/missionCopier/src/main/scala/eu/scy/utils/missioncopier/commands/CopyElosCommand.scala
@@ -1,9 +1,9 @@
 package eu.scy.utils.missioncopier.commands
 
 import scala.collection.JavaConversions._
-import roolo.search.ISearchResult
 import eu.scy.utils.missioncopier.StateModel
 import eu.scy.utils.missioncopier.copying.{SaverEloVersionCopier, EloVersionCopier}
+import roolo.search.{Query, MetadataQueryComponent, IQuery, ISearchResult}
 
 
 /**
@@ -14,26 +14,57 @@ import eu.scy.utils.missioncopier.copying.{SaverEloVersionCopier, EloVersionCopi
  * To change this template use File | Settings | File Templates.
  */
 
-class CopyElosCommand  (override val stateModel: StateModel) extends StateModelCommandHandler(stateModel) {
-   val commands = Seq("copy elos")
-   val description = "copy elos"
+class CopyElosCommand(override val stateModel: StateModel) extends StateModelCommandHandler(stateModel) {
+  val commands = Seq("copy elos")
+  val description = "copy elos"
+  override val paramDescription = "[query]"
 
-   def execute(params: Seq[String]): Unit = {
-     if (stateModel.source == null) {
-        println("cannot copy elos if the source repository is not defined")
-        return
-     }
-     if (stateModel.destination == null) {
-        println("cannot copy elos if the destination repository is not defined")
-        return
-     }
-     copyElos()
-   }
+  def execute(params: Seq[String]): Unit = {
+    if (stateModel.source == null) {
+      println("cannot copy elos if the source repository is not defined")
+      return
+    }
+    if (stateModel.destination == null) {
+      println("cannot copy elos if the destination repository is not defined")
+      return
+    }
+    copyElos(getEloSearchResults(params))
+  }
 
-  private def copyElos() = {
-    val eloVersionCopier:EloVersionCopier = new SaverEloVersionCopier(stateModel)
-    val allElosSearchResults:Seq[ISearchResult] = stateModel.source.repository.listAllElos()
-    val eloUris = for (searchResult <- allElosSearchResults) yield searchResult.getUri
-    eloVersionCopier.copyElos(eloUris)
+  private def copyElos(eloSearchResults: Seq[ISearchResult]): Unit = {
+    if (!eloSearchResults.isEmpty) {
+      val eloUris = for (searchResult <- eloSearchResults) yield searchResult.getUri
+      val eloVersionCopier: EloVersionCopier = new SaverEloVersionCopier(stateModel)
+      eloVersionCopier.copyElos(eloUris)
+    }
+  }
+
+  private def getEloSearchResults(params: Seq[String]): Seq[ISearchResult] = {
+    var eloSearchResults = Seq[ISearchResult]()
+    if (params.isEmpty) {
+      Utils.askYesNo(stateModel, "Copy all ELOs") match {
+        case Some(true) =>
+          eloSearchResults = stateModel.source.repository.listAllElos()
+        case _ =>
+      }
+    } else {
+      val searchText = params.mkString(" ")
+      val queryComponent = new MetadataQueryComponent("contents", searchText);
+      val query = new Query(queryComponent);
+      query.setMaxResults(Integer.MAX_VALUE)
+      println("Searching for ELOs with: " + searchText)
+      val searchResults = stateModel.source.repository.search(query)
+      if (searchResults.isEmpty) {
+        println("No ELOs found")
+      } else {
+        Utils.printSearchResults(searchResults)
+        Utils.askYesNo(stateModel, "Copy these ELOs") match {
+          case Some(true) =>
+            eloSearchResults = searchResults
+          case _ =>
+        }
+      }
+    }
+    eloSearchResults
   }
 }
--- sources/external/missionCopier/src/main/scala/eu/scy/utils/missioncopier/commands/TimeRetrieveElosCommand.scala
+++ sources/external/missionCopier/src/main/scala/eu/scy/utils/missioncopier/commands/TimeRetrieveElosCommand.scala
@@ -1,9 +1,9 @@
 package eu.scy.utils.missioncopier.commands
 
-import eu.scy.utils.missioncopier.StateModel
 import roolo.search.ISearchResult
 import java.net.URI
 import java.util.Locale
+import eu.scy.utils.missioncopier.{RepositoryDefinition, StateModel}
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,13 +15,23 @@ import java.util.Locale
 class TimeRetrieveElosCommand(override val stateModel: StateModel) extends SearchElosCommand(stateModel) {
   override val commands = Seq("time [elos]")
   override val description = "time retrieve elos"
+  override protected val maxNrOfSearchResults = 10
 
-  override protected def handleSearchResults(searchResults: Seq[ISearchResult]) = {
+  override protected def handleSearchResults(searchResults: Seq[ISearchResult], repositoryDefinition: RepositoryDefinition) = {
     Utils.printSearchResults(searchResults)
+    var i = 1;
+    for (searchResult <- searchResults){
+      timeRetrieve(i, searchResult.getUri, repositoryDefinition)
+      i+=1
+    }
   }
 
-  private def timeRetrieve(eloUri: URI) = {
-
+  private def timeRetrieve(i:Int, eloUri: URI, repositoryDefinition: RepositoryDefinition) = {
+    def retrieveElo():Unit = repositoryDefinition.repository.retrieveELO(eloUri)
+    val retrieveEloAction = new TimeAction(retrieveElo)
+    def retrieveEloAllVersions():Unit = repositoryDefinition.repository.retrieveELOAllVersions(eloUri)
+    val retrieveEloAllVersionsAction = new TimeAction(retrieveEloAllVersions)
+    printf("%3d: %s %s\n",i,retrieveEloAction.getDisplayTimes(),retrieveEloAllVersionsAction.getDisplayTimes())
   }
 }
 
@@ -31,21 +41,22 @@ class TimeAction(val action: ()=>Unit){
   val multiTime = timeAction(multiCount)
 
   private def timeAction(times: Int):Long = {
+//    println("timeAction(" + times + ")")
     val startNanos = System.nanoTime()
-    for (i <- 0 to times){
-      action
+    for (i <- 0 until times){
+      action()
     }
     val nanosUsed = System.nanoTime()-startNanos
-    nanosUsed
+    nanosUsed/times
   }
 
-  def printTimes() = {
-    print(nanosToDisplayMillis(singleTime) + " " + nanosToDisplayMillis(multiTime/multiCount))
+  def getDisplayTimes():String = {
+    nanosToDisplayMillis(singleTime) + " " + nanosToDisplayMillis(multiTime)
   }
 
   private def nanosToDisplayMillis(nanos:Long):String = {
     val millis: java.lang.Double = nanos/1e6
-    String.format(Locale.UK, "%4.1f", millis)
+    String.format(Locale.UK, "%8.3f", millis)
   }
 
 }
