b3a2ada9c675ce7a3b966cb5a86b891cf679a6f0
christopher.krueger.83@gmail.com <christopher.krueger.83@gmail.com@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2012-02-22 22:40:03 +0000

    Mission model state is now persisted in tuple space and will be loaded on startup. usage of modal dialog notifications added. some cleanup.

---
+++ trunk/sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/agenda/guidance/event/ClearCurtainEvent.java
@@ -0,0 +1,13 @@
+package eu.scy.agents.agenda.guidance.event;
+
+import java.util.EventObject;
+
+public class ClearCurtainEvent extends EventObject {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ClearCurtainEvent(Object source) {
+		super(source);
+	}
+
+}
---
+++ trunk/sources/modules/agents/agents-impl/src/main/java/eu/scy/agents/agenda/guidance/event/LoadActivitiesEvent.java
@@ -0,0 +1,13 @@
+package eu.scy.agents.agenda.guidance.event;
+
+import java.util.EventObject;
+
+public class LoadActivitiesEvent extends EventObject {
+
+	private static final long serialVersionUID = 1L;
+
+	public LoadActivitiesEvent(Object source) {
+		super(source);
+	}
+
+}
