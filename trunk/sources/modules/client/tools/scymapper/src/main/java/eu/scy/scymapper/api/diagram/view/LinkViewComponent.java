fc622354e192c979eddb1052663b901d2582060a
adam.giemza@gmail.com <adam.giemza@gmail.com@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2013-01-17 10:43:07 +0000

    fixed possible NPE

--- trunk/sources/modules/client/tools/scymapper/src/main/java/eu/scy/scymapper/api/diagram/view/LinkViewComponent.java
+++ trunk/sources/modules/client/tools/scymapper/src/main/java/eu/scy/scymapper/api/diagram/view/LinkViewComponent.java
@@ -16,93 +16,93 @@ import java.awt.geom.Point2D;
  * @created 09.des.2009 17:47:32
  */
 public class LinkViewComponent extends JComponent {
-	private static final double DISTANCE_TRESHOLD = 5.0;
+    private static final double DISTANCE_TRESHOLD = 5.0;
 
-	private ILinkController controller;
-	private ILinkModel model;
+    private ILinkController controller;
+    private ILinkModel model;
 
-	protected int marginLeft = 100;
-	protected int marginTop = 100;
-	protected int marginRight = 100;
-	protected int marginBottom = 100;
+    protected int marginLeft = 100;
+    protected int marginTop = 100;
+    protected int marginRight = 100;
+    protected int marginBottom = 100;
 
-	private final static Logger logger = Logger.getLogger(LinkViewComponent.class);
+    private final static Logger logger = Logger.getLogger(LinkViewComponent.class);
 
-	public LinkViewComponent(ILinkController controller, ILinkModel model) {
-		this.controller = controller;
-		this.model = model;
+    public LinkViewComponent(ILinkController controller, ILinkModel model) {
+        this.controller = controller;
+        this.model = model;
 
-		//DEBUG: setBorder(BorderFactory.createLineBorder(Color.black, 1));
-		setLayout(null);
-		updatePosition();
-	}
+        //DEBUG: setBorder(BorderFactory.createLineBorder(Color.black, 1));
+        setLayout(null);
+        updatePosition();
+    }
 
-	@Override
-	public boolean contains(int x, int y) {
-		if (getModel() == null || getModel().getFrom() == null || getModel().getTo() == null) return false;
+    @Override
+    public boolean contains(int x, int y) {
+        if (getModel() == null || getModel().getFrom() == null || getModel().getTo() == null) return false;
 
-		Point p = new Point(x, y);
+        Point p = new Point(x, y);
 
-		p.translate(getX(), getY());
+        p.translate(getX(), getY());
 
-		Shape shape = getModel().getShape().getShape(getModel().getFrom(), getModel().getTo());
+        Shape shape = getModel().getShape().getShape(getModel().getFrom(), getModel().getTo());
 
-		return getDistance(shape, p) < DISTANCE_TRESHOLD;
-	}
+        return getDistance(shape, p) < DISTANCE_TRESHOLD;
+    }
 
-	private double getDistance(Shape shape, Point point) {
-		double flatness = 0.01;
-		double[] coords = new double[6];
+    private double getDistance(Shape shape, Point point) {
+        double flatness = 0.01;
+        double[] coords = new double[6];
 
-		double minDist = Double.MAX_VALUE;
+        double minDist = Double.MAX_VALUE;
 
-		Point2D.Double prev = null;
-		PathIterator pit = shape.getPathIterator(null, flatness);
+        Point2D.Double prev = null;
+        PathIterator pit = shape.getPathIterator(null, flatness);
 
-		while (!pit.isDone()) {
-			pit.currentSegment(coords);
+        while (!pit.isDone()) {
+            pit.currentSegment(coords);
 
-			if (prev != null) {
-				double distance = Line2D.ptSegDist(prev.x, prev.y, coords[0], coords[1], point.x, point.y);
-				if (distance < minDist) {
-					minDist = distance;
-				}
-			} else
-				prev = new Point2D.Double();
+            if (prev != null) {
+                double distance = Line2D.ptSegDist(prev.x, prev.y, coords[0], coords[1], point.x, point.y);
+                if (distance < minDist) {
+                    minDist = distance;
+                }
+            } else
+                prev = new Point2D.Double();
 
-			prev.x = coords[0];
-			prev.y = coords[1];
+            prev.x = coords[0];
+            prev.y = coords[1];
 
-			pit.next();
-		}
-		return minDist;
-	}
+            pit.next();
+        }
+        return minDist;
+    }
 
-	public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) {
 
-		Graphics2D g2 = (Graphics2D) g.create();
+        Graphics2D g2 = (Graphics2D) g.create();
 
-		Point from = model.getFrom();
-		Point to = model.getTo();
+        Point from = model.getFrom();
+        Point to = model.getTo();
 
-		if (from == null || to == null) {
-			logger.warn("From or to is null");
-			return;
-		}
-		Point relFrom = new Point(from);
-		relFrom.translate(-getX(), -getY());
+        if (from == null || to == null) {
+            logger.warn("From or to is null");
+            return;
+        }
+        Point relFrom = new Point(from);
+        relFrom.translate(-getX(), -getY());
 
-		Point relTo = new Point(to);
-		relTo.translate(-getX(), -getY());
+        Point relTo = new Point(to);
+        relTo.translate(-getX(), -getY());
 
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-		ILinkStyle style = model.getStyle();
+        ILinkStyle style = model.getStyle();
 
-		g2.setStroke(style.getStroke());
-		g2.setColor(style.getBackground());
+        g2.setStroke(style.getStroke());
+        g2.setColor(style.getBackground());
 
-		model.getShape().paint(g2, relFrom, relTo);
+        model.getShape().paint(g2, relFrom, relTo);
 
 //		Shape shape = model.getShape().getShape(relFrom, relTo);
 //		if (shape instanceof CubicCurve2D) {
@@ -138,62 +138,66 @@ public class LinkViewComponent extends JComponent {
 //
 //		}
 
-		g2.dispose();
-	}
+        g2.dispose();
+    }
 
-	public void setFrom(Point p) {
-		controller.setFrom(p);
-	}
+    public void setFrom(Point p) {
+        controller.setFrom(p);
+    }
 
-	public void setTo(Point p) {
-		controller.setTo(p);
-	}
+    public void setTo(Point p) {
+        controller.setTo(p);
+    }
 
-	public Rectangle getInnerBounds() {
-		Point from = model.getFrom();
-		Point to = model.getTo();
+    public Rectangle getInnerBounds() {
+        Point from = model.getFrom();
+        Point to = model.getTo();
 
-		int width, x, height, y;
-		if (to.x < from.x) {
-			x = to.x;
-			width = from.x - to.x;
-		} else {
-			x = from.x;
-			width = to.x - from.x;
-		}
-		if (to.y < from.y) {
-			y = to.y;
-			height = from.y - to.y;
-		} else {
-			y = from.y;
-			height = to.y - from.y;
-		}
+        if (from != null && to != null) {
 
-		return new Rectangle(x, y, width, height);
-	}
-
-	protected void updatePosition() {
-		if (model.getFrom() == null || model.getTo() == null) return;
-
-		Rectangle b = getInnerBounds();
-
-		setBounds(b.x - marginLeft, b.y - marginTop, b.width + marginLeft + marginRight, b.height + marginTop + marginBottom);
-		//setBorder(BorderFactory.createLineBorder(Color.black, 1));
-	}
-
-	public ILinkModel getModel() {
-		return model;
-	}
-
-	public void setModel(ILinkModel model) {
-		this.model = model;
-	}
-
-	public ILinkController getController() {
-		return controller;
-	}
-
-        public void setController(ILinkController controller) {
-            this.controller = controller;
+            int width, x, height, y;
+            if (to.x < from.x) {
+                x = to.x;
+                width = from.x - to.x;
+            } else {
+                x = from.x;
+                width = to.x - from.x;
+            }
+            if (to.y < from.y) {
+                y = to.y;
+                height = from.y - to.y;
+            } else {
+                y = from.y;
+                height = to.y - from.y;
+            }
+            return new Rectangle(x, y, width, height);
         }
+        return null;
+    }
+
+    protected void updatePosition() {
+        if (model.getFrom() == null || model.getTo() == null) return;
+
+        Rectangle b = getInnerBounds();
+        if (b != null) {
+            setBounds(b.x - marginLeft, b.y - marginTop, b.width + marginLeft + marginRight, b.height + marginTop + marginBottom);
+        }
+        //setBorder(BorderFactory.createLineBorder(Color.black, 1));
+    }
+
+    public ILinkModel getModel() {
+        return model;
+    }
+
+    public void setModel(ILinkModel model) {
+        this.model = model;
+    }
+
+    public ILinkController getController() {
+        return controller;
+    }
+
+    public void setController(ILinkController controller) {
+        this.controller = controller;
+    }
 }
