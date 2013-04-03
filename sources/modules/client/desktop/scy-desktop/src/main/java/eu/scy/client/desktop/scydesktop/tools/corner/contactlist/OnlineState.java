e80421c7457e5ba27874abeef06668196ed8125a
jakob.sikken <jakob.sikken@b2e416e9-8f50-0410-b9aa-47b31ee1ff64>
2013-04-03 09:32:50 +0000

    Added the online states to the i18n files.

--- sources/modules/client/desktop/scy-desktop/src/main/java/eu/scy/client/desktop/scydesktop/tools/corner/contactlist/OnlineState.java
+++ sources/modules/client/desktop/scy-desktop/src/main/java/eu/scy/client/desktop/scydesktop/tools/corner/contactlist/OnlineState.java
@@ -5,6 +5,8 @@
 
 package eu.scy.client.desktop.scydesktop.tools.corner.contactlist;
 
+import eu.scy.client.common.scyi18n.ResourceBundleWrapper;
+
 /**
  *
  * @author Sven
@@ -13,14 +15,16 @@ public enum OnlineState {
     
       ONLINE,AWAY,OFFLINE,PENDING,IS_ME;
 
-    @Override
+   private final ResourceBundleWrapper resourceBundleWrapper = new ResourceBundleWrapper(this);
+
+   @Override
     public String toString() {
         switch(this){
-            case ONLINE: return "Online";
-            case AWAY: return "Away";
-            case OFFLINE: return "Offline";
-            case PENDING: return "Pending";
-            case IS_ME: return "That's me";
+            case ONLINE: return resourceBundleWrapper.getString("onlineState.online");
+            case AWAY: return resourceBundleWrapper.getString("onlineState.away");
+            case OFFLINE: return resourceBundleWrapper.getString("onlineState.offline");
+            case PENDING: return resourceBundleWrapper.getString("onlineState.pending");
+            case IS_ME: return resourceBundleWrapper.getString("onlineState.thatsMe");
             default:return "";
         }
     }
